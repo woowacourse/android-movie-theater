17bf3814be8d55ef6888e99325a58d2ae51ab56a
woowahan-pjs <jason@woowahan.com>
2023-04-25 08:01:03 +0900

    feat: set up the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,75 +1,46 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("de.mannodermaus.android-junit5") version "1.10.0.0"
 }
 
 android {
     namespace = "woowacourse.movie"
-    compileSdk = 34
-
-    dataBinding {
-        enable = true
-    }
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "woowacourse.movie"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        testInstrumentationRunnerArguments["runnerBuilder"] = "de.mannodermaus.junit5.AndroidJUnit5Builder"
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
+                "proguard-rules.pro"
             )
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "17"
-    }
-    packaging {
-        resources {
-            excludes += "META-INF/**"
-            excludes += "win32-x86*/**"
-        }
+        jvmTarget = "11"
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("com.google.android.material:material:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity:1.8.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    testImplementation("io.mockk:mockk:1.13.10")
-    testImplementation("io.mockk:mockk-android:1.13.10")
-    testImplementation("io.mockk:mockk-agent:1.13.10")
-    androidTestImplementation("io.mockk:mockk-android:1.13.10")
-    androidTestImplementation("io.mockk:mockk-agent:1.13.10")
-    testImplementation("org.testng:testng:6.9.6")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    testImplementation("org.assertj:assertj-core:3.25.3")
-    testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    androidTestImplementation("org.assertj:assertj-core:3.25.3")
-    androidTestImplementation("io.kotest:kotest-runner-junit5:5.8.0")
-    androidTestImplementation("de.mannodermaus.junit5:android-test-core:1.3.0")
-    androidTestRuntimeOnly("de.mannodermaus.junit5:android-test-runner:1.3.0")
 }
