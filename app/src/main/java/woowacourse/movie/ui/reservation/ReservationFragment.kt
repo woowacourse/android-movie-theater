52862959d204b697d8a4c2c9473871e49f492605
Sujin Kim <sujinkim919@gmail.com>
2023-04-25 21:49:03 +0900

    feat: 예매 내역 뷰가 empty 일 때 문구 추가

--- app/src/main/java/woowacourse/movie/ui/reservation/ReservationFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/reservation/ReservationFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
 import woowacourse.movie.model.ReservationModel
@@ -20,6 +21,8 @@ class ReservationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_reservation, container, false)
+
+        if (ReservationModel.tickets.isNotEmpty()) view.findViewById<TextView>(R.id.reservation_empty).visibility = View.GONE
 
         val reservationView: RecyclerView = view.findViewById(R.id.reservation_recyclerview)
         reservationView.adapter = ReservationAdapter(ReservationModel.tickets) {
