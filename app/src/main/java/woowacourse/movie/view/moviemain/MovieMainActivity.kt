74deaf32034ed2b7b92140ff16e3bb12d668961f
hyemdooly <hyemdooly@gmail.com>
2023-04-25 16:19:47 +0900

    feat(MovieMainActivity): BottomNavigationView 구현

--- app/src/main/java/woowacourse/movie/view/moviemain/MovieMainActivity.kt
+++ app/src/main/java/woowacourse/movie/view/moviemain/MovieMainActivity.kt
@@ -2,18 +2,44 @@ package woowacourse.movie.view.moviemain
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import woowacourse.movie.R
 import woowacourse.movie.view.moviemain.movielist.MovieListFragment
+import woowacourse.movie.view.moviemain.reservationlist.ReservationListFragment
+import woowacourse.movie.view.moviemain.setting.SettingFragment
 
 class MovieMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_list)
+        setContentView(R.layout.activity_movie_main)
 
+        val navigation = findViewById<BottomNavigationView>(R.id.navigation_view)
+
+        navigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_reservation_list -> {
+                    replaceFragment(ReservationListFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.action_home -> {
+                    replaceFragment(MovieListFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.action_setting -> {
+                    replaceFragment(SettingFragment())
+                    return@setOnItemSelectedListener true
+                }
+                else -> return@setOnItemSelectedListener false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
-            add(R.id.fragment_container_view, MovieListFragment())
+            replace(R.id.fragment_container_view, fragment)
         }
     }
 }
