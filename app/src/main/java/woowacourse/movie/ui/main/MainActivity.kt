2eb1ba80040763cd275b41329f5a9f9ab151cbce
briandr97 <briandr@naver.com>
2023-04-25 16:39:25 +0900

    feat(main): main activity 구현

--- app/src/main/java/woowacourse/movie/ui/main/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/ui/main/MainActivity.kt
@@ -1,37 +1,13 @@
 package woowacourse.movie.ui.main
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
-import woowacourse.movie.model.main.MainModelHandler
-import woowacourse.movie.ui.booking.BookingActivity
-import woowacourse.movie.ui.main.adapter.recyclerview.MainAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initAdapter()
-    }
-
-    private fun initAdapter() {
-        val mainAdapter = MainAdapter(
-            this,
-            { clickBook(it) },
-            { clickAdvertisement(it) },
-        )
-        findViewById<RecyclerView>(R.id.listMainMovie).adapter = mainAdapter
-        mainAdapter.initMovies(MainModelHandler.getMainData())
-    }
-
-    private fun clickBook(movieId: Long) {
-        startActivity(BookingActivity.getIntent(this, movieId))
-    }
-
-    private fun clickAdvertisement(intent: Intent) {
-        startActivity(intent)
     }
 }
