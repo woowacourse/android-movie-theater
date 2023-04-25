3da23ff91f4cbc4355eeb8c1d94f223f35383ad1
Doyeop Kim <dygames34@gmail.com>
2023-04-25 15:40:53 +0900

    feat: BottomNavigationView 구현

--- app/src/main/java/woowacourse/movie/activity/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/MainActivity.kt
@@ -1,8 +1,10 @@
 package woowacourse.movie.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import woowacourse.movie.R
 import woowacourse.movie.domain.AdvertisementMock
 import woowacourse.movie.domain.MovieMock
@@ -17,28 +19,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         makeMovieRecyclerView()
+        makeBottomNavigationView()
     }
 
     private fun makeMovieRecyclerView() {
         val movies = List(2500) { MovieMock.createMovies() }.flatten()
         val advertisementDatas = AdvertisementMock.createAdvertisements()
-        val advertisementPolicy = MovieAdvertisementPolicy(3, 1)
+        val advertisementPolicy = MovieAdvertisementPolicy(MOVIE_COUNT, ADVERTISEMENT_COUNT)
 
         val movieRecyclerView = findViewById<RecyclerView>(R.id.main_movie_list)
-        movieRecyclerView.adapter =
-            MovieAdapter(
-                movies, advertisementDatas, advertisementPolicy,
-                ::onClickItem
-            )
+        movieRecyclerView.adapter = MovieAdapter(
+            movies, advertisementDatas, advertisementPolicy, ::onClickItem
+        )
     }
 
     private fun onClickItem(data: MovieListViewData) {
         when (data.viewType) {
-            MovieListViewType.MOVIE ->
-                MovieReservationActivity.from(this, data as MovieViewData).run {
+            MovieListViewType.MOVIE -> MovieReservationActivity.from(this, data as MovieViewData)
+                .run {
                     startActivity(this)
                 }
             MovieListViewType.ADVERTISEMENT -> Unit
         }
+    }
+
+    private fun makeBottomNavigationView() {
+        val bottomNavigationView =
+            findViewById<BottomNavigationView>(R.id.main_bottom_navigation_view)
+        bottomNavigationView.selectedItemId = R.id.action_home
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_list -> {
+
+                    true
+                }
+                R.id.action_home -> {
+
+                    true
+                }
+                R.id.action_setting -> {
+
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    companion object {
+        private const val MOVIE_COUNT = 3
+        private const val ADVERTISEMENT_COUNT = 1
     }
 }
