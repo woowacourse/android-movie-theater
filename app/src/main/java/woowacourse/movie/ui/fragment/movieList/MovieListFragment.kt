61b4b06e6274d1568dce26abd520559e053413ad
mendel <rhthrhrl0@naver.com>
2023-04-25 16:29:41 +0900

    feat(MovieListFragment): 영화 목록 프래그먼트 추가

---
+++ app/src/main/java/woowacourse/movie/ui/fragment/movieList/MovieListFragment.kt
@@ -0,0 +1,74 @@
+package woowacourse.movie.ui.fragment.movieList
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.movie.R
+import woowacourse.movie.data.AdvRepository
+import woowacourse.movie.data.MovieRepository
+import woowacourse.movie.model.AdvState
+import woowacourse.movie.model.MovieState
+import woowacourse.movie.ui.adv.AdvDetailActivity
+import woowacourse.movie.ui.main.MainActivity
+import woowacourse.movie.ui.main.MainPageAdapter
+import woowacourse.movie.ui.main.itemModel.AdvItemModel
+import woowacourse.movie.ui.main.itemModel.MovieItemModel
+import woowacourse.movie.ui.reservation.MovieDetailActivity
+
+class MovieListFragment : Fragment() {
+
+    private lateinit var movieListView: RecyclerView
+    private lateinit var adapter: MainPageAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        movieListView = view.findViewById(R.id.rv_main)
+        adapter = MainPageAdapter(
+            movie = MovieRepository.allMovies().map {
+                it.convertToItemModel { position ->
+                    navigateMovieDetail((adapter.items[position] as MovieItemModel).movieState)
+                }
+            },
+            adv = AdvRepository.allAdv().map {
+                it.convertToItemModel { position ->
+                    navigateAdbDetail((adapter.items[position] as AdvItemModel).advState)
+                }
+            }
+        )
+        movieListView.adapter = adapter
+    }
+
+    private fun navigateMovieDetail(movie: MovieState) {
+        val intent = Intent(activity, MovieDetailActivity::class.java)
+        intent.putExtra(MainActivity.KEY_MOVIE, movie)
+        startActivity(intent)
+    }
+
+    private fun navigateAdbDetail(adbState: AdvState) {
+        val intent = Intent(activity, AdvDetailActivity::class.java)
+        intent.putExtra(MainActivity.KEY_ADV, adbState)
+        startActivity(intent)
+    }
+
+    companion object {
+
+        internal const val KEY_MOVIE = "key_movie"
+        internal const val KEY_ADV = "key_adb"
+    }
+}
--- app/src/main/java/woowacourse/movie/ui/main/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/ui/main/MainActivity.kt
@@ -1,52 +1,21 @@
 package woowacourse.movie.ui.main
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.FragmentContainerView
 import woowacourse.movie.R
-import woowacourse.movie.data.AdvRepository
-import woowacourse.movie.data.MovieRepository
-import woowacourse.movie.model.AdvState
-import woowacourse.movie.model.MovieState
-import woowacourse.movie.ui.adv.AdvDetailActivity
-import woowacourse.movie.ui.main.itemModel.AdvItemModel
-import woowacourse.movie.ui.main.itemModel.MovieItemModel
-import woowacourse.movie.ui.reservation.MovieDetailActivity
+import woowacourse.movie.ui.fragment.movieList.MovieListFragment
 
 class MainActivity : AppCompatActivity() {
-    private val movieListView: RecyclerView by lazy { findViewById(R.id.rv_main) }
-    private val adapter: MainPageAdapter by lazy {
-        MainPageAdapter(
-            movie = MovieRepository.allMovies().map {
-                it.convertToItemModel { position ->
-                    navigateMovieDetail((adapter.items[position] as MovieItemModel).movieState)
-                }
-            },
-            adv = AdvRepository.allAdv().map {
-                it.convertToItemModel { position ->
-                    navigateAdbDetail((adapter.items[position] as AdvItemModel).advState)
-                }
-            }
-        )
-    }
 
+    private val rv: FragmentContainerView by lazy { findViewById(R.id.container) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        movieListView.adapter = adapter
-    }
 
-    private fun navigateMovieDetail(movie: MovieState) {
-        val intent = Intent(this, MovieDetailActivity::class.java)
-        intent.putExtra(KEY_MOVIE, movie)
-        startActivity(intent)
-    }
-
-    private fun navigateAdbDetail(adbState: AdvState) {
-        val intent = Intent(this, AdvDetailActivity::class.java)
-        intent.putExtra(KEY_ADV, adbState)
-        startActivity(intent)
+        supportFragmentManager.beginTransaction()
+            .add(rv.id, MovieListFragment())
+            .commit()
     }
 
     companion object {
