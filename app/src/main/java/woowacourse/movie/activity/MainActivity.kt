af1fc7f2b87ebc5223d87b1a9feab7d4a2aec3bc
Yerin <myheaven_b@naver.com>
2023-04-25 17:58:10 +0900

    feat: setting fragment 기능 구현

--- app/src/main/java/woowacourse/movie/activity/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/MainActivity.kt
@@ -2,11 +2,18 @@ package woowacourse.movie.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import woowacourse.movie.R
+import woowacourse.movie.fragment.SettingFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add(R.id.main_fragment, SettingFragment())
+        }
     }
 }
---
+++ app/src/main/java/woowacourse/movie/fragment/SettingFragment.kt
@@ -0,0 +1,27 @@
+package woowacourse.movie.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import woowacourse.movie.R
+
+class SettingFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+}
