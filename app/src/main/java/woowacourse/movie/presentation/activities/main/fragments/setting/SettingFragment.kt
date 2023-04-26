2b75a9b2a28940af80fb7cb6a3076dc45d78eb34
tmdgh1592 <tmdgh1592@naver.com>
2023-04-26 14:29:29 +0900

    feat(SettingFragment): 설정에서 알림 기능을 On/Off 하는 기능 구현

--- app/src/main/java/woowacourse/movie/presentation/activities/main/fragments/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/activities/main/fragments/setting/SettingFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.woowacourse.data.local.Preferences
 import woowacourse.movie.R
 
 class SettingFragment : Fragment() {
+
+    private val preferences by lazy { Preferences(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,8 +21,24 @@ class SettingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_setting, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initPushSwitch(view)
+    }
+
+    private fun initPushSwitch(view: View) {
+        val switch = view.findViewById<SwitchMaterial>(R.id.notification_push_switch)
+        with(switch) {
+            isChecked = preferences.getBoolean(PUSH_ALLOW_KEY, false)
+            setOnCheckedChangeListener { _, isChecked ->
+                preferences.setBoolean(PUSH_ALLOW_KEY, isChecked)
+            }
+        }
+    }
+
     companion object {
         private val settingFragment = SettingFragment()
+        internal const val PUSH_ALLOW_KEY = "push_allow_key"
 
         fun newInstance(): SettingFragment = settingFragment.apply {
             arguments = Bundle().apply {
