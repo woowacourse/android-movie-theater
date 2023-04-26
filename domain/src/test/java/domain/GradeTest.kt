208eb5927635ef4995f3866595fe6bc8d98569ca
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-04-26 11:03:08 +0900

    test: 테스트 오류 수정

--- domain/src/test/java/domain/GradeTest.kt
+++ domain/src/test/java/domain/GradeTest.kt
@@ -9,20 +9,20 @@ class GradeTest {
     fun `B등급일 때, 가격은 10000원이다`() {
         val grade = Grade.B
 
-        assertThat(grade.ticketPrice).isEqualTo(10_000)
+        assertThat(grade.ticketPrice.price).isEqualTo(10_000)
     }
 
     @Test
     fun `S등급일 때, 가격은 15000원이다`() {
         val grade = Grade.S
 
-        assertThat(grade.ticketPrice).isEqualTo(15_000)
+        assertThat(grade.ticketPrice.price).isEqualTo(15_000)
     }
 
     @Test
     fun `A등급일 때, 가격은 12000원이다`() {
         val grade = Grade.A
 
-        assertThat(grade.ticketPrice).isEqualTo(12_000)
+        assertThat(grade.ticketPrice.price).isEqualTo(12_000)
     }
 }
