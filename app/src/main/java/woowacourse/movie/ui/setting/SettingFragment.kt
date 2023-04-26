e73e99175bbb4ea0cd3e8ebcc5c8e0ccd59b5494
Sujin Kim <sujinkim919@gmail.com>
2023-04-26 15:47:00 +0900

    feat: 푸시 알림 권한 요청 구현

--- app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
@@ -1,18 +1,61 @@
 package woowacourse.movie.ui.setting
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.content.ContextCompat
 import woowacourse.movie.R
 
 class SettingFragment : Fragment() {
+    private lateinit var toggleButton : SwitchCompat
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        val view = inflater.inflate(R.layout.fragment_setting, container, false)
+
+        initToggleButton(view)
+
+        return view
+    }
+
+    private fun initToggleButton(view: View) {
+        toggleButton = view.findViewById(R.id.setting_switch)
+        toggleButton.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                requestNotificationPermission(view)
+            } else {
+
+            }
+        }
+    }
+
+    private fun requestNotificationPermission(view: View) {
+        if (ContextCompat.checkSelfPermission(
+                view.context, android.Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                    // 권한 요청 거부한 경우
+                } else {
+                    requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                }
+            } else {
+                // 안드로이드 12 이하는 Notification에 관한 권한 필요 없음
+            }
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
     }
 }
