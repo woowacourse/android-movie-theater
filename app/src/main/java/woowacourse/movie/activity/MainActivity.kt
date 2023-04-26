7832073be9844612e2b262f24c4330a44112a35b
pingu244 <pingu244@naver.com>
2023-04-26 19:32:29 +0900

    feat: 푸시 알람 권한을 받는 기능 구현

--- app/src/main/java/woowacourse/movie/activity/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/MainActivity.kt
@@ -1,8 +1,14 @@
 package woowacourse.movie.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import woowacourse.movie.R
@@ -14,6 +20,7 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestNotificationPermission()
 
         findViewById<BottomNavigationView>(R.id.bnv_main).setOnItemSelectedListener(this)
 
@@ -23,19 +30,49 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.page_book_history -> {
-                supportFragmentManager.beginTransaction().replace(R.id.fl_main, BookHistoryFragment()).commitAllowingStateLoss()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fl_main, BookHistoryFragment()).commitAllowingStateLoss()
                 return true
             }
             R.id.page_home -> {
-                supportFragmentManager.beginTransaction().replace(R.id.fl_main, HomeFragment()).commitAllowingStateLoss()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_main, HomeFragment())
+                    .commitAllowingStateLoss()
                 return true
             }
             R.id.page_setting -> {
-                supportFragmentManager.beginTransaction().replace(R.id.fl_main, SettingFragment()).commitAllowingStateLoss()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_main, SettingFragment())
+                    .commitAllowingStateLoss()
                 return true
             }
         }
 
         return false
+    }
+
+    private fun requestNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                    // 권한 요청 거부한 경우
+                } else {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            } else {
+                // 안드로이드 12 이하는 Notification에 관한 권한 필요 없음
+            }
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "알림 권한을 받았습니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "알림 권한을 획득하지 못하였습니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
