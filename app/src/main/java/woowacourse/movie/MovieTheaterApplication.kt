f1e1ee05f65455676b48bca041194edbae47e201
2chang5 <chang5hustle@gmail.com>
2023-04-27 11:47:38 +0900

    feat(SharedPreferenceUtil): 푸시 알림 설정 저장 구현

--- app/src/main/java/woowacourse/movie/MovieTheaterApplication.kt
+++ app/src/main/java/woowacourse/movie/MovieTheaterApplication.kt
@@ -2,12 +2,23 @@ package woowacourse.movie
 
 import android.app.Application
 import android.content.IntentFilter
+import android.content.SharedPreferences
 import woowacourse.movie.broadcastreceiver.AlarmReceiver
 
 class MovieTheaterApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         val alarmReceiver = AlarmReceiver()
+        initSettingsPreference()
         registerReceiver(alarmReceiver, IntentFilter())
+    }
+
+    private fun initSettingsPreference() {
+        settingsPreference = getSharedPreferences(SETTINGS, MODE_PRIVATE)
+    }
+
+    companion object {
+        private const val SETTINGS = "settings"
+        lateinit var settingsPreference: SharedPreferences
     }
 }
