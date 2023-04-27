17cc89bae663a492bdb8668023255d2e7c4d992f
re4rk <37167652+re4rk@users.noreply.github.com>
2023-04-28 08:00:58 +0900

    refactor: 코딩 컨벤션에 맞춰 변수 및 함수 위치 변경

--- app/src/main/java/woowacourse/movie/data/MovieRepository.kt
+++ app/src/main/java/woowacourse/movie/data/MovieRepository.kt
@@ -1,12 +1,10 @@
 package woowacourse.movie.data
 
+import java.time.LocalDate
 import woowacourse.movie.R
 import woowacourse.movie.model.MovieState
-import java.time.LocalDate
 
 object MovieRepository {
-    fun allMovies(): List<MovieState> = movies.toList()
-
     private val movies: List<MovieState> = List(25) {
         MovieState(
             R.drawable.slamdunk,
@@ -19,4 +17,6 @@ object MovieRepository {
                 "북산고 농구부는 전국 대회에 출전해 라이벌 산왕공고와 맞붙는다. 멤버 각자가 쌓아온 성과, 그들이 짊어진 과거, 다양한 생각들이 뜨거운 코트 위에서 다시 한번 격렬하게 충돌한다."
         )
     }
+
+    fun allMovies(): List<MovieState> = movies.toList()
 }
--- app/src/main/java/woowacourse/movie/data/TicketsRepository.kt
+++ app/src/main/java/woowacourse/movie/data/TicketsRepository.kt
@@ -3,10 +3,9 @@ package woowacourse.movie.data
 import woowacourse.movie.model.TicketsState
 
 object TicketsRepository {
+    private val tickets: MutableList<TicketsState> = mutableListOf()
 
     fun allTickets(): List<TicketsState> = tickets.toList()
 
     fun addTicket(ticket: TicketsState) = this.tickets.add(ticket)
-
-    private val tickets: MutableList<TicketsState> = mutableListOf()
 }
