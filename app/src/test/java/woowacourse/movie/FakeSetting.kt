11abfbb290957261c053af19c639ee55f0c442d2
Doyeop Kim <dygames34@gmail.com>
2023-04-27 13:15:14 +0900

    refactor: SettingMock을 FakeSetting으로 변경

--- app/src/test/java/woowacourse/movie/FakeSetting.kt
+++ app/src/test/java/woowacourse/movie/FakeSetting.kt
@@ -1,8 +1,8 @@
-package woowacourse.movie.view
+package woowacourse.movie
 
 import woowacourse.movie.system.Setting
 
-class SettingMock : Setting {
+class FakeSetting : Setting {
     private val settings: MutableMap<String, Boolean> = mutableMapOf()
     override fun getValue(key: String, default: Boolean): Boolean {
         return settings[key] ?: default
--- app/src/test/java/woowacourse/movie/SettingTest.kt
+++ app/src/test/java/woowacourse/movie/SettingTest.kt
@@ -2,13 +2,12 @@ package woowacourse.movie
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import woowacourse.movie.view.SettingMock
 
 class SettingTest {
     @Test
     fun `설정에 reservation이라는 키를 가지고 true를 저장하면 reservation이라는 키를 가지고 조회했을때 true를 가져올 수 있다`() {
         // given
-        val setting = SettingMock()
+        val setting = FakeSetting()
         val key = "reservation"
 
         // when
