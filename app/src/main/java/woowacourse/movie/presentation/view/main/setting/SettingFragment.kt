ba30056fc8d61058e52de640de69fe2d4ffd96ab
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-04-28 21:33:38 +0900

    refactor: 알림 수신동의를 했을 때 알림 권한 받을 수 있게 변경

--- app/src/main/java/woowacourse/movie/presentation/view/main/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/main/setting/SettingFragment.kt
@@ -5,7 +5,6 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.SwitchCompat
 import androidx.core.content.ContextCompat
@@ -14,24 +13,27 @@ import woowacourse.movie.Application
 import woowacourse.movie.R
 
 class SettingFragment : Fragment(R.layout.fragment_setting) {
+    private val switchSettingAlarm by lazy { requireView().findViewById<SwitchCompat>(R.id.switch_setting_alarm) }
 
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted: Boolean ->
-
+        if (isGranted.not()) {
+            switchSettingAlarm.isSelected = isGranted
+            Application.prefs.setBoolean(getString(R.string.push_alarm_permission), isGranted)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val allowedPushNotification =
             Application.prefs.getBoolean((getString(R.string.push_alarm_permission)), false)
-        setAlarmView(view, allowedPushNotification)
-
+        setAlarmView(allowedPushNotification)
     }
 
-    private fun setAlarmView(view: View, allowedPushNotification: Boolean) {
-        val switchSettingAlarm = view.findViewById<SwitchCompat>(R.id.switch_setting_alarm)
+    private fun setAlarmView(allowedPushNotification: Boolean) {
+
         switchSettingAlarm.isChecked =
             allowedPushNotification && notificationPermissionIsGranted()
         switchSettingAlarm.setOnCheckedChangeListener { buttonView, isChecked ->
@@ -44,12 +46,7 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
 
     private fun requestNotificationPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                Toast.makeText(requireContext(), "알림 설정은 환경설정에서 할 수 있습니다.", Toast.LENGTH_SHORT)
-                    .show()
-            } else {
-                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-            }
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         }
     }
 
