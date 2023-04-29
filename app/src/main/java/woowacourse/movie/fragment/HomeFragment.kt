9f5e112965fded9f793ad1a59951dfb11eef4c9b
SangHyun Moon <no1msh1217@gmail.com>
2023-04-29 18:39:36 +0900

    refactor: onViewCreated()로 UI 로직 이동

--- app/src/main/java/woowacourse/movie/fragment/HomeFragment.kt
+++ app/src/main/java/woowacourse/movie/fragment/HomeFragment.kt
@@ -16,23 +16,17 @@ import woowacourse.movie.movie.MovieMockData
 import woowacourse.movie.movielist.MovieRecyclerViewAdapter
 
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setMovieRecyclerView(view)
-
-        return view
     }
 
     private fun setMovieRecyclerView(view: View) {
