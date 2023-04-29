b5d956584d554bfb51344785e15d7113d24a5e95
mendel <rhthrhrl0@naver.com>
2023-04-29 20:49:00 +0900

    test: SeatSelectActivity와 MovieDetailActivity의 테스트 코드 추가

--- app/src/main/java/woowacourse/movie/model/ReservationState.kt
+++ app/src/main/java/woowacourse/movie/model/ReservationState.kt
@@ -5,7 +5,7 @@ import kotlinx.parcelize.Parcelize
 import java.time.LocalDateTime
 
 @Parcelize
-data class TicketOptState(
+data class ReservationState(
     val movieState: MovieState,
     val dateTime: LocalDateTime,
     val countState: CountState
