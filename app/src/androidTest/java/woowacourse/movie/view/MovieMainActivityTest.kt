20ce759aad20af3a49f1332ce4530d159f205906
EmilyCh0 <emily9803@ewhain.net>
2023-04-29 23:19:49 +0900

    test(MovieMainActivityTest): 하단바 탭 테스트 추가

--- app/src/androidTest/java/woowacourse/movie/view/MovieMainActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/MovieMainActivityTest.kt
@@ -19,8 +19,20 @@ class MovieMainActivityTest {
     val mActivityTestRule = ActivityScenarioRule(MovieMainActivity::class.java)
 
     @Test
-    fun 설정_버튼을_누르면_설정_Fragment로_바뀐다() {
+    fun 하단바_설정_버튼을_누르면_설정_Fragment로_바뀐다() {
         onView(withId(R.id.action_setting)).perform(click())
         onView(withId(R.id.setting_toggle)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun 하단바_예매_목록을_누르면_예매_목록_Fragment로_바뀐다() {
+        onView(withId(R.id.action_reservation_list)).perform(click())
+        onView(withId(R.id.recyclerview)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun 하단바_홈을_누르면_홈_Fragment로_바뀐다() {
+        onView(withId(R.id.action_home)).perform(click())
+        onView(withId(R.id.movie_recyclerview)).check(matches(isDisplayed()))
     }
 }
