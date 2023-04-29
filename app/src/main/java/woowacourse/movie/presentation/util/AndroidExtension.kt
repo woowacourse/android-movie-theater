f2a4ce258fff8a0c49872108eeb76b8081be6a13
vrexpert <vrexpert2302@gmail.com>
2023-04-29 13:37:01 +0900

    refactor(AndroidExtension): 사용하지 않는 Extension 제거

--- app/src/main/java/woowacourse/movie/presentation/util/AndroidExtension.kt
+++ app/src/main/java/woowacourse/movie/presentation/util/AndroidExtension.kt
@@ -2,9 +2,7 @@ package woowacourse.movie.presentation.util
 
 import android.content.Intent
 import android.os.Build
-import android.os.Bundle
 import android.os.Parcelable
-import java.io.Serializable
 
 @Suppress("DEPRECATION")
 inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String): T? {
@@ -12,41 +10,5 @@ inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String)
         getParcelableExtra(key, T::class.java)
     } else {
         getParcelableExtra(key) as? T
-    }
-}
-
-@Suppress("DEPRECATION")
-inline fun <reified T : Parcelable> Bundle.getParcelableCompat(key: String): T? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelable(key, T::class.java)
-    } else {
-        getParcelable(key) as? T
-    }
-}
-
-@Suppress("DEPRECATION")
-inline fun <reified T : Parcelable> Bundle.getParcelableArrayListCompat(key: String): ArrayList<T>? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelableArrayList(key, T::class.java)
-    } else {
-        getParcelableArrayList(key)
-    }
-}
-
-@Suppress("DEPRECATION")
-inline fun <reified T : Parcelable> Intent.getParcelableArrayListExtraCompat(key: String): ArrayList<T>? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelableArrayListExtra(key, T::class.java)
-    } else {
-        getParcelableArrayListExtra(key)
-    }
-}
-
-@Suppress("DEPRECATION")
-inline fun <reified T : Serializable> Intent.getSerializableExtraCompat(key: String): T? {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getSerializableExtra(key, T::class.java)
-    } else {
-        getSerializableExtra(key) as? T
     }
 }
