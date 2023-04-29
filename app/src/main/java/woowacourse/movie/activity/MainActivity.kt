c7116a4aa93738fa9c81d0fdd7c7af7784518492
SangHyun Moon <no1msh1217@gmail.com>
2023-04-29 18:32:27 +0900

    refactor: 메서드 분리

--- app/src/main/java/woowacourse/movie/activity/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/activity/MainActivity.kt
@@ -9,9 +9,11 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
-import woowacourse.movie.BundleKeys
+import com.google.android.material.switchmaterial.SwitchMaterial
+import woowacourse.movie.BundleKeys.SETTING_PUSH_ALARM_SWITCH_KEY
 import woowacourse.movie.R
 import woowacourse.movie.SharedPreferenceUtil
 import woowacourse.movie.fragment.BookHistoryFragment
@@ -19,68 +21,71 @@ import woowacourse.movie.fragment.HomeFragment
 import woowacourse.movie.fragment.SettingFragment
 
 class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+    val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            Toast.makeText(
+                this,
+                if (isGranted) getString(R.string.notification_permission_is_granted) else
+                    getString(R.string.notification_permission_is_denied),
+                Toast.LENGTH_SHORT
+            ).show()
+            SharedPreferenceUtil.setBooleanValue(this, SETTING_PUSH_ALARM_SWITCH_KEY, isGranted)
+            updatePushAlarmSwitch(isGranted)
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         requestNotificationPermission()
-
         findViewById<BottomNavigationView>(R.id.bnv_main).setOnItemSelectedListener(this)
-
         supportFragmentManager.beginTransaction().add(R.id.fl_main, BookHistoryFragment()).commit()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.page_book_history -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fl_main, BookHistoryFragment()).commitAllowingStateLoss()
+                replaceFragment(BookHistoryFragment())
                 return true
             }
             R.id.page_home -> {
-                supportFragmentManager.beginTransaction().replace(R.id.fl_main, HomeFragment())
-                    .commitAllowingStateLoss()
+                replaceFragment(HomeFragment())
                 return true
             }
             R.id.page_setting -> {
-                supportFragmentManager.beginTransaction().replace(R.id.fl_main, SettingFragment())
-                    .commitAllowingStateLoss()
+                replaceFragment(SettingFragment())
                 return true
             }
         }
-
         return false
     }
 
-    private fun requestNotificationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+    private fun updatePushAlarmSwitch(wantChecked: Boolean) {
+        supportFragmentManager.findFragmentById(R.id.fl_main)?.view?.findViewById<SwitchMaterial>(
+            R.id.sw_setting_can_push
+        )?.isChecked = wantChecked
+    }
 
+    private fun requestNotificationPermission() {
+        if (isPermissionDenied(Manifest.permission.POST_NOTIFICATIONS)) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                    Toast.makeText(this, getString(R.string.if_permission_is_denied_cant_use_notification_service), Toast.LENGTH_LONG)
-                        .show()
-                } else {
-                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                    Toast.makeText(
+                        this,
+                        getString(R.string.if_permission_is_denied_cant_use_notification_service),
+                        Toast.LENGTH_LONG
+                    ).show()
+                    return
                 }
-            } else {
-                // 안드로이드 12 이하는 Notification에 관한 권한 필요 없음
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
             }
         }
     }
 
-    val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-            if (isGranted) {
-                Toast.makeText(this, getString(R.string.notification_permission_is_granted), Toast.LENGTH_SHORT).show()
-                SharedPreferenceUtil(this).setSettingValue(
-                    BundleKeys.SETTING_PUSH_ALARM_SWITCH_KEY,
-                    true
-                )
-            } else {
-                Toast.makeText(this, getString(R.string.notification_permission_is_denied), Toast.LENGTH_SHORT).show()
-                SharedPreferenceUtil(this).setSettingValue(
-                    BundleKeys.SETTING_PUSH_ALARM_SWITCH_KEY,
-                    false
-                )
-            }
-        }
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fl_main, fragment).commitAllowingStateLoss()
+    }
+
+    fun isPermissionDenied(permission: String) =
+        ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED
 }
--- app/src/main/java/woowacourse/movie/fragment/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/fragment/SettingFragment.kt
@@ -1,53 +1,55 @@
 package woowacourse.movie.fragment
 
 import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.switchmaterial.SwitchMaterial
-import woowacourse.movie.BundleKeys
+import woowacourse.movie.BundleKeys.SETTING_PUSH_ALARM_SWITCH_KEY
 import woowacourse.movie.R
 import woowacourse.movie.SharedPreferenceUtil
 import woowacourse.movie.activity.MainActivity
 
 class SettingFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_setting, container, false)
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
 
-        val switch = view.findViewById<SwitchMaterial>(R.id.sw_setting_can_push)
-        val sharedPreferenceUtil = SharedPreferenceUtil(view.context)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val canPushSwitch = view.findViewById<SwitchMaterial>(R.id.sw_setting_can_push)
+
+        synchronizeCanPushSwitch(canPushSwitch)
+        setCanPushSwitchOnClickListener(canPushSwitch)
+        Log.d("bandal", "launch5 onViewCreate")
+
+    }
+
+    private fun synchronizeCanPushSwitch(switch: SwitchMaterial) {
         switch.isChecked =
-            sharedPreferenceUtil.getSettingValue(BundleKeys.SETTING_PUSH_ALARM_SWITCH_KEY, false)
+            SharedPreferenceUtil.getBooleanValue(
+                requireContext(),
+                SETTING_PUSH_ALARM_SWITCH_KEY,
+                false
+            )
+    }
 
-        switch.setOnCheckedChangeListener { _, _ ->
-            if (ContextCompat.checkSelfPermission(
-                    inflater.context,
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) == PackageManager.PERMISSION_DENIED
-            ) {
-                switch.isChecked = false
+    private fun setCanPushSwitchOnClickListener(switch: SwitchMaterial) {
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if ((activity as MainActivity).isPermissionDenied(Manifest.permission.POST_NOTIFICATIONS)) {
                 if (!shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
                     switch.isEnabled = false
-                } else {
-                    (activity as MainActivity).requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
                 }
+                if (isChecked) (activity as MainActivity).requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
             }
-
-            sharedPreferenceUtil.setSettingValue(
-                BundleKeys.SETTING_PUSH_ALARM_SWITCH_KEY,
-                switch.isChecked
-            )
         }
-        return view
     }
 }
