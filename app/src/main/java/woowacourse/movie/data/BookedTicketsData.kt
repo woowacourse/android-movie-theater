c5a9f02eac22c4dae460d811f6548d0f45146189
vrexpert <vrexpert2302@gmail.com>
2023-04-29 18:15:31 +0900

    refactor(BookedTicketsData): 이름 변경

--- app/src/main/java/woowacourse/movie/data/BookedTicketsData.kt
+++ app/src/main/java/woowacourse/movie/data/BookedTicketsData.kt
@@ -3,7 +3,7 @@ package woowacourse.movie.data
 import woowacourse.movie.presentation.model.TicketModel
 import java.time.LocalDateTime
 
-object BookedTickets {
+object BookedTicketsData {
 
     // fake data
     val tickets = mutableListOf<TicketModel>(
@@ -12,28 +12,28 @@ object BookedTickets {
             LocalDateTime.of(2024, 3, 1, 9, 0),
             3,
             33000,
-            listOf("A1", "B1", "C1")
+            listOf("A1", "B1", "C1"),
         ),
         TicketModel(
             2,
             LocalDateTime.of(2024, 3, 10, 22, 0),
             3,
             33000,
-            listOf("A1", "D1")
+            listOf("A1", "D1"),
         ),
         TicketModel(
             3,
             LocalDateTime.of(2024, 1, 1, 15, 0),
             3,
             11000,
-            listOf("A1", "B1")
+            listOf("A1", "B1"),
         ),
         TicketModel(
             4,
             LocalDateTime.of(2024, 1, 1, 9, 0),
             3,
             33000,
-            listOf("A1", "B1", "C1")
-        )
+            listOf("A1", "B1", "C1"),
+        ),
     )
 }
--- app/src/main/java/woowacourse/movie/presentation/bookedticketlist/BookedTicketsFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/bookedticketlist/BookedTicketsFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
-import woowacourse.movie.data.BookedTickets
+import woowacourse.movie.data.BookedTicketsData
 import woowacourse.movie.presentation.complete.CompleteActivity
 import woowacourse.movie.presentation.model.TicketModel
 
@@ -17,7 +17,7 @@ class BookedTicketsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_booked_tickets, container, false)
@@ -31,7 +31,7 @@ class BookedTicketsFragment : Fragment() {
     private fun setBookedTicketsAdapter() {
         requireActivity().findViewById<RecyclerView>(R.id.recyclerBookedTickets).adapter =
             bookedTicketsAdapter
-        bookedTicketsAdapter.submitList(BookedTickets.tickets)
+        bookedTicketsAdapter.submitList(BookedTicketsData.tickets)
     }
 
     private fun bookedTicketsItemClickListener(ticketModel: TicketModel) {
