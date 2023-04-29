0095631eda64dd617d080671bb376107f166676a
whk06061 <gju06061@naver.com>
2023-04-30 07:46:18 +0900

    refactor: Permission 관련 로직 클래스로 분리

---
+++ app/src/main/java/woowacourse/movie/Permission.kt
@@ -0,0 +1,48 @@
+package woowacourse.movie
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import woowacourse.movie.view.error.FragmentError.finishWithNullViewError
+
+object Permission {
+
+    fun requestNotificationPermission(
+        fragment: Fragment,
+        requestPermissionLauncher: ActivityResultLauncher<String>,
+        requestPermission: (View) -> Unit
+    ) {
+        val view: View = fragment.view ?: return fragment.finishWithNullViewError()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            if (!fragment.shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                requestPermission(view)
+            }
+        }
+    }
+
+    fun getRequestPermissionLauncher(
+        fragment: Fragment,
+        onGranted: (View?) -> Unit
+    ): ActivityResultLauncher<String> {
+        return fragment.registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (!isGranted) {
+                onGranted(fragment.view)
+            }
+        }
+    }
+
+    fun checkNotificationPermission(view: View): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.checkSelfPermission(
+                view.context, Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        } else false
+    }
+}
--- app/src/main/java/woowacourse/movie/fragment/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/fragment/SettingFragment.kt
@@ -1,17 +1,15 @@
 package woowacourse.movie.fragment
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.SwitchCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import woowacourse.movie.Permission
+import woowacourse.movie.Permission.checkNotificationPermission
+import woowacourse.movie.Permission.requestNotificationPermission
 import woowacourse.movie.R
 import woowacourse.movie.Setting
 import woowacourse.movie.SharedSetting
@@ -20,11 +18,7 @@ import woowacourse.movie.view.error.FragmentError.finishWithNullViewError
 class SettingFragment : Fragment() {
 
     private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (!isGranted) {
-                view?.findViewById<SwitchCompat>(R.id.setting_push_switch)?.isChecked = false
-            }
-        }
+        Permission.getRequestPermissionLauncher(this, ::onPermissionGranted)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -51,34 +45,21 @@ class SettingFragment : Fragment() {
         switch.setOnCheckedChangeListener { _, isChecked ->
             setting.setValue(SETTING_NOTIFICATION, isChecked)
             if (isChecked && !checkNotificationPermission(view)) {
-                requestNotificationPermission(view)
+                requestNotificationPermission(this, requestPermissionLauncher, ::requestPermission)
             }
         }
     }
 
-    private fun requestNotificationPermission(view: View) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-            if (!shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                showPermissionInstruction(view)
-            }
-        }
+    private fun onPermissionGranted(view: View?) {
+        view?.findViewById<SwitchCompat>(R.id.setting_push_switch)?.isChecked = false
     }
 
-    private fun showPermissionInstruction(view: View) {
+    private fun requestPermission(view: View) {
         Toast.makeText(
             view.context,
-            getString(R.string.permission_instruction_ment),
+            view.context.getString(R.string.permission_instruction_ment),
             Toast.LENGTH_SHORT
         ).show()
-    }
-
-    private fun checkNotificationPermission(view: View): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            ContextCompat.checkSelfPermission(
-                view.context, Manifest.permission.POST_NOTIFICATIONS
-            ) == PackageManager.PERMISSION_GRANTED
-        } else false
     }
 
     companion object {
