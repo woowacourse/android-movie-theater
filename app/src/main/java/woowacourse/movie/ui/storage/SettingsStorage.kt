07d3c030720b05536f4b8a1656035a3f224f4434
inseonyun <yuninseon@naver.com>
2023-04-30 00:53:53 +0900

    refactor(SettingsFragment): sharedPref nullable로 수정

--- app/src/main/java/woowacourse/movie/ui/storage/SettingsStorage.kt
+++ app/src/main/java/woowacourse/movie/ui/storage/SettingsStorage.kt
@@ -5,19 +5,17 @@ import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 
 object SettingsStorage {
-    private lateinit var sharedPref: SharedPreferences
-    private val KEY_PUSH_NOTIFICATION = "push_notification"
+    private var sharedPref: SharedPreferences? = null
+    private const val KEY_PUSH_NOTIFICATION = "push_notification"
 
     fun init(context: Context) {
         sharedPref = context.getSharedPreferences("settings", MODE_PRIVATE)
     }
 
-    fun getPushNotification(): Boolean = sharedPref.getBoolean(KEY_PUSH_NOTIFICATION, false)
+    fun getPushNotification(): Boolean =
+        sharedPref?.getBoolean(KEY_PUSH_NOTIFICATION, false) ?: false
 
     fun editPushNotification(activate: Boolean) {
-        sharedPref
-            .edit()
-            .putBoolean(KEY_PUSH_NOTIFICATION, activate)
-            .apply()
+        sharedPref?.edit()?.putBoolean(KEY_PUSH_NOTIFICATION, activate)?.apply()
     }
 }
