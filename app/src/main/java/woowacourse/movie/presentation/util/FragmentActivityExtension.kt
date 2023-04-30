729d9b2935caf78fa38c9c700de45bfede3c8f53
vrexpert <vrexpert2302@gmail.com>
2023-04-30 15:10:46 +0900

    refactor(FragmentActivityExtension): 확장 범위를 AppCompatActivity 에서 FragmentActivity 로 확장

--- app/src/main/java/woowacourse/movie/presentation/util/FragmentActivityExtension.kt
+++ app/src/main/java/woowacourse/movie/presentation/util/FragmentActivityExtension.kt
@@ -1,13 +1,13 @@
 package woowacourse.movie.presentation.util
 
 import androidx.annotation.IdRes
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 
-inline fun <reified T : Fragment> AppCompatActivity.replace(@IdRes frameId: Int) {
-    supportFragmentManager.commit {
+inline fun <reified T : Fragment> FragmentActivity.replace(@IdRes frameId: Int) {
+    this.supportFragmentManager.commit {
         replace<T>(frameId)
         setReorderingAllowed(true)
     }
