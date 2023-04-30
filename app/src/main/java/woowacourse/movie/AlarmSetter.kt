503cd73e5fc13bfcbf18e9c31f679251fa8793ed
SangHyun Moon <no1msh1217@gmail.com>
2023-04-30 16:12:26 +0900

    refactor: 알람 관리 오브젝트 메서드 시그니처 수정 및 관심사 분리

--- app/src/main/java/woowacourse/movie/AlarmSetter.kt
+++ app/src/main/java/woowacourse/movie/AlarmSetter.kt
@@ -2,8 +2,8 @@ package woowacourse.movie
 
 import android.app.AlarmManager
 import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import woowacourse.movie.movie.MovieBookingSeatInfo
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -11,25 +11,17 @@ import java.time.format.DateTimeFormatter
 object AlarmSetter {
     private const val MOVIE_RUN_BEFORE_TIME = 30L
 
-    fun setMovieStartBeforeAlarm(
-        activity: AppCompatActivity,
-        movieBookingSeatInfo: MovieBookingSeatInfo
-    ) {
-        val alarmManager =
-            activity.getSystemService(AppCompatActivity.ALARM_SERVICE) as AlarmManager
-        val movieReminderIntent = MovieReminder.intent(activity)
-        movieReminderIntent.putExtra(BundleKeys.MOVIE_BOOKING_SEAT_INFO_KEY, movieBookingSeatInfo)
+    fun setMovieStartBeforeAlarm(activity: AppCompatActivity, intent: Intent, alarmDate: String) {
+        val alarmManager = activity.getSystemService(AppCompatActivity.ALARM_SERVICE) as AlarmManager
         val pendingIntent = PendingIntent.getBroadcast(
             activity,
             MovieReminder.NOTIFICATION_ID,
-            movieReminderIntent,
+            intent,
             PendingIntent.FLAG_MUTABLE
         )
+
         val formatter = DateTimeFormatter.ofPattern("yyyy-M-d HH:mm")
-        val movieDateTime = LocalDateTime.parse(
-            movieBookingSeatInfo.movieBookingInfo.formatAlarmDate(),
-            formatter
-        )
+        val movieDateTime = LocalDateTime.parse(alarmDate, formatter)
         val triggerTime = movieDateTime.minusMinutes(MOVIE_RUN_BEFORE_TIME)
         val zone = triggerTime.atZone(ZoneId.systemDefault())
         val mill = zone.toInstant().toEpochMilli()
