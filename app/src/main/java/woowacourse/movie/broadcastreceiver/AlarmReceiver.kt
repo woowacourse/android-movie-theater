5178bd46ad2f310d0ca26f50c9e376e8638062a1
vrexpert <vrexpert2302@gmail.com>
2023-04-30 19:15:53 +0900

    refactor(AlarmReceiver): BroadcastReceiver 에서 BookedTicketNotification 분리

--- app/src/main/java/woowacourse/movie/broadcastreceiver/AlarmReceiver.kt
+++ app/src/main/java/woowacourse/movie/broadcastreceiver/AlarmReceiver.kt
@@ -1,38 +1,36 @@
 package woowacourse.movie.broadcastreceiver
 
-import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import woowacourse.movie.R
-import woowacourse.movie.data.MovieData
 import woowacourse.movie.presentation.complete.CompleteActivity
 import woowacourse.movie.presentation.model.TicketModel
 import woowacourse.movie.presentation.util.getParcelableExtraCompat
 
 class AlarmReceiver : BroadcastReceiver() {
 
+    lateinit var notificationChannel: NotificationChannel
     override fun onReceive(context: Context, intent: Intent) {
         val ticket =
             intent.getParcelableExtraCompat<TicketModel>(CompleteActivity.TICKET)
                 ?: throw NoSuchElementException()
 
-        val channel = setNotificationChannel(context)
-        initNotificationManager(context, channel)
-
-        val pendingIntent = initPendingIntent(context, ticket)
-        val builder = initNotificationBuilder(context, ticket, pendingIntent)
-        lunchNotification(context, builder)
+        if (!::notificationChannel.isInitialized) {
+            createChannel(context)
+        }
+        BookedTicketNotification.sendNotification(context, ticket)
     }
 
-    private fun setNotificationChannel(context: Context): NotificationChannel {
+    private fun createChannel(context: Context) {
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(createNotificationChannel(context))
+    }
+
+    private fun createNotificationChannel(context: Context): NotificationChannel {
         val channelId = context.getString(R.string.alarm_channel_id)
         val channelName = context.getString(R.string.alarm_channel_name)
         val channelDescription = context.getString(R.string.alarm_channel_description)
@@ -41,61 +39,5 @@ class AlarmReceiver : BroadcastReceiver() {
             description = channelDescription
         }
         return channel
-    }
-
-    private fun initNotificationManager(
-        context: Context,
-        channel: NotificationChannel
-    ) {
-        val notificationManager: NotificationManager =
-            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-    }
-
-    private fun initPendingIntent(
-        context: Context,
-        ticket: TicketModel
-    ): PendingIntent? {
-        val intent = CompleteActivity.getIntent(context, ticket)
-
-        return PendingIntent.getActivity(
-            context,
-            0,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
-    }
-
-    private fun initNotificationBuilder(
-        context: Context,
-        ticket: TicketModel,
-        pendingIntent: PendingIntent?
-    ): NotificationCompat.Builder {
-        return NotificationCompat.Builder(context, context.getString(R.string.alarm_channel_id))
-            .setSmallIcon(R.drawable.ic_home)
-            .setContentTitle(context.getString(R.string.booked_ticket_notification_title))
-            .setContentText(
-                context.getString(
-                    R.string.booked_ticket_notification_content,
-                    MovieData.findMovieById(ticket.movieId).title
-                )
-            )
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-    }
-
-    private fun lunchNotification(
-        context: Context,
-        builder: NotificationCompat.Builder
-    ) {
-        with(NotificationManagerCompat.from(context)) {
-            if (ActivityCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) return
-            notify(1, builder.build())
-        }
     }
 }
--- app/src/main/java/woowacourse/movie/presentation/main/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/main/MainActivity.kt
@@ -1,19 +1,16 @@
 package woowacourse.movie.presentation.main
 
 import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import woowacourse.movie.R
 import woowacourse.movie.presentation.bookedticketlist.BookedTicketsFragment
 import woowacourse.movie.presentation.movielist.MovieListFragment
 import woowacourse.movie.presentation.settings.SettingsFragment
+import woowacourse.movie.presentation.util.checkPermissionTiramisu
 import woowacourse.movie.presentation.util.replace
 
 class MainActivity : AppCompatActivity() {
@@ -31,29 +28,11 @@ class MainActivity : AppCompatActivity() {
         requestNotificationPermission()
     }
 
+    @SuppressLint("InlinedApi")
     private fun requestNotificationPermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.POST_NOTIFICATIONS,
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            lunchRequestPermissionWithVersionCheck()
+        if (!applicationContext.checkPermissionTiramisu(Manifest.permission.POST_NOTIFICATIONS)) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
         }
-    }
-
-    private fun lunchRequestPermissionWithVersionCheck() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            lunchRequestPermission()
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    private fun lunchRequestPermission() {
-        if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-            Toast.makeText(this, ALARM_DENIED_TOAST_MESSAGE, Toast.LENGTH_LONG).show()
-            return
-        }
-        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
     }
 
     private fun setInitialFragment() {
@@ -86,6 +65,5 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val BOTTOM_NAVIGATION_WRONG_ITEM_ID_ERROR = "설정된 아이템 제외한 아이템 아이디가 전달되었습니다."
-        private const val ALARM_DENIED_TOAST_MESSAGE = "알림거부 횟수가 2회 이상이라 앱 설정에서 직접 알림 설정을 켜야합니다."
     }
 }
