a7476e2c81901c251278744bd9d003e2962a0f58
whk06061 <gju06061@naver.com>
2023-05-01 22:05:08 +0900

    refactor: 필요한 파라미터만 받도록 수정

--- app/src/main/java/woowacourse/movie/service/Permission.kt
+++ app/src/main/java/woowacourse/movie/service/Permission.kt
@@ -1,47 +1,44 @@
 package woowacourse.movie.service
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
-import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import woowacourse.movie.view.error.FragmentError.finishWithNullViewError
 
 object Permission {
 
     fun requestNotificationPermission(
         fragment: Fragment,
         requestPermissionLauncher: ActivityResultLauncher<String>,
-        requestPermission: (View) -> Unit
+        requestPermission: () -> Unit
     ) {
-        val view: View = fragment.view ?: return fragment.finishWithNullViewError()
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
             if (!fragment.shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                requestPermission(view)
+                requestPermission()
             }
         }
     }
 
     fun getRequestPermissionLauncher(
         fragment: Fragment,
-        onGranted: (View?) -> Unit
+        onGranted: () -> Unit
     ): ActivityResultLauncher<String> {
         return fragment.registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (!isGranted) {
-                onGranted(fragment.view)
+                onGranted()
             }
         }
     }
 
-    fun checkNotificationPermission(view: View): Boolean {
+    fun checkNotificationPermission(context: Context): Boolean {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             ContextCompat.checkSelfPermission(
-                view.context, Manifest.permission.POST_NOTIFICATIONS
+                context, Manifest.permission.POST_NOTIFICATIONS
             ) == PackageManager.PERMISSION_GRANTED
         } else false
     }
