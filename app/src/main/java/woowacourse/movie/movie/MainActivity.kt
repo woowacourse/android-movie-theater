27ccb70dd040d78063e6d05427d608b1a6a3c59b
hyunji1203 <hj27720@gmail.com>
2023-05-02 17:54:00 +0900

    refactor: show와 hide를 이용해 프래그먼트 재활용

--- app/src/main/java/woowacourse/movie/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/movie/MainActivity.kt
@@ -4,12 +4,13 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.fragment.app.commit
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import woowacourse.movie.R
 import woowacourse.movie.databinding.ActivityMainBinding
@@ -25,28 +26,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         requestNotificationPermission()
-        binding.navigationBar?.let(::bindNavigationBar)
+
+        setFragment(TAG_HOME_FRAGMENT, HomeFragment())
+
+        binding.navigationBar?.let(::onNavigationBarClickListener)
     }
 
-    private fun bindNavigationBar(bottomNav: BottomNavigationView) {
-        bottomNav.run {
-            setOnItemSelectedListener {
-                onNavigationItemSelected(it)
-                true
+    private fun onNavigationBarClickListener(bottomNavigationView: BottomNavigationView) {
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> setFragment(TAG_HOME_FRAGMENT, HomeFragment())
+                R.id.history -> setFragment(TAG_HISTORY_FRAGMENT, HistoryFragment())
+                R.id.setting -> setFragment(TAG_SETTING_FRAGMENT, SettingFragment())
             }
-            selectedItemId = R.id.home
+            true
         }
     }
 
-    private fun onNavigationItemSelected(item: MenuItem) {
-        fragmentRepository[item.itemId]?.let { nowFragment ->
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                replace(R.id.fragment_container_view, nowFragment)
-            }
+    private fun setFragment(tag: String, fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        val ft = manager.beginTransaction()
+        if (manager.findFragmentByTag(tag) == null) {
+            ft.add(R.id.fragment_container_view, fragment, tag)
         }
+        val home = manager.findFragmentByTag(TAG_HOME_FRAGMENT)
+        val history = manager.findFragmentByTag(TAG_HISTORY_FRAGMENT)
+        val setting = manager.findFragmentByTag(TAG_SETTING_FRAGMENT)
+
+        hideFragment(ft, home, history, setting)
+
+        if (tag == TAG_HOME_FRAGMENT) {
+            if (home != null) ft.show(home)
+        }
+        if (tag == TAG_HISTORY_FRAGMENT) {
+            if (history != null) ft.show(history)
+        }
+        if (tag == TAG_SETTING_FRAGMENT) {
+            if (setting != null) ft.show(setting)
+        }
+        ft.commitAllowingStateLoss()
+    }
+
+    private fun hideFragment(ft: FragmentTransaction, home: Fragment?, history: Fragment?, setting: Fragment?) {
+        if (home != null) ft.hide(home)
+        if (history != null) ft.hide(history)
+        if (setting != null) ft.hide(setting)
     }
 
     private fun requestNotificationPermission() {
@@ -77,16 +102,12 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val setting_preference_key = "setting"
 
+        private const val TAG_HOME_FRAGMENT = "home_fragment"
+        private const val TAG_HISTORY_FRAGMENT = "history_fragment"
+        private const val TAG_SETTING_FRAGMENT = "setting_fragment"
+
         private const val DENIED_PERMISSION_MESSAGE = "알림 권한이 차단되었습니다"
         private const val PERMIT_PERMISSION_MESSAGE = "알림 권한이 허용되었습니다"
         private const val LOWER_VERSION_MESSAGE = "설정에 가서 알림 권한을 켜주세요"
-
-        val fragmentRepository = mapOf(
-            R.id.history to HistoryFragment(),
-            R.id.home to HomeFragment(),
-            R.id.setting to SettingFragment(),
-        )
-
-
     }
 }
