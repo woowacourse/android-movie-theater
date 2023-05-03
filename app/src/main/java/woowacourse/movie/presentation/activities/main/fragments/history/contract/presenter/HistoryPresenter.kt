756b8d7f385a98f1ee92f57ba3494f9b598b4e65
tmdgh1592 <tmdgh1592@naver.com>
2023-05-03 16:04:49 +0900

    refactor(HistoryFragment): presenter에 view를 attach하도록 변경

--- app/src/main/java/woowacourse/movie/presentation/activities/main/fragments/history/contract/presenter/HistoryPresenter.kt
+++ app/src/main/java/woowacourse/movie/presentation/activities/main/fragments/history/contract/presenter/HistoryPresenter.kt
@@ -4,19 +4,24 @@ import woowacourse.movie.presentation.activities.main.fragments.history.contract
 import woowacourse.movie.presentation.model.Reservation
 import woowacourse.movie.presentation.model.movieitem.ListItem
 
-class HistoryPresenter(view: HistoryContract.View) : HistoryContract.Presenter(view) {
+class HistoryPresenter : HistoryContract.Presenter() {
     private val loadedHistories = mutableSetOf<ListItem>()
+
+    override fun attach(view: HistoryContract.View) {
+        super.attach(view)
+        loadHistories()
+    }
 
     override fun loadHistories() {
         val newHistories = Reservation.provideDummy()
 
         loadedHistories.addAll(newHistories)
-        view.showExtraHistories(newHistories)
+        requireView().showExtraHistories(newHistories)
     }
 
     override fun onClickItem(item: ListItem) {
         when (item) {
-            is Reservation -> view.showDetails(item)
+            is Reservation -> requireView().showDetails(item)
         }
     }
 }
