985f3fc8c34ab95f79a489e4032ba7549b8b4d6f
re4rk <37167652+re4rk@users.noreply.github.com>
2023-05-04 01:20:01 +0900

    refactor : 극장에 해당 영화를 개봉하지 않았으면 보이지 않음

--- app/src/main/java/woowacourse/movie/ui/main/viewHolder/CinemaListViewHolder.kt
+++ app/src/main/java/woowacourse/movie/ui/main/viewHolder/CinemaListViewHolder.kt
@@ -1,5 +1,8 @@
 package woowacourse.movie.ui.main.viewHolder
 
+import android.view.View
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.databinding.ItemCinemaBottomSheetBinding
 import woowacourse.movie.ui.main.itemModel.BottomSheetItemModel
 import woowacourse.movie.ui.main.itemModel.ItemModel
@@ -15,5 +18,20 @@ class CinemaListViewHolder(
     override fun bind(itemModel: ItemModel) {
         val item = itemModel as? BottomSheetItemModel ?: return
         binding.cinema = item.cinema
+        setVisibility(item.cinema.numberOfMovie > 0)
+    }
+
+    private fun setVisibility(isVisible: Boolean) {
+        val param = itemView.layoutParams as RecyclerView.LayoutParams
+        if (isVisible) {
+            param.height = LinearLayout.LayoutParams.WRAP_CONTENT
+            param.width = LinearLayout.LayoutParams.MATCH_PARENT
+            itemView.visibility = View.VISIBLE
+        } else {
+            param.height = 0
+            param.width = 0
+            itemView.visibility = View.GONE
+        }
+        itemView.layoutParams = param
     }
 }
