b20e1bf7d333ce57cb90e689d2d05fda04e596df
Doyeop Kim <dygames34@gmail.com>
2023-05-04 16:47:27 +0900

    test: ReservationResultPresenter에 대한 Test 작성

---
+++ app/src/test/java/woowacourse/movie/presenter/ReservationResultPresenterTest.kt
@@ -0,0 +1,57 @@
+package woowacourse.movie.presenter
+
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+import woowacourse.movie.contract.ReservationResultContract
+import woowacourse.movie.domain.Price
+import woowacourse.movie.domain.Reservation
+import woowacourse.movie.domain.ReservationDetail
+import woowacourse.movie.domain.mock.MovieMock
+import woowacourse.movie.domain.seat.Seats
+import woowacourse.movie.mapper.ReservationMapper.toView
+import java.time.LocalDateTime
+
+class ReservationResultPresenterTest {
+    lateinit var reservationResultPresenter: ReservationResultContract.Presenter
+    lateinit var view: ReservationResultContract.View
+
+    @Before
+    fun init() {
+        view = mockk()
+        reservationResultPresenter = ReservationResultPresenter(view)
+    }
+
+    @Test
+    fun initActivity() {
+        // given
+        every { view.setMovieData(any()) } just runs
+        every { view.setReservationDetailData(any()) } just runs
+        every { view.setSeatData(any(), any()) } just runs
+        every { view.setPriceData(any()) } just runs
+        val reservation = fakeReservation().toView()
+
+        // when
+        reservationResultPresenter.initActivity(reservation)
+
+        // then
+        verify { view.setMovieData(any()) }
+        verify { view.setReservationDetailData(any()) }
+        verify { view.setSeatData(any(), any()) }
+        verify { view.setPriceData(any()) }
+    }
+
+    private fun fakeReservation(): Reservation = Reservation(
+        MovieMock.createMovie(),
+        ReservationDetail(
+            LocalDateTime.now(),
+            1
+        ),
+        Seats(emptyList()),
+        Price(),
+    )
+}
