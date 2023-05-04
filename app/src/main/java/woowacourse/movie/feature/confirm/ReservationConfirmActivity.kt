1c8697dd7850ed951d40b5284eb6e5226fcdb8ad
mendel <rhthrhrl0@naver.com>
2023-05-04 15:33:15 +0900

    refactor(ReservationConfirmActivity): 데이터바인딩 적용

--- app/src/main/java/woowacourse/movie/feature/confirm/ReservationConfirmActivity.kt
+++ app/src/main/java/woowacourse/movie/feature/confirm/ReservationConfirmActivity.kt
@@ -3,48 +3,21 @@ package woowacourse.movie.feature.confirm
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import woowacourse.movie.R
+import woowacourse.movie.databinding.ActivityReservationConfirmBinding
 import woowacourse.movie.feature.common.BackKeyActionBarActivity
 import woowacourse.movie.model.TicketsState
-import woowacourse.movie.util.DateTimeFormatters
-import woowacourse.movie.util.DecimalFormatters
 import woowacourse.movie.util.getParcelableExtraCompat
 import woowacourse.movie.util.keyError
 
 class ReservationConfirmActivity : BackKeyActionBarActivity() {
-    private val titleTextView: TextView by lazy { findViewById(R.id.reservation_title) }
-    private val dateTextView: TextView by lazy { findViewById(R.id.reservation_date) }
-    private val moneyTextView: TextView by lazy { findViewById(R.id.reservation_money) }
-    private val reservationCountTextView: TextView by lazy { findViewById(R.id.reservation_count_and_seat) }
-
+    private lateinit var binding: ActivityReservationConfirmBinding
     override fun onCreateView(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_reservation_confirm)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reservation_confirm)
         val tickets = intent.getParcelableExtraCompat<TicketsState>(KEY_TICKETS)
             ?: return keyError(KEY_TICKETS)
-        setInitReservationData(tickets)
-    }
-
-    private fun setInitReservationData(
-        tickets: TicketsState
-    ) {
-        titleTextView.text = tickets.movieState.title
-        dateTextView.text =
-            DateTimeFormatters.convertToDateTime(tickets.dateTime)
-        reservationCountTextView.text =
-            getString(
-                R.string.person_count_and_seat,
-                tickets.tickets.size,
-                tickets.tickets.map { it.seatPositionState }.joinToString { it.toString() }
-            )
-        setDiscountApplyMoney(tickets)
-    }
-
-    private fun setDiscountApplyMoney(tickets: TicketsState) {
-        val discountApplyMoney = tickets.totalDiscountedMoneyState
-
-        moneyTextView.text =
-            DecimalFormatters.convertToMoneyFormat(discountApplyMoney)
+        binding.tickets = tickets
     }
 
     companion object {
---
+++ app/src/main/java/woowacourse/movie/feature/confirm/ReservationConfirmBindingAdapter.kt
@@ -0,0 +1,15 @@
+package woowacourse.movie.feature.confirm
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import woowacourse.movie.R
+import woowacourse.movie.model.TicketsState
+
+@BindingAdapter("reservationCountAndSeats")
+fun setReservationCountAndSeats(view: TextView, tickets: TicketsState) {
+    view.text = view.context.getString(
+        R.string.person_count_and_seat,
+        tickets.tickets.size,
+        tickets.tickets.map { it.seatPositionState }.joinToString { it.toString() }
+    )
+}
