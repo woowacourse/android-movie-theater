343a0698c1ae6080d3a31bb343a28a15c347eecd
re4rk <37167652+re4rk@users.noreply.github.com>
2023-05-04 11:17:44 +0900

    refactor : 예약 목록 접근 방법을 view binding으로 변경

--- app/src/main/java/woowacourse/movie/ui/main/reservationList/ReservationListFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/main/reservationList/ReservationListFragment.kt
@@ -2,50 +2,57 @@ package woowacourse.movie.ui.main.reservationList
 
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
-import woowacourse.movie.R
 import woowacourse.movie.data.TicketsRepository
+import woowacourse.movie.databinding.FragmentReservationListBinding
 import woowacourse.movie.model.TicketsState
 import woowacourse.movie.ui.adapter.ReservationListAdapter
 import woowacourse.movie.ui.confirm.ReservationConfirmActivity
 import woowacourse.movie.ui.itemModel.TicketsItemModel
 
-class ReservationListFragment :
-    Fragment(R.layout.fragment_reservation_list),
-    ReservationListContract.View {
+class ReservationListFragment : Fragment(), ReservationListContract.View {
     private lateinit var presenter: ReservationListContract.Presenter
+    private lateinit var binding: FragmentReservationListBinding
 
-    private var reservationRecyclerView: RecyclerView? = null
-    private var adapter: ReservationListAdapter? = null
+    private lateinit var adapter: ReservationListAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        setUpBinding()
+        setUpPresenter()
+        return binding.root
+    }
+
+    private fun setUpBinding() {
+        binding = FragmentReservationListBinding.inflate(layoutInflater)
+    }
+
+    private fun setUpPresenter() {
+        presenter = ReservationListPresenter()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        presenter = ReservationListPresenter()
 
         adapter = ReservationListAdapter(
             presenter.getReservationList().map(::TicketsItemModel)
         ) { ticketsItemModel ->
             navigateReservationConfirm(ticketsItemModel.ticketsState)
         }
-
-        reservationRecyclerView = view.findViewById(R.id.reservation_rv)
-        reservationRecyclerView?.adapter = adapter
+        binding.reservationList.adapter = adapter
     }
 
     override fun onHiddenChanged(hidden: Boolean) {
         super.onHiddenChanged(hidden)
-        adapter?.setItemChanged(
+        adapter.setItemChanged(
             TicketsRepository.allTickets().map(::TicketsItemModel)
         )
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        reservationRecyclerView = null
-        adapter = null
     }
 
     private fun navigateReservationConfirm(ticketsState: TicketsState) {
