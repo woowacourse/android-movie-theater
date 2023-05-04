0364f805c868606cb4c1baedc7b508c69dc4d37b
re4rk <37167652+re4rk@users.noreply.github.com>
2023-05-04 11:05:57 +0900

    refactor : 극장 바텀 시트 view binding으로 변경

--- app/src/main/java/woowacourse/movie/ui/main/cinemaBottomSheet/CinemaListBottomSheet.kt
+++ app/src/main/java/woowacourse/movie/ui/main/cinemaBottomSheet/CinemaListBottomSheet.kt
@@ -1,11 +1,12 @@
 package woowacourse.movie.ui.main.cinemaBottomSheet
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentCinemaBottomSheetBinding
 import woowacourse.movie.model.CinemaState
 import woowacourse.movie.model.MovieState
 import woowacourse.movie.ui.adapter.CinemaListAdapter
@@ -15,25 +16,39 @@ import woowacourse.movie.ui.reservation.MovieDetailActivity
 class CinemaListBottomSheet(
     private val movie: MovieState
 ) : BottomSheetDialogFragment(R.layout.fragment_cinema_bottom_sheet), CinemaListContract.View {
-    val presenter: CinemaListContract.Presenter = CinemaListPresenter(this)
+    private lateinit var presenter: CinemaListContract.Presenter
 
-    var button: Button? = null
+    private lateinit var binding: FragmentCinemaBottomSheetBinding
 
-    var bottomSheetListView: RecyclerView? = null
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        setUpBinding()
+        setUpPresenter()
+        return binding.root
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        bottomSheetListView = view.findViewById(R.id.bottom_sheet_movie_list)
-
         presenter.getCinemaList(movie)
     }
 
     override fun setAdapter(cinemas: List<CinemaState>) {
         val adapter = CinemaListAdapter(
             cinemas = cinemas.map { CinemaItemModel(it, movie) }
-        ) { itemModel -> navigateMovieDetail(itemModel) }
-        bottomSheetListView?.adapter = adapter
+        ) { itemModel ->
+            navigateMovieDetail(itemModel)
+        }
+        binding.bottomSheetMovieList.adapter = adapter
+    }
+
+    private fun setUpBinding() {
+        binding = FragmentCinemaBottomSheetBinding.inflate(layoutInflater)
+    }
+
+    private fun setUpPresenter() {
+        presenter = CinemaListPresenter(this)
     }
 
     private fun navigateMovieDetail(cinemaItemModel: CinemaItemModel) {
@@ -42,11 +57,6 @@ class CinemaListBottomSheet(
             cinemaItemModel.cinema.name,
             cinemaItemModel.movie
         )
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        button = null
     }
 
     companion object {
