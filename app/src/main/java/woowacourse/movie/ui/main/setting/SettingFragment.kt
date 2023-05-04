961cb360a9d48724e853bb238482e13bdc9f7421
re4rk <37167652+re4rk@users.noreply.github.com>
2023-05-04 11:21:14 +0900

    refactor : 세팅 접근 방법을 view binding으로 변경

--- app/src/main/java/woowacourse/movie/ui/main/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/main/setting/SettingFragment.kt
@@ -1,27 +1,44 @@
 package woowacourse.movie.ui.main.setting
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.appcompat.widget.SwitchCompat
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import woowacourse.movie.DefaultPreference
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentSettingBinding
 import woowacourse.movie.ui.Toaster
 import woowacourse.movie.ui.main.MainActivity.Companion.PERMISSIONS
 import woowacourse.movie.util.hasPermissions
 
 class SettingFragment : Fragment(R.layout.fragment_setting), SettingContract.View {
     private lateinit var presenter: SettingContract.Presenter
+    private lateinit var binding: FragmentSettingBinding
 
-    private var switch: SwitchCompat? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        setUpBinding()
+        setUpPresenter()
+        return binding.root
+    }
+
+    private fun setUpBinding() {
+        binding = FragmentSettingBinding.inflate(layoutInflater)
+    }
+
+    private fun setUpPresenter() {
+        presenter = SettingPresenter(this, DefaultPreference(requireContext()))
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        presenter = SettingPresenter(this, DefaultPreference(requireContext()))
         presenter.getBoolean(NOTIFICATIONS, false)
 
-        switch = view.findViewById(R.id.notification_switch)
-        switch?.setOnCheckedChangeListener { switchCompat, _ ->
+        binding.notificationSwitch.setOnCheckedChangeListener { switchCompat, _ ->
             val permission = requireContext().hasPermissions(PERMISSIONS)
             val isChecked = when {
                 permission -> switchCompat.isChecked
@@ -35,12 +52,7 @@ class SettingFragment : Fragment(R.layout.fragment_setting), SettingContract.Vie
     }
 
     override fun setSwitchChecked(boolean: Boolean) {
-        switch?.isChecked = boolean
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        switch = null
+        binding.notificationSwitch.isChecked = boolean
     }
 
     companion object {
