cdaf9750857d25da8a0bd54a83e03949b6c5b755
re4rk <37167652+re4rk@users.noreply.github.com>
2023-05-04 11:10:19 +0900

    refactor : 영화 목록 접근 방법을 view binding으로 변경

--- app/src/main/java/woowacourse/movie/ui/main/movieList/MovieListFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/main/movieList/MovieListFragment.kt
@@ -2,10 +2,12 @@ package woowacourse.movie.ui.main.movieList
 
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentMovieListBinding
 import woowacourse.movie.model.AdvState
 import woowacourse.movie.model.MovieState
 import woowacourse.movie.ui.adapter.MovieListAdapter
@@ -17,29 +19,37 @@ import woowacourse.movie.ui.main.cinemaBottomSheet.CinemaListBottomSheet
 class MovieListFragment : Fragment(R.layout.fragment_movie_list) {
     private lateinit var presenter: MovieListContract.Presenter
 
-    private var movieListView: RecyclerView? = null
-    private var adapter: MovieListAdapter? = null
+    private lateinit var binding: FragmentMovieListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        setUpPresenter()
+        setUpBinding()
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         presenter = MovieListPresenter()
 
-        adapter = MovieListAdapter(
+        binding.movieList.adapter = MovieListAdapter(
             movie = presenter.getMovieList().map(::MovieItemModel),
             adv = presenter.getAdvList().map(::AdvItemModel),
             onClickMovie = { movieItemModel -> showCinemaBottomSheet(movieItemModel.movieState) },
             onClickAdv = { advItemModel -> navigateAdbDetail(advItemModel.advState) }
         )
-
-        movieListView = view.findViewById(R.id.rv_main)
-        movieListView?.adapter = adapter
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        movieListView = null
-        adapter = null
+    private fun setUpBinding() {
+        binding = FragmentMovieListBinding.inflate(layoutInflater)
+    }
+
+    private fun setUpPresenter() {
+        presenter = MovieListPresenter()
     }
 
     private fun showCinemaBottomSheet(movie: MovieState) {
