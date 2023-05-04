f9aa23a06f6df78ed0db02c65d638e663ac310a3
Gloria <ippnsj@gmail.com>
2023-05-04 16:14:47 +0900

    feat: 영화 선택 레이아웃 작성

---
+++ app/src/main/java/woowacourse/movie/ui/fragment/movielist/TheaterSelectorFragment.kt
@@ -0,0 +1,19 @@
+package woowacourse.movie.ui.fragment.movielist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import woowacourse.movie.R
+
+class TheaterSelectorFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_theater_selector, container, false)
+    }
+}
