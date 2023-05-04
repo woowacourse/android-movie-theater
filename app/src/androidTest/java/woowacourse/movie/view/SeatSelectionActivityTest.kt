08e3b4da6d836f0a83d453f95db72a59ed86c63b
hyemdooly <hyemdooly@gmail.com>
2023-05-04 23:31:39 +0900

    refactor(ReservationActivityTest, SeatSelectionActivityTest): 리팩터링에 따른 테스트 코드 수정

--- app/src/androidTest/java/woowacourse/movie/view/SeatSelectionActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/SeatSelectionActivityTest.kt
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith
 import woowacourse.movie.R
 import woowacourse.movie.domain.movie.Minute
 import woowacourse.movie.domain.movie.Movie
+import woowacourse.movie.domain.movie.ScreeningDateTimes
 import woowacourse.movie.view.mapper.toUiModel
 import woowacourse.movie.view.model.ReservationOptions
 import woowacourse.movie.view.seatselection.SeatSelectionActivity
@@ -33,22 +34,24 @@ class SeatSelectionActivityTest {
     private val reservationOptions = ReservationOptions(
         "해리 포터와 마법사의 돌",
         LocalDateTime.of(LocalDate.of(2024, 3, 1), LocalTime.of(13, 0)),
-        2
+        2,
     )
 
     private val movie = Movie(
         "해리 포터와 마법사의 돌",
-        LocalDate.of(2024, 3, 1),
-        LocalDate.of(2024, 3, 31),
+        ScreeningDateTimes.of(
+            LocalDate.of(2024, 3, 1),
+            LocalDate.of(2024, 3, 31),
+            listOf(LocalTime.of(12, 0), LocalTime.of(14, 0), LocalTime.of(16, 0)),
+        ),
         Minute(152),
-        R.drawable.harry_potter1_poster,
-        "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다."
+        "《해리 포터와 마법사의 돌》은 2001년 J. K. 롤링의 동명 소설을 원작으로 하여 만든, 영국과 미국 합작, 판타지 영화이다. 해리포터 시리즈 영화 8부작 중 첫 번째에 해당하는 작품이다. 크리스 콜럼버스가 감독을 맡았다.",
     )
 
     private val intent = SeatSelectionActivity.newIntent(
         ApplicationProvider.getApplicationContext(),
         reservationOptions,
-        movie.toUiModel()
+        movie.toUiModel(R.drawable.harry_potter1_poster),
     )
 
     @get:Rule
@@ -56,7 +59,7 @@ class SeatSelectionActivityTest {
 
     @Test
     fun 영화_제목을_표시한다() {
-        onView(withId(R.id.movie_title_textview))
+        onView(withId(R.id.text_title))
             .check(matches(withText("해리 포터와 마법사의 돌")))
     }
 
@@ -74,7 +77,7 @@ class SeatSelectionActivityTest {
     @Test
     fun 인원수에_해당하는_좌석이_모두_선택되지_않았다면_확인_버튼은_비활성화_상태다() {
         onView(withText("A1")).perform(click())
-        onView(withId(R.id.confirm_reservation_button))
+        onView(withId(R.id.btn_next))
             .check(matches(isNotEnabled()))
     }
 
@@ -82,7 +85,7 @@ class SeatSelectionActivityTest {
     fun 인원수에_해당하는_좌석이_모두_선택되었다면_확인_버튼은_활성화_상태다() {
         onView(withText("A1")).perform(click())
         onView(withText("A2")).perform(click())
-        onView(withId(R.id.confirm_reservation_button))
+        onView(withId(R.id.btn_next))
             .check(matches(isEnabled()))
     }
 
@@ -90,7 +93,7 @@ class SeatSelectionActivityTest {
     fun 인원수에_해당하는_좌석이_모두_선택되었다면_최종_금액이_표시된다() {
         onView(withText("A1")).perform(click())
         onView(withText("A2")).perform(click())
-        onView(withId(R.id.reservation_fee_textview)).check(matches(withText("20,000원")))
+        onView(withId(R.id.text_price)).check(matches(withText("20,000원")))
     }
 
     @Test
@@ -98,6 +101,6 @@ class SeatSelectionActivityTest {
         onView(withText("A1")).perform(click())
         onView(withText("A2")).perform(click())
         onView(withText("A1")).perform(click())
-        onView(withId(R.id.reservation_fee_textview)).check(matches(withText("0원")))
+        onView(withId(R.id.text_price)).check(matches(withText("0원")))
     }
 }
