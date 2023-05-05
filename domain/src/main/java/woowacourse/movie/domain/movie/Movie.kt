8ddcb4be3a1a12df9a8eae9d6cdfc269e1957a4d
hyemdooly <hyemdooly@gmail.com>
2023-05-05 15:57:31 +0900

    refactor(Movie, Schedule): Schedule 클래스 생성, Movie 프로퍼티 변경

--- domain/src/main/java/woowacourse/movie/domain/movie/Movie.kt
+++ domain/src/main/java/woowacourse/movie/domain/movie/Movie.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie.domain.movie
 
 import java.time.LocalDate
+import java.time.LocalTime
 
 data class Movie(
     val title: String,
@@ -8,5 +9,11 @@ data class Movie(
     val endDate: LocalDate,
     val runningTime: Minute,
     val summary: String,
-    val schedule: Schedule
-)
+    val schedule: Schedule,
+) {
+    companion object {
+        fun of(title: String, startDate: LocalDate, endDate: LocalDate, runningTime: Minute, summary: String, scheduleOfTheater: Map<String, List<LocalTime>>): Movie {
+            return Movie(title, startDate, endDate, runningTime, summary, Schedule.of(startDate, endDate, scheduleOfTheater))
+        }
+    }
+}
--- domain/src/main/java/woowacourse/movie/domain/movie/Schedule.kt
+++ domain/src/main/java/woowacourse/movie/domain/movie/Schedule.kt
@@ -1,3 +1,16 @@
 package woowacourse.movie.domain.movie
 
-data class Schedule(val schedule: Map<String, ScreeningDateTimes>) // Theater name to Map<LocalDate, List<LocalTimes>
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class Schedule(val schedule: Map<String, ScreeningDateTimes>) {
+    companion object {
+        fun of(startDate: LocalDate, endDate: LocalDate, scheduleOfTheater: Map<String, List<LocalTime>>): Schedule {
+            val schedule = mutableMapOf<String, ScreeningDateTimes>()
+            scheduleOfTheater.forEach { (name, times) ->
+                schedule[name] = ScreeningDateTimes.of(startDate, endDate, times)
+            }
+            return Schedule(schedule)
+        }
+    }
+}
