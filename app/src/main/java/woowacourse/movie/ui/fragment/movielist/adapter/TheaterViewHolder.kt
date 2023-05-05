8264c2f4678c3651573f6b10ba8d8d6f277592f5
Gloria <ippnsj@gmail.com>
2023-05-05 16:05:49 +0900

    refactor: 영화와 극장 아이템 DataBinding 적용

--- app/src/main/java/woowacourse/movie/ui/fragment/movielist/adapter/TheaterViewHolder.kt
+++ app/src/main/java/woowacourse/movie/ui/fragment/movielist/adapter/TheaterViewHolder.kt
@@ -1,26 +1,28 @@
 package woowacourse.movie.ui.fragment.movielist.adapter
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
+import woowacourse.movie.databinding.TheaterItemBinding
 import woowacourse.movie.ui.model.TheaterModel
 
 class TheaterViewHolder(view: View, private val onItemViewClick: (Int) -> Unit) :
     RecyclerView.ViewHolder(view) {
-    private val theaterDescription: TextView by lazy { view.findViewById(R.id.tv_theater_description) }
-    private val theaterName: TextView by lazy { view.findViewById(R.id.tv_theater_name) }
-    private val theaterNextButton: ImageView by lazy { view.findViewById(R.id.iv_theater_next) }
+    private val binding: TheaterItemBinding
 
     init {
+        binding = TheaterItemBinding.bind(view)
         view.setOnClickListener { onItemViewClick(absoluteAdapterPosition) }
-        theaterNextButton.setOnClickListener { onItemViewClick(absoluteAdapterPosition) }
+        binding.ivTheaterNext.setOnClickListener { onItemViewClick(absoluteAdapterPosition) }
     }
 
     fun bind(theater: TheaterModel) {
-        theaterName.text = theaterName.context.getString(R.string.theater_name, theater.name)
-        theaterDescription.text =
-            theaterDescription.context.getString(R.string.theater_description, theater.times.size)
+        binding.tvTheaterName.text =
+            binding.tvTheaterName.context.getString(R.string.theater_name, theater.name)
+        binding.tvTheaterDescription.text =
+            binding.tvTheaterDescription.context.getString(
+                R.string.theater_description,
+                theater.times.size
+            )
     }
 }
