17769506d62d89d29ec021077044f0654137191c
vrexpert <vrexpert2302@gmail.com>
2023-05-05 22:33:48 +0900

    refactor(ChoiceSeatActivity): 데이터 바인딩 적용

--- app/src/main/java/woowacourse/movie/presentation/choiceSeat/ChoiceSeatActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/choiceSeat/ChoiceSeatActivity.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
@@ -13,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import woowacourse.movie.R
 import woowacourse.movie.data.settings.SettingsPreference
+import woowacourse.movie.databinding.ActivityChoiceSeatBinding
 import woowacourse.movie.presentation.allowance.SettingsAllowance
 import woowacourse.movie.presentation.complete.CompleteActivity
 import woowacourse.movie.presentation.model.ReservationModel
@@ -24,18 +23,21 @@ class ChoiceSeatActivity : AppCompatActivity(), ChoiceSeatContract.View {
         val prefKey = SettingsAllowance.NOTIFICATION_PREF_KEY
         ChoiceSeatPresenter(this, SettingsPreference.getInstance(prefKey, this))
     }
+    private lateinit var binding: ActivityChoiceSeatBinding
+
     override val reservation by lazy { initReservation() }
-    private val confirmButton by lazy { findViewById<Button>(R.id.buttonChoiceConfirm) }
+    private val confirmButton by lazy { binding.buttonChoiceConfirm }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choice_seat)
+        val binding = ActivityChoiceSeatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setTheaterSeat()
         initView()
     }
 
     private fun setTheaterSeat() {
-        val seatsTable = findViewById<TableLayout>(R.id.tableSeats)
+        val seatsTable = binding.tableSeats
             .children
             .filterIsInstance<TableRow>()
             .flatMap { it.children }
@@ -115,11 +117,11 @@ class ChoiceSeatActivity : AppCompatActivity(), ChoiceSeatContract.View {
     }
 
     private fun setTitle(title: String) {
-        findViewById<TextView>(R.id.textChoiceTitle).text = title
+        binding.textChoiceTitle.text = title
     }
 
     override fun setPaymentAmount(amount: Int) {
-        findViewById<TextView>(R.id.textChoicePaymentAmount).text =
+        binding.textChoicePaymentAmount.text =
             getString(R.string.payment_amount, amount)
     }
 
@@ -160,6 +162,7 @@ class ChoiceSeatActivity : AppCompatActivity(), ChoiceSeatContract.View {
         private const val GRADE_S = "GRADE_S"
         private const val GRADE_A = "GRADE_A"
         private const val INITIAL_PAYMENT_MONEY = 0
+
         fun getIntent(context: Context, reservation: ReservationModel): Intent {
             return Intent(context, ChoiceSeatActivity::class.java).apply {
                 putExtra(RESERVATION, reservation)
