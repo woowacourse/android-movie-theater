ef15f7544e650b85adefffd24dca3096cd9b8212
hyemdooly <hyemdooly@gmail.com>
2023-05-05 23:45:23 +0900

    refactor(Movie, Schedule): 도메인 모델 구조 변경

--- domain/src/main/java/woowacourse/movie/domain/movie/Movie.kt
+++ domain/src/main/java/woowacourse/movie/domain/movie/Movie.kt
@@ -1,7 +1,6 @@
 package woowacourse.movie.domain.movie
 
 import java.time.LocalDate
-import java.time.LocalTime
 
 data class Movie(
     val title: String,
@@ -10,10 +9,4 @@ data class Movie(
     val runningTime: Minute,
     val summary: String,
     val schedule: Schedule,
-) {
-    companion object {
-        fun of(title: String, startDate: LocalDate, endDate: LocalDate, runningTime: Minute, summary: String, scheduleOfTheater: Map<String, List<LocalTime>>): Movie {
-            return Movie(title, startDate, endDate, runningTime, summary, Schedule.of(startDate, endDate, scheduleOfTheater))
-        }
-    }
-}
+)
--- domain/src/main/java/woowacourse/movie/domain/movie/Schedule.kt
+++ domain/src/main/java/woowacourse/movie/domain/movie/Schedule.kt
@@ -1,16 +1,5 @@
 package woowacourse.movie.domain.movie
 
-import java.time.LocalDate
 import java.time.LocalTime
 
-data class Schedule(val schedule: Map<String, ScreeningDateTimes>) {
-    companion object {
-        fun of(startDate: LocalDate, endDate: LocalDate, scheduleOfTheater: Map<String, List<LocalTime>>): Schedule {
-            val schedule = mutableMapOf<String, ScreeningDateTimes>()
-            scheduleOfTheater.forEach { (name, times) ->
-                schedule[name] = ScreeningDateTimes.of(startDate, endDate, times)
-            }
-            return Schedule(schedule)
-        }
-    }
-}
+data class Schedule(val schedule: Map<String, List<LocalTime>>)
