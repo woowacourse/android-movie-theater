5c6d7ee58a846c995b3545e3bb0a83ab6e244978
re4rk <37167652+re4rk@users.noreply.github.com>
2023-05-06 20:52:52 +0900

    refactor : 티켓 목록 MVP 구조 개선 및 테스트 케이스 변경

--- app/src/test/java/woowacourse/movie/ui/main/reservationList/ReservationListPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/ui/main/reservationList/ReservationListPresenterTest.kt
@@ -1,22 +1,22 @@
 package woowacourse.movie.ui.main.reservationList
 
-import com.example.domain.repository.TicketsRepository
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Test
+import woowacourse.movie.dbHelper.TicketsDbHelper
 
 class ReservationListPresenterTest {
     @Test
     fun `예매 내역을 가져온다`() {
         // given
         val view = mockk<ReservationListContract.View>(relaxed = true)
-        val ticketsRepository = mockk<TicketsRepository>(relaxed = true)
-        val presenter = ReservationListPresenter(view, ticketsRepository)
+        val ticketsDbHelper = mockk<TicketsDbHelper>(relaxed = true)
+        val presenter = ReservationListPresenter(view, ticketsDbHelper)
+
+        every { ticketsDbHelper.getAll() } returns listOf()
 
         // when
-        every { ticketsRepository.allTickets() } returns listOf()
-
         presenter.getReservationList()
 
         // then
