86d71bc951a4da7282aa235997cdc3f871e69c71
hyemdooly <hyemdooly@gmail.com>
2023-05-06 23:18:51 +0900

    refactor(SettingFragment): 데이터 바인딩 적용

--- app/src/main/java/woowacourse/movie/view/moviemain/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/view/moviemain/setting/SettingFragment.kt
@@ -4,33 +4,45 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.google.android.material.switchmaterial.SwitchMaterial
 import woowacourse.movie.R
 import woowacourse.movie.data.setting.SettingPreferencesManager
+import woowacourse.movie.databinding.FragmentSettingBinding
 import woowacourse.movie.view.AlarmController
 import woowacourse.movie.view.model.ReservationUiModel
 
-class SettingFragment : Fragment(R.layout.fragment_setting), SettingContract.View {
+class SettingFragment : Fragment(), SettingContract.View {
 
     private lateinit var toggle: SwitchMaterial
     private lateinit var alarmController: AlarmController
+    private lateinit var binding: FragmentSettingBinding
     override lateinit var presenter: SettingContract.Presenter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        presenter = SettingPresenter(this, SettingPreferencesManager(requireContext()))
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_setting, container, false)
+        binding.presenter = presenter
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         alarmController = AlarmController(requireContext())
-        presenter = SettingPresenter(this, SettingPreferencesManager(requireContext()))
 
         toggle = view.findViewById(R.id.setting_toggle)
         presenter.initToggle()
-        toggle.setOnCheckedChangeListener { _, isChecked ->
-            presenter.onClick(isChecked)
-        }
     }
 
     override fun setToggle(isOn: Boolean) {
