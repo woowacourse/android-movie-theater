7ef640e8a958a3788da2bb8e00ff082fb50d6531
chws0508 <chws0508@naver.com>
2023-05-06 16:16:42 +0900

    feat : ReservationResultPresenter 테스트 구현

---
+++ app/src/test/java/woowacourse/movie/presenter/ReservationResultPresenterTest.kt
@@ -0,0 +1,31 @@
+package woowacourse.movie.presenter
+
+import io.mockk.*
+import org.junit.Before
+import org.junit.Test
+import woowacourse.movie.contract.ReservationResultContract
+import woowacourse.movie.model.TicketsUiModel
+
+class ReservationResultPresenterTest {
+    private lateinit var view: ReservationResultContract.View
+    private lateinit var presenter: ReservationResultContract.Presenter
+    private lateinit var ticketsUiModel: TicketsUiModel
+
+    @Before
+    fun setUp() {
+        view = mockk()
+        ticketsUiModel = TicketsUiModel(listOf())
+        presenter = ReservationResultPresenter(view, ticketsUiModel)
+    }
+
+    @Test
+    fun 예매_가격을_표시한다() {
+        // given
+        val slot = slot<Int>()
+        every { view.setPriceTextView(capture(slot)) } just runs
+        // when
+        presenter.updatePrice()
+        // then
+        verify { view.setPriceTextView(slot.captured) }
+    }
+}
