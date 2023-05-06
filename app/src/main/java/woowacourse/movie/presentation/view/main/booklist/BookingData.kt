23df89199760efdf70f1c32c81a1ea5eebaf152b
ki960213 <ki960213@github.com>
2023-05-06 14:52:16 +0900

    refactor: 내가 만든 코드로 변경

--- app/src/main/java/woowacourse/movie/presentation/view/main/booklist/BookingData.kt
+++
@@ -1,6 +0,0 @@
-package woowacourse.movie.presentation.view.main.booklist
-
-data class BookingData(
-    val movieTitle: String,
-    val date: String
-)
--- app/src/main/java/woowacourse/movie/presentation/view/main/home/ViewType.kt
+++
@@ -1,6 +0,0 @@
-package woowacourse.movie.presentation.view.main.home
-
-enum class ViewType {
-    MOVIE,
-    AD
-}
