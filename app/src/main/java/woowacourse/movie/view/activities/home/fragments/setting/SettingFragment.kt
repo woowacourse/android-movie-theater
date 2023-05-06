d245ed9b1188736be197f4dce8d421eb499da8c0
ki960213 <ki960213@github.com>
2023-05-07 01:23:29 +0900

    refactor(PermissionUtil): 권한을 요청하기 위해 다이얼로그를 띄우는 기능을 SettingFragment.kt 파일에서 PermissionUtil.kt 파일로 옮김

--- app/src/main/java/woowacourse/movie/view/activities/home/fragments/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/view/activities/home/fragments/setting/SettingFragment.kt
@@ -1,20 +1,16 @@
 package woowacourse.movie.view.activities.home.fragments.setting
 
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
 import woowacourse.movie.view.utils.getPushAlarmReceptionIsWanted
 import woowacourse.movie.view.utils.postNotificationPermissionIsGranted
 import woowacourse.movie.view.utils.setPushAlarmReceptionIsWanted
+import woowacourse.movie.view.utils.showDialogToGetPostNotificationPermission
 
 class SettingFragment : Fragment() {
 
@@ -42,23 +38,5 @@ class SettingFragment : Fragment() {
     private fun userWantsToReceivePushAlarm(): Boolean {
         return getPushAlarmReceptionIsWanted(requireContext())
                 && postNotificationPermissionIsGranted(requireContext())
-    }
-
-    private fun showDialogToGetPostNotificationPermission(context: Context) {
-        AlertDialog.Builder(context).apply {
-            setTitle(context.getString(R.string.post_notification_permission_request_dialog_title))
-            setMessage(context.getString(R.string.post_notification_permission_request_dialog_message))
-            setPositiveButton(context.getString(R.string.ok)) { _, _ ->
-                val intent = Intent(
-                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                    Uri.fromParts("package", context.packageName, null)
-                )
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-            }
-            setNegativeButton(context.getString(R.string.cancel)) { _, _ -> }
-        }
-            .create()
-            .show()
     }
 }
