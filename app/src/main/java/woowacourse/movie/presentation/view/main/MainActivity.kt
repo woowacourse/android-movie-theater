1bcfd18edf51ae76887ae9df483bf3f86876acc9
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-05-07 20:32:27 +0900

    refactor: MainActivity databinding 적용

--- app/src/main/java/woowacourse/movie/presentation/view/main/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/main/MainActivity.kt
@@ -5,15 +5,17 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import woowacourse.movie.R
+import woowacourse.movie.databinding.ActivityMainBinding
 import woowacourse.movie.presentation.view.main.booklist.BookListFragment
 import woowacourse.movie.presentation.view.main.home.MovieListFragment
 import woowacourse.movie.presentation.view.main.setting.SettingFragment
 
 class MainActivity : AppCompatActivity(), MainContract.View {
-    private val bottomNavigation: BottomNavigationView by lazy { findViewById(R.id.bottom_navigation) }
+    private lateinit var binding: ActivityMainBinding
     private val presenter: MainContract.Presenter by lazy {
         MainPresenter(
             view = this,
@@ -29,8 +31,7 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setDefaultPermission()
         requestNotificationPermission()
 
@@ -58,11 +59,11 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
     private fun initBottomNavigation() {
         changeFragment(MovieListFragment())
-        bottomNavigation.selectedItemId = R.id.action_home
+        binding.bottomNavigation.selectedItemId = R.id.action_home
     }
 
     private fun setSelectEvent() {
-        bottomNavigation.setOnItemSelectedListener {
+        binding.bottomNavigation.setOnItemSelectedListener {
             return@setOnItemSelectedListener getBottomNavigationClickSuccess(it)
         }
     }
