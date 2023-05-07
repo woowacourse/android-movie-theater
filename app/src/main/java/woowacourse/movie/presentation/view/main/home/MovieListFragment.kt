03c34963d11acc32583873922915fc22b7004a89
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-05-07 20:07:04 +0900

    refactor: MovieListFragment databinding 적용

--- app/src/main/java/woowacourse/movie/presentation/view/main/home/MovieListFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/main/home/MovieListFragment.kt
@@ -1,16 +1,28 @@
 package woowacourse.movie.presentation.view.main.home
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentMovieListBinding
 import woowacourse.movie.presentation.model.Movie
 import woowacourse.movie.presentation.view.main.home.moviedetail.MovieDetailActivity
 
-class MovieListFragment : Fragment(R.layout.fragment_movie_list), MovieListContract.View {
+class MovieListFragment : Fragment(), MovieListContract.View {
     private val presenter by lazy { MovieListPresenter(this) }
+    private lateinit var binding: FragmentMovieListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMovieListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -18,7 +30,6 @@ class MovieListFragment : Fragment(R.layout.fragment_movie_list), MovieListContr
     }
 
     override fun setRecyclerView(movies: List<Movie>) {
-        val recyclerView = requireView().findViewById<RecyclerView>(R.id.rv_movie_list)
         val adapter = MovieListAdapter(
             movies,
             ContextCompat.getDrawable(requireContext(), R.drawable.advertise_wooteco)!!
@@ -27,6 +38,6 @@ class MovieListFragment : Fragment(R.layout.fragment_movie_list), MovieListContr
             intent.putExtra(MovieDetailActivity.MOVIE_DATA_INTENT_KEY, it)
             requireContext().startActivity(intent)
         }
-        recyclerView.adapter = adapter
+        binding.rvMovieList.adapter = adapter
     }
 }
