559ac344893872d3ca4dda1957d62ca30efb0dbd
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-05-07 20:05:07 +0900

    refactor: SettingFragment databinding 적용

--- app/src/main/java/woowacourse/movie/presentation/view/main/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/main/setting/SettingFragment.kt
@@ -3,14 +3,17 @@ package woowacourse.movie.presentation.view.main.setting
 import android.Manifest
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentSettingBinding
 
-class SettingFragment : Fragment(R.layout.fragment_setting), SettingContract.View {
-    private val switchSettingAlarm by lazy { requireView().findViewById<SwitchCompat>(R.id.switch_setting_alarm) }
+class SettingFragment : Fragment(), SettingContract.View {
+    private lateinit var binding: FragmentSettingBinding
     private val presenter: SettingPresenter by lazy {
         SettingPresenter(
             view = this,
@@ -24,13 +27,22 @@ class SettingFragment : Fragment(R.layout.fragment_setting), SettingContract.Vie
         presenter.setAlarmSetting(isGranted)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         presenter.getAlarmSettingInfo()
     }
 
     override fun updatePermissionNotGrantedView() {
-        switchSettingAlarm.isSelected = false
+        binding.switchSettingAlarm.isSelected = false
     }
 
     override fun updateAlarmSettingView(isSet: Boolean, isGranted: Boolean) {
@@ -38,9 +50,9 @@ class SettingFragment : Fragment(R.layout.fragment_setting), SettingContract.Vie
     }
 
     private fun setAlarmView(allowedPushNotification: Boolean, isGranted: Boolean) {
-        switchSettingAlarm.isChecked =
+        binding.switchSettingAlarm.isChecked =
             allowedPushNotification && isGranted
-        switchSettingAlarm.setOnCheckedChangeListener { buttonView, isChecked ->
+        binding.switchSettingAlarm.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked && isGranted.not()) {
                 requestNotificationPermission()
             }
