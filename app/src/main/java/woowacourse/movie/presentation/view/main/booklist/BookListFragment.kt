58f0fc81e1a61d6616591f240c34fbde7bdd3f67
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-05-07 20:18:36 +0900

    refactor: BookListFragment databinding 적용

--- app/src/main/java/woowacourse/movie/presentation/view/main/booklist/BookListFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/main/booklist/BookListFragment.kt
@@ -1,16 +1,27 @@
 package woowacourse.movie.presentation.view.main.booklist
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
-import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentBookListBinding
 import woowacourse.movie.presentation.model.ReservationResult
 import woowacourse.movie.presentation.view.main.home.bookcomplete.BookCompleteActivity
 
-class BookListFragment : Fragment(R.layout.fragment_book_list), BookingListContract.View {
+class BookListFragment : Fragment(), BookingListContract.View {
+    private lateinit var binding: FragmentBookListBinding
     private val presenter: BookingListContract.Presenter by lazy {
         BookingListPresenter(this, requireContext())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBookListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -19,7 +30,7 @@ class BookListFragment : Fragment(R.layout.fragment_book_list), BookingListContr
     }
 
     override fun showReservationListView(bookings: List<ReservationResult>) {
-        requireView().findViewById<RecyclerView>(R.id.rv_book_list).adapter =
+        binding.rvBookList.adapter =
             BookingListAdapter(bookings) {
                 val intent = BookCompleteActivity.getIntent(requireContext())
                     .putExtra(BookCompleteActivity.RESERVATION_ID_INTENT_KEY, it)
