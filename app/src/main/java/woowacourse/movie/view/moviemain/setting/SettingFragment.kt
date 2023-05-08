6f858b35cb29d02f75831d81f711d7925e161593
EmilyCh0 <emily9803@ewhain.net>
2023-05-08 17:37:14 +0900

    refactor(SettingPresenterFactory): SettingPresenter를 생성하는 Factory 클래스 추가

--- app/src/main/java/woowacourse/movie/view/moviemain/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/view/moviemain/setting/SettingFragment.kt
@@ -8,12 +8,7 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.google.android.material.switchmaterial.SwitchMaterial
-import woowacourse.movie.AlarmPreference
 import woowacourse.movie.R
-import woowacourse.movie.data.MovieMockRepository
-import woowacourse.movie.data.ReservationDatabase
-import woowacourse.movie.data.SeatDatabase
-import woowacourse.movie.data.dbhelper.ReservationDbHelper
 import woowacourse.movie.util.isGranted
 import woowacourse.movie.util.requestRequiredPermissions
 import woowacourse.movie.view.alarm.ReservationAlarmManager
@@ -44,17 +39,9 @@ class SettingFragment : Fragment(R.layout.fragment_setting), SettingContract.Vie
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val reservationDb = ReservationDatabase(
-            ReservationDbHelper(requireContext()).writableDatabase,
-            SeatDatabase(requireContext()),
-            MovieMockRepository
-        )
-
-        presenter = SettingPresenter(
-            this,
-            AlarmPreference.getInstance(requireActivity().applicationContext),
-            reservationDb
-        )
+        presenter = SettingPresenterFactory(
+            this, requireActivity().applicationContext
+        ).createSettingPresenter()
 
         reservationAlarmManager = ReservationAlarmManager(requireContext())
     }
