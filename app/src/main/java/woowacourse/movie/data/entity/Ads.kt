84a08a1f183b542c45d4a92d22d1d1fd6af892a3
Gloria <ippnsj@gmail.com>
2023-05-08 14:13:39 +0900

    fix: 영화와 광고 개수 관련 버그 수정
    MovieAdapter 의 아이템 개수를 영화 개수에 맞게 계산하도록 수정
    광고 개수가 필요보다 적을 경우 default 광고를 보여주도록 수정

--- app/src/main/java/woowacourse/movie/data/entity/Ads.kt
+++ app/src/main/java/woowacourse/movie/data/entity/Ads.kt
@@ -5,7 +5,7 @@ import woowacourse.movie.ui.model.AdModel
 
 class Ads {
     private val items: List<AdModel> =
-        List(2) { AdModel(R.drawable.ad, "https://woowacourse.github.io/") }
+        List(1) { AdModel(R.drawable.ad, "https://woowacourse.github.io/") }
 
     fun getAll(): List<AdModel> = items.toList()
 }
--- app/src/main/java/woowacourse/movie/ui/fragment/movielist/adapter/MovieAdapter.kt
+++ app/src/main/java/woowacourse/movie/ui/fragment/movielist/adapter/MovieAdapter.kt
@@ -35,7 +35,7 @@ class MovieAdapter(
         }
     }
 
-    override fun getItemCount(): Int = movies.size + ads.size
+    override fun getItemCount(): Int = movies.size + (movies.size / (AD_INTERVAL - 1))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         when (holder) {
@@ -49,11 +49,19 @@ class MovieAdapter(
         return MovieListViewType.MOVIE_VIEW.ordinal
     }
 
-    private fun isAdTime(position: Int) = (position + 1) % 4 == 0
+    private fun isAdTime(position: Int) = (position + 1) % AD_INTERVAL == 0
 
     private fun getMovie(position: Int): MovieModel = movies[position - (position / AD_INTERVAL)]
 
-    private fun getAd(position: Int): AdModel = ads[position / AD_INTERVAL]
+    private fun getAd(position: Int): AdModel {
+        if ((position / AD_INTERVAL) >= ads.size) {
+            return createDefaultAd()
+        }
+        return ads[position / AD_INTERVAL]
+    }
+
+    private fun createDefaultAd() =
+        AdModel(R.drawable.default_ad, "https://developer.android.com/?hl=ko")
 
     companion object {
         private const val AD_INTERVAL = 4
