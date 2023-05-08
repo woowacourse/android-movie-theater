cfcf70e33c79bac020e39b276312fe42189ddb0c
krrong <khk9664@naver.com>
2023-05-08 10:06:38 +0900

    refactor(HomeFragment): DataBinding 적용

--- app/src/main/java/woowacourse/movie/presentation/activities/main/fragments/home/HomeFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/activities/main/fragments/home/HomeFragment.kt
@@ -3,31 +3,43 @@ package woowacourse.movie.presentation.activities.main.fragments.home
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentHomeBinding
 import woowacourse.movie.presentation.activities.main.fragments.theaterPicker.TheaterPickerDialog
 import woowacourse.movie.presentation.model.item.Ad
 import woowacourse.movie.presentation.model.item.Movie
 import woowacourse.movie.presentation.model.item.Reservation
 import woowacourse.movie.presentation.model.item.Theater
 
-class HomeFragment : Fragment(R.layout.fragment_home), HomeContract.View {
+class HomeFragment : Fragment(), HomeContract.View {
     override lateinit var presenter: HomePresenter
-    private lateinit var movieListAdapter: MovieListAdapter
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         presenter = HomePresenter(this)
-        initMovieListAdapter(view)
+        initMovieListAdapter()
     }
 
-    private fun initMovieListAdapter(view: View) {
-        val movieRecyclerView = view.findViewById<RecyclerView>(R.id.movies_rv)
-        movieListAdapter = MovieListAdapter(
+    private fun initMovieListAdapter() {
+        val movieListAdapter = MovieListAdapter(
             adTypes = Ad.provideDummy(),
             onItemClick = { item ->
                 when (item) {
@@ -39,8 +51,8 @@ class HomeFragment : Fragment(R.layout.fragment_home), HomeContract.View {
             },
         ).also { it.appendAll(Movie.provideDummy()) }
 
-        movieRecyclerView.adapter = movieListAdapter
-        movieRecyclerView.addOnScrollListener(object : OnScrollListener() {
+        binding.moviesRv.adapter = movieListAdapter
+        binding.moviesRv.addOnScrollListener(object : OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 if (!recyclerView.canScrollVertically(DOWN_DIRECTION) && dy > 0) {
