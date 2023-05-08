c74c9785c0d6296724dae88ae7f9120db55cf3b9
ki960213 <ki960213@github.com>
2023-05-08 15:54:25 +0900

    docs: 기능 목록과 프로젝트 명을 영화 극장 선택 미션으로 변경

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "android-movie-ticket"
+rootProject.name = "android-movie-theater"
 include(":app")
 include(":domain")
