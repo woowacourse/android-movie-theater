34e4db7f229c04739b0cb48d91ea74e6c7e8a38e
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-05-10 00:02:26 +0900

    refactor: 데이터 더미 추가

--- app/src/main/java/woowacourse/movie/dto/movie/TheaterDummy.kt
+++ app/src/main/java/woowacourse/movie/dto/movie/TheaterDummy.kt
@@ -2,18 +2,43 @@ package woowacourse.movie.dto.movie
 
 object TheaterDummy {
 
-    val theaterDatas = listOf<TheaterUIModel>(
-        TheaterUIModel("선릉", mapOf(1 to listOf("9:00", "11:00"), 2 to listOf("10:00", "12:00"))),
-        TheaterUIModel(
-            "잠실",
-            mapOf(1 to listOf("9:00", "11:00", "13:00"), 5 to listOf("10:00", "12:00")),
-        ),
-        TheaterUIModel(
-            "강남",
-            mapOf(
-                10 to listOf("9:00", "11:00", "13:00", "20:00"),
-                15 to listOf("10:00", "12:00", "15:00"),
+    val theaterDatas =
+        listOf<TheaterUIModel>(
+            TheaterUIModel(
+                "선릉",
+                getSeolleungSchedule(),
             ),
-        ),
-    )
+            TheaterUIModel(
+                "잠실",
+                getJamsilSchedule(),
+            ),
+            TheaterUIModel(
+                "강남",
+                getGangNamSchedule(),
+            ),
+        )
+
+    private fun getGangNamSchedule(): Map<Int, List<String>> {
+        val schedule = mutableMapOf<Int, List<String>>()
+        repeat(1000) {
+            schedule[it] = listOf("9:00", "11:00", "13:00", "15:00")
+        }
+        return schedule
+    }
+
+    private fun getSeolleungSchedule(): Map<Int, List<String>> {
+        val schedule = mutableMapOf<Int, List<String>>()
+        repeat(1000) {
+            schedule[it] = listOf("10:00", "12:00", "13:00")
+        }
+        return schedule
+    }
+
+    private fun getJamsilSchedule(): Map<Int, List<String>> {
+        val schedule = mutableMapOf<Int, List<String>>()
+        repeat(1000) {
+            schedule[it] = listOf("13:00", "15:00", "17:00", "19:00", "21:00", "23:00")
+        }
+        return schedule
+    }
 }
