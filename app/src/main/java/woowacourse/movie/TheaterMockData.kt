f7b98019e395027ebf7428acb54e8a28d140710d
SangHyun Moon <no1msh1217@gmail.com>
2023-05-14 03:40:36 +0900

    fix: serializable 관련 에러 수정

--- app/src/main/java/woowacourse/movie/TheaterMockData.kt
+++ app/src/main/java/woowacourse/movie/TheaterMockData.kt
@@ -6,7 +6,7 @@ object TheaterMockData {
     val dummyTheaters = listOf(
         Theater(
             name = "이름이 어어어어어엄청나게 긴 극장",
-            screeningMovies = MovieMockData.movies.toMutableList().subList(1, 5),
+            screeningMovies = MovieMockData.movies.toMutableList().subList(1, 5).toList(),
             screenTimes = mapOf(
                 MovieMockData.movies[1] to listOf("13:00", "15:00"),
                 MovieMockData.movies[2] to listOf("14:00", "16:00", "18:00"),
@@ -16,14 +16,14 @@ object TheaterMockData {
         ),
         Theater(
             name = "선릉 극장",
-            screeningMovies = MovieMockData.movies.toMutableList().subList(2, 3),
+            screeningMovies = MovieMockData.movies.toMutableList().subList(2, 3).toList(),
             screenTimes = mapOf(
                 MovieMockData.movies[2] to listOf("14:00", "16:00", "18:00", "20:00"),
             )
         ),
         Theater(
             name = "잠실 극장",
-            screeningMovies = MovieMockData.movies.toMutableList().subList(0, 2),
+            screeningMovies = MovieMockData.movies.toMutableList().subList(0, 2).toList(),
             screenTimes = mapOf(
                 MovieMockData.movies[0] to listOf("13:00", "15:00", "17:00", "19:00", "21:00"),
                 MovieMockData.movies[1] to listOf("12:00", "14:00", "16:00", "18:00", "20:00"),
