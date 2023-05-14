a0759a6705aa57d1e2f1c4a59b42b82bba9b8806
ki960213 <ki960213@gmail.com>
2023-05-15 01:35:42 +0900

    test(ReservationResultPresenterTest): 예매 결과 프레젠터에 대한 테스트 코드 작성

---
+++ app/src/test/java/woowacourse/movie/view/activities/reservationresult/ReservationResultPresenterTest.kt
@@ -0,0 +1,50 @@
+package woowacourse.movie.view.activities.reservationresult
+
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Before
+import org.junit.Test
+import woowacourse.movie.domain.screening.Minute
+import woowacourse.movie.domain.screening.Movie
+import woowacourse.movie.domain.screening.Reservation
+import woowacourse.movie.domain.theater.Point
+import woowacourse.movie.domain.theater.Theater
+import woowacourse.movie.repository.ReservationRepository
+import java.time.LocalDateTime
+
+class ReservationResultPresenterTest {
+
+    private lateinit var view: FakeView
+    private lateinit var reservationRepository: ReservationRepository
+    private val fakeReservation: Reservation = Reservation(
+        Movie("title", Minute(152), "summary"),
+        LocalDateTime.MAX,
+        Theater(5, 4),
+        listOf(Point(1, 2), Point(2, 3))
+    )
+
+    @Before
+    fun setUp() {
+        view = FakeView()
+        reservationRepository = mockk()
+    }
+
+    @Test
+    fun `예매를 로드하면 뷰에 예매 UI 상태를 설정한다`() {
+        val reservationId = 1L
+        every { reservationRepository.findById(reservationId) } returns fakeReservation
+        val sut = ReservationResultPresenter(view, reservationId, reservationRepository)
+
+        sut.loadReservation()
+
+        assert(view.settingReservationIsDone)
+    }
+
+    class FakeView : ReservationResultContract.View {
+        var settingReservationIsDone: Boolean = false
+
+        override fun setReservation(reservationUIState: ReservationUIState) {
+            settingReservationIsDone = true
+        }
+    }
+}
