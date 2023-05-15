e3ccc5dec08b74d7b6e07622286a8dc3c51ac561
ki960213 <ki960213@gmail.com>
2023-05-15 16:34:18 +0900

    test(ScreeningRangeTest): 특정 시각이 상영 기간 날짜 안에 포함되는지 검증하는 테스트 코드 작성

--- domain/src/test/java/woowacourse/movie/domain/screening/ScreeningRangeTest.kt
+++ domain/src/test/java/woowacourse/movie/domain/screening/ScreeningRangeTest.kt
@@ -10,6 +10,18 @@ import java.time.LocalTime
 internal class ScreeningRangeTest {
 
     @Test
+    fun `특정 시각이 상영 기간 날짜 안에 포함되는지 알 수 있다`() {
+        val startDate = LocalDate.of(2021, 3, 31)
+        val endDate = LocalDate.of(2021, 4, 1)
+        val screeningRange = ScreeningRange(startDate, endDate)
+        val dateTime = LocalDateTime.of(2021, 3, 31, 0, 0)
+
+        val actual = screeningRange.contains(dateTime)
+
+        assertThat(actual).isTrue
+    }
+
+    @Test
     fun `상영 기간의 시작 날짜가 마지막 날짜 이후라면 에러가 발생한다`() {
         val startDate = LocalDate.of(2021, 3, 31)
         val endDate = LocalDate.of(2021, 3, 1)
