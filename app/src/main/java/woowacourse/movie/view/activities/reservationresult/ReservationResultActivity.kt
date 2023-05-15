31291b1625bcb647d0b3739c191a50d2443acb8b
ki960213 <ki960213@gmail.com>
2023-05-16 01:52:57 +0900

    refactor(ReservationResultActivity): ReservationResultActivity 클래스에 뷰 바인딩 적용

--- app/src/main/java/woowacourse/movie/view/activities/reservationresult/ReservationResultActivity.kt
+++ app/src/main/java/woowacourse/movie/view/activities/reservationresult/ReservationResultActivity.kt
@@ -3,15 +3,19 @@ package woowacourse.movie.view.activities.reservationresult
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import woowacourse.movie.R
 import woowacourse.movie.data.reservation.ReservationDbHelper
 import woowacourse.movie.data.reservation.ReservationRepositoryImpl
+import woowacourse.movie.databinding.ActivityReservationResultBinding
 import woowacourse.movie.view.activities.common.BackButtonActivity
 import java.text.DecimalFormat
 import java.time.format.DateTimeFormatter
 
 class ReservationResultActivity : BackButtonActivity(), ReservationResultContract.View {
+
+    private val binding: ActivityReservationResultBinding by lazy {
+        ActivityReservationResultBinding.inflate(layoutInflater)
+    }
 
     private val presenter: ReservationResultContract.Presenter by lazy {
         ReservationResultPresenter(
@@ -23,21 +27,18 @@ class ReservationResultActivity : BackButtonActivity(), ReservationResultContrac
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reservation_result)
+        setContentView(binding.root)
 
         presenter.loadReservation()
     }
 
     override fun setReservation(reservationUIState: ReservationUIState) {
-        val movieTitleView = findViewById<TextView>(R.id.movie_title_tv)
-        movieTitleView.text = reservationUIState.movieTitle
+        binding.movieTitleTv.text = reservationUIState.movieTitle
 
-        val screeningDateTimeView = findViewById<TextView>(R.id.screening_date_time_tv)
-        screeningDateTimeView.text =
+        binding.screeningDateTimeTv.text =
             DATE_TIME_FORMATTER.format(reservationUIState.screeningDateTime)
 
-        val audienceCountSeatNamesView = findViewById<TextView>(R.id.audience_count_seat_names_tv)
-        audienceCountSeatNamesView.text = getString(R.string.reservation_detail_infos)
+        binding.audienceCountSeatNamesTv.text = getString(R.string.reservation_detail_infos)
             .format(
                 getString(R.string.general_person),
                 reservationUIState.audienceCount,
@@ -45,8 +46,7 @@ class ReservationResultActivity : BackButtonActivity(), ReservationResultContrac
                 reservationUIState.theaterName
             )
 
-        val reservationFeeView = findViewById<TextView>(R.id.reservation_fee_tv)
-        reservationFeeView.text =
+        binding.reservationFeeTv.text =
             getString(R.string.total_price_format).format(DECIMAL_FORMAT.format(reservationUIState.reservationFee))
     }
 
