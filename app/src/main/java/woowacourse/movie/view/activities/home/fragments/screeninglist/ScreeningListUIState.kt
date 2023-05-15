bcc6ed819fa4b5e088134aea1b29244fe043c066
ki960213 <ki960213@gmail.com>
2023-05-15 20:04:21 +0900

    refactor(ScreeningUIState): 팩토리 메서드 파라미터 줄임

--- app/src/main/java/woowacourse/movie/view/activities/home/fragments/screeninglist/ScreeningListUIState.kt
+++ app/src/main/java/woowacourse/movie/view/activities/home/fragments/screeninglist/ScreeningListUIState.kt
@@ -2,7 +2,6 @@ package woowacourse.movie.view.activities.home.fragments.screeninglist
 
 import woowacourse.movie.R
 import woowacourse.movie.domain.screening.Screening1
-import woowacourse.movie.view.PosterResourceProvider
 
 data class ScreeningListUIState(val screenings: List<ScreeningListViewItemUIState>) {
 
@@ -17,12 +16,7 @@ data class ScreeningListUIState(val screenings: List<ScreeningListViewItemUIStat
             val screeningListViewUIStates = mutableListOf<ScreeningListViewItemUIState>()
 
             screenings.forEachIndexed { index, screening ->
-                screeningListViewUIStates.add(
-                    ScreeningUIState.of(
-                        screening,
-                        PosterResourceProvider.getPosterResourceId(screening)
-                    )
-                )
+                screeningListViewUIStates.add(ScreeningUIState.of(screening))
                 if ((index + 1) % ADVERTISE_INTERVAL == 0)
                     screeningListViewUIStates.add(AdvertisementUIState(R.drawable.ad_image))
             }
--- app/src/main/java/woowacourse/movie/view/activities/home/fragments/screeninglist/ScreeningUIState.kt
+++ app/src/main/java/woowacourse/movie/view/activities/home/fragments/screeninglist/ScreeningUIState.kt
@@ -2,6 +2,7 @@ package woowacourse.movie.view.activities.home.fragments.screeninglist
 
 import androidx.annotation.DrawableRes
 import woowacourse.movie.domain.screening.Screening1
+import woowacourse.movie.view.PosterResourceProvider
 import java.time.LocalDate
 
 data class ScreeningUIState(
@@ -14,13 +15,13 @@ data class ScreeningUIState(
 ) : ScreeningListViewItemUIState() {
 
     companion object {
-        fun of(screening: Screening1, @DrawableRes poster: Int): ScreeningUIState {
+        fun of(screening: Screening1): ScreeningUIState {
             val movie = screening.movie
             val screeningId = screening.id
                 ?: throw IllegalArgumentException("상영의 아이디가 널이면 UI 상태를 생성할 수 없습니다.")
 
             return ScreeningUIState(
-                poster,
+                PosterResourceProvider.getPosterResourceId(screening),
                 movie.title,
                 screening.screeningRange.startDate,
                 screening.screeningRange.endDate,
