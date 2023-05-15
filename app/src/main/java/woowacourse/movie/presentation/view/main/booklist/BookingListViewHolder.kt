0085e4f06f79ca2ef4a9922dcc818508fccbdb31
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-05-16 03:04:12 +0900

    refactor: databinding 적용

--- app/src/main/java/woowacourse/movie/presentation/view/main/booklist/BookingListViewHolder.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/main/booklist/BookingListViewHolder.kt
@@ -2,9 +2,9 @@ package woowacourse.movie.presentation.view.main.booklist
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
+import woowacourse.movie.databinding.ItemBookingListBinding
 import woowacourse.movie.presentation.model.ReservationResult
 
 class BookingListViewHolder(
@@ -15,9 +15,7 @@ class BookingListViewHolder(
         LayoutInflater.from(parent.context)
             .inflate(R.layout.item_booking_list, parent, false)
     ) {
-
-    private val bookingDate: TextView = itemView.findViewById(R.id.tv_booking_date)
-    private val bookingMovieTitle: TextView = itemView.findViewById(R.id.tv_booking_movie_title)
+    private val binding = ItemBookingListBinding.bind(itemView)
 
     init {
         itemView.setOnClickListener {
@@ -26,8 +24,7 @@ class BookingListViewHolder(
     }
 
     fun bind(bookings: ReservationResult) {
-        bookingMovieTitle.text = bookings.movieTitle
-        bookingDate.text = itemView.context.getString(R.string.booking_complete_date)
-            .format(bookings.date.replace("-", "."), bookings.time)
+        binding.reservation = bookings
+        binding.tvBookingMovieTitle.text = bookings.movieTitle
     }
 }
