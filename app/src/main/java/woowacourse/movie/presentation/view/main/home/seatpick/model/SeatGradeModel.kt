48c98615e41bf00f6f8c7935d72b531104fc8528
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-05-16 22:15:31 +0900

    refactor: ColorRes annotaion 명시

--- app/src/main/java/woowacourse/movie/presentation/view/main/home/seatpick/model/SeatGradeModel.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/main/home/seatpick/model/SeatGradeModel.kt
@@ -1,8 +1,9 @@
 package woowacourse.movie.presentation.view.main.home.seatpick.model
 
+import androidx.annotation.ColorRes
 import woowacourse.movie.R
 
-enum class SeatGradeModel(val seatColor: Int) {
+enum class SeatGradeModel(@ColorRes val seatColor: Int) {
     S_CLASS(R.color.seat_grade_s_class),
     A_CLASS(R.color.seat_grade_a_class),
     B_CLASS(R.color.seat_grade_b_class),
