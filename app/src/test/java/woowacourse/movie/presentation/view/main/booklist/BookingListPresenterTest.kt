dd6ce3301599f1d2cfaf741ee42b2d654d6177a7
rhkrwngud445 <rhkrwngud445@gmail.com>
2023-05-17 03:07:48 +0900

    refactor: test코드 주석 제거

--- app/src/test/java/woowacourse/movie/presentation/view/main/booklist/BookingListPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presentation/view/main/booklist/BookingListPresenterTest.kt
@@ -1,19 +1,30 @@
 package woowacourse.movie.presentation.view.main.booklist
 
-import io.mockk.Runs
 import io.mockk.every
-import io.mockk.just
+import io.mockk.justRun
 import io.mockk.mockk
+import io.mockk.slot
 import io.mockk.verify
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import woowacourse.movie.data.database.MovieHelper
+import woowacourse.movie.presentation.model.ReservationResult
 
 internal class BookingListPresenterTest {
-//    @Test
-//    fun `예매 내역을 가져온다`() {
-//        val view = mockk<BookingListContract.View>(relaxed = true)
-//        val presenter = BookingListPresenter(view, mockk(relaxed = true))
-//
-//        every {presenter.getReservationList()} just Runs
-//        verify { view.showReservationListView(listOf()) }
-//    }
+    @Test
+    fun `예매_내역을_가져온다`() {
+        val view = mockk<BookingListContract.View>(relaxed = true)
+        val movieHelper = mockk<MovieHelper>()
+        val presenter = BookingListPresenter(view, mockk(relaxed = true))
+
+        every { movieHelper.readBookingData() } returns listOf()
+        val slot = slot<List<ReservationResult>>()
+        justRun { view.showReservationListView(capture(slot)) }
+
+        presenter.getReservationList()
+
+
+        assertEquals(listOf<ReservationResult>(), slot.captured)
+        verify { view.showReservationListView(listOf()) }
+    }
 }
--- app/src/test/java/woowacourse/movie/presentation/view/main/home/seatpick/SeatPickerPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presentation/view/main/home/seatpick/SeatPickerPresenterTest.kt
@@ -1,21 +1,88 @@
 package woowacourse.movie.presentation.view.main.home.seatpick
 
+import com.example.domain.TicketBundle
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.justRun
 import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.verify
+import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
+import woowacourse.movie.data.SharedPreference
+import woowacourse.movie.data.database.MovieHelper
+import woowacourse.movie.data.model.MovieBookingEntity
+import woowacourse.movie.presentation.model.Movie
+import woowacourse.movie.presentation.model.MovieBookingInfo
+import woowacourse.movie.presentation.model.ReservationResult
 
 
-internal class SeatPickerPresenterTest{
+internal class SeatPickerPresenterTest {
     lateinit var presenter: SeatPickerPresenter
+    lateinit var view: SeatPickerContract.View
+    lateinit var sharedPreferenceUtil: SharedPreference
+    lateinit var movieHelper: MovieHelper
+    lateinit var ticketBundle: TicketBundle
 
     @Before
-    fun setUp(){
-        val view = mockk<SeatPickerContract.View>()
-        presenter = SeatPickerPresenter(view, mockk(relaxed = true), mockk(relaxed = true))
-    }
-    @Test
-    fun `초기화_테스트`() {
-        presenter.confirmBooking()
+    fun setUp() {
+        view = mockk(relaxed = true)
+        sharedPreferenceUtil = mockk(relaxed = true)
+        movieHelper = mockk(relaxed = true)
+        presenter = SeatPickerPresenter(
+            view,
+            sharedPreferenceUtil,
+            movieHelper,
+            MovieBookingInfo(
+                Movie(
+                    0,
+                    "타이틀",
+                    listOf("13:00"),
+                    "2020.08.05",
+                    "130",
+                    "",
+                    mockk(),
+                    mockk()
+
+                ),
+                "2021-08-01",
+                "13:00",
+                1,
+                "극장"
+            )
+
+        )
+
+        ticketBundle = mockk()
     }
 
+    @Test
+    fun `티켓의_총_가격합은_0으로_초기화_된다`() {
+        val totalPrice = 0
+
+        val slot = slot<String>()
+        justRun { view.updateDefaultTotalPriceView(capture(slot)) }
+        presenter.getDefaultTotalPrice()
+
+        assertEquals(totalPrice.toString(), slot.captured)
+    }
+
+    @Test
+    fun `예매_성공시_데이터베이스에_추가한다`() {
+        val movieBookingEntity = MovieBookingEntity(
+            "타이틀",
+            "2021-08-01",
+            "13:00",
+            0,
+            "",
+            0,
+        )
+        every { movieHelper.writeMovie(movieBookingEntity) } just Runs
+        justRun { view.updateNotification(ReservationResult.from(movieBookingEntity), 1) }
+        presenter.bookComplete()
+
+        verify { movieHelper.writeMovie(movieBookingEntity) }
+    }
 }
