2d748cbf47b3baf459a45cefb34ff8ae20b4c9d9
changhwan <chang5hustle@gmail.com>
2023-05-28 14:47:18 +0900

    feat(SettingFragmentTest): 테스트 mockk 를 이용하여 변경

--- app/src/androidTest/java/woowacourse/movie/presentation/settings/SettingsFragmentTest.kt
+++ app/src/androidTest/java/woowacourse/movie/presentation/settings/SettingsFragmentTest.kt
@@ -1,63 +1,88 @@
 package woowacourse.movie.presentation.settings
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
 import org.junit.Assert.assertEquals
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import woowacourse.movie.MovieTheaterApplication
 import woowacourse.movie.R
-import woowacourse.movie.presentation.main.MainActivity
+import woowacourse.movie.model.data.local.SettingPreference
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class SettingsFragmentTest {
 
-    @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    private lateinit var mockSettingsFragmentFactory: MockSettingsFragmentFactory
+    private lateinit var scenario: FragmentScenario<SettingsFragment>
 
-    @Before
-    fun settingPreference() {
-        MovieTheaterApplication.settingsPreference.edit().remove("notification").apply()
+    private class MockSettingsFragmentFactory : FragmentFactory() {
+        val mockSettingStorage = mockk<SettingPreference>()
+
+        override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
+            return when (className) {
+                SettingsFragment::class.java.name -> {
+                    SettingsFragment(getSettingStorage = { mockSettingStorage })
+                }
+                else -> super.instantiate(classLoader, className)
+            }
+        }
     }
 
     @Test
     fun `기존_설정_값이_true일때_스위치_버튼을_누르면_false가_저장된다`() {
         // given
-        val preSet = true
-        SharedPreferenceUtil.setNotificationSettings(preSet)
-        onView(withId(R.id.action_settings)).perform(click())
+        mockSettingsFragmentFactory = MockSettingsFragmentFactory()
+
+        every { mockSettingsFragmentFactory.mockSettingStorage.getNotificationSettings() } returns true
+        val slot = slot<Boolean>()
+        every { mockSettingsFragmentFactory.mockSettingStorage.setNotificationSettings(capture(slot)) } returns true
+
+        scenario =
+            launchFragmentInContainer<SettingsFragment>(
+                factory = mockSettingsFragmentFactory,
+                themeResId = R.style.Theme_Movie
+            )
 
         // when
         onView(withId(R.id.switchPushPermission)).perform(click())
 
         // then
         val expected = false
-        val actual = SharedPreferenceUtil.getNotificationSettings()
-
+        val actual = slot.captured
         assertEquals(expected, actual)
     }
 
     @Test
     fun `기존_설정_값이_false일때_스위치_버튼을_누르면_true가_저장된다`() {
         // given
-        val preSet = false
-        SharedPreferenceUtil.setNotificationSettings(preSet)
-        onView(withId(R.id.action_settings)).perform(click())
+        mockSettingsFragmentFactory = MockSettingsFragmentFactory()
+
+        every { mockSettingsFragmentFactory.mockSettingStorage.getNotificationSettings() } returns false
+        val slot = slot<Boolean>()
+        every { mockSettingsFragmentFactory.mockSettingStorage.setNotificationSettings(capture(slot)) } returns true
+
+        scenario =
+            launchFragmentInContainer<SettingsFragment>(
+                factory = mockSettingsFragmentFactory,
+                themeResId = R.style.Theme_Movie
+            )
 
         // when
         onView(withId(R.id.switchPushPermission)).perform(click())
 
         // then
         val expected = true
-        val actual = SharedPreferenceUtil.getNotificationSettings()
-
+        val actual = slot.captured
         assertEquals(expected, actual)
     }
 }
