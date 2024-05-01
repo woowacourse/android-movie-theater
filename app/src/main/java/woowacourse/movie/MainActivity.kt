904ec6c2aa46c80ea09c9735e78380dde0f041a4
hxeyexn <kck85075@kumoh.ac.kr>
2024-05-01 17:25:16 +0900

    feat: bottom navigation 연결

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,11 +1,45 @@
 package woowacourse.movie
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import woowacourse.movie.databinding.ActivityMainBinding
+import woowacourse.movie.utils.MovieUtils.navigateToFragment
+import woowacourse.movie.view.ReservationHistoryFragment
+import woowacourse.movie.view.SettingFragment
+import woowacourse.movie.view.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.main = this
+        setBottomNavigationView()
+    }
+
+    @SuppressLint("ResourceType")
+    private fun setBottomNavigationView() {
+        val fragments = mapOf(
+            R.id.fragment_home to HomeFragment(),
+            R.id.fragment_list to ReservationHistoryFragment(),
+            R.id.fragment_setting to SettingFragment()
+        )
+        binding.bottomNavigationMain.setOnItemSelectedListener { item ->
+            fragments[item.itemId]?.let { currentFragment ->
+                supportFragmentManager.navigateToFragment(
+                    R.id.fragment_container_main,
+                    currentFragment
+                )
+                true
+            } ?: false
+        }
+        binding.bottomNavigationMain.selectedItemId = R.id.fragment_home
     }
 }
