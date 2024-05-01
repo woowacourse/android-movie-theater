cbf972a5192544c74ce6cf173d3f33706b5da025
songpink <jejeje04@naver.com>
2024-05-01 16:50:33 +0900

    feat: 더미데이터 수정

--- app/src/main/java/woowacourse/movie/list/model/TheaterData.kt
+++ app/src/main/java/woowacourse/movie/list/model/TheaterData.kt
@@ -1,49 +1,62 @@
 package woowacourse.movie.list.model
 
-import woowacourse.movie.common.MovieDataSource
-import woowacourse.movie.reservation.model.DataResource.screeningTimesWeekends
+import woowacourse.movie.common.MovieDataSource.movieList
+import java.time.LocalTime
 
 object TheaterData {
+    val screeningTimes1 =
+        listOf(
+            LocalTime.of(9, 0, 0),
+            LocalTime.of(11, 0, 0),
+            LocalTime.of(17, 0, 0),
+            LocalTime.of(23, 0, 0),
+        )
+
+    val screeningTimes2 =
+        listOf(
+            LocalTime.of(10, 0, 0),
+            LocalTime.of(12, 0, 0),
+            LocalTime.of(14, 0, 0),
+            LocalTime.of(16, 0, 0),
+            LocalTime.of(18, 0, 0),
+        )
+
     val theaters =
         listOf(
             Theater(
                 "선릉",
                 mapOf(
-                    MovieDataSource.movieList[0] to screeningTimesWeekends,
-                    MovieDataSource.movieList[1] to screeningTimesWeekends,
-//                MovieDataSource.movieList[2] to screeningTimesWeekends,
-                    MovieDataSource.movieList[3] to screeningTimesWeekends,
-                    MovieDataSource.movieList[4] to screeningTimesWeekends,
-                    MovieDataSource.movieList[5] to screeningTimesWeekends,
-                    MovieDataSource.movieList[6] to screeningTimesWeekends,
-                    MovieDataSource.movieList[7] to screeningTimesWeekends,
+                    movieList[0] to screeningTimes1,
+                    movieList[2] to screeningTimes2,
+                    movieList[4] to screeningTimes2,
+                    movieList[5] to screeningTimes1,
                 ),
+                0,
             ),
             Theater(
                 "잠실",
                 mapOf(
-                    MovieDataSource.movieList[0] to screeningTimesWeekends,
-                    MovieDataSource.movieList[1] to screeningTimesWeekends,
-//                MovieDataSource.movieList[2] to screeningTimesWeekends,
-                    MovieDataSource.movieList[3] to screeningTimesWeekends,
-                    MovieDataSource.movieList[4] to screeningTimesWeekends,
-                    MovieDataSource.movieList[5] to screeningTimesWeekends,
-                    MovieDataSource.movieList[6] to screeningTimesWeekends,
-                    MovieDataSource.movieList[7] to screeningTimesWeekends,
+                    movieList[0] to screeningTimes1,
+                    movieList[2] to screeningTimes2,
+                    //        movieList[3] to screeningTimesWeekends,
+                    movieList[4] to screeningTimes1,
+                    movieList[5] to screeningTimes2,
+                    movieList[6] to screeningTimes1,
+                    movieList[7] to screeningTimes1,
                 ),
+                1,
             ),
             Theater(
                 "강남",
                 mapOf(
-                    MovieDataSource.movieList[0] to screeningTimesWeekends,
-                    MovieDataSource.movieList[1] to screeningTimesWeekends,
-//                MovieDataSource.movieList[2] to screeningTimesWeekends,
-                    MovieDataSource.movieList[3] to screeningTimesWeekends,
-                    MovieDataSource.movieList[4] to screeningTimesWeekends,
-                    MovieDataSource.movieList[5] to screeningTimesWeekends,
-                    MovieDataSource.movieList[6] to screeningTimesWeekends,
-                    MovieDataSource.movieList[7] to screeningTimesWeekends,
+                    movieList[0] to screeningTimes2,
+                    movieList[1] to screeningTimes1,
+                    movieList[2] to screeningTimes2,
+                    movieList[4] to screeningTimes1,
+                    movieList[5] to screeningTimes2,
+                    movieList[6] to screeningTimes1,
                 ),
+                2,
             ),
         )
 }
--- app/src/main/java/woowacourse/movie/reservation/model/DataResource.kt
+++ app/src/main/java/woowacourse/movie/reservation/model/DataResource.kt
@@ -17,18 +17,41 @@ object DataResource {
             }
 
     val screeningTimesWeekends =
-        List(8) { index ->
-            LocalTime.of(9 + index * 2, 0, 0)
-        }
+        listOf(
+            LocalTime.of(9, 0, 0),
+            LocalTime.of(11, 0, 0),
+            LocalTime.of(13, 0, 0),
+            LocalTime.of(15, 0, 0),
+            LocalTime.of(17, 0, 0),
+            LocalTime.of(19, 0, 0),
+            LocalTime.of(21, 0, 0),
+            LocalTime.of(23, 0, 0),
+        )
 
     val screeningTimesWeekdays =
-        List(8) { index ->
-            if (index != 7) {
-                LocalTime.of(10 + index * 2, 0, 0)
-            } else {
-                LocalTime.of(0, 0, 0)
-            }
-        }
+        listOf(
+            LocalTime.of(10, 0, 0),
+            LocalTime.of(12, 0, 0),
+            LocalTime.of(14, 0, 0),
+            LocalTime.of(16, 0, 0),
+            LocalTime.of(18, 0, 0),
+            LocalTime.of(20, 0, 0),
+            LocalTime.of(22, 0, 0),
+            LocalTime.of(0, 0, 0),
+        )
+//    val screeningTimesWeekends =
+//        List(8) { index ->
+//            LocalTime.of(9 + index * 2, 0, 0)
+//        }
+//
+//    val screeningTimesWeekdays =
+//        List(8) { index ->
+//            if (index != 7) {
+//                LocalTime.of(10 + index * 2, 0, 0)
+//            } else {
+//                LocalTime.of(0, 0, 0)
+//            }
+//        }
 
     var selectedScreeningTime: LocalTime = LocalTime.of(0, 0, 0)
 }
