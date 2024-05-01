98442eb07326704ab01171b6cf04d998fc216cfb
gaeun <geun5744@gmail.com>
2024-05-01 21:34:19 +0900

    feat(BottomSheetFragment): 극장 목록 data binding 적용

--- app/src/main/java/woowacourse/movie/screeningmovie/theaters/TheaterAdapter.kt
+++ app/src/main/java/woowacourse/movie/screeningmovie/theaters/TheaterAdapter.kt
@@ -1,31 +1,20 @@
 package woowacourse.movie.screeningmovie.theaters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import woowacourse.movie.R
+import woowacourse.movie.databinding.ItemTheaterBinding
 import woowacourse.movie.screeningmovie.AdapterClickListener
 
 class TheaterAdapter(
     private val items: List<TheaterUiModel>,
     private val clickListener: AdapterClickListener,
 ) : RecyclerView.Adapter<TheaterAdapter.TheaterViewHolder>() {
-    class TheaterViewHolder(itemView: View, private val clickListener: AdapterClickListener) :
-        RecyclerView.ViewHolder(itemView) {
-        private val navigateButton =
-            itemView.findViewById<ImageButton>(R.id.btn_theater_navigate_next)
-        private val name = itemView.findViewById<TextView>(R.id.tv_theater_name)
-        private val timeCount = itemView.findViewById<TextView>(R.id.tv_theater_time_count)
-
+    class TheaterViewHolder(private val binding: ItemTheaterBinding, private val clickListener: AdapterClickListener) :
+        RecyclerView.ViewHolder(binding.root) {
         fun onBind(item: TheaterUiModel) {
-            name.text = item.name
-            timeCount.text = item.timeCount
-            navigateButton.setOnClickListener {
-                clickListener.onClick(item.id)
-            }
+            binding.theater = item
+            binding.clickListener = clickListener
         }
     }
 
@@ -33,11 +22,8 @@ class TheaterAdapter(
         parent: ViewGroup,
         viewType: Int,
     ): TheaterViewHolder {
-        val view =
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_theater, parent, false)
-
-        return TheaterViewHolder(view, clickListener)
+        val binding = ItemTheaterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TheaterViewHolder(binding, clickListener)
     }
 
     override fun getItemCount(): Int = items.size
--- app/src/main/java/woowacourse/movie/screeningmovie/theaters/TheaterBottomSheetDialogFragment.kt
+++ app/src/main/java/woowacourse/movie/screeningmovie/theaters/TheaterBottomSheetDialogFragment.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import woowacourse.movie.R
 import woowacourse.movie.data.DummyMovies
+import woowacourse.movie.databinding.BottomSheetTheaterBinding
 import woowacourse.movie.moviereservation.MovieReservationActivity
 import woowacourse.movie.screeningmovie.AdapterClickListener
 
@@ -15,18 +14,18 @@ class TheaterBottomSheetDialogFragment :
     BottomSheetDialogFragment(),
     AdapterClickListener,
     TheaterContract.View {
-    private lateinit var rcv: RecyclerView
     private lateinit var presenter: TheaterContract.Presenter
     private var movieId: Long = -1L
+
+    private lateinit var binding: BottomSheetTheaterBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        val view = inflater.inflate(R.layout.bottom_sheet_theater, container, false)
-        rcv = view.findViewById(R.id.rcv_theater)
-        return view
+        binding = BottomSheetTheaterBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(
@@ -44,15 +43,24 @@ class TheaterBottomSheetDialogFragment :
     }
 
     override fun showTheaters(theaterUiModels: List<TheaterUiModel>) {
-        rcv.adapter =
+        binding.rcvTheater.adapter =
             TheaterAdapter(
                 theaterUiModels,
                 this,
             )
     }
 
-    override fun navigateMovieReservation(screeningMovieId: Long, theaterId: Long) {
-        startActivity(MovieReservationActivity.getIntent(requireContext(), screeningMovieId, theaterId))
+    override fun navigateMovieReservation(
+        screeningMovieId: Long,
+        theaterId: Long,
+    ) {
+        startActivity(
+            MovieReservationActivity.getIntent(
+                requireContext(),
+                screeningMovieId,
+                theaterId,
+            ),
+        )
     }
 
     companion object {
