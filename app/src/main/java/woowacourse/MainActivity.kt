ce4dcf0057c69eb9530d3d5fc4a3bba9f34a91da
aprilgom <aprilgom@gmail.com>
2024-05-01 19:59:49 +0900

    feat(MainActivity): binding 적용

--- app/src/main/java/woowacourse/MainActivity.kt
+++ app/src/main/java/woowacourse/MainActivity.kt
@@ -7,24 +7,35 @@ import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import woowacourse.movie.R
+import woowacourse.movie.databinding.ActivityMainBinding
+import woowacourse.movie.databinding.FragmentSettingBinding
 import woowacourse.movie.reservationlist.ReservationListFragment
 import woowacourse.movie.reservationlist.SettingFragment
 import woowacourse.movie.screeningmovie.ScreeningMovieFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var screeningMovieFragment: ScreeningMovieFragment
+    private lateinit var settingMovieFragment: SettingFragment
+    private lateinit var reservationListFragment: ReservationListFragment
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val nav = findViewById<BottomNavigationView>(R.id.bnv_main)
-        val container = findViewById<FragmentContainerView>(R.id.fcv_main)
-        val screeningMovieFragment = ScreeningMovieFragment()
-        val settingMovieFragment = SettingFragment()
-        val reservationListFragment = ReservationListFragment()
-        nav.selectedItemId = R.id.menu_home
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        screeningMovieFragment = ScreeningMovieFragment()
+        settingMovieFragment = SettingFragment()
+        reservationListFragment = ReservationListFragment()
+
+        binding.bnvMain.selectedItemId = R.id.menu_home
         supportFragmentManager.findFragmentById(R.id.fcv_main) ?: changeFragment(
             screeningMovieFragment
         )
-        nav.setOnItemSelectedListener { item ->
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        binding.bnvMain.setOnItemSelectedListener { item ->
             changeFragment(
                 when (item.itemId) {
                     R.id.menu_home ->
