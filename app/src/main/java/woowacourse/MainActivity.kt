88c0849e8bd5f9ba3320bfd0201c603eab6595eb
aprilgom <aprilgom@gmail.com>
2024-05-01 19:52:14 +0900

    feat(ScreeningMovieFragmentTest): Navigation Bar 기능 구현

--- app/src/main/java/woowacourse/MainActivity.kt
+++ app/src/main/java/woowacourse/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import woowacourse.movie.R
+import woowacourse.movie.reservationlist.ReservationListFragment
+import woowacourse.movie.reservationlist.SettingFragment
+import woowacourse.movie.screeningmovie.ScreeningMovieFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +17,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val nav = findViewById<BottomNavigationView>(R.id.bnv_main)
         val container = findViewById<FragmentContainerView>(R.id.fcv_main)
-        /*
-        nav.setOnItemSelectedListener {
-            supportFragmentManager.findFragmentById(R.id.fcv_main) ?: changeFragment()
+        val screeningMovieFragment = ScreeningMovieFragment()
+        val settingMovieFragment = SettingFragment()
+        val reservationListFragment = ReservationListFragment()
+        nav.selectedItemId = R.id.menu_home
+        supportFragmentManager.findFragmentById(R.id.fcv_main) ?: changeFragment(
+            screeningMovieFragment
+        )
+        nav.setOnItemSelectedListener { item ->
+            changeFragment(
+                when (item.itemId) {
+                    R.id.menu_home ->
+                        screeningMovieFragment
+
+                    R.id.menu_setting ->
+                        settingMovieFragment
+
+                    R.id.menu_reservation_list ->
+                        reservationListFragment
+
+                    else -> screeningMovieFragment
+                }
+            )
+            true
         }
-         */
     }
 
     private fun changeFragment(fragment: Fragment) {
@@ -29,3 +51,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
