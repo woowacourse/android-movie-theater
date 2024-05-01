81a454a601229dab658d7b377d0161e947679eb4
dpcks0509 <dpcks0509@gmail.com>
2024-05-01 15:23:08 +0900

    feat: bottomNavigation 구현

---
+++ app/src/main/java/woowacourse/movie/home/view/MovieHomeFragment.kt
@@ -0,0 +1,45 @@
+package woowacourse.movie.home.view
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentMovieHomeBinding
+import woowacourse.movie.home.presenter.MovieHomePresenter
+import woowacourse.movie.home.presenter.contract.MovieHomeContract
+import woowacourse.movie.home.view.adapter.movie.MovieAdapter
+import woowacourse.movie.model.Movie
+import woowacourse.movie.util.MovieIntentConstant
+
+class MovieHomeFragment : Fragment(), MovieHomeContract.View {
+    private lateinit var movieHomePresenter: MovieHomePresenter
+    private lateinit var binding: FragmentMovieHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_movie_home, container, false)
+        movieHomePresenter = MovieHomePresenter(this)
+        movieHomePresenter.loadMovies()
+        return binding.root
+    }
+
+    override fun displayMovies(movies: List<Movie>) {
+        binding.movieRecyclerView.adapter = MovieAdapter(movies, ::displayTheaterSelectionDialog)
+    }
+
+    override fun displayTheaterSelectionDialog(id: Long) {
+        val bundle = Bundle()
+        bundle.putLong(MovieIntentConstant.KEY_MOVIE_ID, id)
+        val theaterSelectionFragment = TheaterSelectionFragment()
+        theaterSelectionFragment.arguments = bundle
+        theaterSelectionFragment.show(parentFragmentManager, theaterSelectionFragment.tag)
+    }
+}
