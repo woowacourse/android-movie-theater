b4aef79d407e15a411cf3d4a8a853521ebd20c7e
hxeyexn <kck85075@kumoh.ac.kr>
2024-05-02 14:24:28 +0900

    refactor: 예약 상세 ui 수정에 따른 테스트코드 리팩토링

--- app/src/androidTest/java/woowacourse/movie/view/reservation/ReservationDetailActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/reservation/ReservationDetailActivityTest.kt
@@ -21,6 +21,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.movie.R
 import java.time.LocalDate
+import java.time.LocalTime
 
 @RunWith(AndroidJUnit4::class)
 class ReservationDetailActivityTest {
@@ -80,51 +81,77 @@ class ReservationDetailActivityTest {
     fun `2024년_3월_4일의_상영일을_스피너로_선택하면_상영일_스피너에_2024-03-04_보여진다`() {
         // given
         onView(withId(R.id.spinner_reservation_detail_screening_date)).perform(click())
-        onData(allOf(`is`(instanceOf(LocalDate::class.java)), `is`(LocalDate.of(2024, 3, 4)))).perform(click())
+        onData(
+            allOf(
+                `is`(instanceOf(LocalDate::class.java)),
+                `is`(LocalDate.of(2024, 3, 4)),
+            ),
+        ).perform(click())
         // then
-        onView(withId(R.id.spinner_reservation_detail_screening_date)).check(matches(withSpinnerText(containsString("2024-03-04"))))
+        onView(withId(R.id.spinner_reservation_detail_screening_date)).check(
+            matches(
+                withSpinnerText(
+                    containsString("2024-03-04"),
+                ),
+            ),
+        )
     }
 
-    // TODO 극장으로 변경 후 재실행
-//    @Test
-//    fun `12시의_상영시간을_스피너로_선택하면_상영시간_스피너에_선택한_상영_시간이_보여진다`() {
-//        onView(withId(R.id.spinner_reservation_detail_screening_time)).perform(click())
-//        onData(allOf(`is`(instanceOf(LocalTime::class.java)), `is`(LocalTime.of(12, 0)))).perform(click())
-//        onView(withId(R.id.spinner_reservation_detail_screening_time)).check(matches(withSpinnerText(containsString("12:00"))))
-//    }
-    // TODO 극장으로 변경 후 재실행
-//    @Test
-//    fun `상영일을_평일로_선택하면_상영시간_스피너의_기본값은_10시이다`() {
-//        onView(withId(R.id.spinner_reservation_detail_screening_date)).perform(click())
-//        onData(allOf(`is`(instanceOf(LocalDate::class.java)), `is`(LocalDate.of(2024, 3, 1)))).perform(click())
-//
-//        onView(withId(R.id.spinner_reservation_detail_screening_time)).perform(click())
-//        onData(allOf(`is`(instanceOf(LocalTime::class.java)), `is`(LocalTime.of(10, 0)))).perform(click())
-//
-//        onView(withId(R.id.spinner_reservation_detail_screening_time)).check(matches(withSpinnerText(containsString("10:00"))))
-//    }
+    @Test
+    fun `9시의_상영시간을_스피너로_선택하면_상영시간_스피너에_선택한_상영_시간이_보여진다`() {
+        onView(withId(R.id.spinner_reservation_detail_screening_time)).perform(click())
+        onData(allOf(`is`(instanceOf(LocalTime::class.java)), `is`(LocalTime.of(9, 0)))).perform(
+            click(),
+        )
+        onView(withId(R.id.spinner_reservation_detail_screening_time)).check(
+            matches(
+                withSpinnerText(
+                    containsString("09:00"),
+                ),
+            ),
+        )
+    }
 
     @Test
     fun `2024년_3월_2일의_상영일을_스피너로_선택한_후_화면을_회전시켜도_선택한_상영일이_유지된다`() {
         onView(withId(R.id.spinner_reservation_detail_screening_date)).perform(click())
-        onData(allOf(`is`(instanceOf(LocalDate::class.java)), `is`(LocalDate.of(2024, 3, 2)))).perform(click())
+        onData(
+            allOf(
+                `is`(instanceOf(LocalDate::class.java)),
+                `is`(LocalDate.of(2024, 3, 2)),
+            ),
+        ).perform(click())
 
         activityRule.scenario.onActivity { activity ->
             activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         }
 
-        onView(withId(R.id.spinner_reservation_detail_screening_date)).check(matches(withSpinnerText(containsString("2024-03-02"))))
+        onView(withId(R.id.spinner_reservation_detail_screening_date)).check(
+            matches(
+                withSpinnerText(
+                    containsString("2024-03-02"),
+                ),
+            ),
+        )
     }
-    // TODO 극장으로 변경 후 재실행
-//    @Test
-//    fun `14시의_상영시간을_스피너로_선택한_후_화면을_회전시켜도_선택한_상영시간이_유지된다`() {
-//        onView(withId(R.id.spinner_reservation_detail_screening_time)).perform(click())
-//        onData(allOf(`is`(instanceOf(LocalTime::class.java)), `is`(LocalTime.of(14, 0)))).perform(click())
-//
-//        activityRule.scenario.onActivity { activity ->
-//            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-//        }
-//
-//        onView(withId(R.id.spinner_reservation_detail_screening_time)).check(matches(withSpinnerText(containsString("14:00"))))
-//    }
+
+    @Test
+    fun `9시의_상영시간을_스피너로_선택한_후_화면을_회전시켜도_선택한_상영시간이_유지된다`() {
+        onView(withId(R.id.spinner_reservation_detail_screening_time)).perform(click())
+        onData(allOf(`is`(instanceOf(LocalTime::class.java)), `is`(LocalTime.of(9, 0)))).perform(
+            click(),
+        )
+
+        activityRule.scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+
+        onView(withId(R.id.spinner_reservation_detail_screening_time)).check(
+            matches(
+                withSpinnerText(
+                    containsString("09:00"),
+                ),
+            ),
+        )
+    }
 }
