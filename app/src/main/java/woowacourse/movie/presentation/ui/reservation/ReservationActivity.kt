40c6bc96989faec5f7d0fd3e834aaa5a0719a62f
JunJangE <wnswkd486@gmail.com>
2024-05-03 17:26:29 +0900

    refactor: 예약 결과 페이지 DataBinding + MVP 적용

--- app/src/main/java/woowacourse/movie/presentation/ui/reservation/ReservationActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/ui/reservation/ReservationActivity.kt
@@ -2,11 +2,9 @@ package woowacourse.movie.presentation.ui.reservation
 
 import android.content.Context
 import android.content.Intent
-import android.widget.TextView
 import woowacourse.movie.R
 import woowacourse.movie.databinding.ActivityReservationBinding
 import woowacourse.movie.domain.model.Reservation
-import woowacourse.movie.domain.model.Seat
 import woowacourse.movie.domain.repository.DummyReservation
 import woowacourse.movie.domain.repository.DummyScreens
 import woowacourse.movie.presentation.base.BaseActivity
@@ -23,13 +21,7 @@ class ReservationActivity : BaseActivity<ActivityReservationBinding>(), View {
         )
     }
 
-    private val title: TextView by lazy { findViewById(R.id.tv_reservation_title) }
-    private val date: TextView by lazy { findViewById(R.id.tv_reservation_date) }
-    private val count: TextView by lazy { findViewById(R.id.tv_reservation_count) }
-    private val amount: TextView by lazy { findViewById(R.id.tv_reservation_amount) }
-
     override fun initStartView() {
-        binding.presenter = presenter
         val id = intent.getIntExtra(PUT_EXTRA_KEY_RESERVATION_ID, DEFAULT_RESERVATION_ID)
         presenter.loadReservation(id)
     }
@@ -38,11 +30,11 @@ class ReservationActivity : BaseActivity<ActivityReservationBinding>(), View {
         reservation: Reservation,
         theaterName: String,
     ) {
+        binding.reservation = reservation
+        binding.theaterName = theaterName
     }
 
     override fun back() = finish()
-
-    private fun List<Seat>.toSeatString(): String = this.joinToString(", ") { "${it.column}${it.row}" }
 
     companion object {
         private const val PUT_EXTRA_KEY_RESERVATION_ID = "reservationId"
