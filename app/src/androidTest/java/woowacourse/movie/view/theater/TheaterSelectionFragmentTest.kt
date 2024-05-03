07a688160a9dc34728a92d08e6a970d83713a9f3
hxeyexn <kck85075@kumoh.ac.kr>
2024-05-03 14:57:27 +0900

    refactor: 좌석 선택 fragment 테스트를 위한 fragment 실행 방법 변경
    - 이전: MainActivity를 이용해 SeatSelectionFragment를 실행
    - 이후: launchFragmentInContainer를 이용해 SeatSelectionFragment를 실행

--- app/src/androidTest/java/woowacourse/movie/view/theater/TheaterSelectionFragmentTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/theater/TheaterSelectionFragmentTest.kt
@@ -1,59 +1,40 @@
 package woowacourse.movie.view.theater
 
-import android.os.Bundle
+import androidx.core.os.bundleOf
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.MediumTest
 import org.hamcrest.CoreMatchers.instanceOf
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.movie.R
 import woowacourse.movie.TestFixture.FIRST_MOVIE_ITEM_POSITION
 import woowacourse.movie.TestFixture.makeTheater
 import woowacourse.movie.utils.MovieUtils.convertScreeningInfoFormat
-import woowacourse.movie.utils.MovieUtils.navigateToBottomMenu
-import woowacourse.movie.view.MainActivity
 import woowacourse.movie.view.home.HomeFragment
 import woowacourse.movie.view.theater.adapter.TheaterSelectionViewHolder
 
 @RunWith(AndroidJUnit4::class)
-@MediumTest
 class TheaterSelectionFragmentTest {
-    @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
-
     @Before
     fun setUp() {
-        activityRule.scenario.onActivity {
-            it.supportFragmentManager.navigateToBottomMenu(
-                R.id.fragment_container_main,
-                HomeFragment(),
-            )
-            val bundle = Bundle()
-            bundle.putInt(HomeFragment.MOVIE_ID, 0)
-            val bottomSheetDialogFragment = TheaterSelectionFragment()
-            bottomSheetDialogFragment.arguments = bundle
-            bottomSheetDialogFragment.show(it.supportFragmentManager, bottomSheetDialogFragment.tag)
-        }
+        val movieId = bundleOf(HomeFragment.MOVIE_ID to FIRST_ITEM_POSITION)
+        launchFragmentInContainer<TheaterSelectionFragment>(movieId)
     }
 
     @Test
     fun `극장_목록을_보여준다`() {
-        Thread.sleep(1000)
         onView(withId(R.id.recycler_view_theater_selection)).check(matches(isDisplayed()))
     }
 
     @Test
     fun `극장_목록을_스크롤했을_때_첫_번째_위치의_TheaterSelectionViewHolder는_극장_이름을_보여준다`() {
-        Thread.sleep(1000)
         onView(withId(R.id.recycler_view_theater_selection)).perform(
             RecyclerViewActions.scrollToHolder(
                 instanceOf(TheaterSelectionViewHolder::class.java),
@@ -65,7 +46,6 @@ class TheaterSelectionFragmentTest {
 
     @Test
     fun `극장_목록을_스크롤했을_때_첫_번째_위치의_TheaterSelectionViewHolder는_극장_시간을_보여준다`() {
-        Thread.sleep(1000)
         onView(withId(R.id.recycler_view_theater_selection)).perform(
             RecyclerViewActions.scrollToHolder(
                 instanceOf(TheaterSelectionViewHolder::class.java),
@@ -78,6 +58,6 @@ class TheaterSelectionFragmentTest {
     }
 
     companion object {
-        const val FIRST_ITEM_POSITION = 0
+        private const val FIRST_ITEM_POSITION = 0
     }
 }
