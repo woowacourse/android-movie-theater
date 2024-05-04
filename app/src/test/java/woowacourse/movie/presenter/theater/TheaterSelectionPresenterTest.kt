e424e12f24b46f0cb30bb5f3aea4435940d14832
김진우 <jinwoo3661@naver.com>
2024-05-04 17:38:22 +0900

    refactor: TheaterSelectionPresenterTest 함수명 리팩토링, 테스트 케이스 추가

--- app/src/test/java/woowacourse/movie/presenter/theater/TheaterSelectionPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presenter/theater/TheaterSelectionPresenterTest.kt
@@ -22,6 +22,13 @@ class TheaterSelectionPresenterTest {
     }
 
     @Test
+    fun `상영 영화에 해당하는 극장 목록을 불러온다`() {
+        every { view.showTheaters(any())} just runs
+        presenter.loadTheaters(0)
+        verify { view.showTheaters(any()) }
+    }
+
+    @Test
     fun `극장_목록에서_극장을_선택하면_선택된_극장_ID와_영화_ID를_가지고_예매_화면으로_이동한다`() {
         every { view.navigateToDetail(any(), any()) } just runs
         presenter.loadTheater(
