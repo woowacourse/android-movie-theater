40ede4d9ca5b47a33c4e51d57b4fa2fc8a2c6dab
kimhyemin <kimhm0728@naver.com>
2024-05-04 22:17:22 +0900

    test: MovieDetailActivity 테스트 수정

--- app/src/androidTest/java/woowacourse/movie/detail/view/MovieDetailActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/detail/view/MovieDetailActivityTest.kt
@@ -6,11 +6,10 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -34,36 +33,30 @@ class MovieDetailActivityTest {
 
     @Test
     fun `선택된_영화의_제목이_표시된다`() {
-        onView(withId(R.id.detailTitle))
+        onView(withId(R.id.tv_title))
             .check(matches(withText("타이타닉 0")))
     }
 
     @Test
-    fun `선택된_영화의_상영시작일이_표시된다`() {
-        onView(withId(R.id.startDate))
-            .check(matches(withText("2024-04-01")))
-    }
-
-    @Test
-    fun `선택된_영화의_상영종료일이_표시된다`() {
-        onView(withId(R.id.endDate))
-            .check(matches(withText("2024-04-28")))
+    fun `선택된_영화의_상영_기간이_표시된다`() {
+        onView(withId(R.id.tv_screening_date_range))
+            .check(matches(withText("상영일: 2024.4.1 ~ 2024.4.28")))
     }
 
     @Test
     fun `선택된_영화의_러닝타임이_표시된다`() {
-        onView(withId(R.id.detailRunningTime))
-            .check(matches(withText("152")))
+        onView(withId(R.id.tv_running_time))
+            .check(matches(withText("러닝타임: 152분")))
     }
 
     @Test
     fun `선택된_영화의_설명이_표시된다`() {
-        onView(withId(R.id.detailDescription))
+        onView(withId(R.id.tv_description))
             .check(
                 matches(
                     withText(
                         "우연한 기회로 티켓을 구해 타이타닉호에 올라탄 자유로운 영혼을 가진 화가 ‘잭’(레오나르도 디카프리오)은 막강한 재력의 약혼자와 함께 1등실에 승선한 ‘로즈’(케이트 윈슬렛)에게 한눈에 반한다. " +
-                            "진실한 사랑을 꿈꾸던 ‘로즈’ 또한 생애 처음 황홀한 감정에 휩싸이고, 둘은 운명 같은 사랑에 빠지는데… 가장 차가운 곳에서 피어난 뜨거운 사랑! 영원히 가라앉지 않는 세기의 사랑이 펼쳐진다!",
+                                "진실한 사랑을 꿈꾸던 ‘로즈’ 또한 생애 처음 황홀한 감정에 휩싸이고, 둘은 운명 같은 사랑에 빠지는데… 가장 차가운 곳에서 피어난 뜨거운 사랑! 영원히 가라앉지 않는 세기의 사랑이 펼쳐진다!",
                     ),
                 ),
             )
@@ -71,25 +64,19 @@ class MovieDetailActivityTest {
 
     @Test
     fun `선택된_영화의_예매_인원수의_초기값이_표시된다`() {
-        onView(withId(R.id.detailReservCount))
+        onView(withId(R.id.tv_reservation_count))
             .check(matches(withText("1")))
     }
 
     @Test
     fun `화면이_가로로_회전되어도_예매_인원수의_값이_유지된다`() {
-        onView(withId(R.id.detailPlusBtn)).perform(click())
+        onView(withId(R.id.btn_plus)).perform(click())
 
         val activityScenario = activityRule.scenario
         activityScenario.onActivity { activity ->
             activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         }
 
-        onView(withId(R.id.detailReservCount)).check(matches(withText("2")))
-    }
-
-    @Test
-    fun `예매_예매_버튼을_클릭하면_영화_상세_페이지로_이동한다`() {
-        onView(withId((R.id.seatSelectionBtn))).perform(click())
-        onView(withId(R.id.seatSelectionActivity)).check(matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.tv_reservation_count)).check(matches(withText("2")))
     }
 }
