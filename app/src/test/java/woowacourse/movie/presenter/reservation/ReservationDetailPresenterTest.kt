1d3cdee67f8271f9fb1da2179e0257c033aa4649
김진우 <jinwoo3661@naver.com>
2024-05-04 17:08:37 +0900

    refactor: 기능 추가에 따른 테스트 코드 리팩토링

--- app/src/test/java/woowacourse/movie/presenter/reservation/ReservationDetailPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presenter/reservation/ReservationDetailPresenterTest.kt
@@ -24,28 +24,28 @@ class ReservationDetailPresenterTest {
     }
 
     @Test
-    fun `현재 영화 정보를 보여준다`() {
+    fun `현재 영화 정보를 불러온다`() {
         every { view.showMovieInformation(any()) } just runs
         presenter.loadMovie(0)
         verify { view.showMovieInformation(any()) }
     }
 
     @Test
-    fun `상영 기간을 보여준다`() {
+    fun `상영 기간을 불러온다`() {
         every { view.showScreeningPeriod(any()) } just runs
         presenter.loadScreeningPeriod(0)
         verify { view.showScreeningPeriod(any()) }
     }
 
     @Test
-    fun `상영 시간을 보여준다`() {
+    fun `상영 시간을 불러온다`() {
         every { view.showScreeningTimes(any(), any()) } just runs
         presenter.loadScreeningTimes(0, "2024-03-01")
         verify { view.showScreeningTimes(any(), any()) }
     }
 
     @Test
-    fun `예약 인원이 1인 상태에서 마이너스 버튼을 누르면 토스트를 보여준다`() {
+    fun `예약 인원이 1인 상태에서 마이너스 버튼을 누르면 토스트를 띄워서 유저에게 알린다`() {
         every { view.showResultToast() } just runs
         presenter.decreaseHeadCount(1)
         verify { view.showResultToast() }
@@ -59,7 +59,7 @@ class ReservationDetailPresenterTest {
     }
 
     @Test
-    fun `예약 인원이 20인 상태에서 플러스 버튼을 누르면 토스트를 보여준다`() {
+    fun `예약 인원이 20인 상태에서 플러스 버튼을 누르면 토스트를 띄어서 유저에게 알린다`() {
         every { view.showResultToast() } just runs
         presenter.increaseHeadCount(20)
         verify { view.showResultToast() }
@@ -70,5 +70,19 @@ class ReservationDetailPresenterTest {
         every { view.changeHeadCount(any()) } just runs
         presenter.increaseHeadCount(2)
         verify { view.changeHeadCount(3) }
+    }
+
+    @Test
+    fun `영화 정보가 존재하지 않는다면 토스트를 띄어서 유저에게 알린다`() {
+        every { view.showIdErrorToast() } just runs
+        presenter.checkIdValidation(-1,0)
+        verify { view.showIdErrorToast() }
+    }
+
+    @Test
+    fun `극장 정보가 존재하지 않는다면 토스트를 띄어서 유저에게 알린다`() {
+        every { view.showIdErrorToast() } just runs
+        presenter.checkIdValidation(0,-1)
+        verify { view.showIdErrorToast() }
     }
 }
