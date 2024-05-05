89485c3a6c6be40631afd99f831f8459afbef125
Hogu59 <dbstjdguswkd@naver.com>
2024-05-06 01:19:34 +0900

    test(presenter): write a few test codes

--- app/src/test/java/woowacourse/movie/presentation/seatSelection/SeatSelectionPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presentation/seatSelection/SeatSelectionPresenterTest.kt
@@ -2,7 +2,9 @@ package woowacourse.movie.presentation.seatSelection
 
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import woowacourse.movie.repository.MovieRepository
 
@@ -12,7 +14,7 @@ class SeatSelectionPresenterTest {
 
     private val ticketCount = 3
 
-    @MockK
+    @MockK(relaxed = true)
     private lateinit var view: SeatSelectionContract.View
 
     @MockK
@@ -23,15 +25,19 @@ class SeatSelectionPresenterTest {
         presenter = SeatSelectionPresenter(view, ticketCount, repository)
     }
 
-    /*
     @Test
     fun `updateSeatSelection로 전달 받은 index 값의 좌석 상태가 빈 좌석일 경우, 선택된 좌석 UI로 업데이트 하도록 view에게 요청한다`() {
+        presenter.updateSeatSelection(0)
 
+        verify { view.updateSelectedSeatUI(0) }
+        verify { view.updateViews() }
     }
 
     @Test
     fun `updateSeatSelection으로 전달 받은 index 값의 좌석 상태가 이미 선택된 좌석일 경우, 좌석 기본 UI로 업데이트 하도록 view에게 요청한다`() {
+        presenter.seatingSystem.trySelectSeat(0)
+        presenter.updateSeatSelection(0)
 
+        verify { view.updateUnSelectedSeatUI(0) }
     }
-     */
 }
