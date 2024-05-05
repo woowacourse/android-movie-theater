842a6a9e935e82899862ef99361a08ca63c5cc37
somin <salth0601@gmail.com>
2024-05-06 00:23:31 +0900

    refactor: BottomTheatersFragment 데이터 바인딩 적용

--- app/src/main/java/woowacourse/movie/presentation/ui/main/home/bottom/BottomTheatersFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/ui/main/home/bottom/BottomTheatersFragment.kt
@@ -2,16 +2,13 @@ package woowacourse.movie.presentation.ui.main.home.bottom
 
 import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentBottomTheatersBinding
 import woowacourse.movie.domain.model.TheaterCount
 
 class BottomTheatersFragment(
@@ -19,30 +16,33 @@ class BottomTheatersFragment(
     private val actionHandler: BottomTheaterActionHandler,
     private val movieId: Int,
 ) : BottomSheetDialogFragment() {
-    private var _binding: ViewBinding? = null
-    val binding: ViewBinding
+    private var _binding: FragmentBottomTheatersBinding? = null
+    val binding: FragmentBottomTheatersBinding
         get() = _binding!!
 
-    private lateinit var rvTheater: RecyclerView
     private lateinit var adapter: BottomTheatersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
-        adapter = BottomTheatersAdapter(actionHandler, theaterCounts, movieId)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_bottom_theaters, container, false)
-        rvTheater = view.findViewById(R.id.rv_bottom_theaters)
-        rvTheater.adapter = adapter
+    ): View {
+        _binding = FragmentBottomTheatersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        return view
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        adapter = BottomTheatersAdapter(actionHandler, theaterCounts, movieId)
+        binding.rvBottomTheaters.adapter = adapter
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -60,8 +60,8 @@ class BottomTheatersFragment(
         return dialog
     }
 
-    override fun onDestroy() {
-        Log.d("테스트", "지금이니?")
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
