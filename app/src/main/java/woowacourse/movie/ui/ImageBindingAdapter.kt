411cfc97caca41db3f1af3ba3a28cc29ce58c8a8
sh1mj1 <7wlgns@gmail.com>
2024-05-05 22:30:03 +0900

    refactor(ImageBindingAdapter): 이미지 관련 바인딩 어댑터 분리

---
+++ app/src/main/java/woowacourse/movie/ui/ImageBindingAdapter.kt
@@ -0,0 +1,27 @@
+@file:JvmName("ImageBindingAdapter")
+
+package woowacourse.movie.ui
+
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import androidx.databinding.BindingAdapter
+import woowacourse.movie.domain.model.Image
+
+@BindingAdapter("image:drawableImage")
+fun setImageResource(
+    imageView: ImageView,
+    @DrawableRes resId: Int,
+)  {
+    imageView.setImageResource(resId)
+}
+
+@BindingAdapter("image:imageSource")
+fun setImageResource(
+    imageView: ImageView,
+    image: Image<Any>,
+)  {
+    when {
+        image.imageSource is Int -> setImageResource(imageView, image.imageSource as Int)
+        // ...
+    }
+}
