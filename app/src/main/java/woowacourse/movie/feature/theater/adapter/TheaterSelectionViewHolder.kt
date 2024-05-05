521a3fa8cc79d7acf2654881cbc806a57836a153
hxeyexn <kck85075@kumoh.ac.kr>
2024-05-06 04:47:30 +0900

    refactor(TheaterSelection): DataBinding 재적용 및 id 수정
    - TheaterViewHolder DataBinding 재적용
    - xml view Id 수정

--- app/src/main/java/woowacourse/movie/feature/theater/adapter/TheaterSelectionViewHolder.kt
+++ app/src/main/java/woowacourse/movie/feature/theater/adapter/TheaterSelectionViewHolder.kt
@@ -1,7 +1,6 @@
 package woowacourse.movie.feature.theater.adapter
 
 import androidx.recyclerview.widget.RecyclerView
-import woowacourse.movie.R
 import woowacourse.movie.databinding.ItemTheaterBinding
 import woowacourse.movie.model.theater.Theater
 
@@ -9,23 +8,19 @@ class TheaterSelectionViewHolder(
     private val binding: ItemTheaterBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     init {
-        binding.theaterViewHolder = this
+        binding.viewHolder = this
     }
 
     fun bind(
-        item: Theater,
+        theater: Theater,
         screeningCount: Int,
         onTheaterSelected: OnTheaterSelected,
     ) {
         with(binding) {
-            name.text = item.name
-            screeningInfo.text =
-                itemView.context.getString(
-                    R.string.theater_screening_time_count,
-                    screeningCount,
-                )
-            constraintLayoutItemTheater.setOnClickListener {
-                onTheaterSelected(item.theaterId)
+            this.theater = theater
+            this.screeningCount = screeningCount
+            clTheater.setOnClickListener {
+                onTheaterSelected(theater.theaterId)
             }
         }
     }
