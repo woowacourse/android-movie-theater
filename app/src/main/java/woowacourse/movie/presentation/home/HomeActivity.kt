8843199905f5444ce801ae3530c8a35fc6c10ee1
JoYehyun <jo990714@naver.com>
2024-05-06 02:26:34 +0900

    refactor: Fragment 관리 클래스 분리

--- app/src/main/java/woowacourse/movie/presentation/home/HomeActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/home/HomeActivity.kt
@@ -1,22 +1,18 @@
 package woowacourse.movie.presentation.home
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import woowacourse.movie.R
 import woowacourse.movie.databinding.ActivityHomeBinding
-import woowacourse.movie.presentation.homefragments.movieList.MovieListFragment
-import woowacourse.movie.presentation.homefragments.reservation.ReservationFragment
-import woowacourse.movie.presentation.homefragments.setting.SettingFragment
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
-    private val movieListFragment by lazy { MovieListFragment() }
-    private val reservationFragment by lazy { ReservationFragment() }
-    private val settingFragment by lazy { SettingFragment() }
+    private val fragmentManagerHelper: FragmentManagerHelper by lazy {
+        FragmentManagerHelper(
+            this,
+            R.id.fragment_container,
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,23 +20,8 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         if (savedInstanceState == null) {
-            selectDefaultMenuItem()
             setupBottomNavigationView()
-        }
-    }
-
-    private fun setupBottomNavigationView() {
-        setupInitialFragment()
-        binding.bottomNavigationView.setOnItemSelectedListener { menu ->
-            val selectedMenu = matchedFragment(menu)
-            replaceFragment(selectedMenu)
-            true
-        }
-    }
-
-    private fun setupInitialFragment() {
-        supportFragmentManager.commit {
-            add(R.id.fragment_container, movieListFragment)
+            selectDefaultMenuItem()
         }
     }
 
@@ -48,18 +29,10 @@ class HomeActivity : AppCompatActivity() {
         binding.bottomNavigationView.selectedItemId = R.id.action_home
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.commit {
-            replace(R.id.fragment_container, fragment)
-        }
-    }
-
-    private fun matchedFragment(menu: MenuItem): Fragment {
-        return when (menu.itemId) {
-            R.id.action_home -> movieListFragment
-            R.id.action_reservation_list -> reservationFragment
-            R.id.action_settings -> settingFragment
-            else -> movieListFragment
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { menu ->
+            fragmentManagerHelper.replace(menu.itemId)
+            true
         }
     }
 }
