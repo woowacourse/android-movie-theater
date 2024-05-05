c48e9f1d21309ef8a2ad02d6695007caf1dbb92b
kkosang <eiwoqp77@gmail.com>
2024-05-05 16:55:01 +0900

    refactor: fragment 네이밍 변경

--- app/src/main/java/woowacourse/movie/presentation/view/navigation/ReservationListFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/navigation/ReservationListFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
 
-class ReservationList : Fragment() {
+class ReservationListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
--- app/src/main/java/woowacourse/movie/presentation/view/navigation/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/navigation/SettingFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
 
-class Setting : Fragment() {
+class SettingFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
