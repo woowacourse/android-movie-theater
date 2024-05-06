bf32f5e56b39c1a37ae75bbad9f0081f8d77a093
김진우 <jinwoo3661@naver.com>
2024-05-06 16:39:28 +0900

    feat: ReservationTicket item & ViewHodler 생성후 구현

---
+++ app/src/main/java/woowacourse/movie/view/history/adapter/viewholder/TicketViewHolder.kt
@@ -0,0 +1,23 @@
+package woowacourse.movie.view.history.adapter.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.movie.databinding.ItemTicketBinding
+import woowacourse.movie.model.ticket.ReservationTicket
+import woowacourse.movie.view.history.adapter.OnClickTicketItem
+
+class TicketViewHolder(
+    private val binding: ItemTicketBinding,
+    private val onClickTicketItem: OnClickTicketItem,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    init {
+        binding.ticketCatalog = this
+    }
+
+    fun bind(item: ReservationTicket) {
+        binding.reservationTicket = item
+    }
+
+    fun onClickTicket(item: ReservationTicket) = onClickTicketItem(item)
+
+}
