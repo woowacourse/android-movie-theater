c8e9afcbc66186b8c82c718be471c1b04267fa4e
김진우 <jinwoo3661@naver.com>
2024-05-06 19:19:50 +0900

    feat: DataBinding 연결

--- app/src/main/java/woowacourse/movie/view/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/view/setting/SettingFragment.kt
@@ -4,20 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding : FragmentSettingBinding?= null
+    private val binding : FragmentSettingBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_setting, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
