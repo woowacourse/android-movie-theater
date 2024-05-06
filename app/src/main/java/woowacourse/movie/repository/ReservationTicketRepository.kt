21da90b442d91ba1ea74dd8ce98a8ad31ff7c431
김진우 <jinwoo3661@naver.com>
2024-05-06 18:56:19 +0900

    feat: ReservationTicketRepository 기능 확장

--- app/src/main/java/woowacourse/movie/repository/ReservationTicketRepository.kt
+++ app/src/main/java/woowacourse/movie/repository/ReservationTicketRepository.kt
@@ -1,12 +1,13 @@
 package woowacourse.movie.repository
 
 import woowacourse.movie.model.ticket.ReservationTicket
+import woowacourse.movie.model.ticket.Ticket
 
 interface ReservationTicketRepository {
 
     fun loadReservationTickets(): List<ReservationTicket>
 
-    fun saveReservationTicket(reservationTicket: ReservationTicket)
+    fun saveReservationTicket(ticket: Ticket)
 
     fun clearReservationTickets()
 }
--- app/src/main/java/woowacourse/movie/repository/ReservationTicketRepositoryImpl.kt
+++ app/src/main/java/woowacourse/movie/repository/ReservationTicketRepositoryImpl.kt
@@ -2,16 +2,26 @@ package woowacourse.movie.repository
 
 import android.content.Context
 import woowacourse.movie.db.history.ReservationHistoryDatabase
+import woowacourse.movie.db.screening.ScreeningDao
+import woowacourse.movie.db.theater.TheaterDao
 import woowacourse.movie.model.ticket.ReservationTicket
+import woowacourse.movie.model.ticket.Ticket
+import woowacourse.movie.model.ticket.toReservationTicket
 
 class ReservationTicketRepositoryImpl(context: Context) : ReservationTicketRepository {
     private val historyDao = ReservationHistoryDatabase.getInstance(context).reservationDao()
+    private val theaterDao = TheaterDao()
+    private val movieDao = ScreeningDao()
 
     override fun loadReservationTickets(): List<ReservationTicket> {
         return historyDao.findReservations()
     }
 
-    override fun saveReservationTicket(reservationTicket: ReservationTicket) {
+    override fun saveReservationTicket(ticket: Ticket) {
+        val reservationTicket = ticket.toReservationTicket(
+            movieTitle = movieDao.find(ticket.movieId).title,
+            theaterName = theaterDao.find(ticket.theaterId).theaterName,
+        )
         historyDao.saveReservationTicket(reservationTicket)
     }
 
