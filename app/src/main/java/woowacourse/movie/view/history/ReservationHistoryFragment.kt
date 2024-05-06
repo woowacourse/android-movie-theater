2173bd070bb5dc4656d5c736c291d45a6e3c41a3
김진우 <jinwoo3661@naver.com>
2024-05-06 15:47:20 +0900

    feat: ReservationHistoryFragment DataBinding

--- app/src/main/java/woowacourse/movie/view/history/ReservationHistoryFragment.kt
+++ app/src/main/java/woowacourse/movie/view/history/ReservationHistoryFragment.kt
@@ -4,20 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentReservationHistoryBinding
 
 class ReservationHistoryFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentReservationHistoryBinding? = null
+    private val binding: FragmentReservationHistoryBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reservation_history, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater,R.layout.fragment_reservation_history,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
