11944f63acaf97b513ccc973f9a8845b41cbf2f8
김진우 <jinwoo3661@naver.com>
2024-05-06 16:39:52 +0900

    feat: ReservationTicketModel 구현

---
+++ app/src/main/java/woowacourse/movie/model/ticket/ReservationTicket.kt
@@ -0,0 +1,12 @@
+package woowacourse.movie.model.ticket
+
+import woowacourse.movie.model.movie.ScreeningDateTime
+import woowacourse.movie.model.seats.Seats
+
+data class ReservationTicket(
+    val movieTitle: String,
+    val theaterName: String,
+    val seats: Seats,
+    val screeningDateTime: ScreeningDateTime,
+    val amount: Int,
+)
