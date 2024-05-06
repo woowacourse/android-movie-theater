982092a3385d2f140f23dae584ecb9b09b1473e2
aprilgom <aprilgom@gmail.com>
2024-05-07 02:58:21 +0900

    test(MovieDetailPresenterTest): Capturing slot 적용

--- app/src/test/kotlin/MovieDetailPresenterTest.kt
+++ app/src/test/kotlin/MovieDetailPresenterTest.kt
@@ -3,15 +3,20 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.just
+import io.mockk.slot
 import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import woowacourse.movie.data.DummyMovieRepository
+import woowacourse.movie.model.Screening
 import woowacourse.movie.moviedetail.MovieDetailContract
 import woowacourse.movie.moviedetail.MovieDetailPresenter
 import woowacourse.movie.moviedetail.uimodel.HeadCountUiModel
+import woowacourse.movie.moviedetail.uimodel.ReservationPlanUiModel
+import woowacourse.movie.moviedetail.uimodel.toMovieReservationUiModel
 
 @ExtendWith(MockKExtension::class)
 class MovieDetailPresenterTest {
@@ -28,12 +33,14 @@ class MovieDetailPresenterTest {
     @Test
     @DisplayName("영화 정보를 불러오면 화면에 나타난다")
     fun show_movie_info_When_load_movie_data() {
-        every { view.showMovieInfo(any()) } just Runs
+        val slot = slot<ReservationPlanUiModel>()
         every { view.showBookingDetail(any(), any()) } just Runs
+        every { view.showMovieInfo(capture(slot)) } just Runs
 
-        presenter.loadMovieDetail(1)
+        presenter.loadMovieDetail(0)
 
         verify(exactly = 1) { view.showMovieInfo(any()) }
+        assertThat(Screening.STUB_A.toMovieReservationUiModel()).isEqualTo(slot.captured)
     }
 
     @Test
