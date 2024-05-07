20b24d25e03eca502e2721ea986f32daf3d48b23
songpink <jejeje04@naver.com>
2024-05-07 21:13:01 +0900

    refactor: "n개의 상영 시간" 텍스트뷰 분리

--- app/src/main/java/woowacourse/movie/list/adapter/TheaterAdapter.kt
+++ app/src/main/java/woowacourse/movie/list/adapter/TheaterAdapter.kt
@@ -10,6 +10,7 @@ import woowacourse.movie.list.model.Theater
 class TheaterAdapter : RecyclerView.Adapter<TheaterAdapter.TheaterViewHolder>() {
     private var movieId: Long = -1
     private var theaters: List<Theater> = emptyList()
+
     private lateinit var itemClickListener: OnItemClickListener
 
     fun initTheatersInfo(movieId: Long, theaters: List<Theater>) {
@@ -20,9 +21,11 @@ class TheaterAdapter : RecyclerView.Adapter<TheaterAdapter.TheaterViewHolder>() 
     inner class TheaterViewHolder(private val binding: TheaterItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(
+            theater: Theater,
             name: String,
             position: Int,
         ) {
+            binding.theater = theater
             binding.theaterName.text = name
             val count = theaters[position].getCount(movieId)
             binding.screeningTimes.text = String.format(
@@ -61,6 +64,6 @@ class TheaterAdapter : RecyclerView.Adapter<TheaterAdapter.TheaterViewHolder>() 
         holder: TheaterViewHolder,
         position: Int,
     ) {
-        holder.bind(theaters[position].name, position)
+        holder.bind(theaters[position], theaters[position].name, position)
     }
 }
