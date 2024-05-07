5fd61bf1cd646d89b23a471ccaf27a2a86afca32
Namyunsuk <yunsuk0328@naver.com>
2024-05-07 19:12:21 +0900

    feat: UserTicketDao 작성

--- app/src/main/java/woowacourse/movie/model/db/UserTicketDao.kt
+++ app/src/main/java/woowacourse/movie/model/db/UserTicketDao.kt
@@ -1,6 +1,25 @@
 package woowacourse.movie.model.db
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
-interface UserTicketDao
+interface UserTicketDao {
+    @Query("SELECT * FROM user_tickets WHERE id=(:id)")
+    fun find(id: Long): UserTicket
+
+    @Query("SELECT * FROM user_tickets")
+    fun findAll(): List<UserTicket>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(data: UserTicket)
+
+    @Delete
+    fun delete(userTicket: UserTicket)
+
+    @Query("DELETE FROM user_tickets")
+    fun deleteAll()
+}
--- app/src/main/java/woowacourse/movie/model/db/UserTicketDatabase.kt
+++ app/src/main/java/woowacourse/movie/model/db/UserTicketDatabase.kt
@@ -6,4 +6,6 @@ import androidx.room.TypeConverters
 
 @Database(entities = [UserTicket::class], version = 1)
 @TypeConverters(UserTicketTypeConverters::class)
-abstract class UserTicketDatabase : RoomDatabase()
+abstract class UserTicketDatabase : RoomDatabase() {
+    abstract fun userTicketDao(): UserTicketDao
+}
