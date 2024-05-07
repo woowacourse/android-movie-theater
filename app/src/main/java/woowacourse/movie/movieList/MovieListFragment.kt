26bc2a272bd9159785f3780904d6bb8ea9d8a1c8
ii2001 <iivks2001@gmail.com>
2024-05-07 13:24:16 +0900

    :recycle: 불필요한 검증 제거

--- app/src/main/java/woowacourse/movie/movieList/MovieListFragment.kt
+++ app/src/main/java/woowacourse/movie/movieList/MovieListFragment.kt
@@ -40,13 +40,8 @@ class MovieListFragment :
     }
 
     override fun navigateToCinemaView(theater: Theater) {
-        val fragment =
-            (
-                childFragmentManager.findFragmentByTag(ChooseCinemasBottomSheetFragment.TAG)
-                    as? ChooseCinemasBottomSheetFragment
-            ) ?: ChooseCinemasBottomSheetFragment()
         val bottomSheet =
-            fragment.withArgs {
+            ChooseCinemasBottomSheetFragment().withArgs {
                 putSerializable(THEATER_KEY, theater)
             }
         bottomSheet.show(childFragmentManager, ChooseCinemasBottomSheetFragment.TAG)
