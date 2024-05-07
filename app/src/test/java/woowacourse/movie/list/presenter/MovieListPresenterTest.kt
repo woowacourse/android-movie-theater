9cb9d35842d20240571bd267b957d7ea0a319c5a
songpink <jejeje04@naver.com>
2024-05-07 15:02:53 +0900

    test: 테스트코드 수정

--- app/src/test/java/woowacourse/movie/list/presenter/MovieListPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/list/presenter/MovieListPresenterTest.kt
@@ -24,52 +24,21 @@ class MovieListPresenterTest {
 
     @Test
     fun `영화 정보를 목록에 띄워야 한다`() {
-        every { view.showMoviesInfo(any(), any()) } just runs
+        every { view.showMoviesInfo() } just runs
         // when
         presenter.setMoviesInfo()
         // then
-        verify { view.showMoviesInfo(any(), any()) }
+        verify { view.showMoviesInfo() }
     }
 
     @Test
     fun `영화 개수는 9개여야 한다(광고 2개 포함)`() {
         // given
         val movieListSlot = slot<List<Movie>>()
-        every { view.showMoviesInfo(capture(movieListSlot), any()) } just runs
+        every { view.makeMovieListAdapter(capture(movieListSlot)) } just runs
         // when
-        presenter.setMoviesInfo()
+        presenter.setMovieListAdapter()
         // then
         assertThat(movieListSlot.captured.size).isEqualTo(9)
-    }
-
-    enum class CarType {
-        A, B, C, D, E
-    }
-
-    enum class RecordingOutcome { RECORDED }
-
-    class Car(val type: CarType) {
-        fun go(goType: CarType) {
-            println("${goType}으로간다")
-        }
-    }
-
-    @Test
-    fun name() {
-        val car = mockk<Car>()
-        val typeSlot = slot<CarType>()
-        val list = mutableListOf<CarType>()
-
-        every {
-            car.go(
-                goType = capture(list)
-            )
-        } just runs
-        car.go(CarType.C)
-        car.go(CarType.A)
-//        println("알송ㅋㅋ ${typeSlot}")
-        val argument = list
-//        assertThat(argument).isEqualTo(3)
-        println("아아알송 $argument")
     }
 }
