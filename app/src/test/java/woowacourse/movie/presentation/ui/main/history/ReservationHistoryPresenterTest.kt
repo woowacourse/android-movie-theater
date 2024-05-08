e66f6b8caa4a7cc7cbdfff0089c17c7b1f018266
JunJangE <wnswkd486@gmail.com>
2024-05-08 18:26:02 +0900

    test: ReservationHistoryPresenterTest

---
+++ app/src/test/java/woowacourse/movie/presentation/ui/main/history/ReservationHistoryPresenterTest.kt
@@ -0,0 +1,69 @@
+package woowacourse.movie.presentation.ui.main.history
+
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
+import io.mockk.verify
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import woowacourse.movie.domain.repository.ReservationRepository
+import woowacourse.movie.presentation.ui.utils.DummyData.dummyReservation
+
+@ExtendWith(MockKExtension::class)
+class ReservationHistoryPresenterTest {
+    @MockK
+    private lateinit var view: ReservationHistoryContract.View
+
+    private lateinit var presenter: ReservationHistoryContract.Presenter
+
+    @MockK
+    private lateinit var repository: ReservationRepository
+
+    @BeforeEach
+    fun setUp() {
+        presenter = ReservationHistoryPresenter(view, repository)
+    }
+
+    @Test
+    fun `ReservationPresenter가 loadReservation()을 했을 때, view에게 reservation 데이터와 theaterName을 전달한다`() {
+        // given
+        every { repository.findReservations() } returns Result.success(listOf(dummyReservation))
+        every { view.showReservations(any()) } just runs
+
+        // when
+        presenter.loadReservations()
+
+        // then
+        verify { view.showReservations(listOf(dummyReservation)) }
+    }
+
+    @Test
+    fun `ReservationPresenter가 loadReservation()을 했을 때, 예외가 발생하면 view에게 예외를 전달한다`() {
+        // given
+        val exception = Exception()
+        every { repository.findReservations() } returns Result.failure(exception)
+        every { view.showToastMessage(e = any()) } just runs
+
+        // when
+        presenter.loadReservations()
+
+        // then
+        verify { view.showToastMessage(exception) }
+    }
+
+    @Test
+    fun `ReservationPresenter가 onReservationClick()을 했을 때, view에게 reservationId를 전달한다`() {
+        // given
+        every { repository.findReservations() } returns Result.success(listOf(dummyReservation))
+        every { view.navigateToReservation(any()) } just runs
+
+        // when
+        presenter.onReservationClick(dummyReservation.reservationId)
+
+        // then
+        verify { view.navigateToReservation(dummyReservation.reservationId) }
+    }
+}
