88f62c40e761a8cae9700a758195dc306cc51a1d
gaeun <geun5744@gmail.com>
2024-05-08 22:46:08 +0900

    refactor: data class 추가 및 STUB 추가

--- app/src/main/java/woowacourse/movie/data/entity/MovieTheater.kt
+++ app/src/main/java/woowacourse/movie/data/entity/MovieTheater.kt
@@ -5,7 +5,27 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "movie_theater")
-class MovieTheater(
+data class MovieTheater(
     @PrimaryKey val id: Long,
     @ColumnInfo(name = "name") val name: String,
-)
+) {
+    companion object {
+        val STUB_A =
+            MovieTheater(
+                0L,
+                "잠실 극장",
+            )
+
+        val STUB_B =
+            MovieTheater(
+                1L,
+                "선릉 극장",
+            )
+
+        val STUB_C =
+            MovieTheater(
+                2L,
+                "강남강남강남강남강남강남강남강남강남강남강남강남강남강남강남강남강남강남강남강남강남강남 ",
+            )
+    }
+}
