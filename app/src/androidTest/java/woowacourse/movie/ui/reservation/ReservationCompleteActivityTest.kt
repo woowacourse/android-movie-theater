3af953b510e7bcb5b5771ed8721d4088149b22bd
haeum808 <haeum808@gmail.com>
2024-05-08 15:57:41 +0900

    refactor: 하드 코딩된 것들 더미 데이터로 변경

--- app/src/androidTest/java/woowacourse/movie/ui/reservation/ReservationCompleteActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/ui/reservation/ReservationCompleteActivityTest.kt
@@ -8,6 +8,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,6 +16,7 @@ import woowacourse.movie.R
 import woowacourse.movie.domain.model.DateTime
 import woowacourse.movie.domain.model.Grade
 import woowacourse.movie.domain.model.Position
+import woowacourse.movie.domain.model.Reservation
 import woowacourse.movie.domain.model.Screen
 import woowacourse.movie.domain.model.Seat
 import woowacourse.movie.domain.model.Seats
@@ -24,53 +26,107 @@ import java.time.LocalTime
 
 @RunWith(AndroidJUnit4::class)
 class ReservationCompleteActivityTest {
+    private lateinit var dummyReservation: Reservation
+
     @get:Rule
     val activityRule: ActivityScenarioRule<ReservationCompleteActivity> =
         ActivityScenarioRule<ReservationCompleteActivity>(
-            Intent(ApplicationProvider.getApplicationContext(), ReservationCompleteActivity::class.java).apply {
+            Intent(
+                ApplicationProvider.getApplicationContext(),
+                ReservationCompleteActivity::class.java,
+            ).apply {
                 putExtra("reservationId", testFixtureReservationId())
                 putExtra("theaterId", 1)
             },
         )
 
+    @Before
+    fun setup() {
+        dummyReservation = DummyReservation.findById(1).getOrThrow()
+    }
+
     @Test
     fun `예약한_영화의_제목을_표시한다`() {
-        onView(withId(R.id.tv_reservation_title)).check(matches(withText("")))
+        val movieTitle = dummyReservation.screen.movie.title
+
+        onView(withId(R.id.tv_reservation_title)).check(matches(withText(movieTitle)))
     }
 
     @Test
-    fun `예약한_영화의_러닝타임을_표시한다`() {
-        onView(withId(R.id.tv_reservation_date)).check(matches(withText("2024-03-01")))
-    }
+    fun `예약_날짜를_보여준다`() {
+        val date = dummyReservation.dateTime?.date
 
-    @Test
-    fun `상영_영화_예매_수를_표시한다`() {
-        onView(withId(R.id.tv_reservation_count)).check(matches(withText("일반 2명 |")))
-    }
-
-    @Test
-    fun `상영_영화_예매_가격을_표시한다`() {
-        onView(withId(R.id.tv_reservation_amount)).check(matches(withText("27,000원(현장 결제)")))
-    }
-
-    @Test
-    fun `예약_좌석을_보여준다`() {
-        onView(withId(R.id.tv_reservation_seats)).check(matches(withText("A1,B2 |")))
+        onView(withId(R.id.tv_reservation_date)).check(matches(withText(date.toString())))
     }
 
     @Test
     fun `예약_시각을_보여준다`() {
-        onView(withId(R.id.tv_reservation_time)).check(matches(withText("10:00")))
+        val time = dummyReservation.dateTime?.time
+
+        onView(withId(R.id.tv_reservation_time)).check(matches(withText(time.toString())))
+    }
+
+    @Test
+    fun `상영_영화_예매_수를_표시한다`() {
+        val ticketCount = dummyReservation.ticket.count
+
+        onView(withId(R.id.tv_reservation_count)).check(
+            matches(
+                withText(
+                    ("일반 %d명 |").format(
+                        ticketCount,
+                    ),
+                ),
+            ),
+        )
+    }
+
+    @Test
+    fun `상영_영화_예매_가격을_표시한다`() {
+        val totalPrice = dummyReservation.seats.totalPrice()
+
+        onView(withId(R.id.tv_reservation_amount)).check(
+            matches(
+                withText(
+                    ("%s(현장 결제)").format(
+                        totalPrice,
+                    ),
+                ),
+            ),
+        )
+    }
+
+    @Test
+    fun `예약_좌석을_보여준다`() {
+        val seats = dummyReservation.seats.seats
+
+        onView(withId(R.id.tv_reservation_seats)).check(
+            matches(
+                withText(
+                    seats.joinToString(
+                        separator = ",",
+                        postfix = " |",
+                    ) { "${'A' + it.position.row}${it.position.col + 1}" },
+                ),
+            ),
+        )
+    }
+
+    companion object {
+        val screen = Screen.NULL
+        val seats =
+            Seats(
+                Seat(Position(0, 0), Grade.S),
+                Seat(Position(1, 1), Grade.A),
+            )
+        val dateTime = DateTime(LocalDate.of(2024, 3, 1), LocalTime.of(10, 0))
     }
 
     private fun testFixtureReservationId(): Int {
         return DummyReservation.save(
-            Screen.NULL,
-            Seats(
-                Seat(Position(0, 0), Grade.S),
-                Seat(Position(1, 1), Grade.A),
-            ),
-            DateTime(LocalDate.of(2024, 3, 1), LocalTime.of(10, 0)),
+            screen,
+            seats,
+            dateTime,
         ).getOrThrow()
     }
 }
