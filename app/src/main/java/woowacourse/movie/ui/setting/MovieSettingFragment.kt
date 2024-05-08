15c431ec4e632c3e622704304a7c91fa0b808e9a
Namyunsuk <yunsuk0328@naver.com>
2024-05-08 20:08:08 +0900

    feat: 푸시 알림 ui 및 이벤트 리스너 설정

--- app/src/main/java/woowacourse/movie/ui/setting/MovieSettingFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/setting/MovieSettingFragment.kt
@@ -1,18 +1,55 @@
 package woowacourse.movie.ui.setting
 
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentMovieSettingBinding
 
 class MovieSettingFragment : Fragment() {
+    private lateinit var binding: FragmentMovieSettingBinding
+    private val sharedPreference: SharedPreferences by lazy { initializeSharedPreference() }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movie_setting, container, false)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_movie_setting,
+                container,
+                false,
+            )
+
+        binding.swAlarmStatus.isChecked = getAlarmChecked()
+        binding.swAlarmStatus.setOnCheckedChangeListener { _, isChecked ->
+            setAlarmChecked(isChecked)
+        }
+
+        return binding.root
+    }
+
+    private fun initializeSharedPreference(): SharedPreferences =
+        activity?.getSharedPreferences("settings", MODE_PRIVATE)
+            ?: throw IllegalStateException()
+
+
+    private fun setAlarmChecked(isChecked: Boolean) {
+        with(sharedPreference.edit()) {
+            putBoolean("settings", isChecked)
+            apply()
+        }
+    }
+
+    private fun getAlarmChecked(): Boolean {
+        return sharedPreference.getBoolean("settings", false)
     }
 }
