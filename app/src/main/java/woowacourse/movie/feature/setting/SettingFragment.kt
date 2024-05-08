22faab8e4c6a520bbaf070b8e1a3afabf2fb421b
kimhyemin <kimhm0728@naver.com>
2024-05-08 14:21:05 +0900

    feat: 알림 권한을 요청하는 기능 구현

--- app/src/main/java/woowacourse/movie/feature/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/feature/setting/SettingFragment.kt
@@ -1,15 +1,25 @@
 package woowacourse.movie.feature.setting
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import woowacourse.movie.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +28,43 @@ class SettingFragment : Fragment() {
     ): View {
         _binding = FragmentSettingBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        requestNotificationPermission()
+    }
+
+    private fun requestNotificationPermission() {
+        if (checkRequestNotificationPermission()) {
+            binding.layoutNotificationSwitch.visibility = View.GONE
+            binding.layoutNotificationSetting.visibility = View.VISIBLE
+            binding.btnPermission.setOnClickListener {
+                val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                intent.putExtra(Settings.EXTRA_APP_PACKAGE, requireContext().packageName)
+                startActivity(intent)
+            }
+
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            return
+        }
+
+        binding.layoutNotificationSwitch.visibility = View.VISIBLE
+        binding.layoutNotificationSetting.visibility = View.GONE
+    }
+
+    private fun checkRequestNotificationPermission(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            return false
+        }
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return false
+        }
+        return true
     }
 
     override fun onDestroyView() {
