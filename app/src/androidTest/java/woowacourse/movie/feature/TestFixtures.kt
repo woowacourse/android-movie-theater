d07c288af538a18539280a7a4b23b6c404dd8a69
kimhyemin <kimhm0728@naver.com>
2024-05-09 00:54:39 +0900

    test: 예매 내역 fragment 테스트 작성

--- app/src/androidTest/java/woowacourse/movie/feature/TestFixtures.kt
+++ app/src/androidTest/java/woowacourse/movie/feature/TestFixtures.kt
@@ -1,3 +1,22 @@
 package woowacourse.movie.feature
 
+import woowacourse.movie.data.entity.Ticket
+import woowacourse.movie.model.MovieSeat
+import woowacourse.movie.model.MovieSelectedSeats
+import java.time.LocalDate
+import java.time.LocalTime
+
 val firstMovieId = 0L
+
+val movieId = 0L
+val screeningDate = LocalDate.of(2024, 4, 1)
+val screeningTime = LocalTime.of(12, 30)
+val selectedSeats =
+    MovieSelectedSeats(3).apply {
+        selectSeat(MovieSeat(1, 1))
+        selectSeat(MovieSeat(2, 2))
+        selectSeat(MovieSeat(3, 3))
+    }
+val theaterName = "선릉"
+
+val ticket = Ticket(0L, movieId, screeningDate, screeningTime, selectedSeats, theaterName)
--- app/src/main/java/woowacourse/movie/MovieTheaterApplication.kt
+++ app/src/main/java/woowacourse/movie/MovieTheaterApplication.kt
@@ -3,8 +3,10 @@ package woowacourse.movie
 import android.app.Application
 import woowacourse.movie.data.TicketDatabase
 import woowacourse.movie.data.TicketRepositoryImpl
+import woowacourse.movie.util.SharedPreferencesManager
 
 class MovieTheaterApplication : Application() {
-    val ticketDatabase by lazy { TicketDatabase.getInstance(this) }
+    private val ticketDatabase by lazy { TicketDatabase.getInstance(this) }
     val ticketRepository by lazy { TicketRepositoryImpl(ticketDatabase.ticketDao()) }
+    val sharedPreferencesManager by lazy { SharedPreferencesManager(this) }
 }
