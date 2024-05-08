6b49329e88b73267b3715fe59d577a3d4c2ffe42
haeum808 <haeum808@gmail.com>
2024-05-08 18:21:07 +0900

    feat: ReservationHistory 엔티티 구현

---
+++ app/src/main/java/woowacourse/movie/db/ReservationHistory.kt
@@ -0,0 +1,19 @@
+package woowacourse.movie.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import woowacourse.movie.domain.model.Reservation
+import woowacourse.movie.domain.model.Theater
+
+@Entity(tableName = "reservationHistory")
+data class ReservationHistory(
+    @PrimaryKey
+    val id: Int,
+
+    @ColumnInfo(name = "reservation")
+    val reservation: Reservation,
+
+    @ColumnInfo(name = "theater")
+    val theater: Theater,
+)
