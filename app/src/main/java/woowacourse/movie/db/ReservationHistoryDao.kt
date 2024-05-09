6e392e351f54a6bda24e0e3771d39bbcfcad722b
haeum808 <haeum808@gmail.com>
2024-05-09 16:12:03 +0900

    feat: ReservationHistoryDao Insert에 onConflict 옵션 추가

--- app/src/main/java/woowacourse/movie/db/ReservationHistoryDao.kt
+++ app/src/main/java/woowacourse/movie/db/ReservationHistoryDao.kt
@@ -2,6 +2,7 @@ package woowacourse.movie.db
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
@@ -12,6 +13,6 @@ interface ReservationHistoryDao {
     @Query("SELECT * FROM reservationHistory WHERE id = :id")
     fun getById(id: Int): ReservationHistory
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(reservationHistory: ReservationHistory)
 }
