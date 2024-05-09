53745dfb9945e24fc0318b350f7cbf2ce19404d4
gaeun <geun5744@gmail.com>
2024-05-09 11:22:12 +0900

    refactor: 데이터베이스 하나로 통일

--- app/src/androidTest/java/woowacourse/movie/database/MovieDatabaseTest.kt
+++ app/src/androidTest/java/woowacourse/movie/database/MovieDatabaseTest.kt
@@ -5,14 +5,22 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import woowacourse.movie.data.dao.MovieDao
 import woowacourse.movie.data.dao.MovieReservationDao
+import woowacourse.movie.data.dao.MovieTheaterDao
 import woowacourse.movie.data.database.MovieReservationDatabase
+import woowacourse.movie.data.entity.Movie
 import woowacourse.movie.data.entity.MovieReservation
+import woowacourse.movie.data.entity.MovieTheater
 import woowacourse.movie.fixtures.context
 import java.io.IOException
 
-class MovieReservationTest {
+class MovieDatabaseTest {
     private lateinit var movieReservationDao: MovieReservationDao
+    private lateinit var movieDao: MovieDao
+
+    private lateinit var theaterDao: MovieTheaterDao
+
     private lateinit var db: MovieReservationDatabase
 
     @Before
@@ -21,7 +29,12 @@ class MovieReservationTest {
             Room.inMemoryDatabaseBuilder(
                 context, MovieReservationDatabase::class.java,
             ).build()
+        movieDao = db.movieDao()
+        theaterDao = db.movieTheaterDao()
         movieReservationDao = db.movieReservationDao()
+
+        movieDao.insert(Movie.STUB)
+        theaterDao.insertAll(MovieTheater.STUB_A, MovieTheater.STUB_B, MovieTheater.STUB_C)
     }
 
     @After
--- app/src/main/java/woowacourse/movie/data/database/MovieReservationDatabase.kt
+++ app/src/main/java/woowacourse/movie/data/database/MovieReservationDatabase.kt
@@ -5,13 +5,23 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import woowacourse.movie.data.converter.LocalDateTimeConverters
 import woowacourse.movie.data.converter.ReservationSeatsConverters
+import woowacourse.movie.data.dao.MovieDao
 import woowacourse.movie.data.dao.MovieReservationDao
+import woowacourse.movie.data.dao.MovieTheaterDao
+import woowacourse.movie.data.dao.ScreeningMovieDao
 import woowacourse.movie.data.entity.Movie
 import woowacourse.movie.data.entity.MovieReservation
 import woowacourse.movie.data.entity.MovieTheater
+import woowacourse.movie.data.entity.ScreeningMovie
 
-@Database(entities = [MovieReservation::class, MovieTheater::class, Movie::class], version = 1)
+@Database(entities = [MovieReservation::class, MovieTheater::class, Movie::class, ScreeningMovie::class], version = 1)
 @TypeConverters(value = [ReservationSeatsConverters::class, LocalDateTimeConverters::class, ReservationSeatsConverters::class])
 abstract class MovieReservationDatabase : RoomDatabase() {
+    abstract fun movieDao(): MovieDao
+
+    abstract fun movieTheaterDao(): MovieTheaterDao
+
     abstract fun movieReservationDao(): MovieReservationDao
+
+    abstract fun screeningMovieDao(): ScreeningMovieDao
 }
