b0d86a0484055a7a2648271e8bd2f100d179033a
Junyoung-WON <wjyo60@gmail.com>
2024-05-09 14:37:35 +0900

    refactor: Parcelize 어노테이션 적용

--- app/src/main/java/woowacourse/movie/presentation/uimodel/MovieTicketUiModel.kt
+++ app/src/main/java/woowacourse/movie/presentation/uimodel/MovieTicketUiModel.kt
@@ -1,13 +1,14 @@
 package woowacourse.movie.presentation.uimodel
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import woowacourse.movie.domain.model.reservation.MovieTicket
 import woowacourse.movie.presentation.view.reservation.seat.SeatSelectionActivity.Companion.SEAT_COL_START_VALUE
 import woowacourse.movie.presentation.view.reservation.seat.SeatSelectionActivity.Companion.SEAT_POSITION_TEXT_FORMAT
 import woowacourse.movie.presentation.view.reservation.seat.SeatSelectionActivity.Companion.SEAT_ROW_START_VALUE
 import java.time.format.DateTimeFormatter
 
+@Parcelize
 data class MovieTicketUiModel(
     val ticketId: Long,
     val title: String,
@@ -20,39 +21,6 @@ data class MovieTicketUiModel(
     val selectedSeats: List<String>,
     val theaterName: String,
 ) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readLong(),
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readString() ?: "",
-        parcel.readInt(),
-        parcel.readInt(),
-        parcel.readInt(),
-        parcel.createStringArrayList() ?: listOf(),
-        parcel.readString() ?: "",
-    )
-
-    override fun writeToParcel(
-        parcel: Parcel,
-        flags: Int,
-    ) {
-        parcel.writeLong(ticketId)
-        parcel.writeString(title)
-        parcel.writeString(screeningDate)
-        parcel.writeString(startTime)
-        parcel.writeString(endTime)
-        parcel.writeInt(runningTime)
-        parcel.writeInt(reservationCount)
-        parcel.writeInt(totalPrice)
-        parcel.writeStringList(selectedSeats)
-        parcel.writeString(theaterName)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
     constructor(movieTicket: MovieTicket) : this(
         movieTicket.ticketId,
         movieTicket.reservationMovieInfo.title,
@@ -88,16 +56,36 @@ data class MovieTicketUiModel(
 
     fun totalPrice(): String = "${String.format("%,d", totalPrice)}원 (현장 결제)"
 
-    companion object CREATOR : Parcelable.Creator<MovieTicketUiModel> {
+    companion object {
         val DEFAULT_DATE_FORMAT: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE
         const val DEFAULT_TIME_FORMAT = "HH:mm"
 
-        override fun createFromParcel(parcel: Parcel): MovieTicketUiModel {
-            return MovieTicketUiModel(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MovieTicketUiModel?> {
-            return arrayOfNulls(size)
+        fun from(movieTicket: MovieTicket): MovieTicketUiModel {
+            return MovieTicketUiModel(
+                movieTicket.ticketId,
+                movieTicket.reservationMovieInfo.title,
+                movieTicket.reservationMovieInfo.dateTime.screeningDate.date.format(
+                    DEFAULT_DATE_FORMAT
+                ),
+                movieTicket.reservationMovieInfo.dateTime.screeningDate.screeningTime.startTime.format(
+                    DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT),
+                ),
+                movieTicket.reservationMovieInfo.dateTime.screeningDate.screeningTime.getEndTime()
+                    .format(
+                        DateTimeFormatter.ofPattern(DEFAULT_TIME_FORMAT),
+                    ),
+                movieTicket.reservationMovieInfo.dateTime.screeningInfo.runningTime,
+                movieTicket.reservationInfo.reservationCount,
+                movieTicket.reservationInfo.selectedSeats.totalPrice(),
+                movieTicket.reservationInfo.selectedSeats.seats.map { seat ->
+                    String.format(
+                        SEAT_POSITION_TEXT_FORMAT,
+                        SEAT_ROW_START_VALUE + seat.row,
+                        SEAT_COL_START_VALUE + seat.col,
+                    )
+                },
+                movieTicket.reservationMovieInfo.theaterName,
+            )
         }
     }
 }
