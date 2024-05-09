f453cde1dd58f2426f66e095d733627d0076d389
aprilgom <aprilgom@gmail.com>
2024-05-09 15:51:02 +0900

    feat(FetchScreeningsWithMovieIdAndTheaterIdUseCase): MovieId와 Theater 실제 Screening들의 리스트를 찾는 유즈케이스 구현

---
+++ domain/src/main/java/woowacourse/movie/usecase/FetchScreeningsWithMovieIdAndTheaterIdUseCase.kt
@@ -0,0 +1,31 @@
+package woowacourse.movie.usecase
+
+import woowacourse.movie.model.Screening
+import woowacourse.movie.repository.MovieRepository
+import woowacourse.movie.repository.ScreeningRefRepository
+import woowacourse.movie.repository.TheaterRepository
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+class FetchScreeningsWithMovieIdAndTheaterIdUseCase(
+    private val movieRepository: MovieRepository,
+    private val theaterRepository: TheaterRepository,
+    private val screeningRefRepository: ScreeningRefRepository,
+) {
+    operator fun invoke(
+        movieId: Long,
+        theaterId: Long,
+    ): Result<List<Screening>> {
+        return runCatching {
+            val screeningRefs =
+                screeningRefRepository.screeningRefsByMovieIdAndTheaterId(movieId, theaterId)
+            screeningRefs.map {
+                val movie = movieRepository.movieById(it.movieId) ?: throw error("")
+                val theater = theaterRepository.theaterById(it.theaterId) ?: throw error("")
+                val localDateTime =
+                    LocalDateTime.ofEpochSecond(it.dateTimeStamp, 0, ZoneOffset.UTC)
+                Screening(it.id, movie, theater, localDateTime)
+            }
+        }
+    }
+}
