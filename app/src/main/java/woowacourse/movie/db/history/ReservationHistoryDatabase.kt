72aaf50bd57f0da164cf7fdfd1e1831178832045
김진우 <jinwoo3661@naver.com>
2024-05-10 01:16:19 +0900

    refactor: db 수정에 따른 Migration 추가

--- app/src/main/java/woowacourse/movie/db/history/ReservationHistoryDatabase.kt
+++ app/src/main/java/woowacourse/movie/db/history/ReservationHistoryDatabase.kt
@@ -5,6 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import woowacourse.movie.model.ticket.ReservationTicket
 
 @Database(
@@ -19,6 +21,15 @@ abstract class ReservationHistoryDatabase : RoomDatabase() {
 
     companion object {
         private var instance: ReservationHistoryDatabase? = null
+        private val migration_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE reservationTicket RENAME TO temp_reservationTicket")
+                db.execSQL("CREATE TABLE IF NOT EXISTS `reservationTicket` (ticketId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, movieId INTEGER NOT NULL, theaterId INTEGER NOT NULL, movieTitle TEXT NOT NULL, theaterName TEXT NOT NULL, seatSelection TEXT NOT NULL, screeningDateTime TEXT NOT NULL, amount INTEGER NOT NULL)")
+                db.execSQL("INSERT INTO reservationTicket (ticketId, movieId, theaterId, movieTitle, theaterName, seatSelection, screeningDateTime, amount) SELECT ticketId, movieId, theaterId, movieTitle, theaterName, seats, screeningDateTime, amount FROM temp_reservationTicket")
+                db.execSQL("DROP TABLE temp_reservationTicket")
+            }
+
+        }
 
         @Synchronized
         fun getInstance(context: Context): ReservationHistoryDatabase {
@@ -28,7 +39,7 @@ abstract class ReservationHistoryDatabase : RoomDatabase() {
                         context.applicationContext,
                         ReservationHistoryDatabase::class.java,
                         "reservationTicket",
-                    ).build()
+                    ).addMigrations(migration_1_2).build()
                 }
         }
     }
