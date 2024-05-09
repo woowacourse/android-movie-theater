e8ad7a100dc301199a77a71613cfe27f13e27ce2
haeum808 <haeum808@gmail.com>
2024-05-09 16:12:58 +0900

    feat: LocalDate, LocalTime, String 타입으로 변경

--- app/src/main/java/woowacourse/movie/db/ReservationHistory.kt
+++ app/src/main/java/woowacourse/movie/db/ReservationHistory.kt
@@ -3,15 +3,17 @@ package woowacourse.movie.db
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import woowacourse.movie.domain.model.Reservation
-import woowacourse.movie.domain.model.Theater
 
 @Entity(tableName = "reservationHistory")
 data class ReservationHistory(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-    @ColumnInfo(name = "reservation")
-    val reservation: Reservation,
-    @ColumnInfo(name = "theater")
-    val theater: Theater,
+    @ColumnInfo(name = "screeningDate")
+    val screeningDate: String,
+    @ColumnInfo(name = "screeningTime")
+    val screeningTime: String,
+    @ColumnInfo(name = "theaterName")
+    val theaterName: String,
+    @ColumnInfo(name = "movieName")
+    val movieName: String,
 )
