b7c77ecc71f0c694bfdbc6af10faa42651d4df45
gaeun <geun5744@gmail.com>
2024-05-09 14:59:02 +0900

    feat: 직렬화, 역직렬화를 위한 adapter 추가

--- app/src/main/java/woowacourse/movie/data/converter/ScreenDateTimeConverters.kt
+++ app/src/main/java/woowacourse/movie/data/converter/ScreenDateTimeConverters.kt
@@ -1,14 +1,18 @@
 package woowacourse.movie.data.converter
 
 import androidx.room.TypeConverter
-import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
 import java.time.LocalDate
 import java.time.LocalTime
 
 class ScreenDateTimeConverters {
-    private val gson = Gson()
+    private val gson =
+        GsonBuilder()
+            .registerTypeAdapter(LocalDate::class.java, LocalDateAdapter())
+            .registerTypeAdapter(LocalTime::class.java, LocalTimeAdapter())
+            .create()
 
     @TypeConverter
     fun fromScreenDateTimes(value: List<Map<LocalDate, List<LocalTime>>>?): String? {
