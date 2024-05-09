723c9947deb7164b7849130f466f014bd69e899f
JoYehyun <jo990714@naver.com>
2024-05-09 16:18:18 +0900

    feat: 알림 기능 설정 데이터 저장 기능

--- app/src/main/java/woowacourse/movie/presentation/homefragments/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/homefragments/setting/SettingFragment.kt
@@ -1,5 +1,6 @@
 package woowacourse.movie.presentation.homefragments.setting
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +8,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import woowacourse.movie.databinding.FragmentSettingBinding
 
-class SettingFragment : Fragment() {
+class SettingFragment : Fragment(), SettingContract.View {
     private var _binding: FragmentSettingBinding? = null
     val binding get() = _binding!!
+    private val presenter by lazy { SettingPresenter(this) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,5 +27,28 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+        val sharedPref = activity?.getPreferences(MODE_PRIVATE)
+        val savedValue = sharedPref?.getBoolean(SHARED_PREF_KEY_NOTIFICATION, false)
+        presenter.loadNotificationState(savedValue)
+
+        binding.switchSetting.setOnCheckedChangeListener { _, isChecked ->
+            saveNotificationState(isChecked)
+        }
+    }
+
+    private fun saveNotificationState(state: Boolean) {
+        val sharedPref = activity?.getPreferences(MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putBoolean(SHARED_PREF_KEY_NOTIFICATION, state)
+            apply()
+        }
+    }
+
+    override fun setNotificationState(isChecked: Boolean) {
+        binding.switchSetting.isChecked = isChecked
+    }
+
+    companion object {
+        const val SHARED_PREF_KEY_NOTIFICATION = "notification"
     }
 }
