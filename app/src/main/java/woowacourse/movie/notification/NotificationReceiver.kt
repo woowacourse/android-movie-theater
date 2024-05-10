7cdc939ead5d82bf82d0d4f6b4736cc4220fa0e0
ii2001 <iivks2001@gmail.com>
2024-05-10 15:19:26 +0900

    :recycle: 브로드 캐스트 함수 분리

--- app/src/main/java/woowacourse/movie/notification/NotificationReceiver.kt
+++ app/src/main/java/woowacourse/movie/notification/NotificationReceiver.kt
@@ -14,27 +14,47 @@ class NotificationReceiver : BroadcastReceiver() {
         val notificationId = intent.getIntExtra("notificationId", 0)
         val ticketId = intent.getIntExtra("ticketId", 0)
 
-        val notificationIntent = Intent(context, PurchaseConfirmationActivity::class.java).apply {
+        val notificationIntent = createNotificationIntent(context, ticketId)
+        val pendingIntent = createPendingIntent(context, notificationIntent)
+        val notification = buildNotification(context, pendingIntent)
+        notifyNotification(context, notificationId, notification)
+    }
+
+    private fun createNotificationIntent(context: Context, ticketId: Int): Intent {
+        return Intent(context, PurchaseConfirmationActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             putExtra("ticketId", ticketId)
         }
+    }
 
-        val pendingIntent = PendingIntent.getActivity(
+    private fun createPendingIntent(context: Context, intent: Intent): PendingIntent {
+        return PendingIntent.getActivity(
             context,
             0,
-            notificationIntent,
-            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
+    }
 
-        val notificationBuilder = NotificationCompat.Builder(context, "ticket_confirmation_channel")
+    private fun buildNotification(
+        context: Context,
+        pendingIntent: PendingIntent
+    ): NotificationCompat.Builder {
+        return NotificationCompat.Builder(context, "ticket_confirmation_channel")
             .setSmallIcon(R.drawable.ic_home_check)
             .setContentTitle("영화 알림")
             .setContentText("영화 예매가 완료되었습니다. 자세한 정보를 확인하려면 여기를 탭하세요.")
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setContentIntent(pendingIntent)
             .setAutoCancel(true)
+    }
 
+    private fun notifyNotification(
+        context: Context,
+        notificationId: Int,
+        builder: NotificationCompat.Builder
+    ) {
         val notificationManager = NotificationManagerCompat.from(context)
-        notificationManager.notify(notificationId, notificationBuilder.build())
+        notificationManager.notify(notificationId, builder.build())
     }
 }
