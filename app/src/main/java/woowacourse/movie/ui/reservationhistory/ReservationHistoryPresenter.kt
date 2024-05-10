e5d4f2f99e0b7cad751113c01f1c35e77f7a0f21
sh1mj1 <7wlgns@gmail.com>
2024-05-10 22:56:39 +0900

    feat(ReservationHistoryPresenter): 스레드와 핸들러 적용

--- app/src/main/java/woowacourse/movie/ui/reservationhistory/ReservationHistoryPresenter.kt
+++ app/src/main/java/woowacourse/movie/ui/reservationhistory/ReservationHistoryPresenter.kt
@@ -1,16 +1,27 @@
 package woowacourse.movie.ui.reservationhistory
 
+import android.os.Handler
+import android.os.Looper
 import woowacourse.movie.domain.repository.ReservationRepository
+import kotlin.concurrent.thread
 
 class ReservationHistoryPresenter(
     private val view: ReservationHistoryContract.View,
     private val reservationRepository: ReservationRepository,
 ) : ReservationHistoryContract.Presenter {
+    private val uiHandler = Handler(Looper.getMainLooper())
+
     override fun loadAllReservationHistory() {
-        reservationRepository.loadAllReservationHistory().onSuccess { reservations ->
-            view.showAllReservationHistory(reservations)
-        }.onFailure { e ->
-            view.showAllReservationHistoryError(e)
+        thread {
+            reservationRepository.loadAllReservationHistory().onSuccess { reservations ->
+                uiHandler.post {
+                    view.showAllReservationHistory(reservations)
+                }
+            }.onFailure { e ->
+                uiHandler.post {
+                    view.showAllReservationHistoryError(e)
+                }
+            }
         }
     }
 }
