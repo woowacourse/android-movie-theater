b61d21593d7687b1985c233620233314db842a7f
gaeun <geun5744@gmail.com>
2024-05-10 17:17:48 +0900

    feat: data init 로직 위치 변경

--- app/src/main/java/woowacourse/movie/TheaterApp.kt
+++ app/src/main/java/woowacourse/movie/TheaterApp.kt
@@ -2,7 +2,13 @@ package woowacourse.movie
 
 import android.app.Application
 import woowacourse.movie.data.RoomMovieRepository
+import woowacourse.movie.data.dao.MovieDao
+import woowacourse.movie.data.dao.MovieTheaterDao
+import woowacourse.movie.data.dao.ScreeningMovieDao
 import woowacourse.movie.data.database.MovieDatabase
+import woowacourse.movie.data.entity.Movie
+import woowacourse.movie.data.entity.MovieTheater
+import woowacourse.movie.data.entity.ScreeningMovie
 
 class TheaterApp : Application() {
     override fun onCreate() {
@@ -13,12 +19,37 @@ class TheaterApp : Application() {
     private fun initRoomRepo() {
         Thread {
             val database = MovieDatabase.database(this)
+            val movieDao = database.movieDao()
+            val movieTheaterDao = database.movieTheaterDao()
+            val screeningMovieDao = database.screeningMovieDao()
+            val reservationDao = database.movieReservationDao()
+
+            addInitData(movieDao, movieTheaterDao, screeningMovieDao)
+
             RoomMovieRepository.initialize(
-                database.movieDao(),
-                database.movieTheaterDao(),
-                database.screeningMovieDao(),
-                database.movieReservationDao(),
+                movieDao,
+                movieTheaterDao,
+                screeningMovieDao,
+                reservationDao,
             )
         }.start()
+    }
+
+    private fun addInitData(
+        movieDao: MovieDao,
+        movieTheaterDao: MovieTheaterDao,
+        screeningMovieDao: ScreeningMovieDao,
+    ) {
+        movieDao.insert(Movie.STUB)
+        movieTheaterDao.insertAll(
+            MovieTheater.STUB_A,
+            MovieTheater.STUB_B,
+            MovieTheater.STUB_C,
+        )
+        screeningMovieDao.insertAll(
+            ScreeningMovie.STUB_A,
+            ScreeningMovie.STUB_B,
+            ScreeningMovie.STUB_C,
+        )
     }
 }
