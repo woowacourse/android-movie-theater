2affc694251436b30020a1f377fc9bddd8acf1b9
김진우 <jinwoo3661@naver.com>
2024-05-10 17:30:01 +0900

    test: MockDao 생성, 데이터베이스 구현

---
+++ app/src/androidTest/java/woowacourse/movie/MockReservationHistoryDao.kt
@@ -0,0 +1,21 @@
+package woowacourse.movie
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import woowacourse.movie.db.history.ReservationHistoryDao
+import woowacourse.movie.model.ticket.ReservationTicket
+
+@Dao
+interface MockReservationHistoryDao : ReservationHistoryDao{
+
+    override fun saveReservationTicket(reservationTicket: ReservationTicket): Long
+
+    override fun findReservations(): List<ReservationTicket>
+
+    override fun findReservationById(ticketId: Long): ReservationTicket?
+
+    @Query("DELETE FROM reservationTicket")
+    fun clearReservations()
+
+}
---
+++ app/src/androidTest/java/woowacourse/movie/MockReservationHistoryDatabase.kt
@@ -0,0 +1,37 @@
+package woowacourse.movie
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import woowacourse.movie.db.history.ReservationHistoryConverters
+import woowacourse.movie.db.history.ReservationHistoryDatabase
+import woowacourse.movie.model.ticket.ReservationTicket
+
+@Database(
+    entities = [
+        ReservationTicket::class,
+    ],
+    version = 1,
+)
+@TypeConverters(ReservationHistoryConverters::class)
+abstract class MockReservationHistoryDatabase : RoomDatabase() {
+    abstract fun reservationHistoryDao(): MockReservationHistoryDao
+
+    companion object {
+        private var instance: MockReservationHistoryDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): MockReservationHistoryDatabase {
+            return instance
+                ?: synchronized(MockReservationHistoryDatabase::class) {
+                    Room.databaseBuilder(
+                        context.applicationContext,
+                        MockReservationHistoryDatabase::class.java,
+                        "mockReservationTicket",
+                    ).build()
+                }
+        }
+    }
+}
