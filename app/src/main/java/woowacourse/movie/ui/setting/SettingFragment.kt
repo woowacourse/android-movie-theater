c949ef42aae5d7c534d09d22eb2d13adbeb3d017
haeum808 <haeum808@gmail.com>
2024-05-10 17:19:34 +0900

    design: 설정 화면

--- app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
@@ -5,14 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
+    private var _binding: FragmentSettingBinding? = null
+    private val binding
+        get() = requireNotNull(_binding)
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
