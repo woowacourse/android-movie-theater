f77a0908c10364814573ba0c63f620078c719e93
songpink <jejeje04@naver.com>
2024-05-10 14:59:33 +0900

    feat: 알림 권한 요청 기능 구현

--- app/src/main/java/woowacourse/movie/list/view/HomeActivity.kt
+++ app/src/main/java/woowacourse/movie/list/view/HomeActivity.kt
@@ -1,7 +1,13 @@
 package woowacourse.movie.list.view
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
@@ -12,6 +18,12 @@ import woowacourse.movie.list.presenter.HomePresenter
 class HomeActivity : AppCompatActivity(), HomeContract.View {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var presenter: HomePresenter
+    private var isNotificationGranted: Boolean = false
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            Log.d("alsong", "isGranted: $isGranted")
+            isNotificationGranted = isGranted
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +32,7 @@ class HomeActivity : AppCompatActivity(), HomeContract.View {
         presenter = HomePresenter(this)
         setContentView(binding.root)
         initBottomNavigation()
+        requestNotificationPermission()
     }
 
     private fun initBottomNavigation() {
@@ -35,5 +48,30 @@ class HomeActivity : AppCompatActivity(), HomeContract.View {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .commitAllowingStateLoss()
+    }
+
+    private fun requestNotificationPermission() {
+        // 권한이 허용되지 않았으면 이 if문에 들어감
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("alsong", "A")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                Log.d("alsong", "B")
+                if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                    // 이전에 권한 요청 거부했다면 이쪽으로 빠짐
+                    Log.d("alsong", "C")
+                } else {
+                    // 처음에 권한 요청을 할 때에는 무조건 이쪽으로 빠짐
+
+                    // 이녀석이 알림을 요청하는 녀석. 동시에 requestPermissionLauncher의 람다함수를 실행한다.
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                    Log.d("alsong", "D")
+                }
+            } else {
+                // 안드로이드 12 이하는 Notification에 관한 권한 필요 없음
+            }
+        }
     }
 }
