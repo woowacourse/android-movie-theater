d818e23cb4da8f389f637ba39873ba16c0f77c9d
kkosang <eiwoqp77@gmail.com>
2024-05-11 14:39:18 +0900

    test: 예매 티켓 Dao 테스트

---
+++ app/src/androidTest/java/woowacourse/movie/repository/db/TicketTestFixture.kt
@@ -0,0 +1,24 @@
+package woowacourse.movie.repository.db
+
+import woowacourse.movie.domain.model.reservation.seat.SeatingChart
+import woowacourse.movie.domain.model.reservation.seat.SelectedSeats
+
+fun ticket(
+    ticketId: Long = 0,
+    movieTitle: String = "해리포터",
+    theaterName: String = "선릉",
+    screenDate: String = "2024-05-01",
+    screenTime: String = "22:00",
+    seats: SelectedSeats = SelectedSeats(
+        SeatingChart(4, 4), 1,
+    )
+): ReservationTicketEntity {
+    return ReservationTicketEntity(
+        ticketId,
+        movieTitle,
+        theaterName,
+        screenDate,
+        screenTime,
+        seats,
+    )
+}
