06e48583b186902f341e29b4ebadfc66c4ef4afb
sh1mj1 <7wlgns@gmail.com>
2024-05-11 23:59:15 +0900

    feat(SettingFragment): push 알림 설정

---
+++ app/src/main/java/woowacourse/movie/preference/AppPreference.kt
@@ -0,0 +1,6 @@
+package woowacourse.movie.preference
+
+object AppPreference {
+    const val APP_SETTINGS_PREFS = "app_settings_prefs"
+    const val NOTIFICATION_ENABLED = "notification_enabled"
+}
--- app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
@@ -1,11 +1,14 @@
 package woowacourse.movie.ui.setting
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import woowacourse.movie.databinding.FragmentSettingBinding
+import woowacourse.movie.preference.AppPreference
 
 class SettingFragment : Fragment() {
     private var _binding: FragmentSettingBinding? = null
@@ -20,8 +23,54 @@ class SettingFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showInitialPushNotification()
+        initChangePushNotificationListener()
+    }
+
+    private fun initChangePushNotificationListener() {
+        binding.switchPushAlarm.setOnCheckedChangeListener { _, isChecked ->
+            val sharedPreferences =
+                requireContext().applicationContext.getSharedPreferences(
+                    AppPreference.APP_SETTINGS_PREFS,
+                    Context.MODE_PRIVATE,
+                )
+
+            sharedPreferences.edit().putBoolean(AppPreference.NOTIFICATION_ENABLED, isChecked).apply()
+            Log.d(
+                TAG,
+                "isNotificationEnabled: ${sharedPreferences.getBoolean(AppPreference.NOTIFICATION_ENABLED, false)}",
+            )
+        }
+    }
+
+    private fun showInitialPushNotification() {
+        val sharedPreferences =
+            requireContext().applicationContext.getSharedPreferences(
+                AppPreference.APP_SETTINGS_PREFS,
+                Context.MODE_PRIVATE,
+            )
+        val pushNotificationsEnabled = sharedPreferences.getBoolean(AppPreference.NOTIFICATION_ENABLED, false)
+        binding.pushNotificationEnabled = pushNotificationsEnabled
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    companion object {
+        private const val TAG = "SettingFragment"
     }
 }
