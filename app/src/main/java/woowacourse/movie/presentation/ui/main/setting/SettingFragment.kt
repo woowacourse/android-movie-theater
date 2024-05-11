e3ab3b26969507d52df4fabfe9cbed892ad39435
JunJangE <wnswkd486@gmail.com>
2024-05-11 22:11:24 +0900

    refactor: 알림 권한 거부일 때 알림 모드 true로 변경시 알림 권한 요청

--- app/src/main/java/woowacourse/movie/presentation/ui/main/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/ui/main/setting/SettingFragment.kt
@@ -1,9 +1,18 @@
 package woowacourse.movie.presentation.ui.main.setting
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.StringRes
+import androidx.core.app.ActivityCompat
 import woowacourse.movie.R
 import woowacourse.movie.data.repository.local.PreferenceRepositoryImpl
 import woowacourse.movie.databinding.FragmentSettingBinding
 import woowacourse.movie.presentation.base.BaseMvpBindingFragment
+import woowacourse.movie.presentation.permision.Permission
+import woowacourse.movie.presentation.permision.Permission.toPermissionText
 import woowacourse.movie.presentation.ui.main.setting.SettingContract.View
 
 class SettingFragment : BaseMvpBindingFragment<FragmentSettingBinding>(), View {
@@ -20,6 +29,60 @@ class SettingFragment : BaseMvpBindingFragment<FragmentSettingBinding>(), View {
     override fun showNotificationMode(mode: Boolean) {
         requireActivity().runOnUiThread {
             binding.mode = mode
+        }
+    }
+
+    override fun checkNotificationPermissions(mode: Boolean) {
+        requestNotificationPermissions(mode, Permission.notificationPermissions.toList())
+    }
+
+    private fun requestNotificationPermissions(
+        mode: Boolean,
+        permissionList: List<String>,
+    ) {
+        val requestList = ArrayList<String>()
+        for (permission in permissionList) {
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    permission,
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestList.add(permission)
+            }
+            if (requestList.isNotEmpty()) {
+                requestMultiplePermissionsLauncher.launch(requestList.toTypedArray())
+                return
+            }
+        }
+        presenter.saveNotificationMode(mode)
+    }
+
+    private val requestMultiplePermissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (permissions.values.all { it }) {
+                presenter.loadNotificationMode()
+            } else {
+                presenter.loadNotificationMode()
+                val stringResId =
+                    permissions.entries.find { !it.value }?.key?.toPermissionText()
+                        ?: R.string.permission_default_text
+
+                showPermissionSnackBar(stringResId)
+            }
+            return@registerForActivityResult
+        }
+
+    private fun showPermissionSnackBar(
+        @StringRes stringResId: Int,
+    ) {
+        presenter.requestNotificationPermission(getString(stringResId)) {
+            setAction(getString(R.string.permission_done)) {
+                val intent =
+                    Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                        data = Uri.fromParts("package", requireContext().packageName, null)
+                    }
+                startActivity(intent)
+            }
         }
     }
 
--- app/src/main/java/woowacourse/movie/presentation/ui/main/setting/SettingPresenter.kt
+++ app/src/main/java/woowacourse/movie/presentation/ui/main/setting/SettingPresenter.kt
@@ -1,8 +1,10 @@
 package woowacourse.movie.presentation.ui.main.setting
 
+import com.google.android.material.snackbar.Snackbar
 import woowacourse.movie.domain.repository.PreferenceRepository
 import woowacourse.movie.presentation.model.message.NotificationMessageType.NotificationFailureMessage
 import woowacourse.movie.presentation.model.message.NotificationMessageType.NotificationSuccessMessage
+import woowacourse.movie.presentation.model.message.PermissionMessageType
 
 class SettingPresenter(
     private val view: SettingContract.View,
@@ -17,6 +19,10 @@ class SettingPresenter(
     }
 
     override fun changeNotificationMode(mode: Boolean) {
+        view.checkNotificationPermissions(!mode)
+    }
+
+    override fun saveNotificationMode(mode: Boolean) {
         repository.saveNotificationMode(mode).onSuccess {
             val message =
                 if (mode) {
@@ -24,9 +30,17 @@ class SettingPresenter(
                 } else {
                     NotificationFailureMessage
                 }
+            view.showNotificationMode(mode)
             view.showSnackBar(message)
         }.onFailure { e ->
             view.showSnackBar(e)
         }
+    }
+
+    override fun requestNotificationPermission(
+        permissionText: String,
+        action: Snackbar.() -> Snackbar,
+    ) {
+        view.showSnackBar(PermissionMessageType.RequestPermissionMessage(permissionText), action)
     }
 }
