03d5719ce6f933274030def9a8ba87524304dc5e
sh1mj1 <7wlgns@gmail.com>
2024-05-11 21:21:18 +0900

    feat(fragment_setting): 설정 화면 뷰를 만든다

--- app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
