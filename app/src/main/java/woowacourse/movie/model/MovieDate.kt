330e6a387b19b7c3a2fcf470b78fb73793441393
kimhyemin <kimhm0728@naver.com>
2024-05-11 17:20:01 +0900

    remove: 불필요한 코드 삭제

--- app/src/main/java/woowacourse/movie/model/MovieDate.kt
+++ app/src/main/java/woowacourse/movie/model/MovieDate.kt
@@ -1,6 +1,5 @@
 package woowacourse.movie.model
 
-import java.time.DayOfWeek
 import java.time.LocalDate
 
 class MovieDate(val startLocalDate: LocalDate, val endLocalDate: LocalDate) {
@@ -19,11 +18,6 @@ class MovieDate(val startLocalDate: LocalDate, val endLocalDate: LocalDate) {
     }
 
     companion object {
-        fun isWeekend(localDate: LocalDate): Boolean {
-            val day = localDate.dayOfWeek
-            return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY
-        }
-
         private const val INVALID_DATE_RANGE_MESSAGE = "영화 상영 날짜의 시작일은 종료일보다 이전이어야 합니다."
     }
 }
