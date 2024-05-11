0bc7467045cdec4657e5ca21f9d673eb85c1eaf7
murjune <sis9221302@gmail.com>
2024-05-12 00:09:24 +0900

    :sparkles: MovieRepositoryStore 구현
    - 동적으로 MovieRepository를 주입하기 위해 구현

--- app/src/main/java/woowacourse/movie/MovieReservationApp.kt
+++ app/src/main/java/woowacourse/movie/MovieReservationApp.kt
@@ -4,15 +4,13 @@ import android.app.Application
 import android.content.IntentFilter
 import androidx.core.content.ContextCompat
 import woowacourse.movie.MovieBroadCastReceiver.Companion.RESERVATION_NOTIFICATION_ACTION
-import woowacourse.movie.data.DefaultMovieRepository
 import woowacourse.movie.data.MovieRepository
 import woowacourse.movie.data.datastore.DefaultNotificationDataStore
-import woowacourse.movie.data.db.MovieDatabase
+import woowacourse.movie.data.MovieRepositoryStore
 
 class MovieReservationApp : Application() {
     val movieRepository: MovieRepository by lazy {
-        val db = MovieDatabase.instance(applicationContext)
-        DefaultMovieRepository(db.movieDao())
+        MovieRepositoryStore.instance(applicationContext).movieRepository()
     }
 
     val notificationDatastore by lazy {
