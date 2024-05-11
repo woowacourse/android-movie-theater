9671df45f3713baac278b859511b39e21616974b
songpink <jejeje04@naver.com>
2024-05-11 14:40:58 +0900

    refactor: 함수 분리

--- app/src/main/java/woowacourse/movie/alarm/MovieBroadcastReceiver.kt
+++ app/src/main/java/woowacourse/movie/alarm/MovieBroadcastReceiver.kt
@@ -15,13 +15,24 @@ import java.util.concurrent.CountDownLatch
 class MovieBroadcastReceiver : BroadcastReceiver() {
     private lateinit var tickets: List<DbTicket>
     override fun onReceive(context: Context, intent: Intent?) {
-        val ticketDb = TicketDatabase.getDatabase(context)
         val latch = CountDownLatch(1)
+        loadTicketsFromDb(context, latch)
+        latch.await()
+        makeNotification(context)
+    }
+
+    private fun loadTicketsFromDb(
+        context: Context,
+        latch: CountDownLatch
+    ) {
         Thread {
+            val ticketDb = TicketDatabase.getDatabase(context)
             tickets = ticketDb.ticketDao().getAll()
             latch.countDown()
         }.start()
-        latch.await()
+    }
+
+    private fun makeNotification(context: Context) {
         val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val builder: NotificationCompat.Builder
         val channelId = "one-channel"
@@ -29,15 +40,20 @@ class MovieBroadcastReceiver : BroadcastReceiver() {
         val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
         manager.createNotificationChannel(channel)
         builder = NotificationCompat.Builder(context, channelId)
-
-        val ticketIntent = MovieTicketActivity.newTicketActivityInstance(context, tickets, 1)
-        val pendingIntent = PendingIntent.getActivity(context, 10, ticketIntent, PendingIntent.FLAG_MUTABLE)
-
-        setBuilderInfo(builder, pendingIntent)
+        val pendingIntent = makePendingIntent(context)
+        setBuilder(builder, pendingIntent)
         manager.notify(1, builder.build())
     }
 
-    private fun setBuilderInfo(builder: NotificationCompat.Builder, pendingIntent: PendingIntent) {
+    private fun makePendingIntent(context: Context): PendingIntent? {
+        val ticketIntent = MovieTicketActivity.newTicketActivityInstance(context, tickets, 1)
+        return PendingIntent.getActivity(context, 10, ticketIntent, PendingIntent.FLAG_MUTABLE)
+    }
+
+    private fun setBuilder(
+        builder: NotificationCompat.Builder,
+        pendingIntent: PendingIntent?
+    ) {
         builder.setSmallIcon(android.R.drawable.ic_popup_reminder)
         builder.setContentTitle("예매 알림")
         builder.setContentText("30분 후에 상영")
