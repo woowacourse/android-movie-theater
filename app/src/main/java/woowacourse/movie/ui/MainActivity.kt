dca7266cf32cbacb571c2399414aa26d4687cba9
sh1mj1 <7wlgns@gmail.com>
2024-05-12 10:01:23 +0900

    feat(MainActivity): 권한을 요청한다
    - 처음에는 권한 dialog 로 묻기
    - 이미 이전에 거부한 적이 있다면 스낵바 띄우기
    - 스낵바가 설정 창으로 이동시킴
    
    - issue:
      메인에서 권한 허용 → settingfrag 이동 → 권한 해제 → 껐다가 켜기 →
      → 메인 액티비티 onResume: notificationPreference: false
      → Settingfrag: notificationEnabled: true
      메인 액티비티 onResume: notificationPreference: true
      문제 발생

--- app/src/main/java/woowacourse/movie/ui/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/ui/MainActivity.kt
@@ -1,12 +1,24 @@
 package woowacourse.movie.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
+import com.google.android.material.snackbar.Snackbar
 import woowacourse.movie.R
 import woowacourse.movie.databinding.ActivityMainBinding
+import woowacourse.movie.preference.NotificationPreference
+import woowacourse.movie.preference.NotificationSharedPreferences
 import woowacourse.movie.ui.home.HomeFragment
 import woowacourse.movie.ui.reservationhistory.ReservationHistoryFragment
 import woowacourse.movie.ui.setting.SettingFragment
@@ -18,13 +30,82 @@ class MainActivity : AppCompatActivity() {
     private val homeFragment: HomeFragment by lazy { HomeFragment() }
     private val settingFragment: SettingFragment by lazy { SettingFragment() }
 
+    private val notificationPreference: NotificationPreference by lazy {
+        NotificationSharedPreferences.getInstance(applicationContext)
+    }
+
+    /*
+        private val notificationPreference by lazy { (application as MovieReservationApp).notificationDatastore }
+
+     */
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                Log.d(TAG, "requestPermissionLauncher: granted")
+            } else {
+                Log.e(TAG, "requestPermissionLauncher: denied")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Log.d(TAG, "onCreate: ")
         if (savedInstanceState == null) {
             addFirstFragment(reservationHistoryFragment)
         }
+
         initBottomNavigationView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: notificationPreference: ${notificationPreference.loadNotificationPreference()}")
+        requestPermission()
+    }
+
+    private fun requestPermission() {
+        when {
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU -> return
+            needPostNotificationPermission() -> {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                    showPermissionSnackBar()
+                } else {
+                    requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+
+            else -> notificationPreference.saveNotificationPreference(true)
+        }
+    }
+
+    private fun needPostNotificationPermission() =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS,
+            ) != PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+
+    private fun showPermissionSnackBar() {
+        Snackbar.make(
+            binding.root,
+            "알림 권한이 필요합니다",
+            Snackbar.LENGTH_INDEFINITE,
+        ).setAction("확인") {
+            createdSettingsIntent()
+        }.show()
+    }
+
+    private fun createdSettingsIntent() {
+        val intent =
+            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                data = Uri.fromParts("package", packageName, null)
+            }
+        startActivity(intent)
     }
 
     private fun addFirstFragment(fragment: Fragment) {
@@ -52,5 +133,9 @@ class MainActivity : AppCompatActivity() {
             setReorderingAllowed(true)
             replace(R.id.fragment_container_view, fragment)
         }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
