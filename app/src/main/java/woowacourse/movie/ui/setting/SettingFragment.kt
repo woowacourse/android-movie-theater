e24904837f4a8c36bc9a7d5ab0dd53d363eba953
sh1mj1 <7wlgns@gmail.com>
2024-05-12 22:45:02 +0900

    chore(SettingFragment): 로그 제거, 메서드명 변경, 문자열 국제화

--- app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
@@ -5,14 +5,13 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import woowacourse.movie.MovieReservationApplication
+import woowacourse.movie.R
 import woowacourse.movie.databinding.FragmentSettingBinding
 import woowacourse.movie.preference.NotificationPreference
 
@@ -33,21 +32,17 @@ class SettingFragment : Fragment() {
         return binding.root
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
-        Log.d(TAG, "onViewCreated: notification enabled ${notificationPreference.loadNotificationPreference()}")
-        showInitialPushNotification()
+        showPushNotificationSetting()
         initChangePushNotificationListener()
     }
 
-    private fun showInitialPushNotification() {
-        val pushNotificationsEnabled = notificationPreference.loadNotificationPreference()
-        Log.d(TAG, "showInitialPushNotification: pushNoti is $pushNotificationsEnabled")
-        binding.pushNotificationEnabled = pushNotificationsEnabled
+    private fun showPushNotificationSetting() {
+        binding.pushNotificationEnabled = notificationPreference.loadNotificationPreference()
     }
 
     private fun initChangePushNotificationListener() {
@@ -55,30 +50,18 @@ class SettingFragment : Fragment() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 binding.pushNotificationEnabled = notificationPreference.loadNotificationPreference()
                 showPermissionChangingGuideSnackBar()
-            } else {
-                Log.d(TAG, "initChangePushNotificationListener: is less than TIRAMISU")
             }
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        showInitialPushNotification()
-        Log.d(TAG, "onResume: notification enabled ${notificationPreference.loadNotificationPreference()}")
-    }
-
     private fun showPermissionChangingGuideSnackBar() {
-        Log.d(TAG, "showPermissionSnackBar: called")
-        Snackbar.make(
-            binding.root,
-            "푸시 알림 권한 설정 창에서 변경해주세요.",
-            Snackbar.LENGTH_LONG,
-        ).setAction("확인") {
-            createdSettingsIntent()
-        }.show()
+        Snackbar.make(binding.root, R.string.push_notification_change_guide, Snackbar.LENGTH_LONG)
+            .setAction(R.string.ok) {
+                startApplicationDetailSettings()
+            }.show()
     }
 
-    private fun createdSettingsIntent() {
+    private fun startApplicationDetailSettings() {
         val intent =
             Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
                 addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -87,12 +70,13 @@ class SettingFragment : Fragment() {
         startActivity(intent)
     }
 
+    override fun onResume() {
+        super.onResume()
+        showPushNotificationSetting()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-        private const val TAG = "SettingFragment"
     }
 }
