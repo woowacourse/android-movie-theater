3c9b4844756e437ea958de30944dde62c3d5e478
sh1mj1 <7wlgns@gmail.com>
2024-05-12 22:33:44 +0900

    feat(SettingFragment): 푸시 알림 권한 변경을 안내하는 스낵바

--- app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/setting/SettingFragment.kt
@@ -1,21 +1,27 @@
 package woowacourse.movie.ui.setting
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import woowacourse.movie.MovieReservationApplication
 import woowacourse.movie.databinding.FragmentSettingBinding
 import woowacourse.movie.preference.NotificationPreference
-import woowacourse.movie.preference.NotificationSharedPreferences
 
 class SettingFragment : Fragment() {
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
 
     private val notificationPreference: NotificationPreference by lazy {
-        NotificationSharedPreferences.getInstance(requireContext().applicationContext)
+        (requireActivity().applicationContext as MovieReservationApplication).notificationPreference
     }
 
     override fun onCreateView(
@@ -27,39 +33,63 @@ class SettingFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
-
+        Log.d(TAG, "onViewCreated: notification enabled ${notificationPreference.loadNotificationPreference()}")
         showInitialPushNotification()
         initChangePushNotificationListener()
     }
 
+    private fun showInitialPushNotification() {
+        val pushNotificationsEnabled = notificationPreference.loadNotificationPreference()
+        Log.d(TAG, "showInitialPushNotification: pushNoti is $pushNotificationsEnabled")
+        binding.pushNotificationEnabled = pushNotificationsEnabled
+    }
+
     private fun initChangePushNotificationListener() {
-        binding.switchPushAlarm.setOnCheckedChangeListener { _, isChecked ->
-            notificationPreference.saveNotificationPreference(enabled = isChecked)
-            Log.d(
-                TAG,
-                "notificationEnabled: ${notificationPreference.loadNotificationPreference()}",
-            )
+        binding.switchPushAlarm.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                binding.pushNotificationEnabled = notificationPreference.loadNotificationPreference()
+                showPermissionChangingGuideSnackBar()
+            } else {
+                Log.d(TAG, "initChangePushNotificationListener: is less than TIRAMISU")
+            }
         }
     }
 
-    private fun showInitialPushNotification() {
-        val pushNotificationsEnabled = notificationPreference.loadNotificationPreference()
-        binding.pushNotificationEnabled = pushNotificationsEnabled
+    override fun onResume() {
+        super.onResume()
+        showInitialPushNotification()
+        Log.d(TAG, "onResume: notification enabled ${notificationPreference.loadNotificationPreference()}")
+    }
+
+    private fun showPermissionChangingGuideSnackBar() {
+        Log.d(TAG, "showPermissionSnackBar: called")
+        Snackbar.make(
+            binding.root,
+            "푸시 알림 권한 설정 창에서 변경해주세요.",
+            Snackbar.LENGTH_LONG,
+        ).setAction("확인") {
+            createdSettingsIntent()
+        }.show()
+    }
+
+    private fun createdSettingsIntent() {
+        val intent =
+            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                data = Uri.fromParts("package", requireContext().packageName, null)
+            }
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy: ")
     }
 
     companion object {
