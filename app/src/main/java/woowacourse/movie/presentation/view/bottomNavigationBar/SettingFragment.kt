f901fea39d8eed072bff91d505f6fa9d0fccf06e
Junyoung-WON <wjyo60@gmail.com>
2024-05-13 17:25:27 +0900

    feat: 알람 설정 화면 구현

--- app/src/main/java/woowacourse/movie/presentation/view/bottomNavigationBar/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/bottomNavigationBar/SettingFragment.kt
@@ -1,18 +1,45 @@
 package woowacourse.movie.presentation.view.bottomNavigationBar
 
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.util.Log
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
+import androidx.preference.SwitchPreferenceCompat
 import woowacourse.movie.R
 
-class SettingFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+class SettingFragment : PreferenceFragmentCompat() {
+    private var notificationPreference: SwitchPreferenceCompat? = null
+
+    override fun onCreatePreferences(
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        rootKey: String?,
+    ) {
+        val sharedPreferences = activity?.getSharedPreferences("movie", MODE_PRIVATE)
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val isNotificationEnabled = sharedPreferences?.getBoolean("notifications", false)
+        if (rootKey != null) {
+            notificationPreference = findPreference("notifications")
+        }
+
+        notificationPreference?.apply {
+            if (isNotificationEnabled != null) {
+                isChecked = isNotificationEnabled
+            }
+            setOnPreferenceChangeListener { _, newValue ->
+                newValue as Boolean
+
+                with(sharedPreferences?.edit()) {
+                    this?.putBoolean("notifications", newValue)
+                    this?.apply()
+                }
+
+                Log.d("Preferences", "Notifications enabled: $newValue")
+                true
+            }
+        }
     }
 }
