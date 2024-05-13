d6ef0b6298bfd4d9684fae5d2f85c54d59f7697b
gaeun <geun5744@gmail.com>
2024-05-13 11:14:20 +0900

    move: 패키지 이동

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,11 +1,10 @@
-package woowacourse
+package woowacourse.movie
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
-import woowacourse.movie.R
 import woowacourse.movie.databinding.ActivityMainBinding
 import woowacourse.movie.reservationlist.ReservationListFragment
 import woowacourse.movie.screeningmovie.ScreeningMovieFragment
