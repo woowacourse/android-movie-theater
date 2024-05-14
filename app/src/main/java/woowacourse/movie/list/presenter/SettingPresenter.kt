a3563455268c4ae2c05fef07423af40dfab754d0
songpink <jejeje04@naver.com>
2024-05-15 01:30:17 +0900

    refactor: shared preference에 activity context가 아니라 application context를 사용하도록 변경

--- app/src/main/java/woowacourse/movie/list/presenter/SettingPresenter.kt
+++ app/src/main/java/woowacourse/movie/list/presenter/SettingPresenter.kt
@@ -1,6 +1,6 @@
 package woowacourse.movie.list.presenter
 
-import android.content.Context
+import androidx.fragment.app.Fragment
 import woowacourse.movie.database.NotificationSharedPreference
 import woowacourse.movie.list.contract.SettingContract
 
@@ -8,7 +8,8 @@ class SettingPresenter(
     private val view: SettingContract.View
 ) : SettingContract.Presenter {
     override fun saveNotificationGranted(isGranted: Boolean) {
-        val sharedPreference = NotificationSharedPreference(view as Context)
+        val applicationContext = (view as Fragment).requireContext().applicationContext
+        val sharedPreference = NotificationSharedPreference(applicationContext)
         sharedPreference.save(isGranted)
     }
 }
