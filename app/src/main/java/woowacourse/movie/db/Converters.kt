9fe3d9f9945d7788ab7ed37620aef39f6aef1887
hxeyexn <kck85075@kumoh.ac.kr>
2024-05-15 00:18:04 +0900

    refactor(Converters): 패키지 이동
    - 이전: db/ticket
    - 이후: db

--- app/src/main/java/woowacourse/movie/db/Converters.kt
+++ app/src/main/java/woowacourse/movie/db/Converters.kt
@@ -1,4 +1,4 @@
-package woowacourse.movie.db.ticket
+package woowacourse.movie.db
 
 import androidx.room.TypeConverter
 import com.google.gson.Gson
--- app/src/main/java/woowacourse/movie/db/ticket/TicketDatabase.kt
+++ app/src/main/java/woowacourse/movie/db/ticket/TicketDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import woowacourse.movie.db.Converters
 
 @Database(entities = [TicketEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
