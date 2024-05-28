00ab3d432333630e7dce24c130ed5c01a24dd4c2
sh1mj1 <7wlgns@gmail.com>
2024-05-29 05:06:49 +0900

    refactor AlarmTime 인터페이스, 구현체 분리

--- app/src/main/java/woowacourse/movie/domain/model/AlarmTime.kt
+++ app/src/main/java/woowacourse/movie/domain/model/AlarmTime.kt
@@ -1,30 +1,10 @@
 package woowacourse.movie.domain.model
 
 import java.time.LocalDateTime
-import java.time.ZoneId
 
 interface AlarmTime {
     fun calculated(
         dateTime: LocalDateTime,
         alarmTimeMinutesBefore: Int,
     ): Long?
-}
-
-class AlarmTimeBeforeMinute : AlarmTime {
-    override fun calculated(
-        dateTime: LocalDateTime,
-        alarmTimeMinutesBefore: Int,
-    ): Long? {
-        val timeLong = dateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
-
-        val alarmTime = timeLong - alarmTimeMinutesBefore * 60 * 1000
-        val currentTime = System.currentTimeMillis()
-
-
-        return if (alarmTime > currentTime) {
-            alarmTime
-        } else {
-            null // 이미 영화 시간이 지났다
-        }
-    }
 }
