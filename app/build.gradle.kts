bf360515496c3ed5cf6beca3910ec6673b56b8b3
박지민 <pjm5489@naver.com>
2025-04-29 15:04:18 +0900

    build: fragment 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.fragment.ktx)
     implementation(libs.google.material)
     testImplementation(libs.assertj.core)
     testImplementation(libs.junit.jupiter)
