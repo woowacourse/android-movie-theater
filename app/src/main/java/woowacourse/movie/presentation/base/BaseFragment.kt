69bfd06c27c7dbf88570573fe999892ee4955aea
moondev03 <moondev03@gmail.com>
2025-04-29 15:46:23 +0900

    feat: BaseFragment 추가
    - DataBinding inflate 중복 코드

---
+++ app/src/main/java/woowacourse/movie/presentation/base/BaseFragment.kt
@@ -0,0 +1,26 @@
+package woowacourse.movie.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int,
+) : Fragment() {
+    private var mBinding: T? = null
+    protected val binding get() = mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        mBinding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
+}
