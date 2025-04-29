6cee6aa5ed5d53271cbcd290a7d3feba1fba6617
gahyunkim <mapledt001@naver.com>
2025-04-29 16:09:27 +0900

    feat: TheaterFragment 코드 구현

--- app/src/main/java/woowacourse/movie/movie/TheaterFragment.kt
+++ app/src/main/java/woowacourse/movie/movie/TheaterFragment.kt
@@ -1,13 +1,17 @@
 package woowacourse.movie.movie
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.TextView
+import androidx.fragment.app.commit
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import woowacourse.movie.BookingDetailActivity
 import woowacourse.movie.R
 
-class TheaterFragment : Fragment() {
+class TheaterFragment : BottomSheetDialogFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,5 +22,22 @@ class TheaterFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_theater, container, false)
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.btn_select_theater).setOnClickListener {
+            parentFragmentManager.commit {
+                setReorderingAllowed(true)
+                val intent = Intent(activity, BookingDetailActivity::class.java)
+                startActivity(intent)
+                addToBackStack(null)
+                dismiss()
+            }
+        }
     }
 }
