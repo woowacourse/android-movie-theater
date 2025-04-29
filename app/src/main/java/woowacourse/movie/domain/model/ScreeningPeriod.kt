c527ff449ee00cb420cb47644d993cfcef78eb1a
oungsi2000 <oungsi1000@gmail.com>
2025-04-29 17:44:45 +0900

    refactor(ScreeningPeriod): 상영 시간 정책 변경으로 인한 로직 변경

--- app/src/main/java/woowacourse/movie/domain/model/ScreeningPeriod.kt
+++ app/src/main/java/woowacourse/movie/domain/model/ScreeningPeriod.kt
@@ -11,6 +11,7 @@ import java.time.LocalTime
 data class ScreeningPeriod(
     val startDate: LocalDate,
     val endDate: LocalDate,
+    val screeningTimes: List<LocalTime>
 ) : Parcelable {
     fun getAvailableDates(now: LocalDateTime): List<LocalDate> {
         val dates = mutableListOf<LocalDate>()
@@ -26,23 +27,10 @@ data class ScreeningPeriod(
         now: LocalDateTime,
         date: LocalDate,
     ): List<LocalTime> {
-        if (now.toLocalDate().isAfter(endDate)) return emptyList()
-
-        val isToday = date == now.toLocalDate()
-        val currentTime = if (isToday) now.toLocalTime() else LocalTime.MIN
-
-        val isWeekend = date.dayOfWeek == DayOfWeek.SATURDAY || date.dayOfWeek == DayOfWeek.SUNDAY
-        val startHour = if (isWeekend) WEEKEND_SCREENING_START else WEEKDAY_SCREENING_START
-
-        return (startHour until FINISH_HOUR step SCREENING_INTERVAL_TIME)
-            .map { LocalTime.of(it, 0) }
-            .filter { it.isAfter(currentTime) }
-    }
-
-    companion object {
-        private val WEEKEND_SCREENING_START = 9
-        private val WEEKDAY_SCREENING_START = 10
-        private val FINISH_HOUR = 24
-        private val SCREENING_INTERVAL_TIME = 2
+        return if (date == now.toLocalDate()) {
+            screeningTimes.filter { it.isAfter(now.toLocalTime()) }
+        } else {
+            screeningTimes
+        }
     }
 }
