12ab3ab5fc2a7cb691b39ce2ce30a6877d1e245c
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-04-29 17:29:02 +0900

    chore: Cinema 클래스 도메인 패키지로 이동

--- app/src/main/java/woowacourse/movie/domain/cinema/Cinema.kt
+++ app/src/main/java/woowacourse/movie/domain/cinema/Cinema.kt
@@ -1,5 +1,21 @@
 package woowacourse.movie.domain.cinema
 
 import woowacourse.movie.domain.reservation.Screening
+import woowacourse.movie.domain.reservation.ShowtimePolicy
+import java.time.LocalDate
+import java.time.LocalTime
 
-class Cinema(val name: String, val screenings: Set<Screening>)
+class Cinema(
+    screenings: List<Screening>,
+    private val showtimePolicy: ShowtimePolicy,
+) {
+    val screenings = screenings.map { it.copy() }
+
+    fun showtimes(
+        screening: Screening,
+        date: LocalDate,
+    ): List<LocalTime> {
+        val showtimes = screening.showtimes(date, showtimePolicy)
+        return showtimes
+    }
+}
--- app/src/test/java/woowacourse/movie/domain/cinema/CinemaTest.kt
+++ app/src/test/java/woowacourse/movie/domain/cinema/CinemaTest.kt
@@ -5,24 +5,8 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import woowacourse.movie.domain.reservation.Movie
 import woowacourse.movie.domain.reservation.Screening
-import woowacourse.movie.domain.reservation.ShowtimePolicy
 import java.time.LocalDate
 import java.time.LocalTime
-
-class Cinema(
-    screenings: List<Screening>,
-    private val showtimePolicy: ShowtimePolicy,
-) {
-    val screenings = screenings.map { it.copy() }
-
-    fun showtimes(
-        screening: Screening,
-        date: LocalDate,
-    ): List<LocalTime> {
-        val showtimes = screening.showtimes(date, showtimePolicy)
-        return showtimes
-    }
-}
 
 class CinemaTest {
     private lateinit var cinema: Cinema
