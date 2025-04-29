096114a7afb49b49d44534011c00bdc7354c6a50
gahyunkim <mapledt001@naver.com>
2025-04-29 16:02:20 +0900

    feat: TheaterFragment 구현

---
+++ app/src/main/java/woowacourse/movie/movie/TheaterFragment.kt
@@ -0,0 +1,22 @@
+package woowacourse.movie.movie
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import woowacourse.movie.R
+
+class TheaterFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_theater, container, false)
+    }
+}
