8a71b4a9efcfd7395b4a86bcee4f75af455cca52
m6z1 <sonmyungj1zz@gmail.com>
2025-04-30 22:47:33 +0900

    refactor: 좌석 선택 뷰 데이터 바인딩 적용

--- app/src/main/java/woowacourse/movie/view/seatSelection/SeatSelectionActivity.kt
+++ app/src/main/java/woowacourse/movie/view/seatSelection/SeatSelectionActivity.kt
@@ -3,7 +3,6 @@ package woowacourse.movie.view.seatSelection
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
@@ -12,7 +11,9 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.children
+import androidx.databinding.DataBindingUtil
 import woowacourse.movie.R
+import woowacourse.movie.databinding.ActivitySeatSelectionBinding
 import woowacourse.movie.model.movie.MovieToReserve
 import woowacourse.movie.model.seat.Seat
 import woowacourse.movie.model.seat.SeatGrade
@@ -20,7 +21,6 @@ import woowacourse.movie.model.ticket.MovieTicket
 import woowacourse.movie.presenter.seatSelection.SeatSelectionContracts
 import woowacourse.movie.presenter.seatSelection.SeatSelectionPresenter
 import woowacourse.movie.view.extension.getSerializableExtraData
-import woowacourse.movie.view.mapper.Formatter.priceToUI
 import woowacourse.movie.view.reservationComplete.ReservationCompleteActivity
 import woowacourse.movie.view.seatSelection.SeatSelectionFormatter.columnToUI
 import woowacourse.movie.view.seatSelection.SeatSelectionFormatter.rowToUI
@@ -29,14 +29,11 @@ class SeatSelectionActivity :
     AppCompatActivity(),
     SeatSelectionContracts.View {
     private val presenter: SeatSelectionContracts.Presenter = SeatSelectionPresenter(this)
-    private val seatsLayout: TableLayout by lazy { findViewById(R.id.tl_seat_selection) }
-    private val titleTextView: TextView by lazy { findViewById(R.id.tv_seat_selection_movie_title) }
-    private val seatPriceTextView: TextView by lazy { findViewById(R.id.tv_seat_selection_price) }
-    private val completeButton: TextView by lazy { findViewById(R.id.tv_seat_selection_complete_button) }
+    private lateinit var binding: ActivitySeatSelectionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_seat_selection)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_seat_selection)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.seat_selection_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -57,12 +54,12 @@ class SeatSelectionActivity :
         )
     }
 
-    private fun getSeatsLayoutRowCount(): Int = seatsLayout.childCount
+    private fun getSeatsLayoutRowCount(): Int = binding.tlSeatSelection.childCount
 
-    private fun getSeatsLayoutColumnCount(): Int = (seatsLayout.getChildAt(0) as TableRow).childCount
+    private fun getSeatsLayoutColumnCount(): Int = (binding.tlSeatSelection.getChildAt(0) as TableRow).childCount
 
     private fun setupClickListener() {
-        completeButton.setOnClickListener {
+        binding.tvSeatSelectionCompleteButton.setOnClickListener {
             showReservationDialog()
         }
     }
@@ -87,7 +84,7 @@ class SeatSelectionActivity :
     }
 
     override fun showSeats(seats: List<Seat>) {
-        seatsLayout.children
+        binding.tlSeatSelection.children
             .filterIsInstance<TableRow>()
             .forEachIndexed { rowIndex, row ->
                 row.children
@@ -153,23 +150,19 @@ class SeatSelectionActivity :
     }
 
     override fun showMovieTitle(title: String) {
-        titleTextView.text = title
+        binding.title = title
     }
 
     override fun showPrice(price: Int) {
-        seatPriceTextView.text =
-            resources.getString(
-                R.string.seat_selection_ticket_price,
-                priceToUI(price),
-            )
+        binding.price = price
     }
 
     override fun showButtonEnabled(enabled: Boolean) {
-        completeButton.isEnabled = enabled
+        binding.tvSeatSelectionCompleteButton.isEnabled = enabled
     }
 
     override fun updateSeatsEnabled(enabled: Boolean) {
-        seatsLayout.children
+        binding.tlSeatSelection.children
             .filterIsInstance<TableRow>()
             .flatMap { row ->
                 row.children.filterIsInstance<TextView>()
