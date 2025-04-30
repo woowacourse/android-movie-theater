92809e5f1c1cbcff3e8ef004e5cb4d33cf2d80e1
m6z1 <sonmyungj1zz@gmail.com>
2025-04-30 14:21:25 +0900

    feat: 메인 화면 바텀 네비게이션 바 구현

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -1,20 +1,54 @@
 package woowacourse.movie
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationBarView
+import woowacourse.movie.databinding.ActivityMainBinding
+import woowacourse.movie.view.movies.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.mainBottomNavigationBar.setOnItemSelectedListener(object :
+            NavigationBarView.OnItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                var selectedFragment: Fragment? = null
+                when (item.itemId) {
+                    R.id.bottom_navigation_reservation_details -> {}
+                    R.id.bottom_navigation_home -> selectedFragment = HomeFragment()
+                    R.id.bottom_navigation_setting -> {}
+                }
+                selectedFragment?.let {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_container, selectedFragment).commit()
+                    return true
+                }
+                return false
+            }
+        })
+    }
+
+    companion object {
+        fun getIntent(context: Context): Intent = Intent(context, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
         }
     }
 }
