3f942470ba455f18c5372a1efda3dbc72af5dde4
jerry8282 <ka06196@naver.com>
2025-04-30 15:49:43 +0900

    feat: dummyMovie 데이터 추가

--- app/src/main/java/woowacourse/movie/domain/Movie.kt
+++ app/src/main/java/woowacourse/movie/domain/Movie.kt
@@ -14,13 +14,31 @@ data class Movie(
 ) : Serializable {
     companion object {
         val dummy: List<Movie> =
-            List(10_000) {
+            listOf(
                 Movie(
                     R.drawable.harry,
-                    "해리 포터$it",
+                    "해리포터",
                     Date(LocalDate.of(2025, 4, 1), LocalDate.of(2025, 4, 30)),
                     152,
-                )
-            }
+                ),
+                Movie(
+                    R.drawable.harry,
+                    "스즈메의 문단속",
+                    Date(LocalDate.of(2025, 4, 1), LocalDate.of(2025, 4, 30)),
+                    152,
+                ),
+                Movie(
+                    R.drawable.harry,
+                    "범죄도시",
+                    Date(LocalDate.of(2025, 4, 1), LocalDate.of(2025, 4, 30)),
+                    152,
+                ),
+                Movie(
+                    R.drawable.harry,
+                    "스타워즈",
+                    Date(LocalDate.of(2025, 4, 1), LocalDate.of(2025, 4, 30)),
+                    152,
+                ),
+            )
     }
 }
