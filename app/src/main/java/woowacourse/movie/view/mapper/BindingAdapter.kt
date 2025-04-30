904b6f3351a4e06d783d1b5b330b0144d9b6c843
m6z1 <sonmyungj1zz@gmail.com>
2025-04-30 22:16:00 +0900

    Create BindingAdapter.kt

---
+++ app/src/main/java/woowacourse/movie/view/mapper/BindingAdapter.kt
@@ -0,0 +1,20 @@
+package woowacourse.movie.view.mapper
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import woowacourse.movie.view.mapper.Formatter.localDateToUI
+import java.time.LocalDate
+
+@BindingAdapter("android:startDate", "android:endDate", requireAll = true)
+fun formatLocalDate(textView: TextView, startDate: LocalDate, endDate: LocalDate) {
+    val startDate: String = localDateToUI(startDate)
+    val endDate: String = localDateToUI(endDate)
+
+    textView.text = "상영일: %s ~ %s".format(startDate, endDate)
+}
+
+@BindingAdapter("android:imageRes")
+fun setImage(imageView: ImageView, resId: Int) {
+    imageView.setImageResource(resId)
+}
