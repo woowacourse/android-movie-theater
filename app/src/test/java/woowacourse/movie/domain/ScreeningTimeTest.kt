c22993a5af52d798b1c684c541261a967eb138e4
chanho0908 <uni070@naver.com>
2025-04-30 16:36:13 +0900

    feat : 예매 시간이 당일일때, 아닐 때 예매 가능한 시간 반환 함수 추가

--- app/src/test/java/woowacourse/movie/domain/ScreeningTimeTest.kt
+++ app/src/test/java/woowacourse/movie/domain/ScreeningTimeTest.kt
@@ -9,81 +9,60 @@ import java.time.LocalTime
 
 class ScreeningTimeTest {
     @Test
-    fun `주말엔 10시 부터 2시간 간격으로 상영 시간을 반환한다`() {
+    fun `선택된 예매 날짜가 당일이면 당일 시간 이전은 제외한 예매 시간을 반환한다`() {
         // when
         val screeningTime =
             ScreeningTime(
                 LocalDateTime.of(2025, 4, 24, 15, 20),
-                LocalDate.of(2025, 4, 26),
+                listOf(
+                    LocalTime.of(9, 0),
+                    LocalTime.of(10, 0),
+                    LocalTime.of(15, 0),
+                    LocalTime.of(16, 0),
+                    LocalTime.of(17, 0),
+                ),
             )
 
         // given
-        val result =
-            screeningTime.getAvailableScreeningTimes()
+        val result = screeningTime.getAvailableScreeningTimes(LocalDate.of(2025, 4, 24))
+
+        // then
+        assertEquals(
+            result,
+            listOf(
+                LocalTime.of(16, 0),
+                LocalTime.of(17, 0),
+            ),
+        )
+    }
+
+    @Test
+    fun `선택된 예매 날짜가 당일이 아니면 모든 예매 시간을 반환한다`() {
+        // when
+        val screeningTime =
+            ScreeningTime(
+                LocalDateTime.of(2025, 4, 24, 15, 20),
+                listOf(
+                    LocalTime.of(9, 0),
+                    LocalTime.of(10, 0),
+                    LocalTime.of(15, 0),
+                    LocalTime.of(16, 0),
+                    LocalTime.of(17, 0),
+                ),
+            )
+
+        // given
+        val result = screeningTime.getAvailableScreeningTimes(LocalDate.of(2025, 4, 25))
 
         // then
         assertEquals(
             result,
             listOf(
                 LocalTime.of(9, 0),
-                LocalTime.of(11, 0),
-                LocalTime.of(13, 0),
-                LocalTime.of(15, 0),
-                LocalTime.of(17, 0),
-                LocalTime.of(19, 0),
-                LocalTime.of(21, 0),
-                LocalTime.of(23, 0),
-            ),
-        )
-    }
-
-    @Test
-    fun `평일엔 9시부터 2시간 간격으로 상영 시간을 반환한다`() {
-        // when
-        val screeningTime =
-            ScreeningTime(
-                LocalDateTime.of(2025, 4, 24, 15, 20),
-                LocalDate.of(2025, 4, 25),
-            )
-
-        // given
-        val result = screeningTime.getAvailableScreeningTimes()
-
-        // result
-        assertEquals(
-            result,
-            listOf(
                 LocalTime.of(10, 0),
-                LocalTime.of(12, 0),
-                LocalTime.of(14, 0),
+                LocalTime.of(15, 0),
                 LocalTime.of(16, 0),
-                LocalTime.of(18, 0),
-                LocalTime.of(20, 0),
-                LocalTime.of(22, 0),
-            ),
-        )
-    }
-
-    @Test
-    fun `선택된 예매 날짜가 당일이면 당일 시간 이전은 제외한 예매 시간을 반환한다`() {
-        // when
-        val screeningTime =
-            ScreeningTime(
-                LocalDateTime.of(2025, 4, 24, 15, 20),
-                LocalDate.of(2025, 4, 24),
-            )
-
-        // given
-        val result = screeningTime.getAvailableScreeningTimes()
-
-        // then
-        assertEquals(
-            result,
-            listOf(
-                LocalTime.of(16, 0),
-                LocalTime.of(18, 0),
-                LocalTime.of(20, 0),
-                LocalTime.of(22, 0),
+                LocalTime.of(17, 0),
             ),
         )
     }
