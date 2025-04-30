8dbf59ef21c435af49d0f8f8c379777928abbc8b
jerry8282 <ka06196@naver.com>
2025-04-30 15:55:51 +0900

    refactor: 현재 시간에 띠라 DialogFragmentUI가 바뀌도록 수정

--- app/src/main/java/woowacourse/movie/domain/ScheduleTime.kt
+++ app/src/main/java/woowacourse/movie/domain/ScheduleTime.kt
@@ -1,12 +1,12 @@
 package woowacourse.movie.domain
 
 import java.io.Serializable
-import java.time.LocalDateTime
+import java.time.LocalTime
 
 data class ScheduleTime(
-    val times: List<LocalDateTime>,
+    val times: List<LocalTime>,
 ) : Serializable {
-    fun afterCurrentTimeSchedule(currentTime: LocalDateTime): List<LocalDateTime> {
+    fun afterCurrentTimeSchedule(currentTime: LocalTime): List<LocalTime> {
         return times.filter { it > currentTime }
     }
 }
