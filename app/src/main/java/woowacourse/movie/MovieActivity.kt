74fde33b37d9b49e02b9dad05969121dc76f1f31
gahyunkim <mapledt001@naver.com>
2025-04-30 10:47:18 +0900

    refactor: main을 movie로 xml명 변경

--- app/src/main/java/woowacourse/movie/MovieActivity.kt
+++ app/src/main/java/woowacourse/movie/MovieActivity.kt
@@ -22,7 +22,7 @@ class MovieActivity : AppCompatActivity(), MovieContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie)
         setUpUi()
 
         presenter = MoviePresenter(this)
