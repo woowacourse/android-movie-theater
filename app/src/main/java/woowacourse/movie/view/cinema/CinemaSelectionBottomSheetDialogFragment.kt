6ba9d8b04ef05820565da15faf114ba5d78854c9
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-04-30 11:19:29 +0900

    feat: CinemaSelectionBottomSheetDialogFragment 팩토리 함수 구현

--- app/src/main/java/woowacourse/movie/view/cinema/CinemaSelectionBottomSheetDialogFragment.kt
+++ app/src/main/java/woowacourse/movie/view/cinema/CinemaSelectionBottomSheetDialogFragment.kt
@@ -1,5 +1,6 @@
 package woowacourse.movie.view.cinema
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,14 +10,19 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import woowacourse.movie.R
 import woowacourse.movie.contract.cinema.CinemaSelectionContract
 import woowacourse.movie.domain.cinema.Cinema
+import woowacourse.movie.domain.reservation.Screening
 import woowacourse.movie.presenter.cinema.CinemaSelectionPresenter
 import woowacourse.movie.view.cinema.adapter.CinemaAdapter
+import woowacourse.movie.view.reservation.ReservationActivity
 
 class CinemaSelectionBottomSheetDialogFragment :
     BottomSheetDialogFragment(),
     CinemaSelectionContract.View {
     private lateinit var cinemasView: RecyclerView
-    private val cinemaAdapter = CinemaAdapter({})
+    private val cinemaAdapter =
+        CinemaAdapter(
+            onClickItem = { presenter.onSelectCinema(arguments.screening ?: error("")) },
+        )
     private val presenter = CinemaSelectionPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,13 +33,12 @@ class CinemaSelectionBottomSheetDialogFragment :
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(
+    ): View? =
+        inflater.inflate(
             R.layout.fragment_cinema_selection_bottom_sheet_dialog,
             container,
             false,
         )
-    }
 
     override fun onViewCreated(
         view: View,
@@ -47,5 +52,38 @@ class CinemaSelectionBottomSheetDialogFragment :
 
     override fun setCinemas(cinemas: List<Cinema>) {
         cinemaAdapter.submitList(cinemas)
+    }
+
+    override fun navigateToReservationScreen(screening: Screening) {
+        val intent = ReservationActivity.newIntent(requireContext(), screening)
+        startActivity(intent)
+    }
+
+    @Suppress("DEPRECATION")
+    private val Bundle?.screening: Screening?
+        get() {
+            if (this == null) return null
+
+            return when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU ->
+                    getSerializable(ARGUMENT_SCREENING, Screening::class.java)
+
+                else -> getSerializable(ARGUMENT_SCREENING) as? Screening
+            }
+        }
+
+    companion object {
+        fun newInstance(screening: Screening): CinemaSelectionBottomSheetDialogFragment {
+            val args =
+                Bundle().apply {
+                    putSerializable(ARGUMENT_SCREENING, screening)
+                }
+
+            val fragment = CinemaSelectionBottomSheetDialogFragment()
+            fragment.arguments = args
+            return fragment
+        }
+
+        private const val ARGUMENT_SCREENING = "ARGUMENT_SCREENING"
     }
 }
