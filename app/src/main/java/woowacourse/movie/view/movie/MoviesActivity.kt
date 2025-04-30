a75084e337ab3c1d1226a372c77777910f5a9bc2
Ham BeomJoon <hbj0802@naver.com>
2025-04-30 21:45:15 +0900

    refactor: moviesActivity 안 쓰는 메서드 정리

--- app/src/main/java/woowacourse/movie/view/movie/MoviesActivity.kt
+++ app/src/main/java/woowacourse/movie/view/movie/MoviesActivity.kt
@@ -1,8 +1,6 @@
 package woowacourse.movie.view.movie
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,18 +11,11 @@ import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import woowacourse.movie.R
 import woowacourse.movie.databinding.ActivityMoviesBinding
-import woowacourse.movie.model.Movie
-import woowacourse.movie.view.Extras
 import woowacourse.movie.view.ReservationListFragment
 import woowacourse.movie.view.SettingFragment
-import woowacourse.movie.view.movie.adapter.MovieAdapter
-import woowacourse.movie.view.reservation.reservation.ReservationActivity
 
-class MoviesActivity :
-    AppCompatActivity(),
-    MovieContract.View {
+class MoviesActivity : AppCompatActivity() {
     private val bnView: BottomNavigationView by lazy { findViewById(R.id.bottom_navigation_view) }
-    private lateinit var moviesAdapter: MovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,29 +51,11 @@ class MoviesActivity :
         }
     }
 
-    override fun showMovies(movies: List<Movie>) {
-        moviesAdapter.submitList(movies)
-    }
-
-    override fun navigateToReservation(movie: Movie) {
-        val intent =
-            Intent(
-                this,
-                ReservationActivity::class.java,
-            ).apply { putExtra(Extras.MovieData.MOVIE_KEY, movie) }
-        startActivity(intent)
-    }
-
-    fun replaceFragment(fragment: Fragment) {
+    private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             replace(R.id.fcv_main, fragment)
             addToBackStack(null)
         }
-    }
-
-    fun showBottomNav(isVisible: Boolean) {
-        val nav = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
-        nav.visibility = if (isVisible) View.VISIBLE else View.GONE
     }
 }
