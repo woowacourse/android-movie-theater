7db9c868a912860fb420a9a12ab5b46ef822afaf
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-04-30 21:37:24 +0900

    feat: 메인 액티비티 추가

---
+++ app/src/main/java/woowacourse/movie/view/MainActivity.kt
@@ -0,0 +1,29 @@
+package woowacourse.movie.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import woowacourse.movie.R
+import woowacourse.movie.view.cinema.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add(R.id.fragment_container_view_main, HomeFragment())
+            }
+        }
+    }
+}
