37c9bb0b5e0366203174756ef14adfb094c095bf
ijh1298 <imjh0417@gmail.com>
2025-04-30 16:34:59 +0900

    feat: TheaterBottomSheetDialogFragment 추가

---
+++ app/src/main/java/woowacourse/movie/ui/movielist/view/TheaterBottomSheetDialogFragment.kt
@@ -0,0 +1,25 @@
+package woowacourse.movie.ui.movielist.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import woowacourse.movie.R
+
+class TheaterBottomSheetDialogFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_theater_bottom_sheet_dialog, container, false)
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
