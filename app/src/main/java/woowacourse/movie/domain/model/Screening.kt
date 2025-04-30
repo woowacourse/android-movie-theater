4491a2e086f553099cffc54c07874afd8ab8261d
oungsi2000 <oungsi1000@gmail.com>
2025-04-30 14:22:10 +0900

    refactor: 상영 객체로 변경
    ScreeningPeriod -> Screening

--- app/src/main/java/woowacourse/movie/domain/model/Screening.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Screening.kt
@@ -2,28 +2,16 @@ package woowacourse.movie.domain.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 
 @Parcelize
-data class ScreeningPeriod(
-    val startDate: LocalDate,
-    val endDate: LocalDate,
+data class Screening(
     val screeningTimes: List<LocalTime>,
-    val cinemaId:Int
+    val cinema: Cinema,
+    val movie: Movie
 ) : Parcelable {
-    fun getAvailableDates(now: LocalDateTime): List<LocalDate> {
-        val dates = mutableListOf<LocalDate>()
-        var date = now.toLocalDate()
-        while (!date.isAfter(endDate)) {
-            dates.add(date)
-            date = date.plusDays(1)
-        }
-        return dates.filterNot { it.isBefore(startDate) }
-    }
-
     fun getAvailableTimesFor(
         now: LocalDateTime,
         date: LocalDate,
