0acad76104a87bf95c9686269384cfd3cff0a8a4
m6z1 <sonmyungj1zz@gmail.com>
2025-04-30 20:05:47 +0900

    chore: 사용하지 않는 함수 제거

--- app/src/main/java/woowacourse/movie/view/mapper/Formatter.kt
+++ app/src/main/java/woowacourse/movie/view/mapper/Formatter.kt
@@ -6,20 +6,10 @@ import java.time.format.DateTimeFormatter
 
 object Formatter {
     private val DATE_UI_FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-    private const val TIME_UI_SUFFIX = "%d:00"
     private val PRICE_UI_FORMATTER = DecimalFormat("#,###")
 
     @JvmStatic
     fun localDateToUI(date: LocalDate): String = date.format(DATE_UI_FORMATTER)
-
-    @JvmStatic
-    fun movieTimeToUI(time: Int): String = TIME_UI_SUFFIX.format(time)
-
-    @JvmStatic
-    fun uiToMovieTime(value: String): Int {
-        val index = value.indexOf(":")
-        return value.substring(0, index).toInt()
-    }
 
     @JvmStatic
     fun priceToUI(price: Int): String = PRICE_UI_FORMATTER.format(price)
