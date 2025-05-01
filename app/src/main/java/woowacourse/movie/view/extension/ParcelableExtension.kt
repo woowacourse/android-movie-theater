1a4a709b9d23b92990a86228958668594108ce26
oungsi2000 <oungsi1000@gmail.com>
2025-05-01 16:31:04 +0900

    refactor: Ktlint 적용

--- app/src/main/java/woowacourse/movie/view/extension/ParcelableExtension.kt
+++ app/src/main/java/woowacourse/movie/view/extension/ParcelableExtension.kt
@@ -3,8 +3,6 @@ package woowacourse.movie.view.extension
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.os.Parcelable
-import woowacourse.movie.domain.model.Screening
 
 inline fun <reified T> Intent.getParcelableCompat(key: String): T =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -16,7 +14,7 @@ inline fun <reified T> Intent.getParcelableCompat(key: String): T =
 
 inline fun <reified T> Bundle.getParcelableCompat(key: String): List<T> =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelableArray(key, T::class.java)?.toList()?: throw IllegalArgumentException()
+        getParcelableArray(key, T::class.java)?.toList() ?: throw IllegalArgumentException()
     } else {
         @Suppress("DEPRECATION")
         getParcelableArray(key)?.toList() as? List<T> ?: throw IllegalArgumentException()
