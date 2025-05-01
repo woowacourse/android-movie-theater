0323856ad02af2024ca868dcaaa35427537a6aa3
yrsel <xxknu88@gmail.com>
2025-05-01 14:10:40 +0900

    chore: KtlintFormat 적용

--- app/src/main/java/woowacourse/movie/ui/settings/view/SettingsFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/settings/view/SettingsFragment.kt
@@ -1,16 +1,17 @@
 package woowacourse.movie.ui.settings.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import woowacourse.movie.R
 
 class SettingsFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
