f723de005d95480ef0a66498040f735442288bde
chanho0908 <uni070@naver.com>
2025-05-01 15:25:57 +0900

    build : Fragment gradle Dependency add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,5 @@ dependencies {
     androidTestImplementation(libs.mannodermaus.junit5.core)
     androidTestRuntimeOnly(libs.mannodermaus.junit5.runner)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.fragment.ktx)
 }
