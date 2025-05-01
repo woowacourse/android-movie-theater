9c7306035e458ee07d13308ab95d5dfac8e9384a
yrsel <xxknu88@gmail.com>
2025-05-01 14:36:53 +0900

    refactor: TheaterBottomSheetDialogFragment 클릭 시, remove 호출 추가

--- app/src/main/java/woowacourse/movie/ui/movielist/view/TheaterBottomSheetDialogFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/movielist/view/TheaterBottomSheetDialogFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.commit
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import woowacourse.movie.R
 import woowacourse.movie.databinding.FragmentTheaterBottomSheetDialogBinding
@@ -59,6 +60,9 @@ class TheaterBottomSheetDialogFragment : BottomSheetDialogFragment() {
     ) {
         if (theater.movieSchedules.isNotEmpty()) {
             startActivity(BookingActivity.newIntent(view.context, theater))
+            parentFragmentManager.commit {
+                remove(this@TheaterBottomSheetDialogFragment)
+            }
         }
     }
 
