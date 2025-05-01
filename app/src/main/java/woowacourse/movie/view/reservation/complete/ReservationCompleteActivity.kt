8af2b5b215342e4c32fa963d98e91156184a896f
Ham BeomJoon <hbj0802@naver.com>
2025-05-01 10:59:45 +0900

    refactor: 예매 완료 화면 dataBinding 적용

--- app/src/main/java/woowacourse/movie/view/reservation/complete/ReservationCompleteActivity.kt
+++ app/src/main/java/woowacourse/movie/view/reservation/complete/ReservationCompleteActivity.kt
@@ -1,12 +1,13 @@
 package woowacourse.movie.view.reservation.complete
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import woowacourse.movie.R
+import woowacourse.movie.databinding.ActivityReservationCompleteBinding
 import woowacourse.movie.model.ReservationInfo
 import woowacourse.movie.view.Extras
 import woowacourse.movie.view.ReservationUiFormatter
@@ -15,20 +16,13 @@ import woowacourse.movie.view.getParcelableExtraCompat
 class ReservationCompleteActivity :
     AppCompatActivity(),
     ReservationCompleteContract.View {
-    private val movieTitleTextView: TextView by lazy { findViewById(R.id.tv_reservation_complete_title) }
-    private val reservationDateTimeTextView: TextView by lazy {
-        findViewById(R.id.tv_reservation_complete_timestamp)
-    }
-    private val ticketCountTextView: TextView by lazy {
-        findViewById(R.id.tv_reservation_complete_count_seats)
-    }
-    private val ticketPriceTextView: TextView by lazy { findViewById<TextView>(R.id.tv_reservation_complete_ticket_price) }
+    private lateinit var binding: ActivityReservationCompleteBinding
     private val presenter: ReservationCompletePresenter by lazy { ReservationCompletePresenter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_reservation_complete)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reservation_complete)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.reservation_complete)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -45,14 +39,14 @@ class ReservationCompleteActivity :
     }
 
     override fun showReservationInfo(reservationInfo: ReservationInfo) {
-        movieTitleTextView.text = reservationInfo.title
-        reservationDateTimeTextView.text =
+        binding.tvReservationCompleteTitle.text = reservationInfo.title
+        binding.tvReservationCompleteTimestamp.text =
             resources.getString(
                 R.string.reservation_complete_date_time,
                 ReservationUiFormatter.localDateToUI(reservationInfo.date),
                 reservationInfo.time,
             )
-        ticketCountTextView.text =
+        binding.tvReservationCompleteCountSeats.text =
             resources.getString(
                 R.string.reservation_complete_ticket_count,
                 reservationInfo.seats.size,
@@ -62,7 +56,7 @@ class ReservationCompleteActivity :
                     .joinToString(),
                 reservationInfo.theaterName,
             )
-        ticketPriceTextView.text =
+        binding.tvReservationCompleteTicketPrice.text =
             resources.getString(
                 R.string.reservation_complete_ticket_price,
                 ReservationUiFormatter.priceToUI(reservationInfo.price),
