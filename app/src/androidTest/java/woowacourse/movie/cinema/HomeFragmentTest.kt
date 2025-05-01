3274d88acb1781fc3fec2ca48289e7aa566abcbc
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-05-01 17:33:35 +0900

    fix: 홈 프래그먼트 테스트

--- app/src/androidTest/java/woowacourse/movie/cinema/HomeFragmentTest.kt
+++ app/src/androidTest/java/woowacourse/movie/cinema/HomeFragmentTest.kt
@@ -8,6 +8,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -21,29 +22,43 @@ import java.time.LocalDate
 
 @RunWith(AndroidJUnit4::class)
 class HomeFragmentTest {
-    val harryPotterPhilosopersStone =
+    private val harryPotterChamberOfSecrets =
+        Movie(
+            1,
+            "해리 포터와 비밀의 방",
+            162,
+        )
+
+    private val harryPotterPrisonerOfAzkaban =
+        Movie(
+            2,
+            "해리 포터와 아즈카반의 죄수",
+            141,
+        )
+    private val harryPotterPhilosopersStone =
         Movie(
             0,
             "해리 포터와 마법사의 돌",
             152,
         )
+
     val fragmentArguments =
         HomeFragment.arguments(
             listOf(
                 Screening(
-                    harryPotterPhilosopersStone,
+                    harryPotterChamberOfSecrets,
                     LocalDate.of(2025, 4, 1),
                     LocalDate.of(2025, 4, 25),
                 ),
                 Screening(
-                    harryPotterPhilosopersStone,
-                    LocalDate.of(2025, 4, 1),
-                    LocalDate.of(2025, 4, 25),
+                    harryPotterPrisonerOfAzkaban,
+                    LocalDate.of(2025, 6, 1),
+                    LocalDate.of(2025, 6, 25),
                 ),
                 Screening(
                     harryPotterPhilosopersStone,
-                    LocalDate.of(2025, 4, 1),
-                    LocalDate.of(2025, 4, 25),
+                    LocalDate.of(2025, 6, 1),
+                    LocalDate.of(2025, 6, 25),
                 ),
                 Advertisement(0),
             ),
@@ -62,47 +77,43 @@ class HomeFragmentTest {
 
     @Test
     fun `상영_리스트가_표시된다`() {
-        onView(ViewMatchers.withId(R.id.recycler_view_home_screening_movies))
+        onView(withId(R.id.recycler_view_home_screening_movies))
             .check(matches(isDisplayed()))
     }
 
     @Test
     fun `영화_목록에_영화가_세_번_노출될_때마다_광고가_한_번_노출된다`() {
-        onView(ViewMatchers.withId(R.id.recycler_view_home_screening_movies))
+        onView(withId(R.id.recycler_view_home_screening_movies))
             .perform(scrollToPosition<RecyclerView.ViewHolder>(4))
 
-        onView(ViewMatchers.withId(R.id.iv_item_advertisement))
+        onView(withId(R.id.iv_item_advertisement))
             .check(matches(isDisplayed()))
     }
-//
-//    @Test
-//    fun `상영_정보에는_영화_제목이_표시된다`() {
-//        onView(ViewMatchers.withId(R.id.rv_screening_movies))
-//            .perform(scrollToPosition<RecyclerView.ViewHolder>(2))
-//
-//        onView(ViewMatchers.withText("해리 포터와 아즈카반의 죄수"))
-//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-//    }
-//
-//    @Test
-//    fun `상영_정보에는_상영일이_표시된다`() {
-//        onView(VewMatchers.withId(R.id.rv_screening_movies))
-//            .perform(scrollToPosition<RecyclerView.ViewHolder>(2))
-//
-//        onView(ViewMatchers.withText("상영일: 2025.6.1 ~ 2025.6.25"))
-//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-//    }
-//
-//    @Test
-//    fun `상영_정보에는_러닝타임이_표시된다`() {
-//        onView(ViewMatchers.withId(R.id.rv_screening_movies))
-//            .perform(scrollToPosition<RecyclerView.ViewHolder>(2))
-//
-//        onView(ViewMatchers.withText("러닝타임: 141분"))
-//            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-//    }
-//
-//    @Test
-//    fun `영화를_선택하면_극장을_선택할_수_있는_창이_나온다`() {
-//    }
+
+    @Test
+    fun `상영_정보에는_영화_제목이_표시된다`() {
+        onView(withId(R.id.recycler_view_home_screening_movies))
+            .perform(scrollToPosition<RecyclerView.ViewHolder>(2))
+
+        onView(ViewMatchers.withText("해리 포터와 마법사의 돌"))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun `상영_정보에는_상영일이_표시된다`() {
+        onView(withId(R.id.recycler_view_home_screening_movies))
+            .perform(scrollToPosition<RecyclerView.ViewHolder>(2))
+
+        onView(ViewMatchers.withText("상영일: 2025.4.1 ~ 2025.4.25"))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun `상영_정보에는_러닝타임이_표시된다`() {
+        onView(withId(R.id.recycler_view_home_screening_movies))
+            .perform(scrollToPosition<RecyclerView.ViewHolder>(2))
+
+        onView(ViewMatchers.withText("러닝타임: 141분"))
+            .check(matches(isDisplayed()))
+    }
 }
