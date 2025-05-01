653857c49419818f3b7b0c9ef8783a997a8e8818
oungsi2000 <oungsi1000@gmail.com>
2025-05-01 16:28:13 +0900

    feat: BindingAdapter 생성

---
+++ app/src/main/java/woowacourse/movie/view/mapper/BindingAdapter.kt
@@ -0,0 +1,42 @@
+package woowacourse.movie.view.mapper
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import woowacourse.movie.R
+import woowacourse.movie.domain.model.RunningTime
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@BindingAdapter("android:startDate", "android:endDate", requireAll = true)
+fun setDateFormatter(view: TextView, startDate: LocalDate, endDate: LocalDate) {
+    val formatter = DateTimeFormatter.ofPattern(view.context.getString(R.string.movie_screening_period_format))
+    view.text = view.context.getString(
+        R.string.movie_date,
+        startDate.format(formatter),
+        endDate.format(formatter)
+    )
+}
+
+@BindingAdapter("android:runningTime")
+fun setRunningTime(view: TextView, runningTime: RunningTime) {
+    view.text = view.context.getString(
+        R.string.running_time,
+        runningTime.minute.toString(),
+    )
+}
+
+@BindingAdapter("android:cinemaName")
+fun setCinemaName(view: TextView, cinemaName: String) {
+    view.text = view.context.getString(
+        R.string.cinema,
+        cinemaName
+    )
+}
+
+@BindingAdapter("android:screeningTimes")
+fun setScreeningItems(view: TextView, size:Int) {
+    view.text = view.context.getString(
+        R.string.screenig_times,
+        size
+    )
+}
