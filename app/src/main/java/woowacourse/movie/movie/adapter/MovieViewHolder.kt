3d9f46b435ec60652a256c89f8e1506c92b568a2
hwannow <vnfdlvgid061@naver.com>
2025-05-02 09:56:46 +0900

    refactor: ViewHolder의 프로퍼티 가시성 변경자 수정

--- app/src/main/java/woowacourse/movie/movie/adapter/MovieViewHolder.kt
+++ app/src/main/java/woowacourse/movie/movie/adapter/MovieViewHolder.kt
@@ -4,8 +4,10 @@ import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.databinding.MovieListItemBinding
 import woowacourse.movie.ui.model.MovieUiModel
 
-class MovieViewHolder(val binding: MovieListItemBinding, val onReserveClick: ReserveClickListener) :
-    RecyclerView.ViewHolder(binding.root) {
+class MovieViewHolder(
+    private val binding: MovieListItemBinding,
+    private val onReserveClick: ReserveClickListener,
+) : RecyclerView.ViewHolder(binding.root) {
     fun bind(movie: MovieUiModel) {
         binding.movie = movie
         binding.clickListener = onReserveClick
--- app/src/main/java/woowacourse/movie/theater/TheaterViewHolder.kt
+++ app/src/main/java/woowacourse/movie/theater/TheaterViewHolder.kt
@@ -4,8 +4,10 @@ import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.databinding.TheaterItemBinding
 import woowacourse.movie.ui.model.TheaterUiModel
 
-class TheaterViewHolder(val binding: TheaterItemBinding, val onSelectClick: SelectClickListener) :
-    RecyclerView.ViewHolder(binding.root) {
+class TheaterViewHolder(
+    private val binding: TheaterItemBinding,
+    private val onSelectClick: SelectClickListener,
+) : RecyclerView.ViewHolder(binding.root) {
     fun bind(theater: TheaterUiModel) {
         binding.theater = theater
         binding.clickListener = onSelectClick
