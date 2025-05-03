cbb94839b78585d77289c1e8587ea204684e8c73
Ham BeomJoon <hbj0802@naver.com>
2025-05-04 00:41:45 +0900

    refactor: 영화관 정보 fetch 로직 추가

--- app/src/main/java/woowacourse/movie/view/model/MovieUiModel.kt
+++ app/src/main/java/woowacourse/movie/view/model/MovieUiModel.kt
@@ -4,6 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import woowacourse.movie.domain.model.Movie
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Parcelize
 data class MovieUiModel(
@@ -19,7 +20,9 @@ fun MovieUiModel.toDomainModel(): Movie =
     Movie(
         title = this.name,
         poster = this.poster,
-        startDate = LocalDate.parse(this.startDate),
-        endDate = LocalDate.parse(this.endDate),
+        startDate = this.startDate.toLocalDate(),
+        endDate = this.endDate.toLocalDate(),
         runningTime = this.runningTime,
     )
+
+fun String.toLocalDate(pattern: String = "yyyy.MM.dd"): LocalDate = LocalDate.parse(this, DateTimeFormatter.ofPattern(pattern))
--- app/src/main/java/woowacourse/movie/view/theater/TheaterPresenter.kt
+++ app/src/main/java/woowacourse/movie/view/theater/TheaterPresenter.kt
@@ -1,23 +1,25 @@
 package woowacourse.movie.view.theater
 
-import woowacourse.movie.domain.model.Movie
 import woowacourse.movie.domain.model.MovieDao
+import woowacourse.movie.domain.model.Theater
 import woowacourse.movie.domain.model.TheaterUIModel
+import woowacourse.movie.domain.model.toUiModel
+import woowacourse.movie.view.model.MovieUiModel
 
 class TheaterPresenter(
     val view: TheaterContract.View,
+    private val movie: MovieUiModel,
 ) : TheaterContract.Presenter {
     private val movieDao: MovieDao by lazy { MovieDao() }
 
-    override fun fetchTheaters(movie: Movie) {
-//        val theaterUIModels =
-//            movieDao
-//                .getTheaterNames()
-//                .map {
-//                    val theater = Theater(it, movieDao.getMovies(it))
-//                    TheaterUIModel(it, movie, theater.getTotalTimeSlotCount(movie))
-//                }
-//        view.showTheaters(theaterUIModels)
+    override fun fetchTheaters() {
+        val theaterNames = movieDao.getTheaterNames()
+        val theaters =
+            theaterNames.map { name ->
+                val movies = movieDao.getMovies(name)
+                Theater(name, movies)
+            }
+        view.showTheaters(theaters.map { it.toUiModel(movie) })
     }
 
     override fun theaterSelected(theaterUIModel: TheaterUIModel) {
