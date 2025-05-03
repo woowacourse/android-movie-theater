0c1b05c980e6c25ce2b9244a6a875af574871fc3
지원 <cheerfulboys@naver.com>
2025-05-03 17:47:46 +0900

    test: 함수 파라미터 추가로 인한 테스트 깨짐에 대해 임시 수정

--- app/src/test/java/woowacourse/movie/presenter/MovieBookedPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presenter/MovieBookedPresenterTest.kt
@@ -7,6 +7,7 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import woowacourse.movie.domain.Theater
 import woowacourse.movie.fixture.MovieFixture
 import woowacourse.movie.moviebooked.MovieBooked
 import woowacourse.movie.moviebooked.MovieBookedPresenter
@@ -24,15 +25,16 @@ class MovieBookedPresenterTest {
     @Test
     fun loadMovie_호출시_View의_showMovieInfo가_호출된다() {
         // given - 예약 정보가 주어짐
+        val theater = Theater("선릉", MovieFixture.THEATER_MOVIES, MovieFixture.THEATER_TIMETABLE)
         val bookingStatus = MovieFixture.BOOKING_STATUS
-        every { view.showBookedStatus(any()) } just Runs
+        every { view.showBookedStatus(any(), any()) } just Runs
 
         // when - loadBookedStatus를 호출 시
-        presenter.loadBookedStatus(bookingStatus)
+        presenter.loadBookedStatus(bookingStatus, theater)
 
         // then - showBookedStatus()가 호출된다
         verify {
-            view.showBookedStatus(any())
+            view.showBookedStatus(any(), any())
         }
     }
 }
