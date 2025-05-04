2fff6256b29b30d572f4c062545826d5dfa2db11
oungsi2000 <oungsi1000@gmail.com>
2025-05-04 19:26:14 +0900

    refactor: AdViewHolder 데이터 바인딩 적용

--- app/src/main/java/woowacourse/movie/view/movies/AdViewHolder.kt
+++ app/src/main/java/woowacourse/movie/view/movies/AdViewHolder.kt
@@ -1,16 +1,13 @@
 package woowacourse.movie.view.movies
 
-import android.view.View
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import woowacourse.movie.R
-import woowacourse.movie.domain.model.Advertisement
 import woowacourse.movie.databinding.ItemAdvertisementBinding
+import woowacourse.movie.domain.model.Advertisement
 
 class AdViewHolder(
     val binding: ItemAdvertisementBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(item: Advertisement) {
-        binding.ad.setImageResource(item.id)
+        binding.advertisement = item
     }
 }
