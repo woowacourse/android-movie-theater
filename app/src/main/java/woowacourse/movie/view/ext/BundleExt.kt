379dbdde962bc81340ed39c6543b0cf3b309b77d
chanho0908 <uni070@naver.com>
2025-05-04 21:07:52 +0900

    feat : ArrayList Serializable을 받기 위한 Bundle 확장함수 추가

--- app/src/main/java/woowacourse/movie/view/ext/BundleExt.kt
+++ app/src/main/java/woowacourse/movie/view/ext/BundleExt.kt
@@ -3,6 +3,7 @@ package woowacourse.movie.view.ext
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
+import java.io.Serializable
 
 inline fun <reified T : Parcelable> Bundle.getParcelableArrayListOrNull(key: String): ArrayList<T>? {
     return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -14,10 +15,22 @@ inline fun <reified T : Parcelable> Bundle.getParcelableArrayListOrNull(key: Str
 }
 
 inline fun <reified T : Parcelable> Bundle.getParcelableOrNull(key: String): T? {
-    return if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         getParcelable(key, T::class.java)
     } else {
         @Suppress("DEPRECATION")
         getParcelable(key) as? T
     }
+}
+
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T : Serializable> Bundle.getSerializableArrayList(key: String): ArrayList<T> {
+    return (
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getSerializable(key, ArrayList::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            getSerializable(key)
+        }
+    ) as ArrayList<T>
 }
