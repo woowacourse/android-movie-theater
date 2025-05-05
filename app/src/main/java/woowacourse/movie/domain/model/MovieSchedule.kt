875dc3fb8b0823cc52908aea87678fba6eec23f7
yrsel <xxknu88@gmail.com>
2025-05-06 01:22:31 +0900

    refactor: MovieSchedule 영화 시작 시간과 좌석에 대한 정보
    - 시간 정보만으로 동등성 비교

--- app/src/main/java/woowacourse/movie/domain/model/MovieSchedule.kt
+++ app/src/main/java/woowacourse/movie/domain/model/MovieSchedule.kt
@@ -2,18 +2,43 @@ package woowacourse.movie.domain.model
 
 import java.io.Serializable
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.LocalTime
 
 class MovieSchedule(
-    val movie: Movie,
-    val screeningDateTime: ScreeningDateTime,
-    val seat: Seats,
+    val screeningDateTime: LocalDateTime,
+    val seats: Seats = Seats(),
 ) : Serializable {
-    fun isScreeningByDateAndTime(
-        selectedMovie: Movie,
-        selectedDate: LocalDate,
-        selectedTime: LocalTime,
-    ): Boolean {
-        return movie == selectedMovie && screeningDateTime.isScreening(selectedDate, selectedTime)
+    val screeningDate: LocalDate = screeningDateTime.toLocalDate()
+    val screeningTime: LocalTime = screeningDateTime.toLocalTime()
+
+    fun isScreeningDate(dateTime: LocalDateTime): Boolean {
+        val date = dateTime.toLocalDate()
+        return screeningDate.isAfter(date) || screeningDate.isEqual(date)
+    }
+
+    fun isTodayScreening(dateTime: LocalDateTime): Boolean {
+        return screeningDate.isEqual(dateTime.toLocalDate()) && screeningTime.isAfter(dateTime.toLocalTime())
+    }
+
+    fun isFutureScreening(dateTime: LocalDateTime): Boolean {
+        return screeningDate.isEqual(dateTime.toLocalDate())
+    }
+
+    fun isEqual(dateTime: LocalDateTime): Boolean {
+        return this.screeningDateTime.isEqual(dateTime)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MovieSchedule
+
+        return screeningDateTime == other.screeningDateTime
+    }
+
+    override fun hashCode(): Int {
+        return screeningDateTime.hashCode()
     }
 }
