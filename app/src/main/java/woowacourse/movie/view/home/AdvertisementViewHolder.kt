5bf46c930cb2901cf04537f0ae43c2cd9be7802c
m6z1 <sonmyungj1zz@gmail.com>
2025-05-05 14:52:54 +0900

    refactor: 광고 클릭 리스너의 view 수정

--- app/src/main/java/woowacourse/movie/view/home/AdvertisementViewHolder.kt
+++ app/src/main/java/woowacourse/movie/view/home/AdvertisementViewHolder.kt
@@ -1,16 +1,14 @@
 package woowacourse.movie.view.home
 
 import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
-import woowacourse.movie.R
 
 class AdvertisementViewHolder(
     view: View,
     advertisementClickListener: () -> Unit,
 ) : RecyclerView.ViewHolder(view) {
     init {
-        view.findViewById<ConstraintLayout>(R.id.cl_advertisement).setOnClickListener {
+        view.setOnClickListener {
             advertisementClickListener.invoke()
         }
     }
