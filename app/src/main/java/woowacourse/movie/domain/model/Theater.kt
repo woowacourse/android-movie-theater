69ad8432b0b4c5bb8091876a6b1266fad41b087c
yrsel <xxknu88@gmail.com>
2025-05-06 01:17:41 +0900

    refactor: Theater 영화따라 상영 가능 개수 반환

--- app/src/main/java/woowacourse/movie/domain/model/Theater.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Theater.kt
@@ -1,23 +1,10 @@
 package woowacourse.movie.domain.model
 
 import java.io.Serializable
-import java.time.LocalDate
-import java.time.LocalTime
 
 data class Theater(
     val name: String,
-    val movieSchedules: List<MovieSchedule>,
+    val theaterSchedules: TheaterSchedules,
 ) : Serializable {
-    fun theaterByMovie(
-        movie: Movie,
-        date: LocalDate,
-        time: LocalTime,
-    ): Theater {
-        val filtered: List<MovieSchedule> =
-            movieSchedules.filter { movieSchedule ->
-                movieSchedule.isScreeningByDateAndTime(movie, date, time)
-            }
-
-        return Theater(name, filtered)
-    }
+    fun scheduleCountByMovieId(movieId: Long) = theaterSchedules[movieId].size
 }
