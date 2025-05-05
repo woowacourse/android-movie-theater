956459e5d4bea7a1ac51b9230cc46c0d52f5a42c
yrsel <xxknu88@gmail.com>
2025-05-06 00:22:27 +0900

    refactor: Movie id 필드 추가

--- app/src/main/java/woowacourse/movie/domain/model/Movie.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Movie.kt
@@ -3,6 +3,7 @@ package woowacourse.movie.domain.model
 import java.io.Serializable
 
 data class Movie(
+    val id: Long,
     val title: String,
     val posterId: Int,
     val releaseDate: ScreeningPeriod,
