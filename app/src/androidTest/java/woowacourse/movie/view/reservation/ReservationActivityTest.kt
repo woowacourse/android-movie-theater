263b39be4db1582efe06d11ccfdd4489ef3e0a30
oungsi2000 <oungsi1000@gmail.com>
2025-05-05 16:28:25 +0900

    test: ReservationActivity 테스트 작성

--- app/src/androidTest/java/woowacourse/movie/view/reservation/ReservationActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/reservation/ReservationActivityTest.kt
@@ -1,39 +1,31 @@
 package woowacourse.movie.view.reservation
 
-import android.content.Intent
 import android.content.pm.ActivityInfo
-import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import org.junit.Before
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import woowacourse.movie.R
-import woowacourse.movie.domain.model.Movie
-import woowacourse.movie.domain.model.RunningTime
-import woowacourse.movie.domain.model.ScreeningPeriod
-import woowacourse.movie.view.fixture.fakeContext
-import java.time.LocalDate
+import woowacourse.movie.view.fixture.TestData
 
+@RunWith(AndroidJUnit4::class)
+@Suppress("FunctionName")
 class ReservationActivityTest {
-    private lateinit var scenario: ActivityScenario<ReservationActivity>
-
-    private val harryPotter =
-        Movie(
-            title = "해리 포터와 마법사의 돌",
-            screeningPeriod = ScreeningPeriod(LocalDate.of(2025, 4, 1), LocalDate.of(2025, 4, 25)),
-            runningTime = RunningTime(152),
-            poster = R.drawable.harrypotter_1.toString(),
+    val intent =
+        ReservationActivity.newIntent(
+            ApplicationProvider.getApplicationContext(),
+            TestData.screening,
         )
 
-    @Before
-    fun setUp() {
-        val intent =
-            Intent(fakeContext, ReservationActivity::class.java).putExtra("movie", harryPotter)
-        scenario = ActivityScenario.launch(intent)
-    }
+    @get:Rule
+    val activityRule = ActivityScenarioRule<ReservationActivity>(intent)
 
     @Test
     fun `영화_제목을_보여준다`() {
@@ -44,7 +36,7 @@ class ReservationActivityTest {
     @Test
     fun `영화_상영기간을_보여준다`() {
         onView(withId(R.id.tv_screening_period))
-            .check(matches(withText("상영일: 2025.4.1 ~ 2025.4.25")))
+            .check(matches(withText("상영일: 2025.5.1 ~ 2025.5.25")))
     }
 
     @Test
@@ -61,13 +53,16 @@ class ReservationActivityTest {
 
     @Test
     fun `예매_인원수가_3일때_마이너스_버튼을_한_번_누르면_2가_된다`() {
+        // given
         onView(withId(R.id.btn_reservation_count_plus))
             .perform(click())
             .perform(click())
 
+        // when
         onView(withId(R.id.btn_reservation_count_minus))
             .perform(click())
 
+        // then
         onView(withId(R.id.tv_reservation_count))
             .check(matches(withText("2")))
     }
@@ -77,7 +72,7 @@ class ReservationActivityTest {
         onView(withId(R.id.btn_reservation_count_plus))
             .perform(click())
 
-        scenario.onActivity { activity ->
+        activityRule.scenario.onActivity { activity ->
             activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         }
 
--- app/src/main/java/woowacourse/movie/view/base/BaseActivity.kt
+++ app/src/main/java/woowacourse/movie/view/base/BaseActivity.kt
@@ -9,12 +9,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import woowacourse.movie.R
 
-abstract class BaseActivity<T: ViewDataBinding>(
+abstract class BaseActivity<T : ViewDataBinding>(
     @LayoutRes private val layoutResId: Int,
 ) : AppCompatActivity(layoutResId) {
     protected lateinit var binding: T
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, layoutResId)
@@ -27,7 +27,7 @@ abstract class BaseActivity<T: ViewDataBinding>(
     }
 
     private fun setWindowInsets() {
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
