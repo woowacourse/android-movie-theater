9c6133ff384c45c2841781a74ccc585cc4536a1f
oungsi2000 <oungsi1000@gmail.com>
2025-05-05 20:01:09 +0900

    refactor: 함수->프로퍼티로 변경

--- app/src/main/java/woowacourse/movie/domain/model/SeatFactory.kt
+++ app/src/main/java/woowacourse/movie/domain/model/SeatFactory.kt
@@ -4,15 +4,14 @@ class SeatFactory(
     val rows: Int,
     val columns: Int,
 ) {
-    fun createSeats(): List<Seat> {
-        val seats = mutableListOf<Seat>()
-        for (row in 0 until rows) {
-            for (column in 0 until columns) {
-                seats.add(Seat(row, column))
+    val seats: List<Seat> =
+        buildList {
+            for (row in 0 until rows) {
+                for (column in 0 until columns) {
+                    add(Seat(row, column))
+                }
             }
         }
-        return seats
-    }
 
     companion object {
         fun default() = SeatFactory(5, 4)
