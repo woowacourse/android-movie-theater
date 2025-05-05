997af22cbdaa4ef3a3ad398b12a70913a381992c
oungsi2000 <oungsi1000@gmail.com>
2025-05-05 21:53:17 +0900

    refactor: 확장함수 에러 메시지 추가

--- app/src/main/java/woowacourse/movie/view/extension/ParcelableExtension.kt
+++ app/src/main/java/woowacourse/movie/view/extension/ParcelableExtension.kt
@@ -6,16 +6,18 @@ import android.os.Bundle
 
 inline fun <reified T> Intent.getParcelableCompat(key: String): T =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        this.getParcelableExtra(key, T::class.java) ?: throw IllegalArgumentException()
+        this.getParcelableExtra(key, T::class.java) ?: throw IllegalArgumentException(ERR_INVALID_ARGUMENT)
     } else {
         @Suppress("DEPRECATION")
-        getParcelableExtra(key) as? T ?: throw IllegalArgumentException()
+        getParcelableExtra(key) as? T ?: throw IllegalArgumentException(ERR_INVALID_ARGUMENT)
     }
 
 inline fun <reified T> Bundle.getParcelableCompat(key: String): List<T> =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        getParcelableArray(key, T::class.java)?.toList() ?: throw IllegalArgumentException()
+        getParcelableArray(key, T::class.java)?.toList() ?: throw IllegalArgumentException(ERR_INVALID_ARGUMENT)
     } else {
         @Suppress("DEPRECATION")
-        getParcelableArray(key)?.toList() as? List<T> ?: throw IllegalArgumentException()
+        getParcelableArray(key)?.toList() as? List<T> ?: throw IllegalArgumentException(ERR_INVALID_ARGUMENT)
     }
+
+const val ERR_INVALID_ARGUMENT = "객체를 가져올 수 없습니다"
