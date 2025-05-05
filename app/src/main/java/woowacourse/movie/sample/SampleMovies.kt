a8fd044f60a430342d3b3fe0c7e8c21829a5ca74
yrsel <xxknu88@gmail.com>
2025-05-06 01:40:18 +0900

    chore: KtlintFormat 적용

--- app/src/main/java/woowacourse/movie/sample/SampleMovies.kt
+++ app/src/main/java/woowacourse/movie/sample/SampleMovies.kt
@@ -1,98 +1,98 @@
 package woowacourse.movie.sample
 
-import java.time.LocalDate
 import woowacourse.movie.R
 import woowacourse.movie.domain.model.Movie
 import woowacourse.movie.domain.model.ScreeningPeriod
+import java.time.LocalDate
 
 val DUMMY_MOVIES =
     mapOf(
         1L to
-                Movie(
-                    1L,
-                    "해리 포터와 마법사의 돌",
-                    R.drawable.harry_potter_one,
-                    ScreeningPeriod(
-                        LocalDate.of(2025, 4, 1),
-                        LocalDate.of(2025, 5, 30),
-                    ),
-                    152,
+            Movie(
+                1L,
+                "해리 포터와 마법사의 돌",
+                R.drawable.harry_potter_one,
+                ScreeningPeriod(
+                    LocalDate.of(2025, 4, 1),
+                    LocalDate.of(2025, 5, 30),
                 ),
+                152,
+            ),
         2L to
-                Movie(
-                    2L,
-                    "해리 포터와 비밀의 방",
-                    R.drawable.harry_potter_two,
-                    ScreeningPeriod(
-                        LocalDate.of(2025, 4, 1),
-                        LocalDate.of(2025, 5, 28),
-                    ),
-                    162,
+            Movie(
+                2L,
+                "해리 포터와 비밀의 방",
+                R.drawable.harry_potter_two,
+                ScreeningPeriod(
+                    LocalDate.of(2025, 4, 1),
+                    LocalDate.of(2025, 5, 28),
                 ),
+                162,
+            ),
         3L to
-                Movie(
-                    3L,
-                    "해리 포터와 아즈카반의 죄수",
-                    R.drawable.harry_potter_three,
-                    ScreeningPeriod(
-                        LocalDate.of(2025, 5, 1),
-                        LocalDate.of(2025, 5, 31),
-                    ),
-                    141,
+            Movie(
+                3L,
+                "해리 포터와 아즈카반의 죄수",
+                R.drawable.harry_potter_three,
+                ScreeningPeriod(
+                    LocalDate.of(2025, 5, 1),
+                    LocalDate.of(2025, 5, 31),
                 ),
+                141,
+            ),
         4L to
-                Movie(
-                    4L,
-                    "해리 포터와 불의 잔",
-                    R.drawable.harry_potter_four,
-                    ScreeningPeriod(
-                        LocalDate.of(2025, 6, 1),
-                        LocalDate.of(2025, 6, 30),
-                    ),
-                    157,
+            Movie(
+                4L,
+                "해리 포터와 불의 잔",
+                R.drawable.harry_potter_four,
+                ScreeningPeriod(
+                    LocalDate.of(2025, 6, 1),
+                    LocalDate.of(2025, 6, 30),
                 ),
+                157,
+            ),
         5L to
-                Movie(
-                    5L,
-                    "어바웃 타임",
-                    R.drawable.about_time,
-                    ScreeningPeriod(
-                        LocalDate.of(2025, 5, 6),
-                        LocalDate.of(2025, 5, 12),
-                    ),
-                    152,
+            Movie(
+                5L,
+                "어바웃 타임",
+                R.drawable.about_time,
+                ScreeningPeriod(
+                    LocalDate.of(2025, 5, 6),
+                    LocalDate.of(2025, 5, 12),
                 ),
+                152,
+            ),
         6L to
-                Movie(
-                    6L,
-                    "이터널 션샤인",
-                    R.drawable.eternal_sunshine,
-                    ScreeningPeriod(
-                        LocalDate.of(2025, 4, 29),
-                        LocalDate.of(2025, 5, 12),
-                    ),
-                    159,
+            Movie(
+                6L,
+                "이터널 션샤인",
+                R.drawable.eternal_sunshine,
+                ScreeningPeriod(
+                    LocalDate.of(2025, 4, 29),
+                    LocalDate.of(2025, 5, 12),
                 ),
+                159,
+            ),
         7L to
-                Movie(
-                    7L,
-                    "라이프 오브 파이",
-                    R.drawable.life_of_pi,
-                    ScreeningPeriod(
-                        LocalDate.of(2025, 4, 29),
-                        LocalDate.of(2025, 4, 30),
-                    ),
-                    159,
+            Movie(
+                7L,
+                "라이프 오브 파이",
+                R.drawable.life_of_pi,
+                ScreeningPeriod(
+                    LocalDate.of(2025, 4, 29),
+                    LocalDate.of(2025, 4, 30),
                 ),
+                159,
+            ),
         8L to
-                Movie(
-                    8L,
-                    "타이타닉",
-                    R.drawable.titanic,
-                    ScreeningPeriod(
-                        LocalDate.of(2025, 5, 1),
-                        LocalDate.of(2025, 5, 17),
-                    ),
-                    170,
+            Movie(
+                8L,
+                "타이타닉",
+                R.drawable.titanic,
+                ScreeningPeriod(
+                    LocalDate.of(2025, 5, 1),
+                    LocalDate.of(2025, 5, 17),
                 ),
+                170,
+            ),
     )
--- app/src/main/java/woowacourse/movie/sample/SampleTheaters.kt
+++ app/src/main/java/woowacourse/movie/sample/SampleTheaters.kt
@@ -1,12 +1,12 @@
 package woowacourse.movie.sample
 
-import java.time.LocalDateTime
 import woowacourse.movie.R
 import woowacourse.movie.domain.model.Advertisement
 import woowacourse.movie.domain.model.MovieSchedule
 import woowacourse.movie.domain.model.Theater
 import woowacourse.movie.domain.model.TheaterSchedules
 import woowacourse.movie.domain.model.Theaters
+import java.time.LocalDateTime
 
 val DUMMY_ADS =
     listOf(
@@ -25,29 +25,29 @@ val DUMMY_THEATERS =
                         TheaterSchedules(
                             mutableMapOf(
                                 1L to
-                                        setOf(
-                                            MovieSchedule(LocalDateTime.of(2025, 4, 10, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 7, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 13, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
-                                        ),
+                                    setOf(
+                                        MovieSchedule(LocalDateTime.of(2025, 4, 10, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 7, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 13, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
+                                    ),
                                 2L to
-                                        setOf(
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 16, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 20, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
-                                        ),
+                                    setOf(
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 8, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 8, 16, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 8, 20, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
+                                    ),
                                 4L to
-                                        setOf(
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 15, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 16, 16, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 17, 16, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 17, 20, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 30, 17, 0)),
-                                        ),
+                                    setOf(
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 15, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 16, 16, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 17, 16, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 17, 20, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 30, 17, 0)),
+                                    ),
                             ),
                         ),
                 ),
@@ -57,29 +57,29 @@ val DUMMY_THEATERS =
                         TheaterSchedules(
                             mutableMapOf(
                                 1L to
-                                        setOf(
-                                            MovieSchedule(LocalDateTime.of(2025, 4, 20, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 7, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 7, 13, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
-                                        ),
+                                    setOf(
+                                        MovieSchedule(LocalDateTime.of(2025, 4, 20, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 7, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 7, 13, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
+                                    ),
                                 4L to
-                                        setOf(
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 16, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 20, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
-                                        ),
+                                    setOf(
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 8, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 8, 16, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 8, 20, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
+                                    ),
                                 5L to
-                                        setOf(
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 6, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 6, 16, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 9, 16, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 9, 20, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 9, 23, 0)),
-                                        ),
+                                    setOf(
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 6, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 6, 16, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 9, 16, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 9, 20, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 9, 23, 0)),
+                                    ),
                             ),
                         ),
                 ),
@@ -89,29 +89,29 @@ val DUMMY_THEATERS =
                         TheaterSchedules(
                             mutableMapOf(
                                 3L to
-                                        setOf(
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 1, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 7, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 7, 13, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
-                                        ),
+                                    setOf(
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 1, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 7, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 7, 13, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
+                                    ),
                                 6L to
-                                        setOf(
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 16, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 9, 20, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 11, 17, 0)),
-                                        ),
+                                    setOf(
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 8, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 8, 16, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 9, 20, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 11, 17, 0)),
+                                    ),
                                 8L to
-                                        setOf(
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 6, 11, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 6, 16, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 11, 16, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 11, 20, 0)),
-                                            MovieSchedule(LocalDateTime.of(2025, 5, 11, 23, 0)),
-                                        ),
+                                    setOf(
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 6, 11, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 6, 16, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 11, 16, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 11, 20, 0)),
+                                        MovieSchedule(LocalDateTime.of(2025, 5, 11, 23, 0)),
+                                    ),
                             ),
                         ),
                 ),
