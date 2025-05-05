86c3085abc05481aa71a6ad8ebe006f37708f914
DoableTuple <doabletuple@gmail.com>
2025-05-05 17:29:47 +0900

    test: 좌석 선택 테스트 케이스 추가

--- app/src/test/java/woowacourse/movie/presenter/SeatPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presenter/SeatPresenterTest.kt
@@ -3,6 +3,8 @@ package woowacourse.movie.presenter
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import io.mockk.verifyOrder
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import woowacourse.movie.domain.model.booking.AdmissionCount
@@ -17,60 +19,104 @@ import java.time.LocalDate
 import java.time.LocalTime
 
 class SeatPresenterTest {
-    private val mockView: SeatContract.View = mockk(relaxed = true)
-    private val mockSeats: Seats = mockk(relaxed = true)
+    private lateinit var view: SeatContract.View
+    private lateinit var seats: Seats
     private lateinit var presenter: SeatPresenter
     private val booking =
         Booking(
-            movieTitle = "Test Booking",
+            movieTitle = "해리 포터와 마법사의 돌",
             screeningDate = LocalDate.now(),
             screeningTime = LocalTime.now(),
-            count = AdmissionCount(2),
-            theaterName = "CGV",
+            count = AdmissionCount(3),
+            theaterName = "선릉 극장",
         )
 
     @BeforeEach
     fun setUp() {
-        presenter = SeatPresenter(mockView, mockSeats, booking)
+        view = mockk(relaxed = true)
+        seats = mockk(relaxed = true)
+        presenter = SeatPresenter(view, seats, booking)
     }
 
     @Test
     fun `좌석을 추가할 수 없으면 토스트 메시지를 보여준다`() {
         // given
         val position = Seat(Column(1), Row(1))
-        every { mockSeats.toggleSeat(any()) }
+        every { seats.toggleSeat(any()) }
 
         // when
         presenter.changeSeat(position)
 
         // then
-        verify { mockView.notifySelectedSeatsCount(booking.count.value) }
+        verify { view.notifySelectedSeatsCount(booking.count.value) }
     }
 
     @Test
     fun `예매 버튼 클릭 시 선택된 좌석이 부족하면 토스트 메시지를 보여준다`() {
         // given
-        every { mockSeats.isNotSelectDone(booking.count.value) } returns true
+        every { seats.isNotSelectDone(booking.count.value) } returns true
 
         // when
         presenter.attemptConfirmBooking()
 
         // then
-        verify { mockView.notifySelectedSeatsCount(booking.count.value) }
-        verify(exactly = 0) { mockView.moveToBookingComplete(any()) }
+        verify { view.notifySelectedSeatsCount(booking.count.value) }
+        verify(exactly = 0) { view.moveToBookingComplete(any()) }
     }
 
     @Test
     fun `예매 버튼 클릭 시 좌석이 충분하면 예매 완료 화면으로 이동한다`() {
         // given
-        every { mockSeats.isNotSelectDone(booking.count.value) } returns false
-        every { mockSeats.item } returns setOf(Seat(Column(1), Row(1)))
-        every { mockSeats.totalPrice() } returns 10000
+        every { seats.isNotSelectDone(booking.count.value) } returns false
+        every { seats.item } returns setOf(Seat(Column(1), Row(1)))
+        every { seats.totalPrice() } returns 10000
 
         // when
         presenter.attemptConfirmBooking()
 
         // then
-        verify { mockView.moveToBookingComplete(any()) }
+        verify { view.moveToBookingComplete(any()) }
+    }
+
+    @Test
+    fun `B, S, A등급 순서로 좌석을 선택하면 가격이 10000, 25000, 37000원 순서로 표시된다`() {
+        // given
+        val seat1 = Seat(Column(1), Row(1))
+        val seat2 = Seat(Column(3), Row(1))
+        val seat3 = Seat(Column(5), Row(1))
+        val presenter = SeatPresenter(view, Seats(), booking)
+
+        // when
+        presenter.changeSeat(seat1)
+        presenter.changeSeat(seat2)
+        presenter.changeSeat(seat3)
+
+        // then
+        verifyOrder {
+            view.showPrice(10000)
+            view.showPrice(25000)
+            view.showPrice(37000)
+        }
+    }
+
+    @Test
+    fun `선택한 순서대로 좌석이 저장된다`() {
+        // given
+        val seat1 = Seat(Column(1), Row(1))
+        val seat2 = Seat(Column(3), Row(1))
+        val seat3 = Seat(Column(5), Row(1))
+        val presenter = SeatPresenter(view, Seats(), booking)
+        val capturedSeats = mutableListOf<Set<Seat>>()
+
+        // when
+        presenter.changeSeat(seat1)
+        presenter.changeSeat(seat2)
+        presenter.changeSeat(seat3)
+
+        // then
+        verify { view.showSeats(capture(capturedSeats)) }
+        assertThat(capturedSeats[0]).isEqualTo(setOf(seat1))
+        assertThat(capturedSeats[1]).isEqualTo(setOf(seat1, seat2))
+        assertThat(capturedSeats[2]).isEqualTo(setOf(seat1, seat2, seat3))
     }
 }
