a248e2a0e8cb7bdbd6fbc160c7beb34298f99d1c
yrsel <xxknu88@gmail.com>
2025-05-06 01:25:01 +0900

    refactor: 샘플 극장 데이터 구조 변경

--- app/src/main/java/woowacourse/movie/sample/SampleTheaters.kt
+++ app/src/main/java/woowacourse/movie/sample/SampleTheaters.kt
@@ -1,61 +1,17 @@
 package woowacourse.movie.sample
 
+import java.time.LocalDateTime
 import woowacourse.movie.R
 import woowacourse.movie.domain.model.Advertisement
-import woowacourse.movie.domain.model.Movie
 import woowacourse.movie.domain.model.MovieSchedule
-import woowacourse.movie.domain.model.ScreeningDateTime
-import woowacourse.movie.domain.model.ScreeningPeriod
-import woowacourse.movie.domain.model.Seats
 import woowacourse.movie.domain.model.Theater
+import woowacourse.movie.domain.model.TheaterSchedules
 import woowacourse.movie.domain.model.Theaters
-import java.time.LocalDate
-import java.time.LocalTime
 
 val DUMMY_ADS =
     listOf(
         Advertisement(
             R.drawable.woowacourse,
-        ),
-    )
-
-val DUMMY_MOVIES =
-    listOf(
-        Movie(
-            "해리 포터와 마법사의 돌",
-            R.drawable.harry_potter_one,
-            ScreeningPeriod(
-                LocalDate.of(2025, 4, 1),
-                LocalDate.of(2025, 5, 30),
-            ),
-            152,
-        ),
-        Movie(
-            "해리 포터와 비밀의 방",
-            R.drawable.harry_potter_two,
-            ScreeningPeriod(
-                LocalDate.of(2025, 4, 1),
-                LocalDate.of(2025, 5, 28),
-            ),
-            162,
-        ),
-        Movie(
-            "해리 포터와 아즈카반의 죄수",
-            R.drawable.harry_potter_three,
-            ScreeningPeriod(
-                LocalDate.of(2025, 5, 1),
-                LocalDate.of(2025, 5, 31),
-            ),
-            141,
-        ),
-        Movie(
-            "해리 포터와 불의 잔",
-            R.drawable.harry_potter_four,
-            ScreeningPeriod(
-                LocalDate.of(2025, 6, 1),
-                LocalDate.of(2025, 6, 30),
-            ),
-            157,
         ),
     )
 
@@ -65,109 +21,97 @@ val DUMMY_THEATERS =
             listOf(
                 Theater(
                     name = "CGV 강남",
-                    movieSchedules =
-                        listOf(
-                            MovieSchedule(
-                                movie = DUMMY_MOVIES[0],
-                                screeningDateTime =
-                                    ScreeningDateTime(
-                                        screeningDate = LocalDate.of(2025, 4, 10),
-                                        screeningTime = LocalTime.of(13, 0),
-                                    ),
-                                seat = Seats(),
-                            ),
-                            MovieSchedule(
-                                movie = DUMMY_MOVIES[1],
-                                screeningDateTime =
-                                    ScreeningDateTime(
-                                        screeningDate = LocalDate.of(2025, 4, 15),
-                                        screeningTime = LocalTime.of(16, 30),
-                                    ),
-                                seat = Seats(),
-                            ),
-                            MovieSchedule(
-                                movie = DUMMY_MOVIES[2],
-                                screeningDateTime =
-                                    ScreeningDateTime(
-                                        screeningDate = LocalDate.of(2025, 5, 3),
-                                        screeningTime = LocalTime.of(22, 0),
-                                    ),
-                                seat = Seats(),
+                    theaterSchedules =
+                        TheaterSchedules(
+                            mutableMapOf(
+                                1L to
+                                        setOf(
+                                            MovieSchedule(LocalDateTime.of(2025, 4, 10, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 7, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 13, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
+                                        ),
+                                2L to
+                                        setOf(
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 16, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 20, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
+                                        ),
+                                4L to
+                                        setOf(
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 15, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 16, 16, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 17, 16, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 17, 20, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 30, 17, 0)),
+                                        ),
                             ),
                         ),
                 ),
                 Theater(
                     name = "롯데시네마 건대입구",
-                    movieSchedules =
-                        listOf(
-                            MovieSchedule(
-                                movie = DUMMY_MOVIES[1],
-                                screeningDateTime =
-                                    ScreeningDateTime(
-                                        screeningDate = LocalDate.of(2025, 4, 20),
-                                        screeningTime = LocalTime.of(12, 0),
-                                    ),
-                                seat = Seats(),
-                            ),
-                            MovieSchedule(
-                                movie = DUMMY_MOVIES[1],
-                                screeningDateTime =
-                                    ScreeningDateTime(
-                                        screeningDate = LocalDate.of(2025, 5, 1),
-                                        screeningTime = LocalTime.of(12, 0),
-                                    ),
-                                seat = Seats(),
-                            ),
-                            MovieSchedule(
-                                movie = DUMMY_MOVIES[1],
-                                screeningDateTime =
-                                    ScreeningDateTime(
-                                        screeningDate = LocalDate.of(2025, 5, 1),
-                                        screeningTime = LocalTime.of(23, 0),
-                                    ),
-                                seat = Seats(),
-                            ),
-                            MovieSchedule(
-                                movie = DUMMY_MOVIES[2],
-                                screeningDateTime =
-                                    ScreeningDateTime(
-                                        screeningDate = LocalDate.of(2025, 5, 10),
-                                        screeningTime = LocalTime.of(18, 45),
-                                    ),
-                                seat = Seats(),
+                    theaterSchedules =
+                        TheaterSchedules(
+                            mutableMapOf(
+                                1L to
+                                        setOf(
+                                            MovieSchedule(LocalDateTime.of(2025, 4, 20, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 7, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 7, 13, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
+                                        ),
+                                4L to
+                                        setOf(
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 16, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 20, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
+                                        ),
+                                5L to
+                                        setOf(
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 6, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 6, 16, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 9, 16, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 9, 20, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 9, 23, 0)),
+                                        ),
                             ),
                         ),
                 ),
                 Theater(
                     name = "메가박스 코엑스",
-                    movieSchedules =
-                        listOf(
-                            MovieSchedule(
-                                movie = DUMMY_MOVIES[2],
-                                screeningDateTime =
-                                    ScreeningDateTime(
-                                        screeningDate = LocalDate.of(2025, 5, 5),
-                                        screeningTime = LocalTime.of(11, 30),
-                                    ),
-                                seat = Seats(),
-                            ),
-                            MovieSchedule(
-                                movie = DUMMY_MOVIES[3],
-                                screeningDateTime =
-                                    ScreeningDateTime(
-                                        screeningDate = LocalDate.of(2025, 6, 1),
-                                        screeningTime = LocalTime.of(19, 0),
-                                    ),
-                                seat = Seats(),
-                            ),
-                            MovieSchedule(
-                                movie = DUMMY_MOVIES[3],
-                                screeningDateTime =
-                                    ScreeningDateTime(
-                                        screeningDate = LocalDate.of(2025, 6, 1),
-                                        screeningTime = LocalTime.of(22, 0),
-                                    ),
-                                seat = Seats(),
+                    theaterSchedules =
+                        TheaterSchedules(
+                            mutableMapOf(
+                                3L to
+                                        setOf(
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 1, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 7, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 7, 13, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 17, 0)),
+                                        ),
+                                6L to
+                                        setOf(
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 8, 16, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 9, 20, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 10, 15, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 11, 17, 0)),
+                                        ),
+                                8L to
+                                        setOf(
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 6, 11, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 6, 16, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 11, 16, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 11, 20, 0)),
+                                            MovieSchedule(LocalDateTime.of(2025, 5, 11, 23, 0)),
+                                        ),
                             ),
                         ),
                 ),
