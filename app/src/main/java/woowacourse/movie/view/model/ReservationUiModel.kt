8499c026faf995afcf4908a23e107944d3a05dfd
Ham BeomJoon <hbj0802@naver.com>
2025-05-06 18:44:45 +0900

    refactor: 최종 예매 정보 모델 수정

--- app/src/main/java/woowacourse/movie/view/model/ReservationUiModel.kt
+++
@@ -1,9 +0,0 @@
-package woowacourse.movie.view.model
-
-data class ReservationUiModel(
-    val title: String,
-    val movieDate: String,
-    val movieTime: String,
-    val ticketCount: Int,
-    val theaterName: String,
-)
