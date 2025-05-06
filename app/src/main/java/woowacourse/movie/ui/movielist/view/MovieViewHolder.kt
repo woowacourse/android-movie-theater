3e219b86478db7032f6180f47ad805ea251b01f6
yrsel <xxknu88@gmail.com>
2025-05-06 16:03:38 +0900

    refactor: MovieAdapter 영화 id 값만 활용하도록 변경

--- app/src/main/java/woowacourse/movie/ui/movielist/view/MovieViewHolder.kt
+++ app/src/main/java/woowacourse/movie/ui/movielist/view/MovieViewHolder.kt
@@ -1,29 +1,18 @@
 package woowacourse.movie.ui.movielist.view
 
-import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
-import woowacourse.movie.R
 import woowacourse.movie.databinding.MovieItemBinding
 import woowacourse.movie.domain.model.Movie
 import woowacourse.movie.utils.StringFormatter
 
 class MovieViewHolder(
-    val itemBinding: MovieItemBinding,
-    val onClickBooking: (Movie) -> Unit,
+    private val itemBinding: MovieItemBinding,
+    val onClickBooking: (Long) -> Unit,
 ) : RecyclerView.ViewHolder(itemBinding.root) {
-    private var currentMovieItem: Movie? = null
-    private val bookingBtn: Button = itemView.findViewById<Button>(R.id.btn_booking)
-
-    init {
-        bookingBtn.setOnClickListener {
-            currentMovieItem?.let {
-                onClickBooking(it)
-            }
-        }
-    }
-
     fun bind(item: Movie) {
-        currentMovieItem = item
+        itemBinding.btnBooking.setOnClickListener {
+            onClickBooking(item.id)
+        }
         itemBinding.stringFormatter = StringFormatter
         itemBinding.movie = item
     }
