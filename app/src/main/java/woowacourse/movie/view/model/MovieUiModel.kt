7376910e54d7b57c8331e78d8802bb3dc539244a
Ham BeomJoon <hbj0802@naver.com>
2025-05-06 18:30:22 +0900

    refactor: MovieUiModel date uiModel로 변경

--- app/src/main/java/woowacourse/movie/view/model/MovieUiModel.kt
+++ app/src/main/java/woowacourse/movie/view/model/MovieUiModel.kt
@@ -3,26 +3,16 @@ package woowacourse.movie.view.model
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import woowacourse.movie.domain.model.Movie
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 @Parcelize
 data class MovieUiModel(
     override val name: String,
     val poster: Int,
-    val startDate: String,
-    val endDate: String,
+    val date: MovieDateUiModel,
     val runningTime: Int,
 ) : MovieListItem(ItemViewType.MOVIE_ITEM),
     Parcelable
 
-fun MovieUiModel.toDomainModel(): Movie =
-    Movie(
-        title = this.name,
-        poster = this.poster,
-        startDate = this.startDate.toLocalDate(),
-        endDate = this.endDate.toLocalDate(),
-        runningTime = this.runningTime,
-    )
+fun Movie.toPresentation(): MovieUiModel = MovieUiModel(title, poster, date.toPresentation(), runningTime)
 
-fun String.toLocalDate(pattern: String = "yyyy.MM.dd"): LocalDate = LocalDate.parse(this, DateTimeFormatter.ofPattern(pattern))
+fun MovieUiModel.toDomain(): Movie = Movie(name, poster, date.toDomain(), runningTime)
