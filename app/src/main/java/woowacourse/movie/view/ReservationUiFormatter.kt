edaeb6d1d982ace16e12eb81f6f61bed275ad253
medAndro <med4ndro@gmail.com>
2025-05-06 18:18:19 +0900

    refactor: 날짜 시간 포맷 변수명 수정

--- app/src/main/java/woowacourse/movie/view/ReservationUiFormatter.kt
+++ app/src/main/java/woowacourse/movie/view/ReservationUiFormatter.kt
@@ -5,13 +5,13 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 object ReservationUiFormatter {
-    fun localDateToUI(date: LocalDate): String = date.format(DATE_UI_FORMATTER)
+    fun localDateToUI(date: LocalDate): String = date.format(DATE_UI_YYYY_MM_DD_DOT)
 
-    fun movieTimeToUI(time: Int): String = TIME_UI_SUFFIX.format(time)
+    fun movieTimeToUI(time: Int): String = TIME_UI_COLON.format(time)
 
-    fun priceToUI(price: Int): String = PRICE_UI_FORMATTER.format(price)
+    fun priceToUI(price: Int): String = PRICE_UI_WITH_COMMA.format(price)
 
-    private val DATE_UI_FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd")
-    private const val TIME_UI_SUFFIX = "%d:00"
-    private val PRICE_UI_FORMATTER = DecimalFormat("#,###")
+    private val DATE_UI_YYYY_MM_DD_DOT = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+    private const val TIME_UI_COLON = "%d:00"
+    private val PRICE_UI_WITH_COMMA = DecimalFormat("#,###")
 }
