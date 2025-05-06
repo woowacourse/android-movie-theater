752285eddf3e38f2bd4fd05902eec581aa93fdcb
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-05-06 10:42:38 +0900

    refactor: Cinema를 class로 변경

--- README.md
+++ README.md
@@ -20,3 +20,4 @@
 - [x] 탭을 savedInstanceState가 없을 시점에만 main으로 변경하도록 수정
 - [x] fragment를 재사용하도록 수정
 - [x] adapter 생성하는 로직과 items를 초기화하는 로직분리
+- [x] Cinema를 class로 변경
--- app/src/main/java/woowacourse/movie/domain/cinema/Cinema.kt
+++ app/src/main/java/woowacourse/movie/domain/cinema/Cinema.kt
@@ -5,12 +5,12 @@ import woowacourse.movie.domain.reservation.ShowtimePolicy
 import java.time.LocalDate
 import java.time.LocalTime
 
-data class Cinema(
+class Cinema(
     val name: String,
     private val _screenings: List<Screening>,
     val showtimePolicy: ShowtimePolicy,
 ) {
-    val screenings = _screenings.map { it.copy() }
+    val screening get() = _screenings.map { it.copy() }
 
     fun showtimeCount(screening: Screening) = showtimes(screening).size
 
@@ -20,5 +20,23 @@ data class Cinema(
     ): List<LocalTime> {
         if (date == null) return screening.showtimes(showtimePolicy)
         return screening.showtimes(date, showtimePolicy)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Cinema
+
+        return name == other.name &&
+            screening == other.screening &&
+            showtimePolicy == other.showtimePolicy
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + screening.hashCode()
+        result = 31 * result + showtimePolicy.hashCode()
+        return result
     }
 }
