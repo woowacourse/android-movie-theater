8ff4ae2df6727b41d95adfda3bffaa056ffdec1f
Ham BeomJoon <hbj0802@naver.com>
2025-05-06 18:46:46 +0900

    refactor: TheaterViewHolder 데이터 바인딩 적용

--- app/src/main/java/woowacourse/movie/view/theater/TheaterViewHolder.kt
+++ app/src/main/java/woowacourse/movie/view/theater/TheaterViewHolder.kt
@@ -1,8 +1,31 @@
 package woowacourse.movie.view.theater
 
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import woowacourse.movie.R
 import woowacourse.movie.databinding.ItemTheaterBinding
-import woowacourse.movie.view.model.TheaterUIModel
+import woowacourse.movie.view.model.TheaterUiModel
+
+@BindingAdapter("theaterNameText")
+fun setTheaterNameText(
+    view: TextView,
+    theater: TheaterUiModel?,
+) {
+    theater ?: return
+    val theaterName = theater.name
+    view.text = view.context.getString(R.string.bottom_sheet_dialog_theater_name, theaterName)
+}
+
+@BindingAdapter("timeSlotText")
+fun setTimeSlotText(
+    view: TextView,
+    theater: TheaterUiModel?,
+) {
+    theater ?: return
+    val count = theater.totalScreeningTimes
+    view.text = view.context.getString(R.string.bottom_sheet_dialog_time_slot, count)
+}
 
 class TheaterViewHolder(
     private val binding: ItemTheaterBinding,
@@ -12,7 +35,7 @@ class TheaterViewHolder(
         binding.theaterClickListener = clickListener
     }
 
-    fun bind(theaterUIModel: TheaterUIModel) {
+    fun bind(theaterUIModel: TheaterUiModel) {
         binding.theater = theaterUIModel
     }
 }
