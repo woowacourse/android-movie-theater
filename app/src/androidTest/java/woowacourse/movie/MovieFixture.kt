a9947dcd8522e3e7f577fb2e0f4e7ff97eefd0ad
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-05-06 16:34:33 +0900

    fix(MovieBookedActivity): intent에 theater 정보 추가

--- app/src/androidTest/java/woowacourse/movie/MovieFixture.kt
+++ app/src/androidTest/java/woowacourse/movie/MovieFixture.kt
@@ -2,11 +2,14 @@ package woowacourse.movie
 
 import woowacourse.movie.domain.BookingStatus
 import woowacourse.movie.domain.Movie
+import woowacourse.movie.domain.Movies
 import woowacourse.movie.domain.ScreeningPeriod
+import woowacourse.movie.domain.Theater
 import woowacourse.movie.domain.Title
 import woowacourse.movie.domain.seat.BookingSeats
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 
 object MovieFixture {
     const val HARRY_POTTER_TITLE = "해리포터와 마법사의 돌"
@@ -27,6 +30,47 @@ object MovieFixture {
             152,
         )
 
+    val MOVIES =
+        Movies(
+            mapOf(
+                Title("해리포터와 불의 잔") to
+                        Movie(
+                            Title("해리포터와 불의 잔"),
+                            R.drawable.harry_potter_and_the_goblet_of_fire,
+                            ScreeningPeriod.ofDot("2025.06.01", "2025.06.30"),
+                            157,
+                        ),
+                Title("해리포터와 아즈카반의 죄수") to
+                        Movie(
+                            Title("해리포터와 아즈카반의 죄수"),
+                            R.drawable.harry_potter_and_the_prisoner_of_azkaban,
+                            ScreeningPeriod.ofDot("2025.05.01", "2025.05.31"),
+                            141,
+                        ),
+            ),
+        )
+
+
     val BOOKING_STATUS =
         BookingStatus(MOVIE, true, BookingSeats(2), LocalDateTime.of(2025, 4, 30, 9, 0, 0))
+
+    val THEATER = Theater(
+        "선릉",
+        MOVIES,
+        mapOf(
+            Title("해리포터와 불의 잔") to
+                    listOf(
+                        LocalTime.of(13, 0),
+                        LocalTime.of(15, 0),
+                        LocalTime.of(17, 0),
+                        LocalTime.of(19, 0),
+                    ),
+            Title("해리포터와 아즈카반의 죄수") to
+                    listOf(
+                        LocalTime.of(9, 0),
+                        LocalTime.of(11, 0),
+                        LocalTime.of(15, 0),
+                    ),
+        ),
+    )
 }
--- app/src/androidTest/java/woowacourse/movie/view/MovieBookedActivityTest.kt
+++ app/src/androidTest/java/woowacourse/movie/view/MovieBookedActivityTest.kt
@@ -7,6 +7,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -14,6 +15,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import woowacourse.movie.MovieFixture
+import woowacourse.movie.R
 import woowacourse.movie.moviebooked.MovieBookedActivity
 
 @RunWith(AndroidJUnit4::class)
@@ -30,15 +32,17 @@ class MovieBookedActivityTest {
                 MovieBookedActivity::class.java,
             ).apply {
                 putExtra("bookingStatus", MovieFixture.BOOKING_STATUS)
+                putExtra("theater", MovieFixture.THEATER)
             }
         scenario = ActivityScenario.launch(intent)
     }
 
     @Test
     fun 예약정보가_올바르게_표시된다() {
-        onView(withText(MovieFixture.HARRY_POTTER_TITLE)).check(matches(isDisplayed()))
-        onView(withText(MovieFixture.BOOKING_DATETIME)).check(matches(isDisplayed()))
-        onView(withText(MovieFixture.BOOKING_TICKET_COUNT)).check(matches(isDisplayed()))
-        onView(withText(MovieFixture.BOOKING_TICKET_PRICE)).check(matches(isDisplayed()))
+        onView(withId(R.id.movie_ticket_price)).check(matches(isDisplayed()))
+        onView(withId(R.id.booking_theater_name)).check(matches(isDisplayed()))
+        onView(withId(R.id.member_count)).check(matches(isDisplayed()))
+        onView(withId(R.id.booking_date_time)).check(matches(isDisplayed()))
+        onView(withId(R.id.movie_title)).check(matches(isDisplayed()))
     }
 }
