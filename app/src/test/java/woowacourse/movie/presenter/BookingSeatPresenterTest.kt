5e924820eca4b93639387f878a8da20e4bb79267
yrsel <xxknu88@gmail.com>
2025-05-07 03:42:00 +0900

    test: BookingSeatPresenterTest 테스트 코드 통과하도록 수정

--- app/src/test/java/woowacourse/movie/presenter/BookingSeatPresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presenter/BookingSeatPresenterTest.kt
@@ -1,11 +1,12 @@
 package woowacourse.movie.presenter
 
-import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import java.time.LocalDateTime
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import woowacourse.movie.domain.model.Headcount
+import woowacourse.movie.domain.model.MovieSchedule
 import woowacourse.movie.domain.model.Seat
 import woowacourse.movie.ui.seat.BookingSeatContract
 import woowacourse.movie.ui.seat.BookingSeatPresenter
@@ -17,48 +18,40 @@ class BookingSeatPresenterTest {
     @BeforeEach
     fun setUp() {
         view = mockk(relaxed = true)
-        every { view.getHeadcount() } returns Headcount(2)
-        every { view.getMovieTitle() } returns "해리 포터와 마법사의 돌"
         presenter = BookingSeatPresenter(view)
-    }
-
-    @Test
-    fun `가격을 업데이트하면 뷰에 반영된다`() {
-        presenter.refreshTotalPrice()
-        verify { view.setTotalPrice(any()) }
-    }
-
-    @Test
-    fun `제목을 업데이트하면 뷰에 반영된다`() {
-        presenter.refreshMovieTitle()
-        verify { view.setMovieTitle(any()) }
+        presenter.loadBookingSeatInfo(
+            1L, MovieSchedule(LocalDateTime.of(2025, 5, 7, 10, 0)),
+            Headcount(2), "선릉 극장"
+        )
     }
 
     @Test
     fun `확인 버튼을 업데이트하면 활성화되거나 비활성화된다`() {
-        presenter.refreshConfirmButton()
-        verify { view.setConfirmButton(any()) }
+        presenter.updateConfirmButton()
+        verify { view.showConfirmButton(any()) }
     }
 
     @Test
     fun `좌석을 선택하면 좌석이 추가되고 View가 업데이트 된다`() {
-        presenter.selectSeat("A1")
-        verify { view.toggleSeat(Seat.fromSeatTag("A1"), true) }
-        verify { view.setTotalPrice(any()) }
-        verify { view.setConfirmButton(any()) }
+        presenter.updateSeat("A1")
+        verify { view.showSeatView(Seat.fromSeatTag("A1"), true) }
+        verify { view.showTotalPrice(any()) }
+        verify { view.showConfirmButton(any()) }
     }
 
     @Test
     fun `선택한 좌석 수가 인원 수와 다르면 확인 버튼이 비활성화된다`() {
-        presenter.selectSeat("A1")
+        presenter.updateSeat("A1")
         // Headcount는 2이지만, 좌석이 아직 하나만 선택되었다.
-        verify { view.setConfirmButton(false) }
+        presenter.updateConfirmButton()
+        verify { view.showConfirmButton(false) }
     }
 
     @Test
     fun `선택한 좌석 수가 인원 수와 같으면 확인 버튼이 활성화된다`() {
-        presenter.selectSeat("A1")
-        presenter.selectSeat("A2")
-        verify { view.setConfirmButton(true) }
+        presenter.updateSeat("A1")
+        presenter.updateSeat("A2")
+        presenter.updateConfirmButton()
+        verify { view.showConfirmButton(true) }
     }
 }
