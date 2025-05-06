4167f3e71975eb763c19b30d78d55f74bdabec64
cucumber99 <dlsguq1@gmail.com>
2025-05-07 04:15:13 +0900

    feat: 광고 추가 기능 분리

---
+++ app/src/main/java/woowacourse/movie/domain/model/Advertise.kt
@@ -0,0 +1,22 @@
+package woowacourse.movie.domain.model
+
+import woowacourse.movie.domain.model.movie.Movie
+import woowacourse.movie.presentation.movies.MoviesItem
+
+class Advertise(private val movies: List<Movie>) {
+    fun insertAdvertisement(): List<MoviesItem> {
+        val result = mutableListOf<MoviesItem>()
+        movies.forEachIndexed { index, movie ->
+            result.add(MoviesItem.MovieItem(movie))
+            if ((index + INDEX_INTERVAL) % ADS_INTERVAL == 0) {
+                result.add(MoviesItem.AdvertisementItem())
+            }
+        }
+        return result
+    }
+
+    companion object {
+        private const val INDEX_INTERVAL = 1
+        private const val ADS_INTERVAL = 3
+    }
+}
