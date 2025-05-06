2d837bce836c7730d68903bc022a783a3b3d058c
yrsel <xxknu88@gmail.com>
2025-05-06 14:04:25 +0900

    refactor: Advertisement에서 광고 주기 판단

--- app/src/main/java/woowacourse/movie/domain/model/Advertisement.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Advertisement.kt
@@ -1,5 +1,10 @@
 package woowacourse.movie.domain.model
 
 class Advertisement(
+    private val itemsBeforeAd: Int,
     val image: Int,
-)
+) {
+    fun isInsertAdvertise(count: Int): Boolean {
+        return count == itemsBeforeAd
+    }
+}
