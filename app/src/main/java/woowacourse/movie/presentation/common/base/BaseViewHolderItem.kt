1b97f9dc773a6ae6be4ebb212bd95b0343950182
moondev03 <moondev03@gmail.com>
2025-05-06 18:04:47 +0900

    refactor: BaseViewHolderItem equals override 제거

--- app/src/main/java/woowacourse/movie/presentation/common/base/BaseViewHolderItem.kt
+++ app/src/main/java/woowacourse/movie/presentation/common/base/BaseViewHolderItem.kt
@@ -3,6 +3,4 @@ package woowacourse.movie.presentation.common.base
 interface BaseViewHolderItem {
     val id: Long
     val viewType: Int
-
-    override operator fun equals(other: Any?): Boolean
 }
