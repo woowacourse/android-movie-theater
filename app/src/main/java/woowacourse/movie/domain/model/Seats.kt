006a02ce4695daf3a3336535eca1f6fe6264cc78
yrsel <xxknu88@gmail.com>
2025-05-07 02:35:41 +0900

    refactor: Seats 기존 좌석 고려할 수 있도록 구성

--- app/src/main/java/woowacourse/movie/domain/model/Seats.kt
+++ app/src/main/java/woowacourse/movie/domain/model/Seats.kt
@@ -2,16 +2,30 @@ package woowacourse.movie.domain.model
 
 import java.io.Serializable
 
-class Seats : Serializable {
-    private val _seats: MutableSet<Seat> = mutableSetOf()
-    val seats get() = _seats.toSet()
-    val size get() = _seats.size
+class Seats(
+    _reservedSeats: MutableSet<Seat> = mutableSetOf(),
+    private val _reservingSeats: MutableSet<Seat> = mutableSetOf(),
+) : Serializable {
+    val reservedSeats = _reservedSeats.toSet()
+    val reservingSeats get() = _reservingSeats.toSet()
 
-    fun contains(seat: Seat) = _seats.contains(seat)
+    fun isReservedSeat(seat: Seat): Boolean {
+        return reservedSeats.contains(seat) || reservingSeats.contains(seat)
+    }
 
-    fun add(seat: Seat) = _seats.add(seat)
+    fun reserve(seat: Seat) {
+        _reservingSeats.add(seat)
+    }
 
-    fun remove(seat: Seat) = _seats.remove(seat)
+    fun cancelReserve(seat: Seat) {
+        _reservingSeats.remove(seat)
+    }
 
-    fun totalPrice(): Int = _seats.sumOf { it.price() }
+    fun isSeatSelectionComplete(headcount: Headcount): Boolean {
+        return reservingSeats.size == headcount.count
+    }
+
+    fun totalPrice(): Int {
+        return reservingSeats.sumOf { seat -> seat.price() }
+    }
 }
