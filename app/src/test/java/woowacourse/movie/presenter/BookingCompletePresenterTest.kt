f6aa5dea9392057e08c58280ff047ea9b447f79e
yrsel <xxknu88@gmail.com>
2025-05-07 03:45:32 +0900

    test: BookingCompletePresenterTest 테스트 코드 통과하도록 수정

--- app/src/test/java/woowacourse/movie/presenter/BookingCompletePresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presenter/BookingCompletePresenterTest.kt
@@ -1,19 +1,19 @@
-package woowacourse.movie.presenter
+ package woowacourse.movie.presenter
 
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import woowacourse.movie.domain.model.BookedTicket
-import woowacourse.movie.domain.model.Headcount
-import woowacourse.movie.domain.model.Seat
-import woowacourse.movie.domain.model.Seats
-import woowacourse.movie.ui.complete.BookingCompleteContract
-import woowacourse.movie.ui.complete.BookingCompletePresenter
-import java.time.LocalDateTime
+ import io.mockk.mockk
+ import io.mockk.verify
+ import java.time.LocalDateTime
+ import org.junit.jupiter.api.BeforeEach
+ import org.junit.jupiter.api.Test
+ import woowacourse.movie.domain.model.BookedTicket
+ import woowacourse.movie.domain.model.Headcount
+ import woowacourse.movie.domain.model.MovieSchedule
+ import woowacourse.movie.domain.model.Seat
+ import woowacourse.movie.domain.model.Seats
+ import woowacourse.movie.ui.complete.BookingCompleteContract
+ import woowacourse.movie.ui.complete.BookingCompletePresenter
 
-class BookingCompletePresenterTest {
+ class BookingCompletePresenterTest {
     private lateinit var view: BookingCompleteContract.View
     private lateinit var presenter: BookingCompletePresenter
 
@@ -21,25 +21,28 @@ class BookingCompletePresenterTest {
     fun setUp() {
         view = mockk(relaxed = true)
         presenter = BookingCompletePresenter(view)
-        every { view.getBookedTicket() } returns
-            BookedTicket(
-                "해리 포터",
-                Headcount(1),
-                LocalDateTime.of(2025, 1, 1, 12, 0),
-                Seats().apply { Seat(1, 1) },
-                "선릉 극장",
+        presenter.loadBookedTicket(
+            bookedTicket = BookedTicket(
+                theaterName = "선릉 극장",
+                movieTitle = "해리 포터",
+                movieSchedule = MovieSchedule(LocalDateTime.of(2025, 1, 1, 12, 0)
+                ,Seats().apply { reserve(Seat(1,1)) }),
+                headcount = Headcount(1)
             )
+        )
     }
 
     @Test
     fun `티켓의 가격이 뷰에 반영된다`() {
-        presenter.refreshTicketPrice()
-        verify { view.setBookedTicketPrice(any()) }
+        verify { view.showTotalPrice(any()) }
     }
 
     @Test
     fun `티켓의 정보가 업데이트되면 뷰에 반영된다`() {
-        presenter.refreshBookedTicketDisplay()
-        verify { view.setBookedTicket(any()) }
+        verify {
+            view.showMovieTitle(any())
+            view.showScreeningDateTime(any())
+            view.showDetailInfos(any(),any(),any())
+        }
     }
-}
+ }
