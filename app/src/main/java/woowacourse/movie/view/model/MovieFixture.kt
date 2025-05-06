8fc267a1224cd88fd8feafc921f5f2b68c89b269
Ham BeomJoon <hbj0802@naver.com>
2025-05-06 18:31:32 +0900

    chore: MovieFixture 메서드 위치 변경

--- app/src/main/java/woowacourse/movie/view/model/MovieFixture.kt
+++ app/src/main/java/woowacourse/movie/view/model/MovieFixture.kt
@@ -2,10 +2,80 @@ package woowacourse.movie.view.model
 
 import woowacourse.movie.R
 import woowacourse.movie.domain.model.Movie
+import woowacourse.movie.domain.model.MovieDate
+import woowacourse.movie.domain.model.Screening
+import woowacourse.movie.domain.model.Theater
+import woowacourse.movie.domain.model.Theaters
 import java.time.LocalDate
 
 object MovieFixture {
-    val screenings =
+    val dummyTheaters: Theaters =
+        Theaters(
+            listOf("선릉", "잠실", "강남").map { theaterName ->
+                val schedule = dummyTheaterMap[theaterName] ?: emptyMap()
+                val screenings =
+                    schedule.mapNotNull { (title, times) ->
+                        val movie = dummyMovies[title] ?: error("Movie not found: $title")
+                        if (times.isEmpty()) null else Screening(movie, times)
+                    }
+                Theater(theaterName, screenings)
+            },
+        )
+
+    private val dummyMovies =
+        mapOf(
+            "라라랜드" to
+                Movie(
+                    "라라랜드",
+                    R.drawable.lalaland,
+                    MovieDate(LocalDate.of(2025, 5, 1), LocalDate.of(2025, 5, 30)),
+                    123,
+                ),
+            "승부" to
+                Movie(
+                    "승부",
+                    R.drawable.match,
+                    MovieDate(LocalDate.of(2025, 4, 11), LocalDate.of(2025, 4, 30)),
+                    114,
+                ),
+            "야당" to
+                Movie(
+                    "야당",
+                    R.drawable.yadang,
+                    MovieDate(LocalDate.of(2025, 4, 21), LocalDate.of(2025, 4, 30)),
+                    109,
+                ),
+            "월플라워" to
+                Movie(
+                    "월플라워",
+                    R.drawable.wall_flower,
+                    MovieDate(LocalDate.of(2025, 4, 1), LocalDate.of(2025, 5, 30)),
+                    129,
+                ),
+            "줄무늬 파자마를 입은 소년" to
+                Movie(
+                    "줄무늬 파자마를 입은 소년",
+                    R.drawable.pajama,
+                    MovieDate(LocalDate.of(2025, 4, 11), LocalDate.of(2025, 5, 30)),
+                    123,
+                ),
+            "A.I." to
+                Movie(
+                    "A.I.",
+                    R.drawable.movie_ai,
+                    MovieDate(LocalDate.of(2025, 4, 21), LocalDate.of(2025, 5, 30)),
+                    149,
+                ),
+            "이미테이션 게임" to
+                Movie(
+                    "이미테이션 게임",
+                    R.drawable.imitation_game,
+                    MovieDate(LocalDate.of(2025, 4, 21), LocalDate.of(2025, 5, 30)),
+                    130,
+                ),
+        )
+
+    private val dummyTheaterMap =
         mapOf(
             "선릉" to
                 mapOf(
@@ -29,66 +99,6 @@ object MovieFixture {
                     "월플라워" to listOf(18, 21),
                     "줄무늬 파자마를 입은 소년" to listOf(19),
                     "야당" to listOf(10, 14),
-                ),
-        )
-
-    val movies =
-        mapOf(
-            "라라랜드" to
-                Movie(
-                    "라라랜드",
-                    R.drawable.lalaland,
-                    LocalDate.of(2025, 4, 1),
-                    LocalDate.of(2025, 4, 30),
-                    123,
-                ),
-            "승부" to
-                Movie(
-                    "승부",
-                    R.drawable.match,
-                    LocalDate.of(2025, 4, 11),
-                    LocalDate.of(2025, 4, 30),
-                    114,
-                ),
-            "야당" to
-                Movie(
-                    "야당",
-                    R.drawable.yadang,
-                    LocalDate.of(2025, 4, 21),
-                    LocalDate.of(2025, 4, 30),
-                    109,
-                ),
-            "월플라워" to
-                Movie(
-                    "월플라워",
-                    R.drawable.wall_flower,
-                    LocalDate.of(2025, 4, 1),
-                    LocalDate.of(2025, 5, 30),
-                    129,
-                ),
-            "줄무늬 파자마를 입은 소년" to
-                Movie(
-                    "줄무늬 파자마를 입은 소년",
-                    R.drawable.pajama,
-                    LocalDate.of(2025, 4, 11),
-                    LocalDate.of(2025, 5, 30),
-                    123,
-                ),
-            "A.I." to
-                Movie(
-                    "A.I.",
-                    R.drawable.movie_ai,
-                    LocalDate.of(2025, 4, 21),
-                    LocalDate.of(2025, 5, 30),
-                    149,
-                ),
-            "이미테이션 게임" to
-                Movie(
-                    "이미테이션 게임",
-                    R.drawable.imitation_game,
-                    LocalDate.of(2025, 4, 21),
-                    LocalDate.of(2025, 5, 30),
-                    130,
                 ),
         )
 }
