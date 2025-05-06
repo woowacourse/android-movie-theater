c218889aa01431000858ac1c30f2aff01e77ab61
Ham BeomJoon <hbj0802@naver.com>
2025-05-06 19:16:54 +0900

    test: test Fixture 수정

--- app/src/test/java/woowacourse/movie/domain/model/Fixture.kt
+++ app/src/test/java/woowacourse/movie/domain/model/Fixture.kt
@@ -1,39 +1,38 @@
 package woowacourse.movie.domain.model
 
 import woowacourse.movie.R
-import woowacourse.movie.view.model.MovieTicket
+import woowacourse.movie.view.model.MovieDateUiModel
 import woowacourse.movie.view.model.MovieUiModel
-import woowacourse.movie.view.model.ReservationInfo
-import woowacourse.movie.view.model.TheaterUIModel
+import woowacourse.movie.view.model.ReservationInfoUiModel
+import woowacourse.movie.view.model.SeatUiModel
+import woowacourse.movie.view.model.SeatsUiModel
+import woowacourse.movie.view.model.TheaterUiModel
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 val dummyMovie =
     MovieUiModel(
         "라라랜드",
         R.drawable.lalaland,
-        "2025.04.01",
-        "2025.04.30",
+        MovieDateUiModel(
+            LocalDate.of(2025, 5, 1),
+            LocalDate.of(2025, 5, 30),
+        ),
         120,
     )
 
-val dummyTicket =
-    MovieTicket(
-        "라라랜드",
-        LocalDate.of(2025, 4, 1),
-        "14:00",
-        3,
-        "선릉",
-    )
+val dummyTheaterUIModel =
+    TheaterUiModel("선릉", 20)
 
-val dummyReservationInfo =
-    ReservationInfo(
+val dummySeats =
+    SeatsUiModel(listOf(SeatUiModel("A1", 10000), SeatUiModel("C1", 15000)))
+
+val dummyReservationInfoUiModel =
+    ReservationInfoUiModel(
         "라라랜드",
-        LocalDate.of(2025, 4, 1),
-        "14:00",
-        Seats.create(),
+        LocalDateTime.of(LocalDate.of(2025, 4, 1), LocalTime.of(14, 0)),
+        dummySeats,
         20000,
         "선릉",
     )
-
-val dummyTheaterUIModel =
-    TheaterUIModel("선릉", dummyMovie, 1)
