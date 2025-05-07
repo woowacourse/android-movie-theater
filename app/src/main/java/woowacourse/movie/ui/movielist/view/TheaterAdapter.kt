fde9a5f9614fe640fdca7567789249277582a1ca
yrsel <xxknu88@gmail.com>
2025-05-07 17:58:25 +0900

    refactor: TheaterAdapter ViewHolder 타입 변경

--- app/src/main/java/woowacourse/movie/ui/movielist/view/TheaterAdapter.kt
+++ app/src/main/java/woowacourse/movie/ui/movielist/view/TheaterAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.databinding.TheaterItemBinding
 import woowacourse.movie.domain.model.Theater
 import woowacourse.movie.ui.movielist.view.listener.TheaterClickListener
@@ -12,37 +11,37 @@ import woowacourse.movie.ui.movielist.view.listener.TheaterClickListener
 class TheaterAdapter(
     private val movieId: Long,
     private val clickListener: TheaterClickListener,
-) : ListAdapter<Theater, RecyclerView.ViewHolder>(
-        object : DiffUtil.ItemCallback<Theater>() {
-            override fun areItemsTheSame(
-                oldItem: Theater,
-                newItem: Theater,
-            ): Boolean {
-                return oldItem.name == newItem.name
-            }
+) : ListAdapter<Theater, TheaterViewHolder>(
+    object : DiffUtil.ItemCallback<Theater>() {
+        override fun areItemsTheSame(
+            oldItem: Theater,
+            newItem: Theater,
+        ): Boolean {
+            return oldItem.name == newItem.name
+        }
 
-            override fun areContentsTheSame(
-                oldItem: Theater,
-                newItem: Theater,
-            ): Boolean {
-                return oldItem == newItem
-            }
-        },
-    ) {
+        override fun areContentsTheSame(
+            oldItem: Theater,
+            newItem: Theater,
+        ): Boolean {
+            return oldItem == newItem
+        }
+    },
+) {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
-    ): RecyclerView.ViewHolder {
+    ): TheaterViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val itemBinding = TheaterItemBinding.inflate(inflater, parent, false)
         return TheaterViewHolder(movieId, itemBinding, clickListener)
     }
 
     override fun onBindViewHolder(
-        holder: RecyclerView.ViewHolder,
+        holder: TheaterViewHolder,
         position: Int,
     ) {
         val item = getItem(position)
-        (holder as TheaterViewHolder).bind(item)
+        holder.bind(item)
     }
 }
