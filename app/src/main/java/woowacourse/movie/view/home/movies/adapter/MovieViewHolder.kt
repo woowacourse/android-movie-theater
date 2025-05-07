66adfae700f8a3960fb5d0567d6b3f2f8db88163
jerry8282 <ka06196@naver.com>
2025-05-08 02:31:11 +0900

    fix: 영화의 상영기간이 제대로 출력되지 않는 오류 수정

--- app/src/main/java/woowacourse/movie/view/home/movies/adapter/MovieViewHolder.kt
+++ app/src/main/java/woowacourse/movie/view/home/movies/adapter/MovieViewHolder.kt
@@ -1,24 +1,16 @@
 package woowacourse.movie.view.home.movies.adapter
 
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
 import woowacourse.movie.databinding.MovieItemBinding
-import woowacourse.movie.domain.movietime.Date
 import woowacourse.movie.view.home.movies.MovieUi
 import woowacourse.movie.view.home.movies.OnMovieEventListener
-import java.time.format.DateTimeFormatter
 
 class MovieViewHolder(
     eventListener: OnMovieEventListener,
-    binding: MovieItemBinding,
+    val binding: MovieItemBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     private var movieUi: MovieUi? = null
-    private val movieImage: ImageView = binding.ivMovieImage
-    private val movieTitle: TextView = binding.tvMovieTitle
-    private val movieDate: TextView = binding.tvMovieDate
-    private val movieTime: TextView = binding.tvMovieTime
 
     init {
         binding.btnReserve.setOnClickListener {
@@ -28,22 +20,14 @@ class MovieViewHolder(
 
     fun bind(movieUi: MovieUi) {
         this.movieUi = movieUi
-        movieImage.setImageResource(movieUi.image)
-        movieTitle.text = movieUi.title
-        setDateTextView(movieUi.date)
+        binding.ivMovieImage.setImageResource(movieUi.image)
+        binding.tvMovieTitle.text = movieUi.title
+        binding.tvMovieDate.text = movieUi.formattedDate(DATETIME_PATTERN)
         setTimeTextView(movieUi.time)
     }
 
-    private fun setDateTextView(date: Date) {
-        val formatter = DateTimeFormatter.ofPattern(DATETIME_PATTERN)
-        val startDateFormatted = date.startDate.format(formatter)
-        val endDateFormatted = date.endDate.format(formatter)
-        movieDate.text =
-            itemView.context?.getString(R.string.movieDate, startDateFormatted, endDateFormatted)
-    }
-
     private fun setTimeTextView(time: Int) {
-        movieTime.text =
+        binding.tvMovieTime.text =
             itemView.context?.getString(R.string.movieTime, time.toString())
     }
 
