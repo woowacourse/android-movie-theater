8119d69df7889b1f89855e3447b2540bd448fbfe
jerry8282 <ka06196@naver.com>
2025-05-08 03:20:32 +0900

    refactor: MovieViewHolder에 DataBinding 적용

--- app/src/main/java/woowacourse/movie/view/home/movies/adapter/MovieViewHolder.kt
+++ app/src/main/java/woowacourse/movie/view/home/movies/adapter/MovieViewHolder.kt
@@ -1,7 +1,7 @@
 package woowacourse.movie.view.home.movies.adapter
 
+import android.view.View.OnClickListener
 import androidx.recyclerview.widget.RecyclerView
-import woowacourse.movie.R
 import woowacourse.movie.databinding.MovieItemBinding
 import woowacourse.movie.view.home.movies.MovieUi
 import woowacourse.movie.view.home.movies.OnMovieEventListener
@@ -13,25 +13,11 @@ class MovieViewHolder(
     private var movieUi: MovieUi? = null
 
     init {
-        binding.btnReserve.setOnClickListener {
-            movieUi?.let { eventListener.onClickShowTheater(it) }
-        }
+        binding.onConfirm = OnClickListener { movieUi?.let { eventListener.onClickShowTheater(it) } }
     }
 
     fun bind(movieUi: MovieUi) {
         this.movieUi = movieUi
-        binding.ivMovieImage.setImageResource(movieUi.image)
-        binding.tvMovieTitle.text = movieUi.title
-        binding.tvMovieDate.text = movieUi.formattedDate(DATETIME_PATTERN)
-        setTimeTextView(movieUi.time)
-    }
-
-    private fun setTimeTextView(time: Int) {
-        binding.tvMovieTime.text =
-            itemView.context?.getString(R.string.movieTime, time.toString())
-    }
-
-    companion object {
-        private const val DATETIME_PATTERN = "yyyy.M.d"
+        binding.movieUi = movieUi
     }
 }
