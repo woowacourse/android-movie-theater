57f7ab37522cfa3c8e975aadd8dbb43a3a3f1d26
cucumber99 <dlsguq1@gmail.com>
2025-05-07 21:06:41 +0900

    refactor: 프래그먼트 재사용 로직 변경

--- app/src/main/java/woowacourse/movie/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/MainActivity.kt
@@ -2,8 +2,6 @@ package woowacourse.movie
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import woowacourse.movie.databinding.ActivityMainBinding
 import woowacourse.movie.presentation.bookinglist.BookingListFragment
 import woowacourse.movie.presentation.movies.MoviesFragment
@@ -12,6 +10,9 @@ import woowacourse.movie.ui.DataBindingBaseActivity
 
 class MainActivity : DataBindingBaseActivity() {
     private val binding by binding<ActivityMainBinding>(R.layout.activity_main)
+    private val bookingListFragment: BookingListFragment by lazy { BookingListFragment() }
+    private val moviesFragment: MoviesFragment by lazy { MoviesFragment() }
+    private val settingFragment: SettingFragment by lazy { SettingFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,20 +24,42 @@ class MainActivity : DataBindingBaseActivity() {
         binding.bottomNavigationView.run {
             setOnItemSelectedListener { item ->
                 when (item.itemId) {
-                    R.id.action_list -> switchFragment<BookingListFragment>()
-                    R.id.action_home -> switchFragment<MoviesFragment>()
-                    R.id.action_settings -> switchFragment<SettingFragment>()
+                    R.id.action_list -> showFragment(
+                        bookingListFragment,
+                        BookingListFragment::class.java.name
+                    )
+
+                    R.id.action_home -> showFragment(
+                        moviesFragment,
+                        MoviesFragment::class.java.name
+                    )
+
+                    R.id.action_settings -> showFragment(
+                        settingFragment,
+                        SettingFragment::class.java.name
+                    )
                 }
                 true
             }
-            selectedItemId = R.id.action_home
         }
     }
 
-    private inline fun <reified T : Fragment> switchFragment() {
-        supportFragmentManager.commit {
-            setReorderingAllowed(true)
-            replace<T>(binding.mainContainer.id)
+    private fun showFragment(fragment: Fragment, tag: String) {
+        val findFragment = supportFragmentManager.findFragmentByTag(tag)
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        supportFragmentManager.fragments.forEach {
+            fragmentTransaction.hide(it)
+        }
+
+        findFragment?.let {
+            fragmentTransaction
+                .show(it)
+                .commit()
+        } ?: run {
+            fragmentTransaction
+                .add(binding.mainContainer.id, fragment, tag)
+                .commitAllowingStateLoss()
         }
     }
 }
