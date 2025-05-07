a777d2755b3b612686537abdba38c225da2c7d88
jerry8282 <ka06196@naver.com>
2025-05-08 01:46:49 +0900

    refactor: HomeFragment RecyclerView DataBinding 적용

--- app/src/main/java/woowacourse/movie/view/home/HomeFragment.kt
+++ app/src/main/java/woowacourse/movie/view/home/HomeFragment.kt
@@ -6,12 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.R
 import woowacourse.movie.databinding.FragmentHomeBinding
 import woowacourse.movie.view.home.movies.MovieItem
 import woowacourse.movie.view.home.movies.MovieUi
-import woowacourse.movie.view.home.movies.adapter.MovieAdapter
+import woowacourse.movie.view.home.movies.OnMovieEventListener
 import woowacourse.movie.view.home.theater.TheaterBottomSheetDialogFragment
 
 class HomeFragment : Fragment(), HomeContract.View {
@@ -37,12 +36,8 @@ class HomeFragment : Fragment(), HomeContract.View {
     }
 
     override fun showMoviesScreen(movieItems: List<MovieItem>) {
-        val recyclerView: RecyclerView = binding.recyclerView
-        val movieAdapter: MovieAdapter =
-            MovieAdapter { movieUi -> showTheaterSelectDialog(movieUi) }
-
-        recyclerView.adapter = movieAdapter
-        movieAdapter.submitList(movieItems)
+        binding.movieItems = movieItems
+        binding.onItemClick = OnMovieEventListener { movieUi -> showTheaterSelectDialog(movieUi) }
     }
 
     private fun showTheaterSelectDialog(movieUi: MovieUi) {
