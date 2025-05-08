1595a905919bcc56f8b7f1241e0e99f472f3109e
yrsel <xxknu88@gmail.com>
2025-05-09 06:13:28 +0900

    feat: BookedTicket <-> BookedTicketEntity 변환

--- app/src/main/java/woowacourse/movie/data/BookedTicketEntity.kt
+++ app/src/main/java/woowacourse/movie/data/BookedTicketEntity.kt
@@ -4,7 +4,11 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.time.LocalDateTime
+import woowacourse.movie.domain.model.BookedTicket
+import woowacourse.movie.domain.model.Headcount
+import woowacourse.movie.domain.model.MovieSchedule
 import woowacourse.movie.domain.model.Seat
+import woowacourse.movie.domain.model.Seats
 
 @Entity(tableName = "booked_tickets")
 data class BookedTicketEntity(
@@ -15,3 +19,16 @@ data class BookedTicketEntity(
     @ColumnInfo(name = "select_seats") val selectSeats: Set<Seat>,
     @ColumnInfo(name = "headcount") val headcount: Int,
 )
+
+fun BookedTicketEntity.toBookedTicket(): BookedTicket {
+    return BookedTicket(
+        theaterName = this.theaterName,
+        movieTitle = this.movieTitle,
+        movieSchedule =
+            MovieSchedule(
+                this.screeningDateTime,
+                Seats(this.selectSeats.toMutableSet()),
+            ),
+        headcount = Headcount(headcount),
+    )
+}
--- app/src/main/java/woowacourse/movie/domain/model/BookedTicket.kt
+++ app/src/main/java/woowacourse/movie/domain/model/BookedTicket.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie.domain.model
 
 import java.io.Serializable
+import woowacourse.movie.data.BookedTicketEntity
 
 class BookedTicket(
     val theaterName: String,
@@ -9,4 +10,14 @@ class BookedTicket(
     val headcount: Headcount,
 ) : Serializable {
     fun totalPrice(): Int = movieSchedule.seats.totalPrice()
+}
+
+fun BookedTicket.toBookedTicketEntity(): BookedTicketEntity {
+    return BookedTicketEntity(
+        theaterName = theaterName,
+        movieTitle = movieTitle,
+        screeningDateTime = movieSchedule.screeningDateTime,
+        selectSeats = movieSchedule.seats.reservingSeats,
+        headcount = headcount.count,
+    )
 }
