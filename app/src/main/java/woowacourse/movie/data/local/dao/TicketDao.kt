8d2d839da0ef6c991964956a943d8f373f51941f
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-05-08 11:11:07 +0900

    feat: TicketDao 추가

---
+++ app/src/main/java/woowacourse/movie/data/local/dao/TicketDao.kt
@@ -0,0 +1,13 @@
+package woowacourse.movie.data.local.dao
+
+import androidx.room.Insert
+import androidx.room.Query
+import woowacourse.movie.data.local.entity.TicketEntity
+
+interface TicketDao {
+    @Insert
+    fun insert(ticketEntity: TicketEntity)
+
+    @Query("SELECT * FROM ticket")
+    fun getAll(): List<TicketEntity>
+}
--- app/src/main/java/woowacourse/movie/data/local/database/MovieDatabase.kt
+++ app/src/main/java/woowacourse/movie/data/local/database/MovieDatabase.kt
@@ -2,7 +2,10 @@ package woowacourse.movie.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import woowacourse.movie.data.local.dao.TicketDao
 import woowacourse.movie.data.local.entity.TicketEntity
 
 @Database(entities = [TicketEntity::class], version = 1)
-abstract class MovieDatabase : RoomDatabase()
+abstract class MovieDatabase : RoomDatabase() {
+    abstract fun ticketDao(): TicketDao
+}
