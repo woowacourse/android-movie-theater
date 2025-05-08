8e07249523e4690c57ebb0746747aa6ae1f40152
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-05-08 13:52:57 +0900

    refactor: 고유 키 자동으로 생성하도록 수정

--- app/src/main/java/woowacourse/movie/data/local/entity/TicketEntity.kt
+++ app/src/main/java/woowacourse/movie/data/local/entity/TicketEntity.kt
@@ -6,7 +6,7 @@ import java.time.LocalDateTime
 
 @Entity(tableName = "ticket")
 data class TicketEntity(
-    @PrimaryKey val id: Long,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
     val title: String,
     val count: Int,
     val showtime: LocalDateTime,
