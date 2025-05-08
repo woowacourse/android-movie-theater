c6c873dda119d8c3b4b0ba3a8939b5b378932614
yrsel <xxknu88@gmail.com>
2025-05-09 07:00:37 +0900

    feat: BookedTicketConverter 엔티티 직렬화 추가

---
+++ app/src/main/java/woowacourse/movie/data/BookedTicketConverter.kt
@@ -0,0 +1,34 @@
+package woowacourse.movie.data
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import woowacourse.movie.domain.model.Seat
+
+class BookedTicketConverter {
+    private val dateTimeFormat = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+    private val gson = Gson()
+
+    @TypeConverter
+    fun LocalDateTime.toEntityValue(): String {
+        return this.format(dateTimeFormat)
+    }
+
+    @TypeConverter
+    fun String.toLocalDateTime(): LocalDateTime {
+        return LocalDateTime.parse(this, dateTimeFormat)
+    }
+
+    @TypeConverter
+    fun Set<Seat>.toEntityValue(): String {
+        return gson.toJson(this)
+    }
+
+    @TypeConverter
+    fun String.toSet(): Set<Seat> {
+        val type = object : TypeToken<Set<Seat>>() {}.type
+        return gson.fromJson(this, type)
+    }
+}
