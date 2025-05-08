b425f1e5a99db472b72174294d100420622a37cd
moondev03 <moondev03@gmail.com>
2025-05-08 19:10:57 +0900

    test: ReservationHistoryFragment 테스트 코드 수정

---
+++ app/src/androidTest/java/woowacourse/movie/presentation/home/history/ReservationHistoryFragmentTest.kt
@@ -0,0 +1,20 @@
+package woowacourse.movie.presentation.home.history
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.junit.Test
+import woowacourse.movie.R
+import woowacourse.movie.presentation.history.ReservationHistoryFragment
+
+class ReservationHistoryFragmentTest {
+    @Test
+    fun `영화목록이_보여진다`() {
+        launchFragmentInContainer { ReservationHistoryFragment() }
+
+        onView(withId(R.id.rv_reservation_history))
+            .check(matches(isDisplayed()))
+    }
+}
