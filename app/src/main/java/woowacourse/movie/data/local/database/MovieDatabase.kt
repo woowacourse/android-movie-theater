eed2e8c033b6ced7698898ac527f0e6eaf1ddffc
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-05-08 13:33:23 +0900

    feat: database 생성 기능 추가

--- app/src/main/java/woowacourse/movie/data/local/database/MovieDatabase.kt
+++ app/src/main/java/woowacourse/movie/data/local/database/MovieDatabase.kt
@@ -1,6 +1,8 @@
 package woowacourse.movie.data.local.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import woowacourse.movie.data.local.dao.TicketDao
 import woowacourse.movie.data.local.entity.TicketEntity
@@ -8,4 +10,20 @@ import woowacourse.movie.data.local.entity.TicketEntity
 @Database(entities = [TicketEntity::class], version = 1)
 abstract class MovieDatabase : RoomDatabase() {
     abstract fun ticketDao(): TicketDao
+
+    companion object {
+        private var instance: MovieDatabase? = null
+
+        fun getMovieDatabase(context: Context): MovieDatabase {
+            return instance ?: buildDatabase(context).also { instance = it }
+        }
+
+        private fun buildDatabase(context: Context): MovieDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                MovieDatabase::class.java,
+                "movie_database",
+            ).build()
+        }
+    }
 }
