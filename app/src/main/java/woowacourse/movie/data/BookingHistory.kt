b2b8f805a978c1581aaa76486f69a8e6531ec369
cucumber99 <dlsguq1@gmail.com>
2025-05-08 20:59:31 +0900

    feat: 예매 내역 엔티티 추가

---
+++ app/src/main/java/woowacourse/movie/data/BookingHistory.kt
@@ -0,0 +1,18 @@
+package woowacourse.movie.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import woowacourse.movie.domain.model.seat.Seat
+import java.time.LocalDateTime
+
+@Entity(tableName = "booking_histories")
+data class BookingHistory(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "movie_title") val title: String?,
+    @ColumnInfo(name = "theater_name") val theater: String?,
+    @ColumnInfo(name = "screening_date_time") val screeningDateTime: LocalDateTime?,
+    @ColumnInfo(name = "head_count") val headCount: Int?,
+    @ColumnInfo(name = "total_amount") val totalAmount: Int?,
+    @ColumnInfo(name = "selected_seats") val selectedSeats: List<Seat>?,
+)
