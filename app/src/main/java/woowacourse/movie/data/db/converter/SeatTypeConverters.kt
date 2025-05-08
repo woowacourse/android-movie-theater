e16b0b3a211b16e037a2638d0f810057e183bdd4
chanho0908 <uni070@naver.com>
2025-05-08 22:45:18 +0900

    feat : LocalDateTime, Seat Type Converter 추가

---
+++ app/src/main/java/woowacourse/movie/data/db/converter/SeatTypeConverters.kt
@@ -0,0 +1,20 @@
+package woowacourse.movie.data.db.converter
+
+import androidx.room.TypeConverter
+import kotlinx.serialization.builtins.SetSerializer
+import kotlinx.serialization.json.Json
+import woowacourse.movie.domain.model.seat.Seat
+
+class SeatTypeConverters {
+    private val json = Json
+
+    @TypeConverter
+    fun fromSeats(seats: Set<Seat>): String {
+            return json.encodeToString(SetSerializer(Seat.serializer()), seats)
+    }
+
+    @TypeConverter
+    fun toSeats(jsonString: String): Set<Seat> {
+        return json.decodeFromString(SetSerializer(Seat.serializer()), jsonString)
+    }
+}
