143ca7c373776763d8fcf46e016f5a2e28c3b0eb
cucumber99 <dlsguq1@gmail.com>
2025-05-08 21:00:54 +0900

    test: 예매 내역 DAO 테스트

---
+++ app/src/androidTest/java/woowacourse/movie/data/BookingHistoryDaoTest.kt
@@ -0,0 +1,76 @@
+package woowacourse.movie.data
+
+import android.content.Context
+import android.database.sqlite.SQLiteConstraintException
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.assertThrows
+
+class BookingHistoryDaoTest {
+    private lateinit var database: BookingHistoryDatabase
+    private lateinit var dao: BookingHistoryDao
+
+    @Before
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        database = Room.inMemoryDatabaseBuilder(context, BookingHistoryDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+        dao = database.bookingHistoryDao()
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+    }
+
+    @Test
+    fun 예매_기록을_추가한다() {
+        // When
+        dao.insert(bookingHistory1)
+        val bookingHistories = dao.getAll()
+
+        // Then
+        assertThat(bookingHistories.contains(bookingHistory1)).isTrue()
+    }
+
+    @Test
+    fun `여러개의_예매_기록을_추가한다`() {
+        // When
+        dao.insert(bookingHistory1, bookingHistory2)
+        val bookingHistories = dao.getAll()
+
+        // Then
+        assertAll(
+            { assertThat(bookingHistories.contains(bookingHistory1)).isTrue() },
+            { assertThat(bookingHistories.contains(bookingHistory2)).isTrue() },
+        )
+    }
+
+    @Test
+    fun 동일한_UID를_가진_예매_기록을_추가할_수_없다() {
+        // Then
+        assertThrows<SQLiteConstraintException> {
+            dao.insert(bookingHistory1, bookingHistory3)
+        }
+    }
+
+    @Test
+    fun 예매_기록을_삭제한다() {
+        // Given
+        dao.insert(bookingHistory1)
+
+        // When
+        dao.delete(bookingHistory1)
+        val bookingHistories = dao.getAll()
+
+        // Then
+        assertThat(bookingHistories.contains(bookingHistory1)).isFalse()
+    }
+}
---
+++ app/src/androidTest/java/woowacourse/movie/data/DaoTestFixture.kt
@@ -0,0 +1,34 @@
+package woowacourse.movie.data
+
+import woowacourse.movie.domain.model.seat.Seat
+import java.time.LocalDateTime
+
+val bookingHistory1 = BookingHistory(
+    1,
+    "Movie",
+    "Theater",
+    LocalDateTime.of(2025, 5, 8, 22, 0),
+    1,
+    10_000,
+    listOf(Seat.of(1, 1))
+)
+
+val bookingHistory2 = BookingHistory(
+    2,
+    "Movie2",
+    "Theater2",
+    LocalDateTime.of(2025, 5, 10, 19, 0),
+    3,
+    30_000,
+    listOf(Seat.of(1, 1), Seat.of(2, 2), Seat.of(3, 3))
+)
+
+val bookingHistory3 = BookingHistory(
+    1,
+    "Movie3",
+    "Theater3",
+    LocalDateTime.of(2025, 5, 12, 14, 0),
+    2,
+    20_000,
+    listOf(Seat.of(1, 1), Seat.of(3, 3))
+)
