fdfe8f4c649dafbe9ec54e4fb4e99e0224c5dffa
Ham BeomJoon <hbj0802@naver.com>
2025-05-08 13:48:13 +0900

    feat: 예매 완료 화면에서 예매 내역 DB에 저장

--- app/src/main/java/woowacourse/movie/presentation/view/reservation/complete/ReservationCompleteActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/reservation/complete/ReservationCompleteActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
 import woowacourse.movie.R
+import woowacourse.movie.data.ReservationDatabase
 import woowacourse.movie.databinding.ActivityReservationCompleteBinding
 import woowacourse.movie.presentation.Extras
 import woowacourse.movie.presentation.ReservationUiFormatter
@@ -16,10 +17,14 @@ import woowacourse.movie.presentation.model.ReservationInfoUiModel
 class ReservationCompleteActivity :
     AppCompatActivity(),
     ReservationCompleteContract.View {
+    private val reservationDao by lazy {
+        ReservationDatabase.getInstance(this).reservationDao()
+    }
     private lateinit var binding: ActivityReservationCompleteBinding
     private val presenter: ReservationCompletePresenter by lazy {
         ReservationCompletePresenter(
             this,
+            reservationDao,
         )
     }
 
