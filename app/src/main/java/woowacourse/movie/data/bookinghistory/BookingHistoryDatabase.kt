9aefe3231a9e6105526ec16f9b8ffa6c930aa9b4
cucumber99 <dlsguq1@gmail.com>
2025-05-08 21:50:57 +0900

    feat: 팩토리 메소드 구현

---
+++ app/src/main/java/woowacourse/movie/data/bookinghistory/BookingHistoryDatabase.kt
@@ -0,0 +1,29 @@
+package woowacourse.movie.data.bookinghistory
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [BookingHistory::class], version = 1)
+@TypeConverters(BookingHistoryConverters::class)
+abstract class BookingHistoryDatabase : RoomDatabase() {
+
+    abstract fun bookingHistoryDao(): BookingHistoryDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: BookingHistoryDatabase? = null
+
+        fun getDatabase(context: Context): BookingHistoryDatabase {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    BookingHistoryDatabase::class.java,
+                    "booking_histories"
+                ).build().also { INSTANCE = it }
+            }
+        }
+    }
+}
