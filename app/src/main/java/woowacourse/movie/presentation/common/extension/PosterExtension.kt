be0987bea589211d7a77de638c37059698aa6285
moondev03 <moondev03@gmail.com>
2025-05-08 15:25:06 +0900

    refactor: 불필요한 확장 함수 제거

--- app/src/main/java/woowacourse/movie/presentation/common/extension/PosterExtension.kt
+++
@@ -1,9 +0,0 @@
-package woowacourse.movie.presentation.common.extension
-
-import woowacourse.movie.presentation.common.model.PosterUiModel
-
-fun PosterUiModel.toInt() =
-    when (val poster = this) {
-        is PosterUiModel.Resource -> poster.resId
-        is PosterUiModel.Url -> 0
-    }
