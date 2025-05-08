22975f45bc8b6438c3f2c9f51619c666a593e219
cucumber99 <dlsguq1@gmail.com>
2025-05-08 20:56:43 +0900

    feat: BookingHistoryContract 작성

---
+++ app/src/main/java/woowacourse/movie/presentation/bookinghistory/BookingHistoryContract.kt
@@ -0,0 +1,15 @@
+package woowacourse.movie.presentation.bookinghistory
+
+import woowacourse.movie.domain.model.movie.MovieTicket
+
+interface BookingHistoryContract {
+    interface View {
+        fun showBookingHistory(tickets: List<MovieTicket>)
+        fun navigateToBookingSummary(ticket: MovieTicket)
+    }
+
+    interface Presenter {
+        fun loadBookingHistory()
+        fun selectBookingHistory(ticket: MovieTicket)
+    }
+}
