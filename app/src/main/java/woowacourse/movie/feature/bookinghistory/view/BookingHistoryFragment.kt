c76b91979f7d38e371acbb57688cc8ea33727ed1
junseo511 <prime54@g.skku.edu>
2025-05-08 13:30:00 +0900

    feat: 예매 내역 뷰 구현

--- app/src/main/java/woowacourse/movie/feature/bookinghistory/view/BookingHistoryFragment.kt
+++ app/src/main/java/woowacourse/movie/feature/bookinghistory/view/BookingHistoryFragment.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentBookingHistoryBinding
 import woowacourse.movie.feature.bookinghistory.contract.BookingHistoryContract
 
 class BookingHistoryFragment :
     Fragment(),
     BookingHistoryContract.View {
+    private val binding: FragmentBookingHistoryBinding by lazy {
+        DataBindingUtil.setContentView(
+            requireActivity(),
+            R.layout.fragment_booking_history,
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -20,4 +29,11 @@ class BookingHistoryFragment :
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? = inflater.inflate(R.layout.fragment_booking_history, container, false)
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
