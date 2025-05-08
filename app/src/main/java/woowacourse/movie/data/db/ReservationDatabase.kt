66ee0ec89b25d9a1cbf2833079007de7b06ed06f
m6z1 <sonmyungj1zz@gmail.com>
2025-05-09 01:19:16 +0900

    feat: DB 구현

---
+++ app/src/main/java/woowacourse/movie/data/db/ReservationDatabase.kt
@@ -0,0 +1,22 @@
+package woowacourse.movie.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import woowacourse.movie.data.dao.ReservationDao
+import woowacourse.movie.data.entity.MovieEntity
+import woowacourse.movie.data.entity.ReservationInfoEntity
+import woowacourse.movie.data.entity.SeatEntity
+import woowacourse.movie.data.entity.TheaterEntity
+
+@Database(
+    entities = [
+        ReservationInfoEntity::class,
+        SeatEntity::class,
+        MovieEntity::class,
+        TheaterEntity::class,
+    ],
+    version = 8,
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun reservationDao(): ReservationDao
+}
