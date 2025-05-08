d0ccc7a82ed1be53697d3bdb2b0f01c9e8d57d69
m6z1 <sonmyungj1zz@gmail.com>
2025-05-09 01:19:36 +0900

    feat: 도메인 - 엔티티 매퍼 함수 구현

---
+++ app/src/main/java/woowacourse/movie/data/ReservationMapper.kt
@@ -0,0 +1,48 @@
+package woowacourse.movie.data
+
+import woowacourse.movie.data.entity.MovieEntity
+import woowacourse.movie.data.entity.MovieTicketEntity
+import woowacourse.movie.data.entity.ReservationInfoEntity
+import woowacourse.movie.data.entity.SeatEntity
+import woowacourse.movie.data.entity.TheaterEntity
+import woowacourse.movie.model.movie.Movie
+import woowacourse.movie.model.seat.Seat
+import woowacourse.movie.model.theater.Theater
+import woowacourse.movie.model.ticket.MovieTicket
+
+fun MovieTicket.toEntity(): MovieTicketEntity =
+    MovieTicketEntity(
+        reservationInfoEntity =
+            ReservationInfoEntity(
+                movieId = movie.id,
+                theaterId = theater.id,
+                movieDateYear = movieDate.year,
+                movieDateMonth = movieDate.month.value,
+                movieDateDay = movieDate.dayOfMonth,
+                movieTimeHour = movieTime.value.hour,
+                movieTimeMinute = movieTime.value.minute,
+                ticketCount = seats.size,
+            ),
+        seats = seats.map { it.toEntity(0L) },
+        movie = movie.toEntity(),
+        theater = theater.toEntity(),
+    )
+
+fun Seat.toEntity(reservationId: Long): SeatEntity =
+    SeatEntity(
+        reservationId = reservationId,
+        row = row.value,
+        column = column.value,
+    )
+
+fun Movie.toEntity(): MovieEntity =
+    MovieEntity(
+        id = id,
+        movieTitle = title,
+    )
+
+fun Theater.toEntity(): TheaterEntity =
+    TheaterEntity(
+        id = id,
+        name = name,
+    )
