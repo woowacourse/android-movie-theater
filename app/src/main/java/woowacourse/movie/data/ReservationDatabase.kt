97b6b387fa73619e4de1c9d8ed126d9ae0ca84fe
Ham BeomJoon <hbj0802@naver.com>
2025-05-08 10:57:24 +0900

    feat: Database Instance 받아오는 함수 추가

--- app/src/main/java/woowacourse/movie/data/ReservationDatabase.kt
+++ app/src/main/java/woowacourse/movie/data/ReservationDatabase.kt
@@ -1,6 +1,8 @@
 package woowacourse.movie.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
@@ -11,4 +13,18 @@ import androidx.room.RoomDatabase
 )
 abstract class ReservationDatabase : RoomDatabase() {
     abstract fun reservationDao(): ReservationDao
+
+    companion object {
+        @Volatile
+        private var instance: ReservationDatabase? = null
+
+        fun getInstance(context: Context): ReservationDatabase =
+            instance ?: Room
+                .databaseBuilder(
+                    context.applicationContext,
+                    ReservationDatabase::class.java,
+                    "reservations",
+                ).build()
+                .also { instance = it }
+    }
 }
