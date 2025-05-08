4931f90a7a1f0516f3c25cd2291058231f26a34a
윤동현 <82762769+donghyun81@users.noreply.github.com>
2025-05-08 11:04:55 +0900

    feat: MovieDatabase 추가

---
+++ app/src/main/java/woowacourse/movie/data/local/database/MovieDatabase.kt
@@ -0,0 +1,8 @@
+package woowacourse.movie.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import woowacourse.movie.data.local.entity.TicketEntity
+
+@Database(entities = [TicketEntity::class], version = 1)
+abstract class MovieDatabase : RoomDatabase()
