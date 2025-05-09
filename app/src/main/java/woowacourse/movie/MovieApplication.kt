5a27daf055b24a903bcce45fbc903fcc6192e2ca
junseo511 <prime54@g.skku.edu>
2025-05-09 17:08:00 +0900

    refactor: 데이터베이스 네이밍 변경

--- app/src/main/java/woowacourse/movie/MovieApplication.kt
+++ app/src/main/java/woowacourse/movie/MovieApplication.kt
@@ -4,18 +4,18 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import androidx.room.Room
-import woowacourse.movie.data.database.BookingDatabase
-import woowacourse.movie.data.database.BookingDatabase.Companion.DATABASE_NAME
+import woowacourse.movie.data.database.MovieDatabase
+import woowacourse.movie.data.database.MovieDatabase.Companion.DATABASE_NAME
 import woowacourse.movie.data.repository.BookingRepositoryImpl
 
 class MovieApplication : Application() {
-    val bookingDatabase by lazy {
+    val movieDatabase by lazy {
         Room
-            .databaseBuilder(this, BookingDatabase::class.java, DATABASE_NAME)
+            .databaseBuilder(this, MovieDatabase::class.java, DATABASE_NAME)
             .allowMainThreadQueries()
             .build()
     }
-    val bookingRepository by lazy { BookingRepositoryImpl(bookingDatabase.bookingDao()) }
+    val bookingRepository by lazy { BookingRepositoryImpl(movieDatabase.bookingDao()) }
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/woowacourse/movie/data/database/MovieDatabase.kt
+++ app/src/main/java/woowacourse/movie/data/database/MovieDatabase.kt
@@ -6,10 +6,10 @@ import woowacourse.movie.data.dao.BookingDao
 import woowacourse.movie.data.entity.BookingInfoEntity
 
 @Database(entities = [BookingInfoEntity::class], version = 1)
-abstract class BookingDatabase : RoomDatabase() {
+abstract class MovieDatabase : RoomDatabase() {
     abstract fun bookingDao(): BookingDao
 
     companion object {
-        const val DATABASE_NAME = "BOOKING_DATABASE"
+        const val DATABASE_NAME = "MOVIE_DATABASE"
     }
 }
