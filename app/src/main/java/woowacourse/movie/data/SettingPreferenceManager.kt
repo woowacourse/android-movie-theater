f46ac5b9e72e3166d72bb1f97db21d4e037fe81d
Ham BeomJoon <hbj0802@naver.com>
2025-05-09 16:33:16 +0900

    refactor: SharedPreferences이름, 키 값 상수화

--- app/src/main/java/woowacourse/movie/data/SettingPreferenceManager.kt
+++ app/src/main/java/woowacourse/movie/data/SettingPreferenceManager.kt
@@ -6,13 +6,18 @@ import androidx.core.content.edit
 class SettingPreferenceManager(
     context: Context,
 ) {
-    private val prefs = context.getSharedPreferences("settings", Context.MODE_PRIVATE)
+    private val prefs = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
 
-    fun getPushAlarmEnabled(): Boolean = prefs.getBoolean("notification", false)
+    fun getPushAlarmEnabled(): Boolean = prefs.getBoolean(KEY_PUSH_ALARM, false)
 
     fun setPushAlarmEnabled(enabled: Boolean) {
         prefs.edit {
-            putBoolean("notification", enabled)
+            putBoolean(KEY_PUSH_ALARM, enabled)
         }
+    }
+
+    companion object {
+        private const val PREFERENCES_NAME = "settings"
+        private const val KEY_PUSH_ALARM = "notification"
     }
 }
