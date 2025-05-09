1e737d58784be2a17456f6ee75ec254a81d8b1be
yrsel <xxknu88@gmail.com>
2025-05-10 08:16:29 +0900

    feat: BookingHistoryAdapter 예매 내역 리사이클러뷰 어댑터 구현

---
+++ app/src/main/java/woowacourse/movie/ui/history/view/BookingHistoryAdapter.kt
@@ -0,0 +1,45 @@
+package woowacourse.movie.ui.history.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import woowacourse.movie.data.BookedTicketEntity
+import woowacourse.movie.databinding.HistoryItemBinding
+
+class BookingHistoryAdapter(
+    private val historyClickListener: HistoryClickListener,
+) : ListAdapter<BookedTicketEntity, BookingHistoryViewHolder>(
+        object : DiffUtil.ItemCallback<BookedTicketEntity>() {
+            override fun areItemsTheSame(
+                oldItem: BookedTicketEntity,
+                newItem: BookedTicketEntity,
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: BookedTicketEntity,
+                newItem: BookedTicketEntity,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        },
+    ) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int,
+    ): BookingHistoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = HistoryItemBinding.inflate(inflater, parent, false)
+        return BookingHistoryViewHolder(binding, historyClickListener)
+    }
+
+    override fun onBindViewHolder(
+        holder: BookingHistoryViewHolder,
+        position: Int,
+    ) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+}
