41a7fb715238e46ef70e25e12b26108595a3676c
chanho0908 <uni070@naver.com>
2025-05-09 10:29:51 +0900

    feat : 알림 권한 요청 기능 추가

--- app/src/main/java/woowacourse/movie/view/MainActivity.kt
+++ app/src/main/java/woowacourse/movie/view/MainActivity.kt
@@ -1,8 +1,15 @@
 package woowacourse.movie.view
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
@@ -10,6 +17,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import woowacourse.movie.R
 import woowacourse.movie.databinding.ActivityMainBinding
+import woowacourse.movie.view.core.ext.showPermissionSnackBar
 import woowacourse.movie.view.history.BookingHistoryFragment
 import woowacourse.movie.view.movies.MovieListFragment
 import woowacourse.movie.view.setting.SettingFragment
@@ -17,11 +25,32 @@ import woowacourse.movie.view.setting.SettingFragment
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+        ) { isGranted ->
+            if (isGranted) {
+                // 권한 허용됨
+            } else {
+                // 권한 거부됨
+                val permission = Manifest.permission.POST_NOTIFICATIONS
+
+                if (!shouldShowRequestPermissionRationale(permission)) {
+                    // 다시 묻지 않음 선택
+                    showPermissionSnackBar(binding.root)
+                } else {
+                    // 일반 거부 상황
+                    showPermissionSnackBar(binding.root)
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+        requestNotificationPermission()
         initView(savedInstanceState)
     }
 
@@ -40,6 +69,25 @@ class MainActivity : AppCompatActivity() {
             binding.navigation.selectedItemId = R.id.action_home
         }
         setNavigationListener(historyFragment, movieListFragment, settingFragment)
+    }
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val permission = Manifest.permission.POST_NOTIFICATIONS
+            val permissionCheck = ContextCompat.checkSelfPermission(this, permission)
+
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                // 이미 권한이 허용됨
+                return
+            }
+
+            if (shouldShowRequestPermissionRationale(permission)) {
+                showNotificationPermissionDialog()
+            } else {
+                // 처음 요청이거나 '다시 묻지 않음' 선택 후 재요청 시도
+                requestPermissionLauncher.launch(permission)
+            }
+        }
     }
 
     private fun setNavigationListener(
@@ -67,6 +115,25 @@ class MainActivity : AppCompatActivity() {
                 else -> return@setOnItemSelectedListener false
             }
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun showNotificationPermissionDialog() {
+        val title = getString(R.string.text_notification_request_title)
+        val content = getString(R.string.text_notification_request_content)
+        val positiveTitle = getString(R.string.text_allow)
+        val negativeTitle = getString(R.string.text_cancel)
+
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(content)
+            .setPositiveButton(
+                positiveTitle,
+            ) { _, _ ->
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+            .setNegativeButton(negativeTitle, null)
+            .show()
     }
 
     private fun replaceFragment(fragment: Fragment) {
--- app/src/main/java/woowacourse/movie/view/core/ext/ContextExt.kt
+++ app/src/main/java/woowacourse/movie/view/core/ext/ContextExt.kt
@@ -1,12 +1,29 @@
 package woowacourse.movie.view.core.ext
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.StringRes
+import com.google.android.material.snackbar.Snackbar
 
 fun Context.showToastFromResource(
     @StringRes resourceId: Int,
     duration: Int = Toast.LENGTH_SHORT,
 ) {
     Toast.makeText(this, resourceId, duration).show()
+}
+
+fun Context.showPermissionSnackBar(view: View) {
+    Snackbar.make(view, "권한이 거부 되었습니다. 설정(앱 정보)에서 권한을 확인해 주세요.", Snackbar.LENGTH_SHORT)
+        .setAction("확인") {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val packageName = this.packageName
+            val uri = Uri.fromParts("package", packageName, null)
+            intent.data = uri
+
+            this.startActivity(intent)
+        }.show()
 }
