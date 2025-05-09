573c653e678faebb3ce675a2b7ba0bcfb9cfe2ac
hwannow <vnfdlvgid061@naver.com>
2025-05-09 21:52:58 +0900

    feat: 설정 화면에서 알림 On/Off 기능 추가

--- app/src/main/java/woowacourse/movie/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/setting/SettingFragment.kt
@@ -1,9 +1,18 @@
 package woowacourse.movie.setting
 
+import android.Manifest
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.edit
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
@@ -12,6 +21,12 @@ import woowacourse.movie.databinding.FragmentSettingBinding
 class SettingFragment : Fragment() {
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
+    private val preference: SharedPreferences by lazy {
+        requireContext().getSharedPreferences(
+            SHARED_NAME,
+            Context.MODE_PRIVATE,
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +35,66 @@ class SettingFragment : Fragment() {
     ): View {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_setting, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.switchAlarm.isChecked = isNotificationPermissionGranted()
+
+        binding.switchAlarm.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                if (!isPermitted()) {
+                    requestNotificationPermission()
+                    return@setOnCheckedChangeListener
+                }
+            }
+            preference.edit {
+                putBoolean(SHARED_SET_ALARM, isChecked)
+            }
+        }
+    }
+
+    private fun isNotificationPermissionGranted(): Boolean {
+        val prefs = requireContext().getSharedPreferences(SHARED_NAME, Context.MODE_PRIVATE)
+        return prefs.getBoolean(SHARED_SET_ALARM, false)
+    }
+
+    private fun isPermitted(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) return true
+
+        return ContextCompat.checkSelfPermission(
+            requireContext(), Manifest.permission.POST_NOTIFICATIONS,
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(requireContext(), "알림 설정 완료", Toast.LENGTH_SHORT).show()
+                preference.edit {
+                    putBoolean(SHARED_SET_ALARM, true)
+                }
+                binding.switchAlarm.isChecked = true
+            } else {
+                Toast.makeText(requireContext(), "알림 거부됨", Toast.LENGTH_SHORT).show()
+                binding.switchAlarm.isChecked = false
+            }
+        }
+
+    companion object {
+        private const val SHARED_NAME = "settings"
+        private const val SHARED_SET_ALARM = "notification"
     }
 }
