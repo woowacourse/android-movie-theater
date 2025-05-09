4ca5837713b5a5b94861c96c3b5c40a04993c42f
yrsel <xxknu88@gmail.com>
2025-05-10 07:37:48 +0900

    feat: BookingHistoryViewHolder 예매 내역 리사이클러뷰 뷰홀더 구현

---
+++ app/src/main/java/woowacourse/movie/ui/history/view/BookingHistoryViewHolder.kt
@@ -0,0 +1,20 @@
+package woowacourse.movie.ui.history.view
+
+import androidx.recyclerview.widget.RecyclerView
+import woowacourse.movie.data.BookedTicketEntity
+import woowacourse.movie.databinding.HistoryItemBinding
+import woowacourse.movie.utils.StringFormatter
+
+class BookingHistoryViewHolder(
+    private val binding: HistoryItemBinding,
+    historyClickListener: HistoryClickListener,
+) : RecyclerView.ViewHolder(binding.root) {
+    init {
+        binding.clickListener = historyClickListener
+    }
+
+    fun bind(item: BookedTicketEntity) {
+        binding.bookedTicketEntity = item
+        binding.stringFormatter = StringFormatter
+    }
+}
