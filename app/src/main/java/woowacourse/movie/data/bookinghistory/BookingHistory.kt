7e9ef06d1e361dff575959018134b05eb5585a62
cucumber99 <dlsguq1@gmail.com>
2025-05-09 16:46:25 +0900

    feat: 고유 키 자동 생성 기능 추가

--- app/src/main/java/woowacourse/movie/data/bookinghistory/BookingHistory.kt
+++ app/src/main/java/woowacourse/movie/data/bookinghistory/BookingHistory.kt
@@ -8,11 +8,11 @@ import java.time.LocalDateTime
 
 @Entity(tableName = "booking_histories")
 data class BookingHistory(
-    @PrimaryKey val uid: Int,
-    @ColumnInfo(name = "movie_title") val title: String?,
-    @ColumnInfo(name = "theater_name") val theater: String?,
-    @ColumnInfo(name = "screening_date_time") val screeningDateTime: LocalDateTime?,
-    @ColumnInfo(name = "head_count") val headCount: Int?,
-    @ColumnInfo(name = "total_amount") val totalAmount: Int?,
-    @ColumnInfo(name = "selected_seats") val selectedSeats: List<Seat>?,
+    @PrimaryKey(autoGenerate = true) val uid: Int = 0,
+    @ColumnInfo(name = "movie_title") val title: String,
+    @ColumnInfo(name = "theater_name") val theater: String,
+    @ColumnInfo(name = "screening_date_time") val screeningDateTime: LocalDateTime,
+    @ColumnInfo(name = "head_count") val headCount: Int,
+    @ColumnInfo(name = "total_amount") val totalAmount: Int,
+    @ColumnInfo(name = "selected_seats") val selectedSeats: List<Seat>,
 )
