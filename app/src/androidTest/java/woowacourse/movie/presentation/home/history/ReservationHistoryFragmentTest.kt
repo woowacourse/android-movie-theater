0184313ce8c13871a9026876110dd9c3c5bc6a25
moondev03 <moondev03@gmail.com>
2025-05-10 17:34:26 +0900

    test: 설정 화면 Presenter 테스트 코드 작성

--- app/src/androidTest/java/woowacourse/movie/presentation/home/history/ReservationHistoryFragmentTest.kt
+++ app/src/androidTest/java/woowacourse/movie/presentation/home/history/ReservationHistoryFragmentTest.kt
@@ -5,15 +5,19 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.junit.Before
 import org.junit.Test
 import woowacourse.movie.R
 import woowacourse.movie.presentation.history.ReservationHistoryFragment
 
 class ReservationHistoryFragmentTest {
+    @Before
+    fun setUp() {
+        launchFragmentInContainer { ReservationHistoryFragment() }
+    }
+
     @Test
     fun `영화_예매_목록이_보여진다`() {
-        launchFragmentInContainer { ReservationHistoryFragment() }
-
         onView(withId(R.id.rv_reservation_history))
             .check(matches(isDisplayed()))
     }
---
+++ app/src/test/java/woowacourse/movie/presentation/setting/SettingPresenterTest.kt
@@ -0,0 +1,59 @@
+package woowacourse.movie.presentation.setting
+
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verifyAll
+import io.mockk.verifySequence
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import woowacourse.movie.domain.NotificationPreferenceRepository
+
+class SettingPresenterTest {
+    private lateinit var view: SettingContract.View
+    private lateinit var repository: NotificationPreferenceRepository
+    private lateinit var presenter: SettingPresenter
+
+    @BeforeEach
+    fun setUp() {
+        view = mockk(relaxed = true)
+        repository = mockk()
+    }
+
+    @Test
+    fun `초기화 시 알림 수신 여부를 View에 전달한다`() {
+        // given
+        every { repository.notificationEnabled() } returns true
+
+        // when
+        presenter = SettingPresenter(view, repository)
+
+        // then
+        verifyAll {
+            repository.notificationEnabled()
+            view.notifyNotificationEnabled(true)
+        }
+    }
+
+    @Test
+    fun `알림 설정을 변경하면 알림 수신 여부가 저장되고 View에 반영된다`() {
+        // given
+        every { repository.updateNotificationEnabled(any()) } just Runs
+        every { repository.notificationEnabled() } returns true
+
+        presenter = SettingPresenter(view, repository)
+
+        // when
+        presenter.updateNotificationEnabled(true)
+
+        // then
+        verifySequence {
+            repository.notificationEnabled()
+            view.notifyNotificationEnabled(true)
+            repository.updateNotificationEnabled(true)
+            repository.notificationEnabled()
+            view.notifyNotificationEnabled(true)
+        }
+    }
+}
