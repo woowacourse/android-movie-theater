5e87eef7dfb404af2bccca1cb9362aa6157c53d3
ijh1298 <imjh0417@gmail.com>
2025-05-10 15:40:18 +0900

    feat: BookingHistoryFragment에 DataBinding, RecyclerView 적용

--- app/src/main/java/woowacourse/movie/ui/history/view/BookingHistoryFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/history/view/BookingHistoryFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentBookingHistoryBinding
 
 class BookingHistoryFragment : Fragment() {
+    private var _binding: FragmentBookingHistoryBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_booking_history, container, false)
+        _binding = FragmentBookingHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
