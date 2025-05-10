26d9da278bbf0cb030ae89d66b6612273f97feb8
chanho0908 <uni070@naver.com>
2025-05-11 00:45:22 +0900

    feat : BroadCastReciever & Alarm 기능 추가

---
+++ app/src/main/java/woowacourse/movie/view/receiver/AlarmReceiver.kt
@@ -0,0 +1,44 @@
+package woowacourse.movie.view.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(
+        context: Context,
+        intent: Intent,
+    ) {
+        val movieTitle =
+            intent.getStringExtra(ALARM_MOVIE_TITLE_KEY)
+                ?: throw IllegalStateException(INVALID_NOTIFICATION_MOVIE_TITLE)
+
+        val ticketId = intent.getLongExtra(ALARM_TICKET_ID_KEY, DEFAULT_TICKET_ID)
+
+        if (ticketId == DEFAULT_TICKET_ID) throw IllegalStateException(INVALID_NOTIFICATION_TICKET_ID)
+
+        val helper = NotificationHelper(context)
+        val notification = helper.notification(ticketId, movieTitle)
+        helper.notify(ticketId.toInt(), notification)
+    }
+
+    companion object {
+        fun newIntent(
+            context: Context,
+            movieTitle: String,
+            ticketId: Long,
+        ): Intent {
+            return Intent(context, AlarmReceiver::class.java).apply {
+                putExtra(ALARM_MOVIE_TITLE_KEY, movieTitle)
+                putExtra(ALARM_TICKET_ID_KEY, ticketId)
+            }
+        }
+
+        private const val ALARM_MOVIE_TITLE_KEY = "ALARM_MOVIE_TITLE_KEY"
+        private const val ALARM_TICKET_ID_KEY = "ALARM_TICKET_ID_KEY"
+
+        private const val DEFAULT_TICKET_ID = 0L
+        private const val INVALID_NOTIFICATION_MOVIE_TITLE = "$ALARM_MOVIE_TITLE_KEY is missing in the Intent."
+        private const val INVALID_NOTIFICATION_TICKET_ID = "$ALARM_TICKET_ID_KEY is missing in the Intent."
+    }
+}
