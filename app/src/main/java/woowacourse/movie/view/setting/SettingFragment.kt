723532875dfa74aabea7000b21c74fdb3b573ae1
DongjooSEO <readyjoo24@gmail.com>
2025-05-10 23:45:03 +0900

    feat: Setting 화면에서 알림 허용 상태 sharedPreference에 저장

--- app/src/main/java/woowacourse/movie/view/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/view/setting/SettingFragment.kt
@@ -1,15 +1,24 @@
 package woowacourse.movie.view.setting
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import woowacourse.movie.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var alarmBtn: SwitchCompat
+
+    private val sharedPref by lazy {
+        requireActivity().getSharedPreferences("setting", Context.MODE_PRIVATE)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +27,9 @@ class SettingFragment : Fragment() {
     ): View {
         _binding = FragmentSettingBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        val isAlarmOn = sharedPref.getBoolean("isAlarmOn", false)
+        setAlarmBtn(isAlarmOn)
 
         return view
     }
@@ -31,5 +43,30 @@ class SettingFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setAlarmBtn(isAlarmOn: Boolean) {
+        alarmBtn = binding.settingAlarmSwitchBtn
+        alarmBtn.isChecked = isAlarmOn
+
+        alarmBtn.setOnCheckedChangeListener { _, isChecked ->
+
+            sharedPref.edit().putBoolean("isAlarmOn", isChecked).apply()
+
+            if (isChecked) {
+                setAlarm()
+            } else {
+                cancelAlarm()
+            }
+        }
+    }
+
+    private fun setAlarm() {
+        Log.d("alarm", "on")
+    }
+
+    private fun cancelAlarm() {
+        Log.d("alarm", "off")
     }
 }
