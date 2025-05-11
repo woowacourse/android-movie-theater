e77090837e677dbf574e7d9082431b66bc14c12f
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-05-11 22:57:24 +0900

    feat: bind data

--- app/src/main/java/woowacourse/movie/view/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/view/setting/SettingFragment.kt
@@ -4,22 +4,30 @@ import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import woowacourse.movie.data.ApplicationSettings
 import woowacourse.movie.databinding.FragmentSettingBinding
 import woowacourse.movie.view.ApplicationSettingProvider
 
 class SettingFragment : Fragment() {
-    private lateinit var applicationSettings: ApplicationSettings
-
     private var _binding: FragmentSettingBinding? = null
     private val binding: FragmentSettingBinding get() = requireNotNull(_binding) { "_binding is null" }
+
+    private lateinit var applicationSettings: ApplicationSettings
+    private lateinit var onNotificationClickListener: OnClickListener
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
         applicationSettings = (activity as ApplicationSettingProvider).provideApplicationSetting()
+        onNotificationClickListener =
+            OnClickListener { view ->
+                view as SwitchCompat
+                applicationSettings.notificationEnabled = view.isChecked
+            }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,20 +40,8 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        binding.applicationSettings = applicationSettings
+        binding.onNotificationClickListener = onNotificationClickListener
         return binding.root
-    }
-
-    override fun onViewCreated(
-        view: View,
-        savedInstanceState: Bundle?,
-    ) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.settingNotificationSwitch.isChecked = applicationSettings.notificationEnabled
-
-        binding.settingNotificationSwitch.setOnCheckedChangeListener { view, checked ->
-            applicationSettings.notificationEnabled = checked
-            view.isChecked = applicationSettings.notificationEnabled
-        }
     }
 }
