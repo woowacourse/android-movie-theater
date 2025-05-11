d3949598c9ee315b810514cdf6516aef1d5f26ab
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-05-11 18:50:46 +0900

    feat: make primaryKey generated auto

--- app/src/main/java/woowacourse/movie/ReservationEntity.kt
+++ app/src/main/java/woowacourse/movie/ReservationEntity.kt
@@ -3,13 +3,19 @@ package woowacourse.movie
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import woowacourse.movie.domain.reservation.Seat
+import woowacourse.movie.domain.ticket.Reservation
+import java.time.LocalDateTime
 
 @Entity(tableName = "reservation")
 data class ReservationEntity(
-    @PrimaryKey
-    val id: Int,
     val title: String,
-    val showtime: String,
+    val showtime: LocalDateTime,
     val seats: Set<Seat>,
     val cinemaName: String,
-)
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+) {
+    fun toDomain(): Reservation = Reservation(title, showtime, seats, cinemaName)
+}
+
+fun Reservation.toEntity() = ReservationEntity(title, showtime, seats, cinemaName)
