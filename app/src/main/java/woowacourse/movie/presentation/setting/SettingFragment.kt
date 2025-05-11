5d87226612df011855598770b4a9e3eea97920e2
cucumber99 <dlsguq1@gmail.com>
2025-05-12 00:13:40 +0900

    feat: SettingFragment 구현

--- app/src/main/java/woowacourse/movie/presentation/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/presentation/setting/SettingFragment.kt
@@ -4,15 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
+import woowacourse.movie.data.notification.NotificationPreferenceImpl
+import woowacourse.movie.databinding.FragmentSettingBinding
 
-class SettingFragment : Fragment() {
+class SettingFragment :
+    Fragment(),
+    SettingContract.View {
+    private val presenter: SettingContract.Presenter by lazy {
+        SettingPresenter(
+            this,
+            NotificationPreferenceImpl(requireContext())
+        )
+    }
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+    ): View {
+        _binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_setting, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presenter.loadNotificationSetting()
+        binding.switchNotification.setOnCheckedChangeListener { _, isChecked ->
+            presenter.changeNotificationSetting(isChecked)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun showNotificationSetting(isEnabled: Boolean) {
+        binding.switchNotification.isChecked = isEnabled
     }
 }
