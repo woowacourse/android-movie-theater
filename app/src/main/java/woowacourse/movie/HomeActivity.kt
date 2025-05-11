2653c1a35f3c0213230630a78e7dd7fe7876fe52
지원 <cheerfulboys@naver.com>
2025-05-12 00:57:21 +0900

    feat: 푸시 알림을 위한 권한 허용 기능 추가

--- app/src/main/java/woowacourse/movie/HomeActivity.kt
+++ app/src/main/java/woowacourse/movie/HomeActivity.kt
@@ -1,8 +1,17 @@
 package woowacourse.movie
 
+import android.app.AlarmManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
@@ -14,12 +23,21 @@ import woowacourse.movie.reservation.ReservationFragment
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (!isGranted) {
+                showNotificationPermissionDeniedDialog()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         initBinding()
         applyWindowInserts()
         setUpBottomNavigation()
+        setUpNotificationsPermission()
+
     }
 
     private fun setUpBottomNavigation() {
@@ -55,5 +73,31 @@ class HomeActivity : AppCompatActivity() {
         supportFragmentManager.commit {
             replace(R.id.main_frame, fragment)
         }
+    }
+
+    private fun setUpNotificationsPermission() {
+        val permission = "android.permission.POST_NOTIFICATIONS"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this, permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(permission)
+            }
+        }
+    }
+
+    private fun showNotificationPermissionDeniedDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("알림 권한이 필요합니다")
+            .setMessage("예매 알림을 받으시려면 알림 권한을 허용해주세요.")
+            .setPositiveButton("설정으로 이동") { _, _ ->
+                val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+                    putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                }
+                startActivity(intent)
+            }
+            .setNegativeButton("취소", null)
+            .show()
     }
 }
