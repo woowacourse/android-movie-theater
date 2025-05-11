0d55ed094f34bb9e1947048f74e5955e43401cab
yrsel <xxknu88@gmail.com>
2025-05-12 04:13:25 +0900

    feat: SettingsFragment 알림 설정 버튼 기기 권한 허용과 동기화 구현

--- app/src/main/java/woowacourse/movie/ui/settings/view/SettingsFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/settings/view/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package woowacourse.movie.ui.settings.view
 
 import android.Manifest
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,16 +26,16 @@ class SettingsFragment : Fragment() {
         requireContext().getSharedPreferences(getString(R.string.preference_key), MODE_PRIVATE)
     }
     private val isEnablePostNotification: Boolean
-        get() =
-            sharedPrefs.getBoolean(getString(R.string.preference_post_notification), true)
+        get() = sharedPrefs.getBoolean(getString(R.string.preference_post_notification), true)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
-        binding.isEnablePostNotification = isMovieReminderEnabled()
+        syncNotificationPermissionWithPrefsAndUI()
         return binding.root
     }
 
@@ -40,38 +43,45 @@ class SettingsFragment : Fragment() {
         view: View,
         savedInstanceState: Bundle?,
     ) {
+        super.onViewCreated(view, savedInstanceState)
         binding.switchPushNotification.setOnClickListener {
-            toggleMovieNotification()
-            binding.isEnablePostNotification = isEnablePostNotification
+            updateNotificationSetting(!isEnablePostNotification)
         }
     }
 
-    private fun isMovieReminderEnabled(): Boolean {
+    override fun onResume() {
+        super.onResume()
+        syncNotificationPermissionWithPrefsAndUI()
+    }
+
+    private fun syncNotificationPermissionWithPrefsAndUI()  {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             val isGranted =
                 ContextCompat.checkSelfPermission(
                     requireContext(),
                     Manifest.permission.POST_NOTIFICATIONS,
                 ) == PackageManager.PERMISSION_GRANTED
-            updateNotificationSetting(isGranted)
+            sharedPrefs.edit {
+                putBoolean(getString(R.string.preference_post_notification), isGranted)
+            }
         }
 
-        return isEnablePostNotification
-    }
-
-    private fun toggleMovieNotification() {
-        updateNotificationSetting(!isEnablePostNotification)
-
-        if (isEnablePostNotification) {
-            // DB에서 알림 예약 가능한 모든 영화들을 예약
-        } else {
-            // 예약된 모든 영화 알림 해제
-        }
+        binding.isEnablePostNotification = isEnablePostNotification
     }
 
     private fun updateNotificationSetting(isEnabled: Boolean) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val intent =
+                Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                    data = Uri.fromParts("package", requireContext().packageName, null)
+                }
+            startActivity(intent)
+            return
+        }
+
         sharedPrefs.edit {
             putBoolean(getString(R.string.preference_post_notification), isEnabled)
         }
+        binding.isEnablePostNotification = isEnabled
     }
 }
