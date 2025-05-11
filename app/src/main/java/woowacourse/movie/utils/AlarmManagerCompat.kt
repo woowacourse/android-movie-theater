95c7a32bb6dc6471c47e3bf6ba89f06ee2789cb9
yrsel <xxknu88@gmail.com>
2025-05-12 01:48:14 +0900

    feat: AlarmManagerCompat 버전 호환 도움 클래스 추가

---
+++ app/src/main/java/woowacourse/movie/utils/AlarmManagerCompat.kt
@@ -0,0 +1,33 @@
+package woowacourse.movie.utils
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import androidx.core.net.toUri
+
+object AlarmManagerCompat {
+    fun setExact(
+        context: Context,
+        triggerAtMillis: Long,
+        pendingIntent: PendingIntent,
+    ) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (alarmManager.canScheduleExactAlarms()) {
+                alarmManager.setExact(AlarmManager.RTC_WAKEUP, triggerAtMillis, pendingIntent)
+            } else {
+                val intent =
+                    Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM).apply {
+                        data = "package:$${context.packageName}".toUri()
+                    }
+                context.startActivity(intent)
+            }
+        } else {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, triggerAtMillis, pendingIntent)
+        }
+    }
+}
