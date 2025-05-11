765f2b1486b9fd1c660485bb042d97f9ab44b1a6
ijh1298 <imjh0417@gmail.com>
2025-05-11 14:19:15 +0900

    feat: 알림 기능 설정 Boolean SharedPreference 내 저장

--- app/src/main/java/woowacourse/movie/ui/settings/view/SettingsFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/settings/view/SettingsFragment.kt
@@ -1,16 +1,53 @@
 package woowacourse.movie.ui.settings.view
 
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? = inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+        val sharedPreference = requireContext().getSharedPreferences("settings", MODE_PRIVATE)
+        val notificationFlag = sharedPreference.getBoolean("notification", false)
+        binding.switchSettingPostNotification.isChecked = notificationFlag
+
+        binding.notificationSwitchListener =
+            NotificationSwitchListener { isChecked ->
+                when (isChecked) {
+                    true -> {
+                        val editor: SharedPreferences.Editor = sharedPreference.edit()
+                        editor.putBoolean("notification", true).apply()
+                    }
+
+                    false -> {
+                        val editor: SharedPreferences.Editor = sharedPreference.edit()
+                        editor.putBoolean("notification", false).apply()
+                    }
+                }
+
+                val value = sharedPreference.getBoolean("notification", false)
+                Log.d("SH_PREF", "$value")
+            }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/woowacourse/movie/utils/BindingAdapters.kt
+++ app/src/main/java/woowacourse/movie/utils/BindingAdapters.kt
@@ -2,7 +2,9 @@ package woowacourse.movie.utils
 
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
+import androidx.appcompat.widget.SwitchCompat
 import androidx.databinding.BindingAdapter
+import woowacourse.movie.ui.settings.view.NotificationSwitchListener
 
 object BindingAdapters {
     @JvmStatic
@@ -12,5 +14,16 @@ object BindingAdapters {
         @DrawableRes imageRes: Int,
     ) {
         view.setImageResource(imageRes)
+    }
+
+    @JvmStatic
+    @BindingAdapter("onCheckedChanged")
+    fun setOnCheckedChangeListener(
+        switch: SwitchCompat,
+        listener: NotificationSwitchListener,
+    ) {
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            listener.onClick(isChecked)
+        }
     }
 }
