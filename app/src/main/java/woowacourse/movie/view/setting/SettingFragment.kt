64970690668a3fdbf333c81ff7a46dcdefa23ae2
m6z1 <sonmyungj1zz@gmail.com>
2025-05-12 01:01:14 +0900

    feat: 설정 화면 알림 수신 여부 로직 구현

--- app/src/main/java/woowacourse/movie/view/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/view/setting/SettingFragment.kt
@@ -4,11 +4,23 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
+import woowacourse.movie.data.storage.DefaultNotificationPermissionStorage
 import woowacourse.movie.databinding.FragmentSettingBinding
+import woowacourse.movie.presenter.setting.SettingContracts
+import woowacourse.movie.presenter.setting.SettingPresenter
 
-class SettingFragment : Fragment(R.layout.fragment_setting) {
+class SettingFragment :
+    Fragment(R.layout.fragment_setting),
+    SettingContracts.View {
     private var _binding: FragmentSettingBinding? = null
     private val binding get() = _binding!!
+
+    private val presenter: SettingContracts.Presenter by lazy {
+        SettingPresenter(
+            this,
+            DefaultNotificationPermissionStorage(requireContext()),
+        )
+    }
 
     override fun onViewCreated(
         view: View,
@@ -17,6 +29,18 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
         super.onViewCreated(view, savedInstanceState)
 
         _binding = FragmentSettingBinding.bind(view)
+        presenter.updateNotificationPermission()
+        setupClickListener()
+    }
+
+    private fun setupClickListener() {
+        binding.clNotificationPermissionButton.setOnClickListener {
+            presenter.updateNotificationPermission(!binding.scSettingNotificationPermission.isChecked)
+        }
+    }
+
+    override fun showNotificationPermission(isGranted: Boolean) {
+        binding.scSettingNotificationPermission.isChecked = isGranted
     }
 
     override fun onDestroyView() {
