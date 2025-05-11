bb635ef913d299b356380471a6bc9d5c84a53b73
yrsel <xxknu88@gmail.com>
2025-05-12 01:59:28 +0900

    feat: MovieReminderReceiver 영화 알림 수신 역할

---
+++ app/src/main/java/woowacourse/movie/notification/MovieReminderReceiver.kt
@@ -0,0 +1,84 @@
+package woowacourse.movie.notification
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import woowacourse.movie.R
+import woowacourse.movie.domain.model.BookedTicket
+import woowacourse.movie.ui.complete.BookingCompleteActivity
+import woowacourse.movie.utils.Destination
+import woowacourse.movie.utils.intentSerializable
+
+class MovieReminderReceiver : BroadcastReceiver() {
+    override fun onReceive(
+        context: Context?,
+        intent: Intent?,
+    ) {
+        context?.let {
+            if (ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.POST_NOTIFICATIONS,
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                val bookedTicket =
+                    intent?.intentSerializable(EXTRA_BOOKED_TICKET, BookedTicket::class.java)!!
+                showNotification(it, bookedTicket)
+            }
+        }
+    }
+
+    private fun showNotification(
+        context: Context,
+        bookedTicket: BookedTicket,
+    ) {
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel =
+            NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+        notificationManager.createNotificationChannel(channel)
+
+        val intent = BookingCompleteActivity.newIntent(context, bookedTicket, Destination.HOME)
+        val pendingIntent =
+            PendingIntent.getActivity(
+                context,
+                bookedTicket.hashCode(),
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+            )
+
+        val notification =
+            NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(
+                    context.getString(R.string.notification_message, bookedTicket.movieTitle),
+                )
+                .setSmallIcon(R.drawable.woowacourse)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .build()
+
+        notificationManager.notify(bookedTicket.hashCode(), notification)
+    }
+
+    companion object {
+        private const val EXTRA_BOOKED_TICKET = "EXTRA_BOOKED_TICKET"
+        private const val CHANNEL_ID = "MOVIE_NOTIFICATION_CHANNEL"
+        private const val CHANNEL_NAME = "MOVIE_NOTIFICATION"
+
+        fun newIntent(
+            context: Context,
+            bookedTicket: BookedTicket,
+        ): Intent {
+            return Intent(context, MovieReminderReceiver::class.java).apply {
+                putExtra(EXTRA_BOOKED_TICKET, bookedTicket)
+            }
+        }
+    }
+}
