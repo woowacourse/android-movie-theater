0a57c156c273b759b101d1381a247fbff4adaa89
yrsel <xxknu88@gmail.com>
2025-05-12 05:02:08 +0900

    feat: SettingsFragment 설정 허용 변경 시 알림 가능한 영화들 모두 등록

--- app/src/main/java/woowacourse/movie/ui/settings/view/SettingsFragment.kt
+++ app/src/main/java/woowacourse/movie/ui/settings/view/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package woowacourse.movie.ui.settings.view
 
 import android.Manifest
+import android.app.PendingIntent
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
@@ -20,6 +21,9 @@ import woowacourse.movie.R
 import woowacourse.movie.data.BookedTicketDatabase
 import woowacourse.movie.databinding.FragmentSettingsBinding
 import woowacourse.movie.domain.model.BookedTicket
+import woowacourse.movie.notification.MovieReminderReceiver
+import woowacourse.movie.utils.AlarmManagerCompat
+import java.time.ZoneId
 
 class SettingsFragment : Fragment(), SettingsContract.View {
     private var _binding: FragmentSettingsBinding? = null
@@ -59,6 +63,19 @@ class SettingsFragment : Fragment(), SettingsContract.View {
         syncNotificationPermissionWithPrefsAndUI()
     }
 
+    override fun notifyMovieReminderRegistered(availableNotificationTickets: List<BookedTicket>) {
+        availableNotificationTickets.forEach { bookedTicket ->
+            scheduleNotification(bookedTicket)
+        }
+    }
+
+    override fun notifyMovieReminderCleared() {
+        /*
+         * 설정 변경 시 기존 알림 설정한 데이터 제거 필요
+         * PendingIntent 정보를 room에 저장해야 정확히 비교 후 제거할 수 있을텐데 일단 진행
+         * */
+    }
+
     private fun syncNotificationPermissionWithPrefsAndUI() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             val isGranted =
@@ -70,7 +87,7 @@ class SettingsFragment : Fragment(), SettingsContract.View {
                 putBoolean(getString(R.string.preference_post_notification), isGranted)
             }
 
-            if (isEnablePostNotification != isGranted){
+            if (isEnablePostNotification != isGranted) {
                 presenter.handleMovieNotificationByToggle(isGranted)
             }
 
@@ -95,12 +112,21 @@ class SettingsFragment : Fragment(), SettingsContract.View {
         presenter.handleMovieNotificationByToggle(isEnabled)
     }
 
-    override fun notifyMovieReminderRegistered(availableNotificationTickets: List<BookedTicket>) {
-        TODO("Not yet implemented")
-    }
+    private fun scheduleNotification(bookedTicket: BookedTicket) {
+        val triggerTime = bookedTicket.movieSchedule.screeningDateTime.minusMinutes(30L)
+        val triggerAtMillis =
+            triggerTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
 
-    override fun notifyMovieReminderCleared() {
-        TODO("Not yet implemented")
-    }
+        val requestCode = bookedTicket.hashCode()
+        val intent = MovieReminderReceiver.newIntent(requireContext(), bookedTicket)
+        val pendingIntent =
+            PendingIntent.getBroadcast(
+                requireContext(),
+                requestCode,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+            )
 
+        AlarmManagerCompat.setExact(requireContext(), triggerAtMillis, pendingIntent)
+    }
 }
