cfe1e792faa62a909dfaf0299af36769a4fa7aae
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-05-11 21:52:20 +0900

    feat: implement data binding

--- app/src/main/java/woowacourse/movie/view/setting/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/view/setting/SettingFragment.kt
@@ -1,13 +1,27 @@
 package woowacourse.movie.view.setting
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import woowacourse.movie.R
+import woowacourse.movie.data.ApplicationSettings
+import woowacourse.movie.databinding.FragmentSettingBinding
+import woowacourse.movie.view.ApplicationSettingProvider
 
 class SettingFragment : Fragment() {
+    private lateinit var applicationSettings: ApplicationSettings
+
+    private var _binding: FragmentSettingBinding? = null
+    private val binding: FragmentSettingBinding get() = requireNotNull(_binding) { "_binding is null" }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        applicationSettings = (activity as ApplicationSettingProvider).provideApplicationSetting()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -16,7 +30,22 @@ class SettingFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.settingNotificationSwitch.isChecked = applicationSettings.notificationEnabled
+
+        binding.settingNotificationSwitch.setOnCheckedChangeListener { view, checked ->
+            applicationSettings.notificationEnabled = checked
+            view.isChecked = applicationSettings.notificationEnabled
+        }
     }
 }
