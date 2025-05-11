6048aa911a3d3762a8af8efd4386f90d0b47933b
지원 <cheerfulboys@naver.com>
2025-05-11 19:09:45 +0900

    feat: 푸시 알림 여부 저장 기능 추가

--- app/src/main/java/woowacourse/movie/SettingFragment.kt
+++ app/src/main/java/woowacourse/movie/SettingFragment.kt
@@ -1,12 +1,17 @@
 package woowacourse.movie
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
 import androidx.fragment.app.Fragment
+import androidx.core.content.edit
 
 class SettingFragment : Fragment() {
+    private lateinit var sharedPreference: SharedPreferences
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -14,5 +19,19 @@ class SettingFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedPreference = requireContext().getSharedPreferences("settings", Context.MODE_PRIVATE)
+        val switch = view.findViewById<Switch>(R.id.switch_push)
+        switch.isChecked = sharedPreference.getBoolean("notification", false)
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            sharedPreference.edit(commit = true) {
+                putBoolean("notification", isChecked)
+                println(isChecked)
+            }
+        }
     }
 }
