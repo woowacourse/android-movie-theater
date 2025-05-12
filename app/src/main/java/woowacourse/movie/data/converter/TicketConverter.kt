9372330ae0275b2d86bcfecf5d5ecc0e187f3599
oungsi2000 <oungsi1000@gmail.com>
2025-05-12 17:35:06 +0900

    refactor: 안쓰는 타입 컨버터 제거

--- app/src/main/java/woowacourse/movie/data/converter/TicketConverter.kt
+++ app/src/main/java/woowacourse/movie/data/converter/TicketConverter.kt
@@ -2,7 +2,6 @@ package woowacourse.movie.data.converter
 
 import androidx.room.TypeConverter
 import woowacourse.movie.domain.model.ReservationCount
-import woowacourse.movie.domain.model.Seat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -25,19 +24,5 @@ class TicketConverter {
     @TypeConverter
     fun fromReservationCount(value: ReservationCount): Int {
         return value.value
-    }
-
-    @TypeConverter
-    fun fromSeatList(value: List<Seat>): String {
-        return value.joinToString(",") { it.row.toString() + it.column.toString() }
-    }
-
-    @TypeConverter
-    fun toSeatList(value: String): List<Seat> {
-        return value.split(",").map {
-            val row = it.substring(0, 1).toInt()
-            val column = it.substring(1).toInt()
-            Seat(row, column)
-        }
     }
 }
