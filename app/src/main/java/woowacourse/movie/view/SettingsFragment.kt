12a4a49f3cf84a4327999779af24f0c47bd06165
devfeijoa <devfeijoa@gmail.com>
2025-05-12 14:27:12 +0900

    feat: 알림 수신 여부 스위치 유지 기능 구현

--- app/src/main/java/woowacourse/movie/view/SettingsFragment.kt
+++ app/src/main/java/woowacourse/movie/view/SettingsFragment.kt
@@ -4,13 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import woowacourse.movie.R
+import woowacourse.movie.databinding.FragmentSettingsBinding
+import woowacourse.movie.sharedPreference.SettingSharedPreferenceManager
 
 class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+    private val prefs: SettingSharedPreferenceManager by lazy {
+        SettingSharedPreferenceManager(
+            requireContext(),
+        )
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? = inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.scNotificationEnabled.isChecked = prefs.isNotificationEnabled()
+
+        binding.scNotificationEnabled.setOnCheckedChangeListener { buttonView, isChecked ->
+            prefs.updateNotificationEnabled(isChecked)
+        }
+    }
 }
