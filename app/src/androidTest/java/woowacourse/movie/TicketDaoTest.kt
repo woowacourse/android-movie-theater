3871ae9eb7512fc50628669e33267ff2130d5b96
hwannow <vnfdlvgid061@naver.com>
2025-05-12 10:28:03 +0900

    test: DAO 테스트 코드 작성

---
+++ app/src/androidTest/java/woowacourse/movie/TicketDaoTest.kt
@@ -0,0 +1,66 @@
+package woowacourse.movie
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import woowacourse.movie.data.dao.TicketDao
+import woowacourse.movie.data.database.MovieDatabase
+import woowacourse.movie.data.entity.TicketEntity
+import woowacourse.movie.fixture.HARRY_POTTER
+import woowacourse.movie.fixture.SEAT_A1
+import woowacourse.movie.fixture.SEAT_A2
+import woowacourse.movie.fixture.SEAT_E1
+import woowacourse.movie.fixture.STAR_IS_BORN
+import woowacourse.movie.fixture.createTicket
+import woowacourse.movie.mapper.toEntity
+
+class TicketDaoTest {
+    private val dummyData: TicketEntity = createTicket(HARRY_POTTER, listOf(SEAT_A1, SEAT_E1), 2).toEntity().copy(id = 1)
+    private lateinit var db: MovieDatabase
+    private lateinit var dao: TicketDao
+
+    @Before
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db =
+            Room
+                .inMemoryDatabaseBuilder(context, MovieDatabase::class.java)
+                .allowMainThreadQueries()
+                .build()
+
+        dao = db.TicketDao()
+        dao.saveTicket(dummyData)
+    }
+
+    @Test
+    fun `티켓_정보를_모두_불러온다`() {
+        // given & when
+        val actual = dao.findTicket()
+        val expected = listOf(dummyData)
+
+        // then
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `티켓_정보를_저장한다`() {
+        // given
+        val ticket: TicketEntity = createTicket(STAR_IS_BORN, listOf(SEAT_A1, SEAT_A2), 2).toEntity().copy(id = 2)
+
+        // when
+        dao.saveTicket(ticket)
+        val actual = dao.findTicket()
+
+        // then
+        assertThat(actual).contains(ticket)
+    }
+
+    @After
+    fun tearDown() {
+        db.close()
+    }
+}
--- app/src/androidTest/java/woowacourse/movie/fixture/MovieFixture.kt
+++ app/src/androidTest/java/woowacourse/movie/fixture/MovieFixture.kt
@@ -4,6 +4,7 @@ import woowacourse.movie.model.Movie
 import java.time.LocalDate
 
 const val HARRY_POTTER = "해리 포터와 마법사의 돌"
+const val STAR_IS_BORN = "스타 이즈 본"
 
 fun createMovie(name: String): Movie {
     return Movie(
