db94a66e2b55f5d978c61efc3a53131a2e2d46d1
devfeijoa <devfeijoa@gmail.com>
2025-05-12 14:16:05 +0900

    feat: SharedPreference 구현

---
+++ app/src/main/java/woowacourse/movie/sharedPreference/SettingSharedPreferenceManager.kt
@@ -0,0 +1,20 @@
+package woowacourse.movie.sharedPreference
+
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+class SettingSharedPreferenceManager(
+    private val context: Context,
+) {
+    private val prefs: SharedPreferences = context.getSharedPreferences("settings", MODE_PRIVATE)
+
+    fun updateNotificationEnabled(isEnabled: Boolean) {
+        prefs.edit {
+            putBoolean("notification", isEnabled)
+        }
+    }
+
+    fun isNotificationEnabled(): Boolean = prefs.getBoolean("notification", false)
+}
