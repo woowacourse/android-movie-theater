d24a0c4938ccb0f8a310563461b379662e7198ef
Giovanni Junseo Kim <kingjunseo@gmail.com>
2025-05-12 15:43:48 +0900

    feat: remove useless code

--- app/src/main/java/woowacourse/movie/view/reservation/ReservationViewHolder.kt
+++ app/src/main/java/woowacourse/movie/view/reservation/ReservationViewHolder.kt
@@ -1,48 +1,29 @@
 package woowacourse.movie.view.reservation
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import woowacourse.movie.databinding.ItemReservationBinding
 import woowacourse.movie.domain.ticket.Reservation
-import java.time.LocalDateTime
 
 class ReservationViewHolder(
     private val binding: ItemReservationBinding,
     private val onSelectTicket: (Reservation) -> Unit,
 ) : RecyclerView.ViewHolder(binding.root) {
-    private var showtime: LocalDateTime? = null
-    private var cinemaName: String = ""
-    private var title: String = ""
-    private var needDivider: Boolean = true
-
     fun bind(
         reservation: Reservation,
         needDivider: Boolean,
     ) {
-        initData(reservation, needDivider)
-        bindData()
-        initEventListeners(reservation)
+        bindData(reservation, needDivider)
     }
 
-    private fun initData(
+    private fun bindData(
         reservation: Reservation,
-        _needDivider: Boolean,
+        needDivider: Boolean,
     ) {
-        showtime = reservation.showtime
-        cinemaName = "극장 이름"
-        title = reservation.title
-        needDivider = _needDivider
-    }
-
-    private fun bindData() {
-        binding.showtime = showtime
-        binding.cinemaName = cinemaName
-        binding.title = title
+        binding.showtime = reservation.showtime
+        binding.cinemaName = "극장 이름"
+        binding.title = reservation.title
         binding.needDivider = needDivider
-    }
-
-    private fun initEventListeners(reservation: Reservation) {
-        binding.root.setOnClickListener {
-            onSelectTicket(reservation)
-        }
+        binding.onClickItemListener = View.OnClickListener { onSelectTicket(reservation) }
     }
 }
