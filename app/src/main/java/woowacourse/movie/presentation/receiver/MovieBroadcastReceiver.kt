1e7586b5e1a201dc958726ea7fd5d94f4c98f282
이지윤 <02leejiyun@naver.com>
2025-05-12 13:23:37 +0900

    feat: 알림 기능을 끄면 알림이 오지 않도록 구현

--- app/src/main/java/woowacourse/movie/presentation/receiver/MovieBroadcastReceiver.kt
+++ app/src/main/java/woowacourse/movie/presentation/receiver/MovieBroadcastReceiver.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import woowacourse.movie.common.util.getSerializableExtraCompat
+import woowacourse.movie.data.repository.NotificationSettingRepository
 import woowacourse.movie.domain.model.Ticket
 import woowacourse.movie.presentation.notification.ticket.TicketNotification
 
@@ -12,8 +13,12 @@ class MovieBroadcastReceiver : BroadcastReceiver() {
         context: Context,
         intent: Intent,
     ) {
+        val notificationSetting = NotificationSettingRepository(context)
+        if (!notificationSetting.isGranted()) return
+
         val ticket = intent.getSerializableExtraCompat(EXTRA_TICKET, Ticket::class.java)
         ticket ?: return
+
         val ticketNotification = TicketNotification(context)
         ticketNotification.sendNotification(ticket)
     }
