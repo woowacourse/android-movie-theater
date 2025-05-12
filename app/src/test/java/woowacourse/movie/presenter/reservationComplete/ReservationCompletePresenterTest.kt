729bb956a5ef15db0d9135f445e99eadc12a92ba
m6z1 <sonmyungj1zz@gmail.com>
2025-05-12 14:59:32 +0900

    test: ReservationCompletePresenter 테스트 수정
    - 저장소에서 영화 티켓 정보를 받아 콜백으로 영화 티켓 보여주는 로직으로 수정

--- app/src/test/java/woowacourse/movie/presenter/reservationComplete/ReservationCompletePresenterTest.kt
+++ app/src/test/java/woowacourse/movie/presenter/reservationComplete/ReservationCompletePresenterTest.kt
@@ -4,12 +4,15 @@ import io.kotest.core.spec.style.AnnotationSpec.After
 import io.mockk.Runs
 import io.mockk.clearAllMocks
 import io.mockk.every
+import io.mockk.invoke
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import woowacourse.movie.data.entity.MovieTicketEntity
 import woowacourse.movie.data.storage.ReservationStorage
+import woowacourse.movie.presenter.MOVIE_TICKET_ENTITY
 
 class ReservationCompletePresenterTest {
     private lateinit var presenter: ReservationCompletePresenter
@@ -27,12 +30,15 @@ class ReservationCompletePresenterTest {
     fun `영화 티켓을 업데이트 하면 영화 티켓에 대한 정보가 보인다`() {
         // given:
         every { view.showMovieTicket(any()) } just Runs
+        every { reservationStorage.getMovieTicket(any(), captureLambda()) } answers {
+            lambda<(MovieTicketEntity) -> Unit>().invoke(MOVIE_TICKET_ENTITY)
+        }
 
         // when:
         presenter.updateTicketData(1L)
 
         // then:
-        verify { view.showMovieTicket(any()) }
+        verify { view.showMovieTicket(MOVIE_TICKET_ENTITY) }
     }
 
     @After
