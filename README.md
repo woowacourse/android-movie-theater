23bdd94958d37f6138d57710a494b70daf8a8a05
hwannow <vnfdlvgid061@naver.com>
2025-05-15 09:13:18 +0900

    refactor: onReceive 메서드 분리

--- README.md
+++ README.md
@@ -47,10 +47,10 @@
 - [x] Test | 패키지 맞추기
 - [x] SettingFragment.initAlarmState | Indent 줄이기
 - [x] SettingPresenter.setPermissionState | 사용하지 않는 인자 지우기
+- [x] AlarmReceiver | onReceive 함수 분리하기
 - [ ] TicketListPresenter | 프레젠터가 안드로이드 의존성을 가지지 않도록 수정
 - [ ] SeatSelectionActivity | PendingIntent를 받는 곳에서 생성하도록 변경
 - [ ] SettingFragment | 권한이 필요한 경우 설정화면으로 이동하도록 변경
 - [ ] SettingPresenter | 테스트
-- [ ] AlarmReceiver | onReceive 함수 분리하기
 - [ ] TicketDao | 같은 데이터를 저장할 경우 충돌 고민
 - [ ] TicketDao | findTicket 네이밍 고민
--- app/src/main/java/woowacourse/movie/AlarmReceiver.kt
+++ app/src/main/java/woowacourse/movie/AlarmReceiver.kt
@@ -1,12 +1,15 @@
 package woowacourse.movie
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import woowacourse.movie.booking.complete.BookingCompleteActivity
 import woowacourse.movie.mapper.IntentCompat
@@ -18,42 +21,10 @@ class AlarmReceiver : BroadcastReceiver() {
         intent: Intent,
     ) {
         if (!isPermitted(context)) return
+        val ticket = extractTicket(intent)
 
-        val ticket = IntentCompat.getParcelableExtra(intent, KEY_TICKET_ALARM, TicketUiModel::class.java)
-
-        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as android.app.NotificationManager
-
-        val channel =
-            android.app.NotificationChannel(
-                CHANNEL_ID,
-                CHANNEL_NAME,
-                android.app.NotificationManager.IMPORTANCE_HIGH,
-            )
-
-        if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
-            notificationManager.createNotificationChannel(channel)
-        }
-
-        val bookIntent = BookingCompleteActivity.newIntent(context, ticket)
-        val pendingIntent: PendingIntent =
-            PendingIntent.getActivity(
-                context,
-                CHANNEL_REQUEST_CODE,
-                bookIntent,
-                PendingIntent.FLAG_IMMUTABLE,
-            )
-
-        val notification =
-            androidx.core.app.NotificationCompat.Builder(context, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle(context.getString(R.string.notification_title))
-                .setContentText(context.getString(R.string.notification_content_text, ticket.title))
-                .setPriority(androidx.core.app.NotificationCompat.PRIORITY_HIGH)
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent)
-                .build()
-
-        notificationManager.notify(ticket.hashCode(), notification)
+        ensureNotificationChannel(context)
+        showNotification(context, ticket)
     }
 
     private fun isPermitted(context: Context): Boolean {
@@ -62,6 +33,52 @@ class AlarmReceiver : BroadcastReceiver() {
         return ContextCompat.checkSelfPermission(
             context, Manifest.permission.POST_NOTIFICATIONS,
         ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun extractTicket(intent: Intent): TicketUiModel {
+        return IntentCompat.getParcelableExtra(intent, KEY_TICKET_ALARM, TicketUiModel::class.java)
+    }
+
+    private fun ensureNotificationChannel(context: Context) {
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
+            val channel =
+                NotificationChannel(
+                    CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_HIGH,
+                )
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showNotification(
+        context: Context,
+        ticket: TicketUiModel,
+    ) {
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val intent = BookingCompleteActivity.newIntent(context, ticket)
+        val pendingIntent =
+            PendingIntent.getActivity(
+                context,
+                CHANNEL_REQUEST_CODE,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE,
+            )
+
+        val notification =
+            NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(context.getString(R.string.notification_content_text, ticket.title))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build()
+
+        notificationManager.notify(ticket.hashCode(), notification)
     }
 
     companion object {
