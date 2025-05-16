36377cea86faf9fd02b2e9a6e19cd13ea9fed440
oungsi2000 <oungsi1000@gmail.com>
2025-05-16 15:00:35 +0900

    refactor: db 초기화 로직 이동

--- app/src/main/java/woowacourse/movie/provider/RepositoryProvider.kt
+++ app/src/main/java/woowacourse/movie/provider/RepositoryProvider.kt
@@ -2,6 +2,7 @@ package woowacourse.movie.provider
 
 import android.content.Context
 import woowacourse.movie.data.MovieTheaterDatabase
+import woowacourse.movie.data.dao.TicketDao
 import woowacourse.movie.data.dummy.DummyCinema
 import woowacourse.movie.repository.SettingRepository
 import woowacourse.movie.repository.TicketRepository
@@ -9,10 +10,6 @@ import woowacourse.movie.repository.mapper.toEntity
 
 object RepositoryProvider {
     fun ticketRepository(context: Context): TicketRepository {
-        val cinemaDao = MovieTheaterDatabase.db(context.applicationContext).cinemaDao()
-        cinemaDao.save(
-            DummyCinema.dummyCinemas.map { it.toEntity() },
-        )
         return TicketRepository(
             ticketDao(context.applicationContext),
         )
@@ -23,5 +20,19 @@ object RepositoryProvider {
             context.applicationContext.getSharedPreferences("setting", Context.MODE_PRIVATE),
         )
 
-    fun ticketDao(context: Context) = MovieTheaterDatabase.db(context.applicationContext).ticketDao()
+    fun ticketDao(context: Context): TicketDao {
+        initData(context.applicationContext)
+        return db(context.applicationContext).ticketDao()
+    }
+
+    private fun initData(context: Context) {
+        val cinemaDao = db(context.applicationContext).cinemaDao()
+        cinemaDao.save(
+            DummyCinema.dummyCinemas.map { it.toEntity() },
+        )
+    }
+
+    private fun db(context: Context): MovieTheaterDatabase {
+        return MovieTheaterDatabase.db(context.applicationContext)
+    }
 }
