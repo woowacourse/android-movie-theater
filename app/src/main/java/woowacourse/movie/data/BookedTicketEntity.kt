16327c6e212d7ddb14cd347612138410c2855047
yrsel <xxknu88@gmail.com>
2025-05-17 12:12:22 +0900

    refactor: BookedTicket id 추가 및 변환 로직 제거

--- app/src/main/java/woowacourse/movie/data/BookedTicketEntity.kt
+++ app/src/main/java/woowacourse/movie/data/BookedTicketEntity.kt
@@ -3,12 +3,8 @@ package woowacourse.movie.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import woowacourse.movie.domain.model.BookedTicket
-import woowacourse.movie.domain.model.Headcount
-import woowacourse.movie.domain.model.MovieSchedule
-import woowacourse.movie.domain.model.Seat
-import woowacourse.movie.domain.model.Seats
 import java.time.LocalDateTime
+import woowacourse.movie.domain.model.Seat
 
 @Entity(tableName = "booked_tickets")
 data class BookedTicketEntity(
@@ -19,16 +15,3 @@ data class BookedTicketEntity(
     @ColumnInfo(name = "select_seats") val selectSeats: Set<Seat>,
     @ColumnInfo(name = "headcount") val headcount: Int,
 )
-
-fun BookedTicketEntity.toBookedTicket(): BookedTicket {
-    return BookedTicket(
-        theaterName = this.theaterName,
-        movieTitle = this.movieTitle,
-        movieSchedule =
-            MovieSchedule(
-                this.screeningDateTime,
-                Seats(this.selectSeats.toMutableSet()),
-            ),
-        headcount = Headcount(headcount),
-    )
-}
--- app/src/main/java/woowacourse/movie/domain/model/BookedTicket.kt
+++ app/src/main/java/woowacourse/movie/domain/model/BookedTicket.kt
@@ -1,23 +1,13 @@
 package woowacourse.movie.domain.model
 
-import woowacourse.movie.data.BookedTicketEntity
 import java.io.Serializable
 
 class BookedTicket(
+    val id: Long? = null,
     val theaterName: String,
     val movieTitle: String,
     val movieSchedule: MovieSchedule,
     val headcount: Headcount,
 ) : Serializable {
     fun totalPrice(): Int = movieSchedule.seats.totalPrice()
-}
-
-fun BookedTicket.toBookedTicketEntity(): BookedTicketEntity {
-    return BookedTicketEntity(
-        theaterName = theaterName,
-        movieTitle = movieTitle,
-        screeningDateTime = movieSchedule.screeningDateTime,
-        selectSeats = movieSchedule.seats.reservingSeats,
-        headcount = headcount.count,
-    )
 }
