9f1eb9811b4365fb8d96ab83da9a8a638eab441c
yrsel <xxknu88@gmail.com>
2025-05-18 00:48:48 +0900

    refactor: MovieReminderReceiver 함수 분리

--- app/src/main/java/woowacourse/movie/notification/MovieReminderReceiver.kt
+++ app/src/main/java/woowacourse/movie/notification/MovieReminderReceiver.kt
@@ -8,6 +8,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import woowacourse.movie.R
@@ -20,15 +21,20 @@ class MovieReminderReceiver : BroadcastReceiver() {
         context: Context,
         intent: Intent,
     ) {
-        if (
-            ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS)
-            == PackageManager.PERMISSION_GRANTED
-        ) {
+        if (hasPostNotificationPermission(context)) {
             val bookedTicket =
                 intent.intentSerializable(EXTRA_BOOKED_TICKET, BookedTicket::class.java)!!
             showNotification(context, bookedTicket)
         }
     }
+
+    private fun hasPostNotificationPermission(context: Context) =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            (ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS)
+                    == PackageManager.PERMISSION_GRANTED)
+        } else {
+            true
+        }
 
     private fun showNotification(
         context: Context,
