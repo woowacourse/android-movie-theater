36357a616cc8ca06f76d94f3d0b948eb3b20f1cb
윤동현 <donghyunyoon81@gmail.com>
2025-05-18 16:19:03 +0900

    refactor: 바인딩 어댑터 고도화

---
+++ app/src/main/java/woowacourse/movie/ui/view/binding/TextViewBinding.kt
@@ -0,0 +1,18 @@
+package woowacourse.movie.ui.view.binding
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@BindingAdapter(value = ["formatDate", "formatPattern"])
+fun setDateFormat(view: TextView, localDateTime: LocalDateTime?, format: String?) {
+    val formatter = DateTimeFormatter.ofPattern(format)
+    view.text = formatter.format(localDateTime)
+}
+
+@BindingAdapter(value = ["formatTime", "formatPattern"])
+fun setTimeFormat(view: TextView, localDateTime: LocalDateTime?, format: String?) {
+    val formatter = DateTimeFormatter.ofPattern(format)
+    view.text = formatter.format(localDateTime)
+}
