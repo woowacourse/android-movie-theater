74a3d55bd15af462148247e0a77a61065d056e35
jerry8282 <ka06196@naver.com>
2025-05-20 16:28:50 +0900

    refactor: setDateTime BindingAdapter 패키지 위치 변경

--- app/src/main/java/woowacourse/movie/view/reservation/result/ReservationCompleteActivity.kt
+++ app/src/main/java/woowacourse/movie/view/reservation/result/ReservationCompleteActivity.kt
@@ -4,18 +4,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.BindingAdapter
 import woowacourse.movie.data.mapper.toDomain
 import woowacourse.movie.databinding.ActivityReservationCompleteBinding
 import woowacourse.movie.view.dialog.DialogFactory
 import woowacourse.movie.view.reservation.TicketUi
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class ReservationCompleteActivity : AppCompatActivity(), ReservationCompleteContract.View {
     private val presenter by lazy {
@@ -81,14 +77,4 @@ class ReservationCompleteActivity : AppCompatActivity(), ReservationCompleteCont
             Intent(context, ReservationCompleteActivity::class.java)
                 .putExtra(KEY_TICKET, ticketUi)
     }
-}
-
-@BindingAdapter("dateTime")
-fun setDateTime(
-    view: TextView,
-    date: LocalDateTime,
-) {
-    val formatter = DateTimeFormatter.ofPattern("yyyy.M.d. HH:mm")
-    val dateTimeFormat = date.format(formatter)
-    view.text = dateTimeFormat.toString()
 }
