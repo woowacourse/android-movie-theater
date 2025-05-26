b35d7b6273bb75443aac44baa39f2d8e2fdfe61b
leeyerin0210 <lyl8661@naver.com>
2025-05-26 15:34:15 +0900

    refactor: 한번만 채널 생성하도록 변경

--- app/src/main/java/woowacourse/movie/presentation/AlarmReceiver.kt
+++ app/src/main/java/woowacourse/movie/presentation/AlarmReceiver.kt
@@ -1,18 +1,17 @@
 package woowacourse.movie.presentation
 
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import woowacourse.movie.R
 import woowacourse.movie.presentation.model.TicketBundleUiModel
 import woowacourse.movie.presentation.view.history.detailResult.ReservationResultActivity
 import woowacourse.movie.presentation.view.setting.PushPreferenceHelper
+import woowacourse.movie.presentation.util.NotificationUtil
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -24,11 +23,11 @@ class AlarmReceiver : BroadcastReceiver() {
 
         val ticket = intent.getParcelableExtra<TicketBundleUiModel>("ticket") ?: return
 
+
+        NotificationUtil.ensureNotificationChannel(context)
+
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel("movie_alarm", "예매 알림", NotificationManager.IMPORTANCE_HIGH)
-            notificationManager.createNotificationChannel(channel)
-        }
+
 
         val activityIntent = Intent(context, ReservationResultActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
