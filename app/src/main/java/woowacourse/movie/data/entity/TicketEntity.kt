97454b4c53e92f69f245115b2f6b5493fdb83078
leeyerin0210 <lyl8661@naver.com>
2025-05-30 16:32:10 +0900

    refactor: Seat toDomain 로직 수정

--- app/src/main/java/woowacourse/movie/data/entity/TicketEntity.kt
+++ app/src/main/java/woowacourse/movie/data/entity/TicketEntity.kt
@@ -26,13 +26,19 @@ data class TicketEntity(
     val bundleId: Int,
 )
 
-fun TicketEntity.toTicket(): Ticket = Ticket(seatLabel.toSeat(type), price)
+fun TicketEntity.toTicket(): Ticket = Ticket(seatLabel.toDomain(type), price)
 
-fun String.toSeat(type: String): Seat {
-    val rowChar = this[0] // 'A'
-    val colNum = this.substring(1).toInt() // '1'
+fun String.toDomain(type: String): Seat {
+    require(length >= 2) { "좌석 라벨은 최소 2글자 이상이어야 합니다." }
+    require(this[0].isLetter()) { "좌석 라벨의 첫 글자는 문자여야 합니다." }
+    require(substring(1).all { it.isDigit() }) { "좌석 라벨의 두 번째 문자부터는 숫자여야 합니다." }
+
+    val rowChar = this[0]
+    val colNum = this.substring(1).toInt()
 
     val row = rowChar - 'A'
     val col = colNum - 1
+    require(row >= 0) { "좌석 행 인덱스는 0 이상이어야 합니다." }
+    require(col >= 0) { "좌석 열 인덱스는 0 이상이어야 합니다." }
     return Seat(row, col, SeatType.valueOf(type))
 }
