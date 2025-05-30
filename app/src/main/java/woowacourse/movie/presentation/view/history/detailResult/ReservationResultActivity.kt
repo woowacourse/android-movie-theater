2545c3d601cab7ef3892b629e85dc900878ffd28
leeyerin0210 <lyl8661@naver.com>
2025-05-30 16:59:25 +0900

    refactor: 널 체크 하도록 수정

--- app/src/main/java/woowacourse/movie/presentation/view/history/detailResult/ReservationResultActivity.kt
+++ app/src/main/java/woowacourse/movie/presentation/view/history/detailResult/ReservationResultActivity.kt
@@ -8,18 +8,22 @@ import woowacourse.movie.presentation.base.BaseActivity
 import woowacourse.movie.presentation.model.TicketBundleUiModel
 import woowacourse.movie.presentation.view.home.reservation.result.ReservationResultFragment
 
-class ReservationResultActivity : BaseActivity<ActivityReservationResultBinding>(R.layout.activity_reservation_result) {
+class ReservationResultActivity :
+    BaseActivity<ActivityReservationResultBinding>(R.layout.activity_reservation_result) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (savedInstanceState == null) {
             val ticket = intent.getParcelableExtra<TicketBundleUiModel>("ticket")
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                replace(
-                    R.id.fragment_container_view,
-                    ReservationResultFragment.newInstance(ticket!!),
-                )
+            if (ticket != null) {
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    replace(
+                        R.id.fragment_container_view,
+                        ReservationResultFragment.newInstance(ticket),
+                    )
+                }
             }
         }
     }
